873798598f89a7b8f8b30609714da78bcbdfbcca
Daniel Erat <derat@chromium.org>
2017-08-18 11:05:15 -0700

    tast: Add common/testing and common/testing/attr.
    Add a shared package used to define, select, and run tests.
    
    BUG=chromium:737628
    TEST=added unit tests; also used a bunch with the rest of
         the not-yet-landed code
    
    Change-Id: I5c7d189fa225145681ae2657a90b0e7053e68d9b

---
+++ src/chromiumos/tast/common/testing/state.go
@@ -0,0 +1,147 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"runtime/debug"
+	"strings"
+	"time"
+)
+
+// Key type for objects attached to context.Context objects.
+type contextKeyType string
+
+// Key used for attaching a *State to a context.Context.
+var logKey contextKeyType = "log"
+
+// Error describes an error encountered while running a test.
+type Error struct {
+	Reason string `json:"reason"`
+	File   string `json:"file"`
+	Line   int    `json:"line"`
+	Stack  string `json:"stack"`
+}
+
+// Output contains a piece of output (either i.e. an error or log message) from a test.
+type Output struct {
+	T   time.Time
+	Err *Error
+	Msg string
+}
+
+// State holds state relevant to the execution of a single test.
+// Parts of its interface are patterned after Go's testing.T type.
+// It is intended to be safe when called concurrently by multiple goroutines
+// while a test is running.
+type State struct {
+	ch      chan Output        // channel to which logging messages and errors are written
+	arch    string             // string used for replacing "{arch}" in data paths
+	dataDir string             // directory in which the test's data files will be located
+	outDir  string             // directory to which the test should write output files
+	ctx     context.Context    // context for the test run
+	cancel  context.CancelFunc // cancel function associated with ctx
+}
+
+// NewState returns a new State object. The test's output will be streamed to ch.
+func NewState(ctx context.Context, ch chan Output, arch, dataDir, outDir string, timeout time.Duration) *State {
+	s := &State{
+		ch:      ch,
+		arch:    arch,
+		dataDir: dataDir,
+		outDir:  outDir,
+	}
+	s.ctx, s.cancel = context.WithTimeout(context.WithValue(ctx, logKey, s), timeout)
+	return s
+}
+
+// Context returns the context that should be used by tests.
+func (s *State) Context() context.Context { return s.ctx }
+
+// DataPath returns the absolute path to use to access a data file previously
+// registered via Test.Data.
+func (s *State) DataPath(p string) string {
+	p = TestDataPathForArch(p, s.arch)
+	fp := filepath.Clean(filepath.Join(s.dataDir, p))
+	if !strings.HasPrefix(fp, s.dataDir+"/") {
+		s.Fatalf("Invalid data path %q (expected relative path without ..)", p)
+	}
+	return fp
+}
+
+// OutDir returns a directory into which the test may place arbitrary files
+// that should be included with the test results.
+func (s *State) OutDir() string { return s.outDir }
+
+// Log formats its arguments using default formatting and logs them.
+func (s *State) Log(args ...interface{}) {
+	s.ch <- Output{T: time.Now(), Msg: fmt.Sprint(args...)}
+}
+
+// Logf is similar to Log but formats its arguments using fmt.Sprintf.
+func (s *State) Logf(format string, args ...interface{}) {
+	s.ch <- Output{T: time.Now(), Msg: fmt.Sprintf(format, args...)}
+}
+
+// Error formats its arguments using default formatting and marks the test
+// as having failed (using the arguments as a reason for the failure)
+// while letting the test continue execution.
+func (s *State) Error(args ...interface{}) {
+	e := s.newError(fmt.Sprint(args...))
+	s.ch <- Output{T: time.Now(), Err: e}
+}
+
+// Errorf is similar to Error but formats its arguments using fmt.Sprintf.
+func (s *State) Errorf(format string, args ...interface{}) {
+	e := s.newError(fmt.Sprintf(format, args...))
+	s.ch <- Output{T: time.Now(), Err: e}
+}
+
+// Fatal is similar to Error but additionally immediately ends the test.
+func (s *State) Fatal(args ...interface{}) {
+	e := s.newError(fmt.Sprint(args...))
+	s.ch <- Output{T: time.Now(), Err: e}
+	runtime.Goexit()
+}
+
+// Fatalf is similar to Fatal but formats its arguments using fmt.Sprintf.
+func (s *State) Fatalf(format string, args ...interface{}) {
+	e := s.newError(fmt.Sprintf(format, args...))
+	s.ch <- Output{T: time.Now(), Err: e}
+	runtime.Goexit()
+}
+
+// newError returns a new Error object with reason rsn. It attaches additional
+// information and expects that the error was initiated by the code that called
+// the function that called newError.
+func (s *State) newError(rsn string) *Error {
+	_, fn, ln, _ := runtime.Caller(2)
+	return &Error{
+		Reason: rsn,
+		File:   fn,
+		Line:   ln,
+		Stack:  string(debug.Stack()),
+	}
+}
+
+// ContextLog formats its arguments using default formatting and logs them
+// via ctx, previously provided by State.Context. It is intended to be used for
+// informational logging by packages providing support for tests. Tests should
+// just call State.Log or State.Logf instead.
+func ContextLog(ctx context.Context, args ...interface{}) {
+	if s, ok := ctx.Value(logKey).(*State); ok {
+		s.Log(args...)
+	}
+}
+
+// ContextLogf is similar to ContextLog but formats its arguments using fmt.Sprintf.
+func ContextLogf(ctx context.Context, format string, args ...interface{}) {
+	if s, ok := ctx.Value(logKey).(*State); ok {
+		s.Logf(format, args...)
+	}
+}
---
+++ src/chromiumos/tast/common/testing/state_test.go
@@ -0,0 +1,148 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"context"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	gotesting "testing"
+	"time"
+)
+
+// readOutput reads and returns Output entries from ch.
+func readOutput(ch chan Output) []Output {
+	res := make([]Output, 0)
+	for o := range ch {
+		res = append(res, o)
+	}
+	return res
+}
+
+func TestLog(t *gotesting.T) {
+	ch := make(chan Output, 2)
+	s := NewState(context.Background(), ch, "", "", "", time.Second)
+	s.Log("msg ", 1)
+	s.Logf("msg %d", 2)
+	close(ch)
+	out := readOutput(ch)
+	if len(out) != 2 || out[0].Msg != "msg 1" || out[1].Msg != "msg 2" {
+		t.Errorf("Bad test output: %v", out)
+	}
+}
+
+func TestReportError(t *gotesting.T) {
+	ch := make(chan Output, 2)
+	s := NewState(context.Background(), ch, "", "", "", time.Second)
+
+	// Keep these lines next to each other (see below comparison).
+	s.Error("error ", 1)
+	s.Errorf("error %d", 2)
+	close(ch)
+
+	out := readOutput(ch)
+	if len(out) != 2 {
+		t.Fatalf("Got %v output(s); want 2", len(out))
+	}
+
+	e0, e1 := out[0].Err, out[1].Err
+	if e0 == nil || e1 == nil {
+		t.Fatal("Got nil error(s)")
+	}
+	if act, exp := []string{e0.Reason, e1.Reason}, []string{"error 1", "error 2"}; !reflect.DeepEqual(act, exp) {
+		t.Errorf("Got reasons %v; want %v", act, exp)
+	}
+	if _, fn, _, _ := runtime.Caller(0); e0.File != fn || e1.File != fn {
+		t.Errorf("Got filenames %q and %q; want %q", e0.File, e1.File, fn)
+	}
+	if e0.Line+1 != e1.Line {
+		t.Errorf("Got non-sequential line numbers %d and %d", e0.Line, e1.Line)
+	}
+	if len(e0.Stack) == 0 || len(e1.Stack) == 0 {
+		t.Errorf("Got empty stack trace(s) %q and %q", string(e0.Stack), string(e1.Stack))
+	}
+}
+
+func TestFatal(t *gotesting.T) {
+	ch := make(chan Output, 2)
+	s := NewState(context.Background(), ch, "", "", "", time.Second)
+
+	// Log the fatal message in a goroutine so the main goroutine that's running the test won't exit.
+	done := make(chan bool)
+	died := true
+	go func() {
+		defer func() {
+			close(done)
+			close(ch)
+		}()
+		s.Fatalf("fatal %s", "msg")
+		died = false
+	}()
+	<-done
+
+	if !died {
+		t.Errorf("Test continued after call to Fatalf")
+	}
+	out := readOutput(ch)
+	if len(out) != 1 {
+		t.Errorf("Got %v outputs; want 1", len(out))
+	} else if out[0].Err == nil || out[0].Err.Reason != "fatal msg" {
+		t.Errorf("Got output %v; want reason %q", out[0].Err, "fatal msg")
+	}
+}
+
+func TestContextLog(t *gotesting.T) {
+	ch := make(chan Output, 2)
+	s := NewState(context.Background(), ch, "", "", "", time.Second)
+	ContextLog(s.Context(), "msg ", 1)
+	ContextLogf(s.Context(), "msg %d", 2)
+	close(ch)
+	out := readOutput(ch)
+	if len(out) != 2 || out[0].Msg != "msg 1" || out[1].Msg != "msg 2" {
+		t.Errorf("Bad test output: %v", out)
+	}
+}
+
+func TestDataPathValid(t *gotesting.T) {
+	const (
+		arch    = "myarch"
+		dataDir = "/tmp/data"
+	)
+
+	for _, tc := range []struct{ in, exp string }{
+		{"foo", filepath.Join(dataDir, "foo")},
+		{"foo/bar", filepath.Join(dataDir, "foo/bar")},
+		{"foo/bar", filepath.Join(dataDir, "foo/bar")},
+		{"{arch}/bin", filepath.Join(dataDir, arch, "bin")},
+	} {
+		s := NewState(context.Background(), make(chan Output), arch, dataDir, "", time.Second)
+		if act := s.DataPath(tc.in); act != tc.exp {
+			t.Errorf("DataPath(%q) = %q; want %q", tc.in, act, tc.exp)
+		}
+	}
+}
+
+func TestDataPathNoParent(t *gotesting.T) {
+	ch := make(chan Output, 1)
+	s := NewState(context.Background(), ch, "arch", "/data", "", time.Second)
+
+	// Request an invalid data path to cause a fatal error. Do this in a goroutine
+	// so the main goroutine that's running the test won't exit.
+	done := make(chan bool)
+	go func() {
+		defer func() {
+			close(done)
+			close(ch)
+		}()
+		s.DataPath("../foo.bin")
+	}()
+	<-done
+
+	out := readOutput(ch)
+	if len(out) != 1 || out[0].Err == nil {
+		t.Errorf("Got %v when requesting data path containing '..'; wanted 1 error", out)
+	}
+}

2d8cacb93949a09fa538a95c1e19ec2eab6cb573
Daniel Erat <derat@chromium.org>
2017-08-18 11:05:15 -0700

    tast: Add common/testing and common/testing/attr.
    Add a shared package used to define, select, and run tests.
    
    BUG=chromium:737628
    TEST=added unit tests; also used a bunch with the rest of
         the not-yet-landed code
    
    Change-Id: I5c7d189fa225145681ae2657a90b0e7053e68d9b

---
+++ src/chromiumos/tast/common/testing/registry_test.go
@@ -0,0 +1,73 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"reflect"
+	gotesting "testing"
+)
+
+func TestTestsForPattern(t *gotesting.T) {
+	reg := NewRegistry()
+	allTests := []*Test{
+		&Test{Name: "test.Foo", Func: Func1},
+		&Test{Name: "test.Bar", Func: Func1},
+		&Test{Name: "blah.Foo", Func: Func1},
+	}
+	for _, test := range allTests {
+		if err := reg.AddTest(test); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, tc := range []struct {
+		pat      string
+		expected []*Test
+	}{
+		{"test.Foo", []*Test{allTests[0]}},
+		{"test.Bar", []*Test{allTests[1]}},
+		{"test.*", []*Test{allTests[0], allTests[1]}},
+		{"*.Foo", []*Test{allTests[0], allTests[2]}},
+		{"*.*", []*Test{allTests[0], allTests[1], allTests[2]}},
+		{"*", []*Test{allTests[0], allTests[1], allTests[2]}},
+		{"", []*Test{}},
+		{"bogus", []*Test{}},
+		// Test that periods are escaped.
+		{"test.Fo.", []*Test{}},
+	} {
+		if tests, err := reg.TestsForPattern(tc.pat); err != nil {
+			t.Fatalf("TestsForPattern(%q) failed: %v", tc.pat, err)
+		} else if !reflect.DeepEqual(tests, tc.expected) {
+			t.Errorf("TestsForPattern(%q) = %v; want %v", tc.pat, tests, tc.expected)
+		}
+	}
+
+	// Now test multiple patterns.
+	for _, tc := range []struct {
+		pats     []string
+		expected []*Test
+	}{
+		{[]string{"test.Foo"}, []*Test{allTests[0]}},
+		{[]string{"test.Foo", "test.Foo"}, []*Test{allTests[0]}},
+		{[]string{"test.Foo", "test.Bar"}, []*Test{allTests[0], allTests[1]}},
+	} {
+		if tests, err := reg.TestsForPatterns(tc.pats); err != nil {
+			t.Fatalf("TestsForPatterns(%v) failed: %v", tc.pats, err)
+		} else if !reflect.DeepEqual(tests, tc.expected) {
+			t.Errorf("TestsForPatterns(%v) = %v; want %v", tc.pats, tests, tc.expected)
+		}
+	}
+}
+
+func TestAddTestFailsForInvalidTests(t *gotesting.T) {
+	reg := NewRegistry()
+
+	if err := reg.AddTest(&Test{Name: "Invalid%@!", Func: Func1}); err == nil {
+		t.Errorf("Didn't get error when adding test with invalid name")
+	}
+	if err := reg.AddTest(&Test{Name: "MissingFunction"}); err == nil {
+		t.Errorf("Didn't get error when adding test with missing function")
+	}
+}

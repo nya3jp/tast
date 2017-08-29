99625a245e1c139cfab0bc4e48e4645b5d5ded72
Daniel Erat <derat@chromium.org>
2017-08-29 09:20:47 -0700

    tast: Add local/chrome.
    Add a package used by local tests to interact with Chrome.
    
    BUG=chromium:737628
    TEST=used in conjunction with forthcoming local tests
    
    Change-Id: I26ed846ecea1911c53ce6e3f0eeb930c1a6c04b3

---
+++ src/chromiumos/tast/local/chrome/arc.go
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package chrome
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"chromiumos/tast/common/testing"
+)
+
+// waitForAndroidBooted waits for the Android container to report that it's finished booting.
+func waitForAndroidBooted(ctx context.Context) error {
+	testing.ContextLog(ctx, "Waiting for Android to finish booting")
+	if err := poll(ctx, func() bool {
+		b, err := exec.Command("android-sh", "-c", "getprop sys.boot_completed").CombinedOutput()
+		return err == nil && strings.TrimRight(string(b), "\n") != "1"
+	}); err != nil {
+		return fmt.Errorf("Android didn't boot: %v", err)
+	}
+	return nil
+}
+
+// TODO(derat): Add an enablePlayStore function based on enable_play_store() in
+// client/common_lib/cros/arc_util.py.

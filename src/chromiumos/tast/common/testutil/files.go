f3c3b7da9d1bc22e8a6941b2be71a8fad804d2c9
Daniel Erat <derat@chromium.org>
2017-09-28 16:59:39 -0700

    tast: Update README.md to link to package docs.
    Also add a description to the common/testutil package.
    
    BUG=none
    TEST=checked link via md_browser.py
    
    Change-Id: I2fe3fa25eb2b64edb6394a7163c00f3942cc1131

--- src/chromiumos/tast/common/testutil/files.go
+++ src/chromiumos/tast/common/testutil/files.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Package testutil provides support code for unit tests.
 package testutil
 
 import (

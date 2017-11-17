ef0d7b863eb6909a15504d5eec3e694c02a70def
Daniel Erat <derat@chromium.org>
2017-11-17 12:25:24 -0800

    tast: Refactor SSH test code.
    Move some SSH-key-generating functionality into the
    common/host/test package so it can be used by other packages
    that need to start a test SSH server.
    
    Also update common/host/test.SSHServer to support returning
    canned responses for particular commands instead of actually
    executing them.
    
    Per the "Interfaces" section of
    https://github.com/golang/go/wiki/CodeReviewComments, this
    is the preferred approach for testing in Go: rather than
    giving the package's users e.g. a FakeHost implementation
    of a Host interface, make it easier for them to use the real
    SSH implementation in their tests.
    
    BUG=chromium:782265
    TEST=tests still pass
    
    Change-Id: I31718271d29d58a76110fc7f0401c9cbbdf01aa9

---
+++ src/chromiumos/tast/common/host/test/ssh_keys.go
@@ -0,0 +1,54 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package test
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// GenerateKeys generates SSH user and host keys of size bits.
+// This can be time-consuming, so a test file may want to only call this once in
+// its init function and reuse the results.
+func GenerateKeys(bits int) (userKey, hostKey *rsa.PrivateKey, err error) {
+	if userKey, err = rsa.GenerateKey(rand.Reader, bits); err != nil {
+		return nil, nil, fmt.Errorf("failed to generate user RSA key: %v", err)
+	}
+	if hostKey, err = rsa.GenerateKey(rand.Reader, bits); err != nil {
+		return nil, nil, fmt.Errorf("failed to generate host RSA key: %v", err)
+	}
+	return userKey, hostKey, nil
+}
+
+// WriteKey writes key to a temporary file and returns its path.
+// The caller is responsible for unlinking the temp file when complete.
+func WriteKey(key *rsa.PrivateKey) (path string, err error) {
+	data := pem.EncodeToMemory(&pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   x509.MarshalPKCS1PrivateKey(key),
+	})
+
+	f, err := ioutil.TempFile("", "tast_unittest.ssh_key.")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	if err = f.Chmod(0600); err != nil {
+		os.Remove(f.Name())
+		return "", err
+	}
+	if _, err = f.Write(data); err != nil {
+		os.Remove(f.Name())
+		return "", err
+	}
+	return f.Name(), nil
+}

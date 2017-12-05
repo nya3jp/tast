dadccefbd3fbb77b86ab3f2929fcc527f4c737b1
Daniel Erat <derat@chromium.org>
2017-12-04 21:46:32 -0800

    tast: Move files around.
    Move the tast command from chromiumos/tast/cmd to
    chromiumos/cmd/tast, and move all other packages from
    chromiumos/tast/common/ to chromiumos/tast/.
    
    BUG=chromium:791878
    TEST=still builds
    CQ-DEPEND=I58fa9ef006f9a0976c370252ce29f1c23a8db724
    CQ-DEPEND=I6c065c7a2a422b8a4ee3b5dd467928a5c01f533f
    
    Change-Id: I702870e6efe13dd1365eccd38deac45082fbe2eb

---
+++ src/chromiumos/cmd/tast/build/build_test.go
@@ -0,0 +1,74 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package build
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+
+	"chromiumos/tast/testutil"
+)
+
+func TestBuildTests(t *testing.T) {
+	tempDir := testutil.TempDir(t, "build_test.")
+	defer os.RemoveAll(tempDir)
+
+	const (
+		testDir   = "test"
+		commonDir = "common"
+		sysDir    = "sys"
+	)
+
+	var err error
+	cfg := &Config{
+		TestWorkspace:   filepath.Join(tempDir, testDir),
+		CommonWorkspace: filepath.Join(tempDir, commonDir),
+		SysGopath:       filepath.Join(tempDir, sysDir),
+		OutDir:          filepath.Join(tempDir, "out"),
+	}
+	if cfg.Arch, err = GetLocalArch(); err != nil {
+		t.Fatal("Failed to get local arch: ", err)
+	}
+
+	// In order to test that the supplied common and system GOPATHs are used, build a main
+	// package that prints constants exported by a common and system package.
+	const (
+		commonPkgName   = "commonpkg" // common package's name
+		commonConstName = "Msg"       // name of const exported by common package
+		commonConstVal  = "foo"       // value of const exported by common package
+
+		sysPkgName   = "syspkg" // system package's name
+		sysConstName = "Msg"    // name of const exported by system package
+		sysConstVal  = "bar"    // value of const exported by system package
+	)
+	commonPkgCode := fmt.Sprintf("package %s\nconst %s = %q", commonPkgName, commonConstName, commonConstVal)
+	sysPkgCode := fmt.Sprintf("package %s\nconst %s = %q", sysPkgName, sysConstName, sysConstVal)
+	mainCode := fmt.Sprintf("package main\nimport %q\nimport %q\nfunc main() { print(%s.%s+%s.%s) }",
+		commonPkgName, sysPkgName, commonPkgName, commonConstName, sysPkgName, sysConstName)
+
+	if err := testutil.WriteFiles(tempDir, map[string]string{
+		filepath.Join(commonDir, "src", commonPkgName, "lib.go"): commonPkgCode,
+		filepath.Join(sysDir, "src", sysPkgName, "lib.go"):       sysPkgCode,
+		filepath.Join(testDir, "src/foo/cmd/main.go"):            mainCode,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	bin := filepath.Join(tempDir, "foo")
+	if out, err := BuildTests(context.Background(), cfg, "foo/cmd", bin); err != nil {
+		t.Fatalf("Failed to build: %v: %s", err, string(out))
+	}
+
+	exp := commonConstVal + sysConstVal
+	if out, err := exec.Command(bin).CombinedOutput(); err != nil {
+		t.Errorf("Failed to run %s: %v", bin, err)
+	} else if string(out) != exp {
+		t.Errorf("%s printed %q; want %q", bin, string(out), exp)
+	}
+}

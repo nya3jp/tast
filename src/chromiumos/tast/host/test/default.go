50dd521b8acc01d12ed18e27e6597463f147c20b
Daniel Erat <derat@chromium.org>
2017-12-26 02:09:16 -0800

    tast: Add MustGenerateKeys and TestData to host/test.
    Move some common code into the chromiumos/tast/host/test
    package so it doesn't need to be duplicated across tests
    that run SSH servers.
    
    BUG=chromium:784944
    TEST=built with FEATURES=test
    
    Change-Id: Ie9a8fc20f47736d1996868c98e97e432a6b1555b
    Reviewed-on: https://chromium-review.googlesource.com/844334
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Jason Clinton <jclinton@chromium.org>

---
+++ src/chromiumos/tast/host/test/default.go
@@ -0,0 +1,50 @@
+// Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package test
+
+import (
+	"crypto/rsa"
+	"os"
+)
+
+const (
+	defaultKeyBits = 1024
+)
+
+// MustGenerateKeys can be called from a test file's init function to generate
+// 1024-bit user and host keys. Panics on error.
+func MustGenerateKeys() (userKey, hostKey *rsa.PrivateKey) {
+	var err error
+	if userKey, hostKey, err = GenerateKeys(defaultKeyBits); err != nil {
+		panic(err)
+	}
+	return userKey, hostKey
+}
+
+// TestData contains common data that can be used by tests that interact with an SSHServer.
+type TestData struct {
+	Srv         *SSHServer
+	UserKeyPath string
+}
+
+// NewTestData initializes and returns a TestData struct. Panics on error.
+func NewTestData(userKey, hostKey *rsa.PrivateKey) *TestData {
+	d := TestData{}
+	var err error
+	if d.Srv, err = NewSSHServer(&userKey.PublicKey, hostKey); err != nil {
+		panic(err)
+	}
+	if d.UserKeyPath, err = WriteKey(userKey); err != nil {
+		d.Srv.Close()
+		panic(err)
+	}
+	return &d
+}
+
+// Close stops the SSHServer and deletes the user key file.
+func (d *TestData) Close() {
+	d.Srv.Close()
+	os.Remove(d.UserKeyPath)
+}

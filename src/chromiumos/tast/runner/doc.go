5e12468080703acaab32c4a5a30cf50d06173f1a
Daniel Erat <derat@chromium.org>
2017-12-30 03:39:40 -0800

    tast: Fix a few test bundle output-related bugs.
    - Make bundles include the executable name, rather than the
      first arg, in usage messages.
    - Make bundles log to stdout rather than stderr when run
      without -report; otherwise, the test runner fails to copy
      test output when run manually.
    - For consistency, make runners also log to stdout when run
      without -report.
    - Make runners print usage from the correct flag.FlagSet
      struct instead of from the (unused) global flag set.
    - Make runners avoid logging errors returned by
      flag.FlagSet.Parse, since the flag package already writes
      errors to stderr.
    - Make runners exit with success after writing a TestError
      control message when running with -report. Otherwise, the
      tast command just prints a generic error message about the
      runner exiting with a nonzero status code.
    - Make the tast command just print basenames of source
      files responsible for RunError control messages.
    
    Also add more documentation about bundle and runner logging
    and status codes.
    
    BUG=chromium:784944
    TEST=added some tests and tested manually in conjunction
         with not-yet-reviewed change to make tast command
         support test bundles
    
    Change-Id: I57f62224e8245fe7e0d0be3ac84afe746934d1b6

--- src/chromiumos/tast/runner/doc.go
+++ src/chromiumos/tast/runner/doc.go
@@ -11,5 +11,15 @@
 	There is a local test runner that executes local bundles on-device and a
 	remote test runner that executes remote bundles on the system where the
 	tast command is running (e.g. a developer's workstation).
+
+	The tast command passes the -report flag when executing test runners,
+	which instructs runners to report progress by writing JSON-marshaled
+	control messages to stdout. In this mode, the runner exits with status
+	code 0 in almost all cases (the one exception being malformed arguments),
+	since the result of the run is already communicated via control messages.
+
+	When a test runner is executed manually without the -report flag, the
+	runner instead logs human-readable progress to stdout. The runner exits
+	with a nonzero status code if an error is encountered.
 */
 package runner

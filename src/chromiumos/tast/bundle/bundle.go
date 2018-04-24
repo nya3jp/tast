36f4ad44cdb6c09573377b46bd20095e0d7a275b
Daniel Erat <derat@chromium.org>
2018-04-24 16:40:14 -0700

    tast: Rework bundle execution flow.
    Update the bundle package to have a single run() function
    that both parses args from stdin and performs the requested
    action. This removes some trickiness from readArgs(), which
    formerly either listed tests itself or returned a runConfig
    struct that callers were expected to pass to runTests().
    
    BUG=chromium:817688
    TEST=unit tests pass; also manually verified that listing
         and running local and remote tests works
    CQ-DEPEND=I8d29b42741b2e2aa5d654cf1b45ebd6b12fb80e9
    
    Change-Id: Iea5ffedb7f9d3917d0692110329a7340ceed272f

--- src/chromiumos/tast/bundle/bundle.go
+++ src/chromiumos/tast/bundle/bundle.go
@@ -6,6 +6,7 @@ package bundle
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -22,87 +23,144 @@ const (
 	statusBadTests    = 3 // errors in test registration (bad names, missing test functions, etc.)
 	statusBadPatterns = 4 // one or more bad test patterns were passed to the bundle
 	statusNoTests     = 5 // no tests were matched by the supplied patterns
-
-	// Number of characters in prefixes from the log package, e.g. "2017/08/17 09:29:54 ".
-	logPrefixLen = 20
 )
 
-// writeError writes an error to stderr.
-func writeError(msg string) {
+// bundleError implements the error interface and contains an additional status code.
+type bundleError struct {
+	msg    string
+	status int
+}
+
+func (e *bundleError) Error() string {
+	return fmt.Sprintf("%v (status %v)", e.msg, e.status)
+}
+
+// newBundleError creates a bundleError with the passed status code and formatted string.
+func newBundleErrorf(status int, format string, args ...interface{}) *bundleError {
+	return &bundleError{fmt.Sprintf(format, args...), status}
+}
+
+// writeError writes a newline-terminated fatal error to stderr and returns the status code to use when exiting.
+func writeError(stderr io.Writer, err error) int {
+	var msg string
+	var status int
+
+	if be, ok := err.(*bundleError); ok {
+		msg = be.msg
+		status = be.status
+	} else {
+		msg = err.Error()
+		status = statusError
+	}
+
 	if len(msg) > 0 && msg[len(msg)-1] != '\n' {
 		msg += "\n"
 	}
-	io.WriteString(os.Stderr, msg)
+	io.WriteString(stderr, msg)
+
+	return status
 }
 
-// runConfig describes how runTests should run tests.
+// run reads a JSON-marshaled Args struct from stdin and performs the requested action.
+// Default arguments may be specified via args, which will also be updated from stdin.
+// The caller should exit with the returned status code.
+func run(ctx context.Context, stdin io.Reader, stdout, stderr io.Writer,
+	args *Args, cfg *runConfig, bt bundleType) int {
+	tests, err := readArgs(stdin, args, bt)
+	if err != nil {
+		return writeError(stderr, err)
+	}
+
+	switch args.Mode {
+	case ListTestsMode:
+		if err := testing.WriteTestsAsJSON(stdout, tests); err != nil {
+			return writeError(stderr, err)
+		}
+		return statusSuccess
+	case RunTestsMode:
+		if err := runTests(ctx, stdout, args, cfg, tests); err != nil {
+			return writeError(stderr, err)
+		}
+		return statusSuccess
+	default:
+		return writeError(stderr, newBundleErrorf(statusBadArgs, "invalid mode %v", args.Mode))
+	}
+}
+
+// runConfig contains additional parameters used by runTests.
 type runConfig struct {
-	// args contains arguments passed to the bundle by the runner.
-	args *Args
-	// mw is used to send control messages to the controlling process.
-	// It is initialized by readArgs and is nil if the -report flag was not passed.
-	mw *control.MessageWriter
-	// tests contains tests to run. It is initialized by readArgs.
-	tests []*testing.Test
-	// setupFunc is run before each test if non-nil.
-	setupFunc func() error
+	// runSetupFunc is run at the beginning of the entire test run if non-nil.
+	// ctx (or a derived context with additional values) should be returned by the function.
+	runSetupFunc func(ctx context.Context) (context.Context, error)
+	// runCleanupFunc is run at the end of the entire test run if non-nil.
+	runCleanupFunc func(ctx context.Context) error
+	// testSetupFunc is run before each test if non-nil.
+	testSetupFunc func(ctx context.Context) error
 	// defaultTestTimeout contains the default maximum time allotted to each test.
 	// It is only used if testing.Test.Timeout is unset.
 	defaultTestTimeout time.Duration
 }
 
-// runTests runs tests per cfg.
-
-// If an error is encountered in the test harness (as opposed to in a test), it is returned
-// immediately.
+// runTests runs tests per args and cfg and writes control messages to stdout.
 //
-// If cfg.mw is nil (i.e. tests were executed manually rather than by the tast command),
-// failure is reported if any tests failed. If cfg.mw is non-nil, success is reported even
-// if tests fail, as the tast command knows how to interpret test results.
-func runTests(ctx context.Context, cfg *runConfig) int {
-	if len(cfg.tests) == 0 {
-		writeError("No tests matched by pattern(s)")
-		return statusNoTests
+// If an error is encountered in the test harness (as opposed to in a test), an error is returned.
+// Otherwise, nil is returned (test errors will be reported via TestError control messages).
+func runTests(ctx context.Context, stdout io.Writer, args *Args, cfg *runConfig,
+	tests []*testing.Test) error {
+	mw := control.NewMessageWriter(stdout)
+
+	if len(tests) == 0 {
+		return newBundleErrorf(statusNoTests, "no tests matched by pattern(s)")
 	}
 
-	for _, t := range cfg.tests {
+	if cfg.runSetupFunc != nil {
+		var err error
+		if ctx, err = cfg.runSetupFunc(ctx); err != nil {
+			return newBundleErrorf(statusError, "run setup failed: %v", err)
+		}
+	}
+
+	for _, t := range tests {
 		// Make a copy of the test with the default timeout if none was specified.
 		test := *t
 		if test.Timeout == 0 {
 			test.Timeout = cfg.defaultTestTimeout
 		}
 
-		cfg.mw.WriteMessage(&control.TestStart{Time: time.Now(), Test: test})
+		mw.WriteMessage(&control.TestStart{Time: time.Now(), Test: test})
 
-		outDir := filepath.Join(cfg.args.OutDir, test.Name)
+		outDir := filepath.Join(args.OutDir, test.Name)
 		if err := os.MkdirAll(outDir, 0755); err != nil {
-			writeError("Failed to create output dir: " + err.Error())
-			return statusError
+			return newBundleErrorf(statusError, "failed to create output dir: %v", err)
 		}
 
-		if cfg.setupFunc != nil {
-			if err := cfg.setupFunc(); err != nil {
-				writeError("Failed to run setup: " + err.Error())
-				return statusError
+		if cfg.testSetupFunc != nil {
+			if err := cfg.testSetupFunc(ctx); err != nil {
+				return newBundleErrorf(statusError, "test setup failed: %v", err)
 			}
 		}
 		ch := make(chan testing.Output)
-		s := testing.NewState(ctx, ch, filepath.Join(cfg.args.DataDir, test.DataDir()), outDir,
+		s := testing.NewState(ctx, ch, filepath.Join(args.DataDir, test.DataDir()), outDir,
 			test.Timeout, test.CleanupTimeout)
 
 		done := make(chan bool, 1)
 		go func() {
-			copyTestOutput(ch, cfg.mw)
+			copyTestOutput(ch, mw)
 			done <- true
 		}()
 		test.Run(s)
 		close(ch)
 		<-done
 
-		cfg.mw.WriteMessage(&control.TestEnd{Time: time.Now(), Name: test.Name})
+		mw.WriteMessage(&control.TestEnd{Time: time.Now(), Name: test.Name})
 	}
 
-	return statusSuccess
+	if cfg.runCleanupFunc != nil {
+		if err := cfg.runCleanupFunc(ctx); err != nil {
+			return newBundleErrorf(statusError, "run cleanup failed: %v", err)
+		}
+	}
+	return nil
 }
 
 // copyTestOutput reads test output from ch and writes it to mw.

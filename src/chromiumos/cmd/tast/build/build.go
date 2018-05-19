nya3jp/tast

commit 96bc6953d2104225a7f1469527b0e0e651f0597d

tast: Use "go install" when building test bundles.
Update the tast command to use "go install" rather than "go
build -i -pkgdir" when building test bundles (or
local_test_runner). "go build" and "go install" now cache
compiled packages to $HOME/.cache/go-build, so there's no
need to create our own directory for this. (Go also handles
cleaning old files out of the new directory.)

BUG=chromium:813790
TEST=verified that "go build", "go run", and "go list" still
     work (with both -buildtype=local and -buildtype=remote,
     for the latter two) and that building takes ~400 ms
     with -checkbuilddeps=false to bypass slow equery

Change-Id: I421ccac419aec8dd6e7cb140a92b1e2b071d28f4
Reviewed-on: https://chromium-review.googlesource.com/1025399
Commit-Ready: Dan Erat <derat@chromium.org>
Tested-by: Dan Erat <derat@chromium.org>
Reviewed-by: Rahul Chaudhry <rahulchaudhry@chromium.org>

src/chromiumos/cmd/tast/build/build.go
@@ -9,8 +9,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"chromiumos/cmd/tast/timing"
@@ -36,9 +36,10 @@ var archToCompiler map[string]string = map[string]string{
 	"aarch64": "armv7a-cros-linux-gnueabi-go",
 }
 
-// Build builds executable package pkg to path as dictated by cfg.
+// Build builds executable package pkg to outDir as dictated by cfg.
+// The executable file's name is assigned by "go install" (i.e. it's the last component of pkg).
 // stageName is used as the name of a new stage reported via the timing package.
-func Build(ctx context.Context, cfg *Config, pkg, path, stageName string) (out []byte, err error) {
+func Build(ctx context.Context, cfg *Config, pkg, outDir, stageName string) (out []byte, err error) {
 	if tl, ok := timing.FromContext(ctx); ok {
 		st := tl.Start(stageName)
 		defer st.End()
@@ -65,12 +66,11 @@ func Build(ctx context.Context, cfg *Config, pkg, path, stageName string) (out [
 		}
 	}
 
-	pkgDir := filepath.Join(cfg.OutDir, cfg.Arch)
-	cmd := exec.Command(comp, "build", "-i", "-ldflags=-s -w", "-pkgdir", pkgDir, "-o", path, pkg)
-	cmd.Env = []string{
-		"PATH=/usr/bin",
-		"GOPATH=" + strings.Join([]string{cfg.TestWorkspace, cfg.CommonWorkspace, cfg.SysGopath}, ":"),
-	}
+	cmd := exec.Command(comp, "install", "-ldflags=-s -w", pkg)
+	cmd.Env = append(os.Environ(),
+		"GOPATH="+strings.Join([]string{cfg.TestWorkspace, cfg.CommonWorkspace, cfg.SysGopath}, ":"),
+		"GOBIN="+outDir,
+	)
 	if out, err = cmd.CombinedOutput(); err != nil {
 		return out, err
 	}

src/chromiumos/cmd/tast/build/build_test.go
@@ -30,7 +30,6 @@ func TestBuild(t *testing.T) {
 		TestWorkspace:   filepath.Join(tempDir, testDir),
 		CommonWorkspace: filepath.Join(tempDir, commonDir),
 		SysGopath:       filepath.Join(tempDir, sysDir),
-		OutDir:          filepath.Join(tempDir, "out"),
 	}
 	if cfg.Arch, err = GetLocalArch(); err != nil {
 		t.Fatal("Failed to get local arch: ", err)
@@ -46,6 +45,8 @@ func TestBuild(t *testing.T) {
 		sysPkgName   = "syspkg" // system package's name
 		sysConstName = "Msg"    // name of const exported by system package
 		sysConstVal  = "bar"    // value of const exported by system package
+
+		mainPkgName = "foo" // main() package's name
 	)
 	commonPkgCode := fmt.Sprintf("package %s\nconst %s = %q", commonPkgName, commonConstName, commonConstVal)
 	sysPkgCode := fmt.Sprintf("package %s\nconst %s = %q", sysPkgName, sysConstName, sysConstVal)
@@ -55,17 +56,18 @@ func TestBuild(t *testing.T) {
 	if err := testutil.WriteFiles(tempDir, map[string]string{
 		filepath.Join(commonDir, "src", commonPkgName, "lib.go"): commonPkgCode,
 		filepath.Join(sysDir, "src", sysPkgName, "lib.go"):       sysPkgCode,
-		filepath.Join(testDir, "src/foo/cmd/main.go"):            mainCode,
+		filepath.Join(testDir, "src", mainPkgName, "main.go"):    mainCode,
 	}); err != nil {
 		t.Fatal(err)
 	}
 
-	bin := filepath.Join(tempDir, "foo")
-	if out, err := Build(context.Background(), cfg, "foo/cmd", bin, ""); err != nil {
+	outDir := filepath.Join(tempDir, "out")
+	if out, err := Build(context.Background(), cfg, mainPkgName, outDir, ""); err != nil {
 		t.Fatalf("Failed to build: %v: %s", err, string(out))
 	}
 
 	exp := commonConstVal + sysConstVal
+	bin := filepath.Join(outDir, filepath.Base(mainPkgName))
 	if out, err := exec.Command(bin).CombinedOutput(); err != nil {
 		t.Errorf("Failed to run %s: %v", bin, err)
 	} else if string(out) != exp {

src/chromiumos/cmd/tast/build/config.go
@@ -28,8 +28,8 @@ type Config struct {
 	SysGopath string
 	// Arch is the architecture to build for (as a machine name or processor given by "uname -m").
 	Arch string
-	// OutDir is the path to a directory where compiled code is stored (after appending arch).
-	OutDir string
+	// BaseOutDir is the path to the base directory under which compiled executables are stored.
+	BaseOutDir string
 	// PortagePkg is the Portage package that contains the test executable (when tests are
 	// included in a system image rather than being compiled by the tast command).
 	// If non-empty, BuildTests checks that the package's direct dependencies are installed
@@ -37,16 +37,11 @@ type Config struct {
 	PortagePkg string
 }
 
-// OutPath returns the path to a file named fn within cfg's architecture-specific output dir.
-func (c *Config) OutPath(fn string) string {
-	return filepath.Join(c.OutDir, c.Arch, fn)
-}
-
 // SetFlags adds common build-related flags to f that store values in Config.
 // trunkDir is the path to the Chrome OS checkout (within the chroot).
 func (c *Config) SetFlags(f *flag.FlagSet, trunkDir string) {
 	f.StringVar(&c.Arch, "arch", "", "target architecture (per \"uname -m\")")
-	f.StringVar(&c.OutDir, "outdir", defaultBuildOutDir, "directory storing build artifacts")
+	f.StringVar(&c.BaseOutDir, "outdir", defaultBuildOutDir, "directory storing build artifacts")
 	f.StringVar(&c.SysGopath, "sysgopath", "/usr/lib/gopath",
 		"Go workspace containing system package source code")
 	f.StringVar(&c.TestWorkspace, "testdir", filepath.Join(trunkDir, defaultTestDir),

src/chromiumos/cmd/tast/build_cmd.go
@@ -15,7 +15,7 @@ import (
 	"github.com/google/subcommands"
 )
 
-// buildCmd implements subcommands.Command to support compiling test executables.
+// buildCmd implements subcommands.Command to support compiling executables.
 type buildCmd struct {
 	cfg      build.Config
 	pkg, out string
@@ -24,8 +24,8 @@ type buildCmd struct {
 func (*buildCmd) Name() string     { return "build" }
 func (*buildCmd) Synopsis() string { return "build tests" }
 func (*buildCmd) Usage() string {
-	return `build <flags> <pkg> <out>:
-	Builds an executable package containing tests.
+	return `build <flags> <pkg> <outdir>:
+	Builds an executable package.
 `
 }
 
@@ -48,7 +48,7 @@ func (b *buildCmd) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{
 	}
 
 	if out, err := build.Build(ctx, &b.cfg, f.Args()[0], f.Args()[1], ""); err != nil {
-		lg.Logf("Failed building tests: %v\n%s", err, string(out))
+		lg.Logf("Failed building: %v\n%s", err, string(out))
 		return subcommands.ExitFailure
 	}
 	return subcommands.ExitSuccess

src/chromiumos/cmd/tast/run/local.go
@@ -144,16 +144,16 @@ func buildAndPushBundle(ctx context.Context, cfg *Config, hst *host.SSH) (bundle
 	if cfg.checkPortageDeps {
 		cfg.buildCfg.PortagePkg = fmt.Sprintf("chromeos-base/tast-local-tests-%s-9999", cfg.buildBundle)
 	}
-	src := cfg.buildCfg.OutPath(filepath.Join(localBundleBuildSubdir, cfg.buildBundle))
+	buildDir := filepath.Join(cfg.buildCfg.BaseOutDir, cfg.buildCfg.Arch, localBundleBuildSubdir)
 	pkg := path.Join(localBundlePkgPathPrefix, cfg.buildBundle)
 	cfg.Logger.Logf("Building %s from %s", pkg, cfg.buildCfg.TestWorkspace)
-	if out, err := build.Build(ctx, &cfg.buildCfg, pkg, src, "build_bundle"); err != nil {
+	if out, err := build.Build(ctx, &cfg.buildCfg, pkg, buildDir, "build_bundle"); err != nil {
 		return "", fmt.Errorf("build failed: %v\n\n%s", err, out)
 	}
 	cfg.Logger.Logf("Built test bundle in %v", time.Now().Sub(start).Round(time.Millisecond))
 
 	cfg.Logger.Status("Pushing test bundle to target")
-	if err := pushBundle(ctx, cfg, hst, src, localBundlePushDir); err != nil {
+	if err := pushBundle(ctx, cfg, hst, filepath.Join(buildDir, cfg.buildBundle), localBundlePushDir); err != nil {
 		return "", fmt.Errorf("failed to push bundle: %v", err)
 	}
 
@@ -317,16 +317,16 @@ func buildAndPushLocalRunner(ctx context.Context, cfg *Config, hst *host.SSH) er
 		bc.PortagePkg = localRunnerPortagePkg
 	}
 
-	src := bc.OutPath(filepath.Base(localRunnerPath))
+	buildDir := filepath.Join(bc.BaseOutDir, bc.Arch)
 	cfg.Logger.Debugf("Building %s from %s", localRunnerPkg, bc.CommonWorkspace)
-	if out, err := build.Build(ctx, &bc, localRunnerPkg, src, "build_runner"); err != nil {
+	if out, err := build.Build(ctx, &bc, localRunnerPkg, buildDir, "build_runner"); err != nil {
 		return fmt.Errorf("failed to build test runner: %v\n\n%s", err, out)
 	}
 
 	cfg.Logger.Debugf("Pushing test runner to %s on target", localRunnerPath)
 	start := time.Now()
-	bytes, err := hst.PutTree(ctx, filepath.Dir(src), filepath.Dir(localRunnerPath),
-		[]string{filepath.Base(src)})
+	bytes, err := hst.PutTree(ctx, buildDir, filepath.Dir(localRunnerPath),
+		[]string{filepath.Base(localRunnerPath)})
 	if err != nil {
 		return fmt.Errorf("failed to copy test runner: %v", err)
 	}

src/chromiumos/cmd/tast/run/remote.go
@@ -52,17 +52,17 @@ func remote(ctx context.Context, cfg *Config) (subcommands.ExitStatus, []TestRes
 		if cfg.checkPortageDeps {
 			cfg.buildCfg.PortagePkg = fmt.Sprintf("chromeos-base/tast-remote-tests-%s-9999", cfg.buildBundle)
 		}
-		bundleDest := cfg.buildCfg.OutPath(filepath.Join(remoteBundleBuildSubdir, cfg.buildBundle))
+		buildDir := filepath.Join(cfg.buildCfg.BaseOutDir, cfg.buildCfg.Arch, remoteBundleBuildSubdir)
 		pkg := path.Join(remoteBundlePkgPathPrefix, cfg.buildBundle)
-		cfg.Logger.Debugf("Building %s from %s to %s", pkg, cfg.buildCfg.TestWorkspace, bundleDest)
-		if out, err := build.Build(ctx, &cfg.buildCfg, pkg, bundleDest, "build_bundle"); err != nil {
+		cfg.Logger.Debugf("Building %s from %s to %s", pkg, cfg.buildCfg.TestWorkspace, buildDir)
+		if out, err := build.Build(ctx, &cfg.buildCfg, pkg, buildDir, "build_bundle"); err != nil {
 			cfg.Logger.Logf("Failed building test bundle: %v\n\n%s", err, out)
 			return subcommands.ExitFailure, nil
 		}
 		cfg.Logger.Logf("Built test bundle in %v", time.Now().Sub(buildStart).Round(time.Millisecond))
 
 		// Only run tests from the newly-built bundle, and get test data from the source tree.
-		bundleGlob = bundleDest
+		bundleGlob = filepath.Join(buildDir, cfg.buildBundle)
 		dataDir = filepath.Join(cfg.buildCfg.TestWorkspace, "src")
 	} else {
 		bundleGlob = filepath.Join(cfg.remoteBundleDir, "*")

commit 12172ae89a5f402291573aa4c6913e08163162d8

tast: Fix cross-compiling test bundles.
Fix a regression introduced by 96bc6953 (which switched from
"go build" to "go install") that made the "tast build" and
"tast run -build" commands fail when cross-compiling with
the error "go install: cannot install cross-compiled
binaries when GOBIN is set".

"go install" is much faster for no-op builds, but doesn't
permit cross-compiling binaries to custom locations.
Continue using "go install" when building for the local arch
but fall back to "go build" when cross-compiling.

BUG=chromium:844832
TEST=manual: "tast run -build" works when run against a
     kevin DUT

Change-Id: I532ab47c1cdd5d792b765378842568be0392b50a

src/chromiumos/cmd/tast/build/build.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"chromiumos/cmd/tast/timing"
@@ -66,11 +67,34 @@ func Build(ctx context.Context, cfg *Config, pkg, outDir, stageName string) (out
 		}
 	}
 
-	cmd := exec.Command(comp, "install", "-ldflags=-s -w", pkg)
-	cmd.Env = append(os.Environ(),
-		"GOPATH="+strings.Join([]string{cfg.TestWorkspace, cfg.CommonWorkspace, cfg.SysGopath}, ":"),
-		"GOBIN="+outDir,
-	)
+	const ldFlags = "-ldflags=-s -w"
+
+	env := append(os.Environ(), "GOPATH="+strings.Join([]string{cfg.TestWorkspace, cfg.CommonWorkspace, cfg.SysGopath}, ":"))
+
+	// This is frustrating:
+	//
+	// - "go build" always rebuilds the final executable (but not its dependencies).
+	// - "go install" avoids rebuilding the executable when it hasn't changed, so it's faster.
+	// - We need to set $GOBIN when using "go install" to avoid installing alongside the source code.
+	// - But "go install" refuses to cross-compile if $GOBIN is set (by design; see https://golang.org/issue/11778).
+	//
+	// So, use "go install" when compiling for the local arch to get faster builds,
+	// but fall back to using "go build" when cross-compiling.
+	larch, err := GetLocalArch()
+	if err != nil {
+		return out, fmt.Errorf("failed to get local arch: %v", err)
+	}
+
+	var cmd *exec.Cmd
+	if larch == cfg.Arch {
+		cmd = exec.Command(comp, "install", ldFlags, pkg)
+		env = append(env, "GOBIN="+outDir)
+	} else {
+		dest := filepath.Join(outDir, filepath.Base(pkg))
+		cmd = exec.Command(comp, "build", ldFlags, "-o", dest, pkg)
+	}
+	cmd.Env = env
+
 	if out, err = cmd.CombinedOutput(); err != nil {
 		return out, err
 	}
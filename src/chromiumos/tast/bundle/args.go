118ba79738e191db3b1280f1afdf9aff0d8c5796
Daniel Erat <derat@chromium.org>
2018-05-25 15:42:12 -0700

    tast: Fix "omitEmpty" options in JSON field tags.
    Change CheckSoftwareDeps "omitEmpty" options in the bundle
    and runner packages' RunTestsArgs structs to "omitempty",
    the correct capitalization. "omitEmpty" is silently ignored.
    
    BUG=none
    TEST=manually verified that running tests with
         -checktestdeps=always still works
    
    Change-Id: Id9ac790a8cf05af171cc6ab274cd4ad31279cd36
    Reviewed-on: https://chromium-review.googlesource.com/1074169
    Commit-Ready: Dan Erat <derat@chromium.org>
    Tested-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Rahul Chaudhry <rahulchaudhry@chromium.org>

--- src/chromiumos/tast/bundle/args.go
+++ src/chromiumos/tast/bundle/args.go
@@ -60,7 +60,7 @@ type RemoteArgs struct {
 type RunTestsArgs struct {
 	// CheckSoftwareDeps is true if each test's SoftwareDeps field should be checked against
 	// AvailableSoftwareFeatures and UnavailableSoftwareFeatures.
-	CheckSoftwareDeps bool `json:"runTestsCheckSoftwareDeps,omitEmpty"`
+	CheckSoftwareDeps bool `json:"runTestsCheckSoftwareDeps,omitempty"`
 	// AvailableSoftwareFeatures contains a list of software features supported by the DUT.
 	AvailableSoftwareFeatures []string `json:"runTestsAvailableSoftwareFeatures,omitempty"`
 	// UnavailableSoftwareFeatures contains a list of software features supported by the DUT.

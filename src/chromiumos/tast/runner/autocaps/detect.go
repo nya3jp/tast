f5a2cf07b753d519f2bed9f8c4225328f8453887
Daniel Erat <derat@chromium.org>
2018-09-23 21:33:07 -0700

    tast: Add autocaps package for autotest-capability.
    Add a new autocaps package that local_test_runner will use
    to parse autotest-capability files in
    /usr/local/etc/autotest-capability. A later change will make
    local_test_runner call this code when determining the DUT's
    features.
    
    BUG=chromium:885007
    TEST=added unit test
    CQ-DEPEND=Id9ff05f6179260ba2f9648d7c55287beb39e131e
    
    Change-Id: I684298cb69ac278198962b2cb11e070e183b5b14

---
+++ src/chromiumos/tast/runner/autocaps/detect.go
@@ -0,0 +1,95 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package autocaps
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var cpuModelRegexp *regexp.Regexp // matches "Model name" line from lscpu
+
+func init() {
+	cpuModelRegexp = regexp.MustCompile(`^Model name:\s+(.+)`)
+}
+
+const (
+	// PCI vendor:device ID string for Kepler.
+	keplerID = "1ae0:001a"
+)
+
+// SysInfo contains information about the system.
+type SysInfo struct {
+	// CPUModel contains the value from the "Model name:" line printed by lscpu.
+	CPUModel string
+	// HasKepler is true if "lspci -n -d <keplerID>" prints non-empty output.
+	HasKepler bool
+}
+
+// loadSysInfo returns a SysInfo struct describing the system where this code is running.
+func loadSysInfo() (*SysInfo, error) {
+	info := SysInfo{}
+	out, err := exec.Command("lscpu").Output()
+	if err != nil {
+		return nil, fmt.Errorf("lscpu failed: %v", err)
+	}
+	for _, ln := range strings.Split(string(out), "\n") {
+		if ms := cpuModelRegexp.FindStringSubmatch(ln); ms != nil {
+			info.CPUModel = strings.TrimSpace(ms[1])
+			break
+		}
+	}
+
+	if out, err = exec.Command("lspci", "-n", "-d", keplerID).Output(); err != nil {
+		return nil, fmt.Errorf("lspci failed: %v", err)
+	}
+	info.HasKepler = strings.TrimSpace(string(out)) != ""
+
+	return &info, nil
+}
+
+// cpuModelMap maps from SysInfo.CPUModel values to the corresponding detector match values.
+// This comes from client/cros/video/detectors/intel_cpu.py in the Autotest repo.
+var cpuModelMap map[string]string = map[string]string{
+	"Intel(R) Celeron(R) 2955U @ 1.40GHz":      "intel_celeron_2955U",
+	"Intel(R) Celeron(R) 2957U @ 1.40GHz":      "intel_celeron_2957U",
+	"Intel(R) Celeron(R) CPU 1007U @ 1.50GHz":  "intel_celeron_1007U",
+	"Intel(R) Celeron(R) CPU 847 @ 1.10GHz":    "intel_celeron_847",
+	"Intel(R) Celeron(R) CPU 867 @ 1.30GHz":    "intel_celeron_867",
+	"Intel(R) Celeron(R) CPU 877 @ 1.40GHz":    "intel_celeron_877",
+	"Intel(R) Celeron(R) CPU B840 @ 1.90GHz":   "intel_celeron_B840",
+	"Intel(R) Core(TM) i3-4005U CPU @ 1.70GHz": "intel_i3_4005U",
+	"Intel(R) Core(TM) i3-4010U CPU @ 1.70GHz": "intel_i3_4010U",
+	"Intel(R) Core(TM) i3-4030U CPU @ 1.90GHz": "intel_i3_4030U",
+	"Intel(R) Core(TM) i5-2450M CPU @ 2.50GHz": "intel_i5_2450M",
+	"Intel(R) Core(TM) i5-2467M CPU @ 1.60GHz": "intel_i5_2467M",
+	"Intel(R) Core(TM) i5-2520M CPU @ 2.50GHz": "intel_i5_2520M",
+	"Intel(R) Core(TM) i5-3427U CPU @ 1.80GHz": "intel_i7_3427U",
+	"Intel(R) Core(TM) i7-4600U CPU @ 2.10GHz": "intel_i7_4600U",
+}
+
+// runDetector returns the directives contained in rule.Capabilities if rule matches info.
+func runDetector(rule *detectRule, info *SysInfo) (directives []string, err error) {
+	var val string
+	switch rule.Detector {
+	case "intel_cpu":
+		val = cpuModelMap[info.CPUModel]
+	case "kepler":
+		if info.HasKepler {
+			val = "kepler"
+		}
+	default:
+		return nil, fmt.Errorf("unknown detector %q", rule.Detector)
+	}
+
+	for _, m := range rule.Match {
+		if m == val {
+			return rule.Capabilities, nil
+		}
+	}
+	return nil, nil
+}

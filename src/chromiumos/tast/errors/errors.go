dbe5967e6d7ca8f312cc5b73b5916c005cb3768f
Shuhei Takahashi <nya@chromium.org>
2018-09-25 15:30:31 +0900

    WIP: tast: Introduce our own errors package (approach 2)
    This change will improve test failure messages like this:
    
    2018/10/01 18:31:22 [18:31:21.638] Error at chrome_login.go:71: Chrome login failed: OOBE didn't show up (Oobe.readyForTesting not found): context deadline exceeded; last error following: "!!(typeof Oobe == 'function' && Oobe.readyForTesting)" is false
    2018/10/01 18:31:22 [18:31:21.638] Stack trace:
    Chrome login failed
            at chromiumos/tast/local/bundles/cros/ui.ChromeLogin (chrome_login.go:71)
            at chromiumos/tast/testing.runAndRecover (test.go:143)
            at chromiumos/tast/testing.(*Test).Run.func2 (test.go:124)
            at runtime.goexit (asm_amd64.s:2362)
    OOBE didn't show up (Oobe.readyForTesting not found)
            at chromiumos/tast/local/chrome.(*Chrome).logIn (chrome.go:479)
            at chromiumos/tast/local/chrome.New (chrome.go:172)
            at chromiumos/tast/local/bundles/cros/ui.ChromeLogin (chrome_login.go:43)
            at chromiumos/tast/testing.runAndRecover (test.go:143)
            at chromiumos/tast/testing.(*Test).Run.func2 (test.go:124)
            at runtime.goexit (asm_amd64.s:2362)
    context deadline exceeded; last error following
            at chromiumos/tast/testing.Poll (poll.go:70)
            at chromiumos/tast/local/chrome.(*Conn).WaitForExpr (conn.go:147)
            at chromiumos/tast/local/chrome.(*Chrome).logIn (chrome.go:478)
            at chromiumos/tast/local/chrome.New (chrome.go:172)
            at chromiumos/tast/local/bundles/cros/ui.ChromeLogin (chrome_login.go:43)
            at chromiumos/tast/testing.runAndRecover (test.go:143)
            at chromiumos/tast/testing.(*Test).Run.func2 (test.go:124)
            at runtime.goexit (asm_amd64.s:2362)
    "!!(typeof Oobe == 'function' && Oobe.readyForTesting)" is false
            at chromiumos/tast/local/chrome.(*Conn).WaitForExpr (conn.go:146)
            at chromiumos/tast/local/chrome.(*Chrome).logIn (chrome.go:478)
            at chromiumos/tast/local/chrome.New (chrome.go:172)
            at chromiumos/tast/local/bundles/cros/ui.ChromeLogin (chrome_login.go:43)
            at chromiumos/tast/testing.runAndRecover (test.go:143)
            at chromiumos/tast/testing.(*Test).Run.func2 (test.go:124)
            at runtime.goexit (asm_amd64.s:2362)
    
    BUG=chromium:887166
    TEST=fast_build.sh -T
    TEST=tast run DUT ui.ChromeLogin
    
    Change-Id: I00ad50fe64ba0fbd28d50f1083f76e3c5197cf88

---
+++ src/chromiumos/tast/errors/errors.go
@@ -0,0 +1,94 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// TODO(nya): Write more explanation here
+
+package errors
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"chromiumos/tast/errors/stack"
+)
+
+type richError struct {
+	msg   string       // error message to be prepended to cause
+	s     *stack.Stack // stack trace where this error was created
+	cause error        // original error that caused this error
+}
+
+// trace formats a trace of chained errors.
+func trace(err error) string {
+	var chain []string
+	for err != nil {
+		if e, ok := err.(*richError); !ok {
+			chain = append(chain, err.Error())
+			err = nil
+		} else {
+			chain = append(chain, fmt.Sprintf("%s\n%s", e.msg, e.s))
+			err = e.cause
+		}
+	}
+	return strings.Join(chain, "\n")
+}
+
+// Error implements error interface.
+func (e *richError) Error() string {
+	if e.cause == nil {
+		return e.msg
+	}
+	return fmt.Sprintf("%s: %s", e.msg, e.cause.Error())
+}
+
+// Format implements fmt.Formatter interface.
+// In addition to usual %v and %s, richError also supports formatting a trace
+// of chained errors by %+v.
+func (e *richError) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			io.WriteString(s, trace(e))
+			return
+		}
+		fallthrough
+	case 's':
+		io.WriteString(s, e.Error())
+	}
+}
+
+// New creates a new error with the given message.
+// This is similar to standard errors.New, but also records the location where
+// it was called.
+func New(msg string) error {
+	s := stack.New(1)
+	return &richError{msg, s, nil}
+}
+
+// Errorf creates a new error with the given message.
+// This is similar to standard fmt.Errorf, but also records the location where
+// it was called.
+func Errorf(format string, args ...interface{}) error {
+	s := stack.New(1)
+	msg := fmt.Sprintf(format, args...)
+	return &richError{msg, s, nil}
+}
+
+// Wrap creates a new error with the given message, wrapping another error.
+// This function also records the location where it was called.
+// If cause is nil, this is the same as New.
+func Wrap(cause error, msg string) error {
+	s := stack.New(1)
+	return &richError{msg, s, cause}
+}
+
+// Wrapf creates a new error with the given message, wrapping another error.
+// This function also records the location where it was called.
+// If cause is nil, this is the same as Errorf.
+func Wrapf(cause error, format string, args ...interface{}) error {
+	s := stack.New(1)
+	msg := fmt.Sprintf(format, args...)
+	return &richError{msg, s, cause}
+}

d66f23bb39290fd1416f577e35d69696bef283f1
Shuhei Takahashi <nya@chromium.org>
2018-10-18 08:10:11 +0900

    tast-lint: Run Golint.
    We run golint and take problems whose confidence is >=0.8.
    
    We filter results in addition in some ways:
    - Lines marked "// NOLINT" are ignored. This is because golint is
      actually designed to make "suggestions"; it sometimes gives
      false positive but does not provide a way to selectively ignore
      errors by itself. Nevertheless, we believe running golint in
      the pre-upload hook is worth doing.
    - unexported-type-in-api is ignored.
    - Test main functions are allowed to be exported without
      comments.
    
    Also, -debug option is added to turn on verbose output. It can be
    used to print the information required to add more filtering
    in the case of false positives.
    
    CQ-DEPEND=CL:1287349
    CQ-DEPEND=CL:1287449
    BUG=chromium:894750
    TEST=fast_build.sh -T
    TEST=tools/run_lint.sh
    
    Change-Id: Ic095877d0069ca917e9fb406b087cf458bcac78d
    Reviewed-on: https://chromium-review.googlesource.com/1287469
    Commit-Ready: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

---
+++ src/chromiumos/cmd/tast-lint/check/golint.go
@@ -0,0 +1,83 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"golang.org/x/lint"
+)
+
+// minConfidence is the confidence threshold for problems reported from Golint.
+// Golint's default is 0.8.
+const minConfidence = 0.79
+
+// testMainPathRegexp matches a file name of a Tast test main file.
+var testMainPathRegexp = regexp.MustCompile(`/src/chromiumos/tast/(?:local|remote)/bundles/[^/]+/[^/]+/[^/]+\.go$`)
+
+// isTestMainFile checks if path is a Test test main file.
+func isTestMainFile(path string) bool {
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return false
+	}
+	return testMainPathRegexp.MatchString(path)
+}
+
+// shouldIgnore is called to filter irrelevant issues reported by Golint.
+func shouldIgnore(p lint.Problem) bool {
+	if p.Confidence < minConfidence {
+		return true
+	}
+
+	// Allow ignoring an issue locally by a magic comment.
+	if strings.Contains(p.LineText, "NOLINT") {
+		return true
+	}
+
+	// Ignore unexported-type-in-api.
+	if p.Category == "unexported-type-in-api" {
+		return true
+	}
+
+	// Tast test functions can be exported without comment.
+	if isTestMainFile(p.Position.Filename) &&
+		p.Category == "comments" &&
+		strings.Contains(p.Text, "should have comment or be unexported") {
+		return true
+	}
+
+	return false
+}
+
+// Golint runs Golint to find issues.
+func Golint(path string, code []byte, debug bool) []*Issue {
+	ps, err := (&lint.Linter{}).Lint(path, code)
+	if err != nil {
+		panic(err)
+	}
+
+	var issues []*Issue
+	for _, p := range ps {
+		if shouldIgnore(p) {
+			continue
+		}
+
+		var msg string
+		if debug {
+			msg = fmt.Sprintf("[%s; %.2f] %s", p.Category, p.Confidence, p.Text)
+		} else {
+			msg = p.Text
+		}
+		issues = append(issues, &Issue{
+			Pos: p.Position,
+			Msg: msg,
+		})
+	}
+	return issues
+}
---
+++ src/chromiumos/cmd/tast-lint/check/golint_test.go
@@ -0,0 +1,53 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"testing"
+)
+
+func TestGolint(t *testing.T) {
+	const code = `package main
+
+func Kitten() {}
+`
+	expects := []string{
+		"testfile.go:3:1: exported function Kitten should have comment or be unexported",
+	}
+
+	issues := Golint("testfile.go", []byte(code), false)
+	verifyIssues(t, issues, expects)
+}
+
+func TestGolint_NoLint(t *testing.T) {
+	const code = `package main
+
+func Kitten() {} // NOLINT
+`
+	issues := Golint("testfile.go", []byte(code), false)
+	verifyIssues(t, issues, nil)
+}
+
+func TestGolint_UnexportedTypeInAPI(t *testing.T) {
+	const code = `package main
+
+type cute bool
+
+// Kitten is cute.
+func Kitten() cute { return true }
+`
+	issues := Golint("testfile.go", []byte(code), false)
+	verifyIssues(t, issues, nil)
+}
+
+func TestGolint_TestMainFunction(t *testing.T) {
+	const code = `package main
+
+func Kitten() {}
+`
+	issues := Golint("src/chromiumos/tast/local/bundles/cros/example/kitten.go", []byte(code), false)
+	// Test main functions can be exported without comments.
+	verifyIssues(t, issues, nil)
+}

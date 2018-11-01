a7a040258b180f84e61f4aafed259bd596129db5
Shuhei Takahashi <nya@chromium.org>
2018-11-01 12:31:51 +0900

    tast-lint: Blacklist time.Sleep calls.
    The current check of fmt.Errorf is refactored to check other calls
    too.
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I052dea0c0335e320f849e9f4778c707fbc64bfe2

---
+++ src/chromiumos/cmd/tast-lint/check/forbidden_imports.go
@@ -0,0 +1,32 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"strconv"
+)
+
+// ForbiddenImports makes sure blacklisted errors packages are not imported.
+func ForbiddenImports(fs *token.FileSet, f *ast.File) []*Issue {
+	var issues []*Issue
+
+	for _, im := range f.Imports {
+		p, err := strconv.Unquote(im.Path.Value)
+		if err != nil {
+			continue
+		}
+		if p == "errors" || p == "github.com/pkg/errors" {
+			issues = append(issues, &Issue{
+				Pos:  fs.Position(im.Pos()),
+				Msg:  fmt.Sprintf("chromiumos/tast/errors package should be used instead of %s package", p),
+				Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
+			})
+		}
+	}
+	return issues
+}

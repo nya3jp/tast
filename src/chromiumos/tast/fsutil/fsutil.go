5957b66e5eaf0c2b4b7c4949f56f259336163158
Daniel Erat <derat@chromium.org>
2018-11-16 15:31:33 -0800

    tast: Add fsutil package.
    We have many implementations of code to copy a file from one
    location to another, all of which are subtly broken (e.g.
    os.File.Close() isn't checked). This change adds a fsutil
    package with CopyFile and MoveFile functions.
    
    A proposal to add a ioutil.CopyFile method was rejected
    (https://golang.org/cl/1591), largely on the grounds of it
    being challenging to implement correct behavior on all
    platforms. I don't think that that argument applies here,
    and I also think that having well-tested canonical
    implementations will be better than having multiple untested
    ones like we do now.
    
    BUG=none
    TEST=added unit tests; also verified that tests using the
         new package still pass
    
    Change-Id: I1cbf944ab3d30a11063aea997b1d8467f6b1eb63

---
+++ src/chromiumos/tast/fsutil/fsutil.go
@@ -0,0 +1,90 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package fsutil implements common file operations.
+package fsutil
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+// CopyFile copies the regular file at path src to dst.
+// dst is truncated if it already exists and inherits src's mode.
+// If any operations fail, the destination file will be removed.
+func CopyFile(src, dst string) error {
+	sf, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sf.Close()
+
+	fi, err := sf.Stat()
+	if err != nil {
+		return err
+	} else if !fi.Mode().IsRegular() {
+		return fmt.Errorf("source not regular file (mode %s)", fi.Mode())
+	}
+
+	// Copy to a temp file.
+	df, err := ioutil.TempFile(filepath.Dir(dst), "."+filepath.Base(dst)+".")
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(df, sf); err != nil {
+		df.Close()
+		os.Remove(df.Name())
+		return err
+	}
+	if err := df.Close(); err != nil {
+		os.Remove(df.Name())
+		return err
+	}
+
+	// Finally, set the mode and rename to the requested name.
+	if err := os.Chmod(df.Name(), fi.Mode()); err != nil {
+		os.Remove(df.Name())
+		return err
+	}
+	if err := os.Rename(df.Name(), dst); err != nil {
+		os.Remove(df.Name())
+		return err
+	}
+	return nil
+}
+
+// MoveFile moves the file at src to dst.
+// The source and destination paths may be on different filesystems.
+// The mode will be preserved, and ownership will also be preserved if possible
+// (i.e. if called with an EUID of 0 or if moving the file within a filesystem).
+func MoveFile(src, dst string) error {
+	fi, err := os.Stat(src)
+	if err != nil {
+		return err
+	} else if !fi.Mode().IsRegular() {
+		return fmt.Errorf("source not regular file (mode %s)", fi.Mode())
+	}
+
+	// Try to do a rename first. This only works if we're not moving across filesystems.
+	if err := os.Rename(src, dst); err == nil {
+		return nil
+	}
+
+	// Otherwise, copy and restore metadata before deleting src.
+	if err := CopyFile(src, dst); err != nil {
+		return err
+	}
+	st := fi.Sys().(*syscall.Stat_t)
+	if os.Geteuid() == 0 {
+		if err := os.Chown(dst, int(st.Uid), int(st.Gid)); err != nil {
+			os.Remove(dst)
+			return err
+		}
+	}
+	return os.Remove(src)
+}
---
+++ src/chromiumos/tast/fsutil/fsutil_test.go
@@ -0,0 +1,101 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package fsutil
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"chromiumos/tast/testutil"
+)
+
+// setUpFile creates a temporary directory containing a file with the supplied data and mode.
+// The temporary directory's and file's paths are returned.
+// A fatal test error is reported if any operations fail.
+func setUpFile(t *testing.T, data string, mode os.FileMode) (td, fn string) {
+	td = testutil.TempDir(t)
+	fn = filepath.Join(td, "src.txt")
+	if err := ioutil.WriteFile(fn, []byte(data), mode); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Chmod(fn, mode); err != nil {
+		t.Fatal(err)
+	}
+	return td, fn
+}
+
+// checkFile checks that the file at path has the supplied data and mode.
+// Test errors are reported for any discrepancies.
+func checkFile(t *testing.T, path, data string, mode os.FileMode) {
+	if fi, err := os.Stat(path); err != nil {
+		t.Errorf("Failed to stat %v: %v", path, err)
+	} else if newMode := fi.Mode() & os.ModePerm; newMode != mode {
+		t.Errorf("%v has mode 0%o; want 0%o", path, newMode, mode)
+	}
+
+	if b, err := ioutil.ReadFile(path); err != nil {
+		t.Errorf("Failed to read %v: %v", path, err)
+	} else if string(b) != data {
+		t.Errorf("%v contains %q; want %q", path, string(b), data)
+	}
+}
+
+func TestCopyFile(t *testing.T) {
+	const (
+		data = "this is not the most interesting text ever written"
+		mode = 0461
+	)
+	td, src := setUpFile(t, data, mode)
+	defer os.RemoveAll(td)
+
+	dst := filepath.Join(td, "dst.txt")
+	if err := CopyFile(src, dst); err != nil {
+		t.Fatalf("CopyFile(%q, %q) failed: %v", src, dst, err)
+	}
+
+	checkFile(t, dst, data, mode)
+}
+
+func TestMoveFile(t *testing.T) {
+	const (
+		data = "another boring file"
+		mode = 0401
+	)
+	td, src := setUpFile(t, data, mode)
+	defer os.RemoveAll(td)
+
+	dst := filepath.Join(td, "dst.txt")
+	if err := MoveFile(src, dst); err != nil {
+		t.Fatalf("MoveFile(%q, %q) failed: %v", src, dst, err)
+	}
+
+	checkFile(t, dst, data, mode)
+
+	if _, err := os.Stat(src); err == nil {
+		t.Errorf("%s still exists", src)
+	} else if !os.IsNotExist(err) {
+		t.Errorf("Failed to stat %s: %v", src, err)
+	}
+}
+
+func TestCopyFileOrMoveFileWithDir(t *testing.T) {
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+
+	src := filepath.Join(td, "dir")
+	if err := os.Mkdir(src, 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	// Both functions should reject directories.
+	if err := CopyFile(src, filepath.Join(td, "copyDst")); err == nil {
+		t.Error("CopyFile unexpectedly succeeded for directory ", src)
+	}
+	if err := MoveFile(src, filepath.Join(td, "moveDst")); err == nil {
+		t.Error("MoveFile unexpectedly succeeded for directory ", src)
+	}
+}

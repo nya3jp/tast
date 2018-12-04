f490206e0a2b2ee82bced5791db352ee90bcdd8b
Eric Caruso <ejcaruso@chromium.org>
2018-12-03 16:38:38 -0800

    local_test_runner: plumb ASAN use flag through software deps
    This allows tests to run only when ASAN is disabled.
    
    BUG=chromium:877733
    TEST=run security.ASLR with checktestdeps and ensure it fails
      if the system has the use flag
    CQ-DEPEND=CL:1359773
    
    Change-Id: I4d8f53422c24de6cfdb2608c136cde67b1ccb2f4

--- src/chromiumos/cmd/local_test_runner/main.go
+++ src/chromiumos/cmd/local_test_runner/main.go
@@ -34,6 +34,7 @@ func main() {
 			// The one exception is tast_vm, which is inserted by VM builders via -extrauseflags.
 			"android":           "arc",
 			"android_p":         `arc && "android-container-pi"`,
+			"aslr":              "!asan",                                // ASan instrumentation breaks ASLR
 			"audio_play":        "!betty && !tast_vm && !veyron_rialto", // VMs don't have audio hardware
 			"audio_record":      "internal_mic && !tast_vm",             // VMs don't have audio hardware
 			"camera_720p":       "!snow && !skate && !spring",           // daisy variants' cameras don't support 1280x720

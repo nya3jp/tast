8e7f2273acb5c1ad5e2783142ac58fdeae3c2a81
Qijiang Fan <fqj@chromium.org>
2019-01-10 18:25:12 +0900

    tast: Add "selinux_current" add "selinux_experimental" dependencies.
    BUG=b:114172200
    TEST=tast run betty security.SELinux*
    
    Change-Id: I2099e3cfd0499c58778bb8c9d425ef6865be9bcb

--- src/chromiumos/cmd/local_test_runner/main.go
+++ src/chromiumos/cmd/local_test_runner/main.go
@@ -40,23 +40,25 @@ func main() {
 			// TODO(b/73436929) Grunt cannot run 720p due to performance issue,
 			// we should remove grunt after hardware encoding supported.
 			// daisy variants' cameras don't support 1280x720.
-			"camera_720p":       "!snow && !skate && !spring && !grunt",
-			"chrome":            "!chromeless_tty",
-			"chrome_internal":   "chrome_internal",
-			"chrome_login":      "!chromeless_tty && !rialto",
-			"containers":        "containers",
-			"cups":              "cups",
-			"display_backlight": "display_backlight",
-			"gpu_sandboxing":    "!betty && !tast_vm", // no GPU sandboxing on VMs: https://crbug.com/914688
-			"memd":              "memd",
-			"ml_service":        "ml_service",
-			"no_android":        "!arc",
-			"no_symlink_mount":  "!lxc",                         // boards using LXC set CONFIG_SECURITY_CHROMIUMOS_NO_SYMLINK_MOUNT=n
-			"reboot":            "!betty && !tast_vm",           // weird missing-runner-after-reboot bug: https://crbug.com/909955
-			"screenshot":        "display_backlight && !rk3399", // screenshot command broken on RK3399: https://crbug.com/880597
-			"selinux":           "selinux",
-			"tablet_mode":       "touchview",
-			"tpm":               "!mocktpm && !tast_vm",
+			"camera_720p":          "!snow && !skate && !spring && !grunt",
+			"chrome":               "!chromeless_tty",
+			"chrome_internal":      "chrome_internal",
+			"chrome_login":         "!chromeless_tty && !rialto",
+			"containers":           "containers",
+			"cups":                 "cups",
+			"display_backlight":    "display_backlight",
+			"gpu_sandboxing":       "!betty && !tast_vm", // no GPU sandboxing on VMs: https://crbug.com/914688
+			"memd":                 "memd",
+			"ml_service":           "ml_service",
+			"no_android":           "!arc",
+			"no_symlink_mount":     "!lxc",                         // boards using LXC set CONFIG_SECURITY_CHROMIUMOS_NO_SYMLINK_MOUNT=n
+			"reboot":               "!betty && !tast_vm",           // weird missing-runner-after-reboot bug: https://crbug.com/909955
+			"screenshot":           "display_backlight && !rk3399", // screenshot command broken on RK3399: https://crbug.com/880597
+			"selinux":              "selinux",
+			"selinux_current":      "selinux && !selinux_experimental",
+			"selinux_experimental": "selinux && selinux_experimental",
+			"tablet_mode":          "touchview",
+			"tpm":                  "!mocktpm && !tast_vm",
 			// Some VM builds actually can run nested VM with right host configuration.
 			// But we haven't enable this feature on builders. For now, just disable
 			// vm_host feature for VM builds.

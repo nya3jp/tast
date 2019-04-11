0f0b6291b6893fe91314d9115bd744f3bd718d33
Shuhei Takahashi <nya@chromium.org>
2019-04-11 16:34:15 +0900

    tast: Introduce testing.Sleep.
    It is forbidden by tast-lint to use time.Sleep in test codes for
    two reasons: it does not honor context timeout, and also sleeping
    can be a cause of flakiness. However we got some legitimate use
    cases of sleep (like sending keyboard events) in more and more
    tests, so it's good time to introduce a utility function for it.
    
    BUG=chromium:951690
    TEST=fast_build.sh -T
    
    Change-Id: Ic78e6ca412c17632d06623fb23a87176ee7cd99f

---
+++ src/chromiumos/tast/testing/util.go
@@ -0,0 +1,27 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"context"
+	"time"
+)
+
+// Sleep pauses the current goroutine for d or until ctx expires.
+//
+// Please consider using testing.Poll instead. Sleeping without polling for
+// a condition is discouraged, since it makes tests flakier (when the sleep
+// duration isn't long enough) or slower (when the duration is too long).
+func Sleep(ctx context.Context, d time.Duration) error {
+	tm := time.NewTimer(d)
+	defer tm.Stop()
+
+	select {
+	case <-tm.C:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}

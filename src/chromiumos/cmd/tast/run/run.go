1b7ef5f7d21c5bb4989ea7473a61025d8c6e2582
Shuhei Takahashi <nya@chromium.org>
2019-04-19 09:35:49 +0900

    tast: Diagnose SSH connection aborts.
    When the SSH connection to the target aborted on running local
    tests, run the following diagnosis:
    
    - On connecting to the target first time, record boot_id.
    - If the SSH connection aborted, try reconnecting to the target
      up to 1 minute.
    - Compare boot_id with the recorded one. If it's the same, the
      target did not reboot, so it is likely the connection was aborted
      by network issues.
    - Read the last 1000 lines of systemd journal from the previous
      boot. If the log contains a message from pre-shutdown, the target
      rebooted normally.
    - Read console-ramoops file. If the log contains crashes, the
      target rebooted for a kernel crash.
    
    BUG=chromium:930239
    TEST=fast_build.sh && ~/go/bin/tast run DUT example.Reboot
    TEST=fast_build.sh && ~/go/bin/tast run DUT example.KernelCrash
    TEST=fast_build.sh -T
    
    Change-Id: I0fcfffbbd13ce9ec2fb4a2f44f346ee5e88a6ab1
    Reviewed-on: https://chromium-review.googlesource.com/1575178
    Commit-Ready: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/cmd/tast/run/run.go
+++ src/chromiumos/cmd/tast/run/run.go
@@ -46,12 +46,15 @@ func errorStatusf(cfg *Config, code subcommands.ExitStatus, format string, args 
 // If an error is encountered, status.ErrorMsg will be logged to cfg.Logger before returning,
 // but the caller may wish to log it again later to increase its prominence if additional messages are logged.
 func Run(ctx context.Context, cfg *Config) (status Status, results []TestResult) {
+	ranRemote := false
+
 	if cfg.build {
 		switch cfg.buildType {
 		case localType:
 			status, results = local(ctx, cfg)
 		case remoteType:
 			status, results = remote(ctx, cfg)
+			ranRemote = true
 		default:
 			// This shouldn't be reached; Config.SetFlags validates buildType.
 			panic(fmt.Sprintf("Invalid build type %d", int(cfg.buildType)))
@@ -64,21 +67,23 @@ func Run(ctx context.Context, cfg *Config) (status Status, results []TestResult)
 			var rres []TestResult
 			status, rres = remote(ctx, cfg)
 			results = append(results, rres...)
+			ranRemote = true
 		}
 	}
 
 	if status.ExitCode == subcommands.ExitFailure {
+		// Provide a more-descriptive status if the SSH connection was lost.
+		if !ranRemote && cfg.hst != nil && !ctxutil.DeadlineBefore(ctx, time.Now().Add(sshPingTimeout)) {
+			if err := cfg.hst.Ping(ctx, sshPingTimeout); err != nil {
+				msg := diagnoseSSHDrop(ctx, cfg)
+				status = errorStatusf(cfg, subcommands.ExitFailure, "Lost SSH connection; %s", msg)
+			}
+		}
+
 		// If we didn't get to the point where we started trying to run tests,
 		// report that to the caller so they can avoid writing a useless results dir.
 		if !cfg.startedRun {
 			status.FailedBeforeRun = true
-		}
-
-		// Provide a more-descriptive status if the SSH connection was lost.
-		if cfg.hst != nil && !ctxutil.DeadlineBefore(ctx, time.Now().Add(sshPingTimeout)) {
-			if err := cfg.hst.Ping(ctx, sshPingTimeout); err != nil {
-				status = errorStatusf(cfg, subcommands.ExitFailure, "Lost SSH connection to %v: %v", cfg.Target, err)
-			}
 		}
 
 		// Write the failure message to a file so it can be used to annotate interrupted tests.

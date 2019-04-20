855a2f719f6e78dbc7f2f590c7e18116faf10126
Shuhei Takahashi <nya@chromium.org>
2019-04-21 08:11:50 +0900

    tast: Try several paths for console-ramoops.
    On some systems, console-ramoops is found on a different path.
    
    BUG=chromium:930239
    TEST=fast_build.sh -T
    
    Change-Id: Ifb1072b058e0bc18e7d163454f49e707ab59c6e2

--- src/chromiumos/cmd/tast/run/diagnose.go
+++ src/chromiumos/cmd/tast/run/diagnose.go
@@ -79,7 +79,13 @@ func diagnoseReboot(ctx context.Context, cfg *Config) string {
 	journal := string(out)
 
 	// Read console-ramoops. It might not exist for normal reboots.
-	out, _ = cfg.hst.Run(ctx, "cat /sys/fs/pstore/console-ramoops-0")
+	out, err = cfg.hst.Run(ctx, "cat /sys/fs/pstore/console-ramoops-0")
+	if err != nil {
+		out, err = cfg.hst.Run(ctx, "cat /sys/fs/pstore/console-ramoops")
+		if err != nil {
+			cfg.Logger.Log("console-ramoops not found")
+		}
+	}
 	ramOops := string(out)
 
 	if m := shutdownReasonRe.FindStringSubmatch(journal); m != nil {

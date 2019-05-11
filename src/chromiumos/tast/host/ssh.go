dc4cfb71fabf1a437ca8a7e1602dbfc213acde2b
Daniel Erat <derat@chromium.org>
2019-05-10 22:49:21 -0700

    tast: Add host.Forwarder.
    Add a Forwarder class to the host package to facilitate
    forwarding TCP connections between a local port and a remote
    address by way of a remote SSH server.
    
    This is based heavily on jeffcarp@'s ServodProxy change at
    https://crrev.com/c/1542128. I'm adding a generic class to
    the host package instead of making this Servo-specific since
    I suspect that similar functionality will be needed
    elsewhere in the future.
    
    BUG=chromium:942366
    TEST=added unit tests; also manually tested by modifying
         example.ServoEcho test to communicate with a servod
         instance in the lab by way of a forwarded connection
    
    Change-Id: I2931852c7bbf88c1c5e18ebaef862b61ef3d35cb

--- src/chromiumos/tast/host/ssh.go
+++ src/chromiumos/tast/host/ssh.go
@@ -732,14 +732,15 @@ func (s *SSH) Ping(ctx context.Context, timeout time.Duration) error {
 	}
 }
 
-// DialTCP creates a TCP connection to addr that will be tunneled over the SSH connection.
-func (s *SSH) DialTCP(addr *net.TCPAddr) (net.Conn, error) {
-	return s.cl.DialTCP("tcp", nil, addr)
-}
-
 // ListenTCP opens a remote TCP port for listening.
 func (s *SSH) ListenTCP(addr *net.TCPAddr) (net.Listener, error) {
 	return s.cl.ListenTCP(addr)
+}
+
+// NewForwarder creates a new Forwarder that forwards connections from localAddr to remoteAddr using s.
+func (s *SSH) NewForwarder(localAddr, remoteAddr string, errFunc func(error)) (*Forwarder, error) {
+	connFunc := func() (net.Conn, error) { return s.cl.Dial("tcp", remoteAddr) }
+	return NewForwarder(localAddr, connFunc, errFunc)
 }
 
 // SSHCommandHandle represents an in-progress remote command.

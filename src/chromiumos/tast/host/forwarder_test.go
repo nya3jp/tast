2985495c1b4b16f35570162a62725eb1f130e397
Daniel Erat <derat@chromium.org>
2019-05-10 22:49:21 -0700

    tast: Add host.Forwarder.
    Add a Forwarder class to the host package to facilitate
    forwarding TCP connections between a local port and a remote
    address by way of a remote SSH server.
    
    This is based heavily on jeffcarp@'s ServodProxy change at
    https://crrev.com/c/1542128. I'm adding a generic class to
    the host package instead of making this Servo-specific since
    I suspect that similar functionality will be needed
    elsewhere in the future.
    
    BUG=chromium:942366
    TEST=added unit tests; also manually tested by modifying
         example.ServoEcho test to communicate with a servod
         instance in the lab by way of a forwarded connection
    
    Change-Id: I2931852c7bbf88c1c5e18ebaef862b61ef3d35cb

---
+++ src/chromiumos/tast/host/forwarder_test.go
@@ -0,0 +1,98 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package host
+
+import (
+	"errors"
+	"io"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestForwarder(t *testing.T) {
+	// The remote address here is arbitrary. The test SSH server doesn't support
+	// forwarded connectinos, so we'll swap in a function later that just returns
+	// one end of a pipe to simulate creating remote connections.
+	fwd, err := NewForwarder(nil, "127.0.0.1:0", "192.0.2.1:12345")
+	if err != nil {
+		t.Fatal("NewForwarder failed:", err)
+	}
+	t.Log("Forwarder listening at", fwd.LocalAddr())
+	defer fwd.Close()
+
+	local, remote := net.Pipe()
+	defer local.Close()
+	fwd.connFunc = func() (net.Conn, error) { return local, nil }
+
+	conn, err := net.Dial("tcp", fwd.LocalAddr().String())
+	if err != nil {
+		t.Fatal("Dial failed:", err)
+	}
+	defer conn.Close()
+
+	const (
+		sendData = "send data" // sent from local to remote
+		recvData = "recv data" // sent from remote to local
+	)
+
+	// Start a goroutine that reads sendData and then sends recvData.
+	go func() {
+		defer remote.Close()
+
+		b := make([]byte, len(sendData))
+		if _, err := io.ReadFull(remote, b); err != nil {
+			t.Fatal("Read failed:", err)
+		} else if string(b) != sendData {
+			t.Fatalf("Read %q; want %q", b, sendData)
+		}
+		if _, err := io.WriteString(remote, recvData); err != nil {
+			t.Fatalf("Writing %q failed: %v", recvData, err)
+		}
+	}()
+
+	// In the main goroutine, send sendData and read recvData.
+	if _, err := io.WriteString(conn, sendData); err != nil {
+		t.Fatalf("Writing %q failed: %v", sendData, err)
+	}
+	b := make([]byte, len(recvData))
+	if _, err := io.ReadFull(conn, b); err != nil {
+		t.Fatal("Read failed:", err)
+	} else if string(b) != recvData {
+		t.Fatalf("Read %q; want %q", b, recvData)
+	}
+}
+
+func TestForwarderError(t *testing.T) {
+	fwd, err := NewForwarder(nil, "127.0.0.1:0", "192.0.2.1:12345")
+	if err != nil {
+		t.Fatal("NewForwarder failed:", err)
+	}
+	defer fwd.Close()
+
+	// Make the forwarder receive an error when it tries to open the remote connection,
+	// and configure an error handler that copies errors to a channel.
+	connErr := errors.New("intentional error")
+	fwd.connFunc = func() (net.Conn, error) { return nil, connErr }
+	ch := make(chan error, 1)
+	fwd.SetErrorFunc(func(err error) { ch <- err })
+
+	// We should be able to establish a connection to the forwarder's local address,
+	// but the error handler should receive the connection error.
+	conn, err := net.Dial("tcp", fwd.LocalAddr().String())
+	if err != nil {
+		t.Fatal("Dial failed:", err)
+	}
+	defer conn.Close()
+
+	select {
+	case err := <-ch:
+		if err != connErr {
+			t.Fatalf("Got error %q; want %q", err, connErr)
+		}
+	case <-time.After(time.Minute):
+		t.Fatal("Didn't receive any error")
+	}
+}

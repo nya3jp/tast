3e59f3dbb519c1adc11f833e6087da81d143b1a8
Keigo Oka <oka@chromium.org>
2019-06-28 16:55:55 +0900

    tast-lint: disallow importing bundle subpackages in a different package
    TEST=fast_build.sh -T
    BUG=chromium:979088
    
    Change-Id: I5a4feabc3b71376e75d83d05a88567de635116a7

---
+++ src/chromiumos/cmd/tast-lint/check/forbidden_bundle_imports.go
@@ -0,0 +1,69 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+var bundlePrefix = []string{
+	"chromiumos/tast/local/bundles/cros/",
+	"chromiumos/tast/remote/bundles/cros/",
+}
+
+// bundlePackage analyzes full package path and returns matching bundle prefix and the package name.
+// Returns ok=false if it's not a bundle (sub)package.
+func bundlePackage(p string) (prefix string, pkg string, ok bool) {
+	for _, pref := range bundlePrefix {
+		if strings.HasPrefix(p, pref) {
+			t := strings.SplitN(strings.TrimPrefix(p, pref), "/", 2)
+			return strings.TrimRight(pref, "/"), t[0], true
+		}
+	}
+	return "", "", false
+}
+
+// ForbiddenBundleImports makes sure libraries in another package under bundles/ are not imported.
+func ForbiddenBundleImports(fs *token.FileSet, f *ast.File) []*Issue {
+	filename := fs.Position(f.Package).Filename
+	mypkg := strings.TrimPrefix(filepath.Dir(filename), "src/")
+
+	var issues []*Issue
+	for _, im := range f.Imports {
+		p, err := strconv.Unquote(im.Path.Value)
+		if err != nil {
+			continue
+		}
+
+		bundle, pkg, ok := bundlePackage(p)
+		if !ok {
+			continue
+		}
+
+		// Allow import from main.go .
+		if mypkg == bundle {
+			continue
+		}
+
+		expPrefix := path.Join(bundle, pkg)
+		if strings.HasPrefix(mypkg+"/", expPrefix+"/") {
+			continue
+		}
+
+		issues = append(issues, &Issue{
+			Pos:  fs.Position(im.Pos()),
+			Msg:  fmt.Sprintf("import of %s is not allowed from %s; must be from %s or its descendant", p, mypkg, expPrefix),
+			Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#bundle-subpackage",
+		})
+	}
+
+	return issues
+}

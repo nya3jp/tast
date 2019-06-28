195251749f994696e4da8a786d8c30feb0e5559c
Shuhei Takahashi <nya@chromium.org>
2019-06-28 10:28:27 +0900

    tast: Make build fail if cgo is enabled.
    This change adds cgo.go which is built only when cgo is enabled
    and gives a compilation error, which should prevent Tast binaries
    to be built with cgo enabled.
    
    cgo.go is placed in the testing package because it is linked in
    every Tast binary.
    
    BUG=chromium:976196
    TEST=fast_build.sh -T
    TEST=fast_build.sh
    TEST=fast_build.sh && ~/go/bin/tast run -build=true DUT example.pass
    TEST=sudo emerge --nodeps chromeos-base/tast-cmd
    TEST=emerge-betty --nodeps chromeos-base/tast-local-tests-cros
    TEST=fast_build.sh  # without CGO_ENABLED=0 build fails
    
    Cq-Depend: chromium:1663976
    Cq-Depend: chromium:1664194
    Change-Id: Iaed964182a089dcbf974196c5cdb083a2c554537
    Reviewed-on: https://chromium-review.googlesource.com/1683693
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Ready: Shuhei Takahashi <nya@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/testing/cgo.go
@@ -0,0 +1,14 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Build this file only when cgo is enabled.
+// +build cgo
+
+package testing
+
+func init() {
+	// Cgo must be disabled on building Tast binaries (crbug.com/976196).
+	// The following line will give a build error if cgo is enabled.
+	var cgoMustBeDisabledToBuildTast struct{}
+}

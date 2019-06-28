6ae02de792bc26e76871ae9298584ef14ed53f38
Keigo Oka <oka@chromium.org>
2019-06-28 16:55:55 +0900

    tast-lint: disallow importing bundle subpackages in a different package
    TEST=fast_build.sh -T
    BUG=chromium:979088
    
    Change-Id: I5a4feabc3b71376e75d83d05a88567de635116a7

---
+++ src/chromiumos/cmd/tast-lint/check/forbidden_bundle_imports.go
@@ -0,0 +1,68 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+// ForbiddenBundleImports makes sure libraries in another package under bundles/ are not imported.
+func ForbiddenBundleImports(fs *token.FileSet, f *ast.File) []*Issue {
+	filename := fs.Position(f.Package).Filename
+	mypkg := strings.TrimPrefix(filepath.Dir(filename), "src/")
+	// src/chromiumos/tast/local/bundles/cros/arc/boot.go
+	fmt.Printf("filename: %q, pkg: %q", filename, mypkg) // FIXME
+
+	bundlePrefix := []string{
+		"chromiumos/tast/local/bundles/cros/",
+		"chromiumos/tast/remote/bundles/cros/",
+	}
+
+	analyze := func(fullpkg string) (prefix string, pkg string, subpkg string) {
+		for _, pref := range bundlePrefix {
+			if !strings.HasPrefix(fullpkg, pref) {
+				continue
+			}
+			rel := strings.TrimPrefix(fullpkg, pref)
+			t := strings.SplitN(rel, "/", 2)
+			if len(t) < 2 {
+				continue
+			}
+			return pref, t[0], t[1]
+		}
+		return "", "", ""
+	}
+
+	var issues []*Issue
+	for _, im := range f.Imports {
+		p, err := strconv.Unquote(im.Path.Value)
+		if err != nil {
+			continue
+		}
+
+		pref, pkg, subpkg := analyze(p)
+		if subpkg == "" {
+			continue
+		}
+
+		exppkg := filepath.Join(pref, pkg)
+		if mypkg == exppkg {
+			continue
+		}
+
+		issues = append(issues, &Issue{
+			Pos:  fs.Position(im.Pos()),
+			Msg:  fmt.Sprintf("%s can be imported only from %s, but yours is %s", p, exppkg, mypkg),
+			Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#import-bundle-subpackage",
+		})
+	}
+
+	return issues
+}
---
+++ src/chromiumos/cmd/tast-lint/check/forbidden_bundle_imports_test.go
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"testing"
+)
+
+func TestForbiddenBundleImports(t *testing.T) {
+	const code = `package main
+
+import (
+	"chromiumos/tast/local/bundles/cros/bar/baz"
+	"chromiumos/tast/local/bundles/cros/foo/baz"
+	"chromiumos/tast/local/bar/baz"
+	"chromiumos/tast/remote/bundles/cros/foo/baz"
+)
+`
+	expects := []string{
+		"src/chromiumos/tast/local/bundles/cros/foo/testfile.go:4:2: chromiumos/tast/local/bundles/cros/bar/baz can be imported only from chromiumos/tast/local/bundles/cros/bar, but yours is chromiumos/tast/local/bundles/cros/foo",
+		"src/chromiumos/tast/local/bundles/cros/foo/testfile.go:7:2: chromiumos/tast/remote/bundles/cros/foo/baz can be imported only from chromiumos/tast/remote/bundles/cros/foo, but yours is chromiumos/tast/local/bundles/cros/foo",
+	}
+
+	f, fs := parse(code, "src/chromiumos/tast/local/bundles/cros/foo/testfile.go")
+	issues := ForbiddenBundleImports(fs, f)
+	verifyIssues(t, issues, expects)
+}

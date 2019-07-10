48d6167e2373d46c866abe153b51d48b55f9da32
Jason Jeremy Iman <jasongustaman@google.com>
2019-07-10 16:44:06 +0900

    tast: Update "android_pi*" dependency for ARCVM.
    Currently, ARCVM dependency uses android-container* USE flag.
    This patch update ARCVM dependency to use android_vm* USE flag.
    
    BUG=b:134967947
    TEST=tast run -buildlocalrunner <DUT> <test>
    
    Cq-Depend: chromium:1695383
    Change-Id: I5fa60b92f10248442e89e98c35fb51669d745fa6

--- src/chromiumos/cmd/local_test_runner/main.go
+++ src/chromiumos/cmd/local_test_runner/main.go
@@ -65,8 +65,8 @@ func main() {
 			"android_all_vm":    `arc && arcvm`,
 			"android_all_both":  `arc`,
 			"android_p":         `arc && !arcvm && "android-container-pi"`,
-			"android_p_vm":      `arc && arcvm && "android-container-pi"`,
-			"android_p_both":    `arc && "android-container-pi"`,
+			"android_p_vm":      `arc && arcvm && "android-vm-pi"`,
+			"android_p_both":    `arc && ("android-container-pi" || "android-vm-pi")`,
 			"arc_camera3":       `"arc-camera3"`,
 			"aslr":              "!asan",                                                     // ASan instrumentation breaks ASLR
 			"audio_play":        "!betty && !tast_vm && !veyron_rialto && !(fizz && moblab)", // VMs and some boards don't have a speaker
@@ -81,7 +81,7 @@ func main() {
 			"cros_config":             "unibuild",
 			"cros_internal":           "internal",
 			"crosvm_gpu":              `"crosvm-gpu"`,
-			"crossystem":              "!betty && !tast_vm",                     // VMs don't support few crossystem sub-commands: https://crbug.com/974615
+			"crossystem":              "!betty && !tast_vm", // VMs don't support few crossystem sub-commands: https://crbug.com/974615
 			"cups":                    "cups",
 			"diagnostics":             "diagnostics",
 			"display_backlight":       "display_backlight",

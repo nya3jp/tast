f29d5887c37424c8916107395727ed1b8203d54e
Keigo Oka <oka@chromium.org>
2019-07-18 11:27:37 +0900

    tast-lint: function docs should begin with the function name
    TEST=run for bundles https://crbug.com/982157#c3
    TEST=fast_build.sh -T
    BUG=chromium:982157
    
    Change-Id: I31788c988660107cdd87131bacd441ec81a77b58

---
+++ src/chromiumos/cmd/tast-lint/check/comments_test.go
@@ -0,0 +1,48 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import "testing"
+
+func TestComments(t *testing.T) {
+	const code = `package pkg
+
+// foo does something.
+func foo() {}
+
+// Do something bad.
+func bad() {}
+
+func noCommentIsOK() {}
+
+// multiLine comment is OK.
+// Yeah.
+func multiLine() {}
+
+// linting
+// multiLineBad should fail.
+func multiLineBad() {}
+
+// bar does something.
+func (x *recv) bar() {}
+
+func (x *recv) noCommentIsFine() {}
+
+// This is not good.
+func (x *recv) notGood() {}
+`
+
+	const path = "test.go"
+
+	f, fs := parse(code, path)
+
+	issues := Comments(fs, f)
+	expects := []string{
+		"test.go:6:1: Function comments should begin with the function name",
+		"test.go:15:1: Function comments should begin with the function name",
+		"test.go:24:1: Function comments should begin with the function name",
+	}
+	verifyIssues(t, issues, expects)
+}

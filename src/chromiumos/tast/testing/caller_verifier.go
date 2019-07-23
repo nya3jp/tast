d2f3f3ce363eb7a5e522b1be35a212708b06f080
Hidehiko Abe <hidehiko@chromium.org>
2019-07-23 22:48:59 +0900

    tast: Verifies testing.AddTest() can be called at most once in a file.
    BUG=chromium:984388
    TEST=Made a test violates the new check, and made sure manually.
    
    Change-Id: Iee4d43873b23b39098ebda96da8fb0525a8a6884
    Reviewed-on: https://chromium-review.googlesource.com/1714566
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    Legacy-Commit-Queue: Commit Bot <commit-bot@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/testing/caller_verifier.go
@@ -0,0 +1,46 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"fmt"
+	"regexp"
+	"runtime"
+)
+
+// callerVerifier is to verify testing.AddTest() function callers.
+type callerVerifier struct {
+	// funcPattern is a regexp pattern to be matched with the caller function.
+	funcPattern *regexp.Regexp
+
+	// files is a set of filepaths that are registered.
+	files map[string]struct{}
+}
+
+// newCallerVerifier returns an instance of the callerVerifier with the given pattern.
+func newCallerVerifier(pattern *regexp.Regexp) *callerVerifier {
+	return &callerVerifier{
+		funcPattern: pattern,
+		files:       make(map[string]struct{}),
+	}
+}
+
+// verifyAndRegister makes sure following things.
+// - the function name at the given pc matches with the required pattern.
+// - this function is not called twice or more for a file of the pc.
+func (v *callerVerifier) verifyAndRegister(pc uintptr) error {
+	rf := runtime.FuncForPC(pc)
+	if !v.funcPattern.MatchString(rf.Name()) {
+		return fmt.Errorf("test registration needs to be done in %s: %s", v.funcPattern, rf.Name())
+	}
+
+	file, _ := rf.FileLine(pc)
+	if _, ok := v.files[file]; ok {
+		return fmt.Errorf("testing.AddTest can be called at most once in a file: %s", file)
+	}
+	v.files[file] = struct{}{}
+
+	return nil
+}
---
+++ src/chromiumos/tast/testing/caller_verifier_test.go
@@ -0,0 +1,35 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"regexp"
+	"runtime"
+	gotesting "testing"
+)
+
+func TestFuncNameVerify(t *gotesting.T) {
+	v := newCallerVerifier(regexp.MustCompile("^chromiumos/tast/testing.TestFuncNameVerify$"))
+	pc, _, _, _ := runtime.Caller(0)
+	if err := v.verifyAndRegister(pc); err != nil {
+		t.Error("Unexpected verification failure: ", err)
+	}
+
+	v = newCallerVerifier(regexp.MustCompile("^chromiumos/tast/testing.NoMatchFuncName$"))
+	if err := v.verifyAndRegister(pc); err == nil {
+		t.Error("Unexpected verification pass for unmatched function name")
+	}
+}
+
+func TestRegisterTwiceVerify(t *gotesting.T) {
+	v := newCallerVerifier(regexp.MustCompile(".*"))
+	pc, _, _, _ := runtime.Caller(0)
+	if err := v.verifyAndRegister(pc); err != nil {
+		t.Fatal("Unexpected verification failure: ", err)
+	}
+	if err := v.verifyAndRegister(pc); err == nil {
+		t.Fatal("Unexpected verification pass for two times registration")
+	}
+}

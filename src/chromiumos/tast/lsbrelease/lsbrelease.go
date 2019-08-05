6c39fc0206039f20c27697f63b22b7d8d1342deb
Satoshi Niwa <niwa@google.com>
2019-08-05 14:46:03 +0900

    Allow tast/local/bundles/crosint/arc to access lsbrelease
    needed by https://chrome-internal-review.googlesource.com/c/chromeos/platform/tast-tests-private/+/1560178
    
    TEST=tast run my_crbook arc.PerfBoot
    BUG=b:135964304
    
    Change-Id: I768018fdd85c47ac76dd05f3f4f05d06bb4d90b4

--- src/chromiumos/tast/lsbrelease/lsbrelease.go
+++ src/chromiumos/tast/lsbrelease/lsbrelease.go
@@ -47,6 +47,7 @@ const (
 var allowedPkgs = []string{
 	"chromiumos/cmd/tast/symbolize",
 	"chromiumos/tast/local/arc",                          // For SDKVersion.
+	"chromiumos/tast/local/bundles/crosint/arc",          // For Board.
 	"chromiumos/tast/local/bundles/crosint/arc/authperf", // For Version.
 	"chromiumos/tast/local/bundles/cros/platform/updateserver",
 	"chromiumos/tast/local/rialto",

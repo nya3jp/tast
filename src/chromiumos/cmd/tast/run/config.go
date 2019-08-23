ae33bd2325c5a508d0754e9eed447b3e41fcdd35
Shuhei Takahashi <nya@chromium.org>
2019-08-24 02:29:22 +0900

    tast: Decide the output directory of local tests in the tast command.
    Previously the output directory which local test bundles write
    test artifacts to was decided by the local_test_runner and the
    directory path was notified to the tast command by a RunEnd
    control message sent at the end of all tests.
    
    This is problematic in two points:
    - When local_test_runner is aborted for timeout or crashes,
      RunEnd is not sent and thus it is impossible to locate and
      pull test artifacts.
    - When local_test_runner is restarted for crashes, test
      artifacts are written to different directories every time.
    
    This change makes the tast command always set OutDir in its
    RunTests request (runner.Args.RunTests.BundleArgs.OutDir) by
    generating a random directory name. Runners are also updated
    to create the output directory if it does not exist.
    
    For protocol compatibility, RunEnd.OutDir is still filled, and
    the tast command issues a mkdir command via SSH to create the
    output directory before running local_test_runner so that the
    new tast command works well with older local_test_runner.
    
    The change to fix the issue that test artifacts are not saved
    when the global timeout is hit is coming next.
    
    BUG=chromium:972457
    TEST=fast_build.sh -T
    TEST=fast_build.sh
    TEST=~/go/bin/tast run -build=false DUT example.Perf
    TEST=~/go/bin/tast run -build=true DUT example.Perf
    
    Change-Id: I6f87676149722d5fc5fa2453b8b0c11109ef746e

--- src/chromiumos/cmd/tast/run/config.go
+++ src/chromiumos/cmd/tast/run/config.go
@@ -9,6 +9,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
@@ -86,6 +87,7 @@ type Config struct {
 	localRunner    string // path to executable that runs local test bundles
 	localBundleDir string // dir where packaged local test bundles are installed
 	localDataDir   string // dir containing packaged local test data
+	localOutDir    string // dir where intermediate test outputs are written
 
 	remoteRunner    string // path to executable that runs remote test bundles
 	remoteBundleDir string // dir where packaged remote test bundles are installed
@@ -167,6 +169,7 @@ func (c *Config) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&c.localRunner, "localrunner", "", "executable that runs local test bundles")
 	f.StringVar(&c.localBundleDir, "localbundledir", "", "directory containing builtin local test bundles")
 	f.StringVar(&c.localDataDir, "localdatadir", "", "directory containing builtin local test data")
+	f.StringVar(&c.localOutDir, "localoutdir", "", "directory where intermediate test outputs are written")
 
 	// These are configurable since files may be installed elsewhere when running in the lab.
 	f.StringVar(&c.remoteRunner, "remoterunner", "", "executable that runs remote test bundles")
@@ -233,6 +236,10 @@ func (c *Config) DeriveDefaults() error {
 	} else {
 		setIfEmpty(&c.buildWorkspace, filepath.Join(c.trunkDir, b.workspace))
 	}
+
+	// Generate a random directory path to avoid conflicts.
+	randomLocalOutDir := fmt.Sprintf("/usr/local/tmp/tast_out.%s.%d", time.Now().Format("20060102-150405"), rand.Int())
+	setIfEmpty(&c.localOutDir, randomLocalOutDir)
 
 	larch, err := build.GetLocalArch()
 	if err != nil {

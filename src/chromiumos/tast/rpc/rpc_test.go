e3a2b5b946912e1baf6874acef003897dd27c1a8
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:57:07 +0900

    tast: Support defining services.
    testing.AddService is added to allow local bundles to register
    gRPC services.
    
    BUG=chromium:969627
    TEST=fast_build.sh -T
    
    Change-Id: Iadc534dbbf04e120534a0ba87adcc3997f279571

---
+++ src/chromiumos/tast/rpc/rpc_test.go
@@ -0,0 +1,124 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rpc
+
+import (
+	"context"
+	"io"
+	gotesting "testing"
+
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/testing"
+)
+
+type pingServer struct {
+	onPing func(context.Context) error
+}
+
+func (s *pingServer) Ping(ctx context.Context, _ *PingRequest) (*PingResponse, error) {
+	if err := s.onPing(ctx); err != nil {
+		return nil, err
+	}
+	return &PingResponse{}, nil
+}
+
+var _ PingServer = (*pingServer)(nil)
+
+type pingPair struct {
+	Client PingClient
+
+	conn *grpc.ClientConn
+	stop func() error
+}
+
+func (p *pingPair) Close(ctx context.Context) error {
+	firstErr := p.conn.Close()
+	if err := p.stop(); firstErr == nil {
+		firstErr = err
+	}
+	return firstErr
+}
+
+func newPingPair(ctx context.Context, t *gotesting.T, onPing func(context.Context) error) *pingPair {
+	t.Helper()
+	success := false
+
+	sr, cw := io.Pipe()
+	cr, sw := io.Pipe()
+
+	svc := &testing.Service{
+		Register: func(srv *grpc.Server, s *testing.ServiceState) {
+			RegisterPingServer(srv, &pingServer{onPing})
+		},
+	}
+
+	stopped := make(chan error, 1)
+	go func() {
+		stopped <- RunServer(sr, sw, []*testing.Service{svc})
+	}()
+	stop := func() error {
+		cw.Close()
+		cr.Close()
+		return <-stopped
+	}
+	defer func() {
+		if !success {
+			stop()
+		}
+	}()
+
+	conn, err := newPipeClientConn(ctx, cr, cw)
+	if err != nil {
+		t.Fatal("newPipeClientConn failed: ", err)
+	}
+	defer func() {
+		if !success {
+			conn.Close()
+		}
+	}()
+
+	success = true
+	return &pingPair{
+		Client: NewPingClient(conn),
+		conn:   conn,
+		stop:   stop,
+	}
+}
+
+func TestRPCSuccess(t *gotesting.T) {
+	ctx := context.Background()
+	called := false
+	pp := newPingPair(ctx, t, func(context.Context) error {
+		called = true
+		return nil
+	})
+	defer pp.Close(ctx)
+
+	if _, err := pp.Client.Ping(ctx, &PingRequest{}); err != nil {
+		t.Error("Ping failed: ", err)
+	}
+	if !called {
+		t.Error("onPing not called")
+	}
+}
+
+func TestRPCFailure(t *gotesting.T) {
+	ctx := context.Background()
+	called := false
+	pp := newPingPair(ctx, t, func(context.Context) error {
+		called = true
+		return errors.New("failure")
+	})
+	defer pp.Close(ctx)
+
+	if _, err := pp.Client.Ping(ctx, &PingRequest{}); err == nil {
+		t.Error("Ping unexpectedly succeeded")
+	}
+	if !called {
+		t.Error("onPing not called")
+	}
+}

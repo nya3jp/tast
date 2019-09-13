bcbfeb73e0782c8902327a30db675508d0160cfa
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:04:56 +0900

    tast: Forward logs in gRPC services.
    This change allows gRPC services to call testing.ContextLog to
    leave logs in the calling remote tests.
    
    BUG=chromium:969627
    TEST=fast_build.sh
    
    Change-Id: Ic0988ad6d56b2f220ccf4d3b7e2f34565826ed05

--- src/chromiumos/tast/rpc/ping.pb.go
+++ src/chromiumos/tast/rpc/ping.pb.go
@@ -6,10 +6,14 @@ Package rpc is a generated protocol buffer package.
 
 It is generated from these files:
 	ping.proto
+	logging.proto
 
 It has these top-level messages:
 	PingRequest
 	PingResponse
+	ReadLogsRequest
+	ReadLogsResponse
+	LogEntry
 */
 package rpc
 

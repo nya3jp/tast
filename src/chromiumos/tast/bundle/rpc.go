f8a62a713417051f1ac2287ad41ea8d76066f64e
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:04:55 +0900

    WIP: tast: Forward TestContext on gRPC calls.
    BUG=chromium:965896
    TEST=TBD
    
    Change-Id: Ie4eb61a96b3a14ccc5b4a84b8098f82acefe5cd5

--- src/chromiumos/tast/bundle/rpc.go
+++ src/chromiumos/tast/bundle/rpc.go
@@ -5,9 +5,12 @@
 package bundle
 
 import (
+	"context"
+	"errors"
 	"io"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
 
 	"chromiumos/tast/rpc"
@@ -16,7 +19,7 @@ import (
 
 // runRPCServer runs a gRPC server on stdin and stdout.
 func runRPCServer(stdin io.Reader, stdout io.Writer) error {
-	srv := grpc.NewServer()
+	srv := grpc.NewServer(interceptors()...)
 	reflection.Register(srv)
 
 	for _, s := range testing.GlobalRegistry().AllServices() {
@@ -27,4 +30,47 @@ func runRPCServer(stdin io.Reader, stdout io.Writer) error {
 		return err
 	}
 	return nil
+}
+
+type serverStreamWithContext struct {
+	grpc.ServerStream
+	ctx context.Context
+}
+
+func (s *serverStreamWithContext) Context() context.Context {
+	return s.ctx
+}
+
+var _ grpc.ServerStream = (*serverStreamWithContext)(nil)
+
+func interceptors() []grpc.ServerOption {
+	before := func(ctx context.Context) (context.Context, error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, errors.New("metadata not available")
+		}
+		tc, err := testing.TestContextFromRPCMetadata(md)
+		if err != nil {
+			return nil, err
+		}
+		return testing.WithTestContext(ctx, tc), nil
+	}
+
+	return []grpc.ServerOption{
+		grpc.UnaryInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (res interface{}, err error) {
+			ctx, err = before(ctx)
+			if err != nil {
+				return nil, err
+			}
+			return handler(ctx, req)
+		}),
+		grpc.StreamInterceptor(func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+			ctx, err := before(stream.Context())
+			if err != nil {
+				return err
+			}
+			stream = &serverStreamWithContext{stream, ctx}
+			return handler(srv, stream)
+		}),
+	}
 }

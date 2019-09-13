72ed9b241bad1dfd8c112fcaa208ebcb62a3a6d0
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:04:56 +0900

    tast: Forward logs in gRPC services.
    This change allows gRPC services to call testing.ContextLog to
    leave logs in the calling remote tests.
    
    BUG=chromium:969627
    TEST=fast_build.sh
    
    Change-Id: Ic0988ad6d56b2f220ccf4d3b7e2f34565826ed05

---
+++ src/chromiumos/tast/rpc/logging_client.go
@@ -0,0 +1,73 @@
+// Copyright 2019 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rpc
+
+import (
+	"context"
+	"io"
+
+	"google.golang.org/grpc"
+)
+
+// remoteLoggingClient is a client of the tast.core.Logging gRPC service that
+// calls a logging function for every received ReadLogsResponse.
+//
+// It is used by gRPC clients to receive logs from gRPC services.
+type remoteLoggingClient struct {
+	stopCh chan struct{} // closed when Close is called to stop the streaming RPC call
+	doneCh chan error    // closed when the streaming RPC call is done
+}
+
+// newRemoteLoggingClient constructs remoteLoggingClient using conn. logger is
+// called for every received ReadLogsResponse.
+func newRemoteLoggingClient(ctx context.Context, conn *grpc.ClientConn, logger func(msg string)) (*remoteLoggingClient, error) {
+	cl := NewLoggingClient(conn)
+	st, err := cl.ReadLogs(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read the initial response to check success and make sure we have been
+	// subscribed to logs.
+	if _, err := st.Recv(); err != nil {
+		return nil, err
+	}
+
+	stopCh := make(chan struct{})
+	doneCh := make(chan error)
+
+	// Call CloseSend when Close is called.
+	go func() {
+		<-stopCh
+		st.CloseSend()
+	}()
+
+	// Start a goroutine to call logger for every received ReadLogsResponse.
+	go func() {
+		doneCh <- func() error {
+			for {
+				res, err := st.Recv()
+				if err != nil {
+					if err == io.EOF {
+						return nil
+					}
+					return err
+				}
+				logger(res.Entry.Msg)
+			}
+		}()
+	}()
+
+	return &remoteLoggingClient{
+		stopCh: stopCh,
+		doneCh: doneCh,
+	}, nil
+}
+
+// Close finishes the remote logging.
+func (l *remoteLoggingClient) Close() error {
+	close(l.stopCh)
+	return <-l.doneCh
+}

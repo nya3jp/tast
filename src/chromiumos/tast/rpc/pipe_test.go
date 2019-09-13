0e6c290eb55c469606afbba9daccd349dd69834c
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:04:53 +0900

    tast: Add NewPipeClientConn.
    This function constructs ClientConn suitable for talking with a gRPC
    server over SSH. This function will be called from support libraries
    in remote tests to establish a gRPC connection.
    
    BUG=chromium:969627
    TEST=fast_build.sh -T
    
    Change-Id: If44705b82c5d805b764387d309b4eac9f2bed83c

--- src/chromiumos/tast/rpc/pipe_test.go
+++ src/chromiumos/tast/rpc/pipe_test.go
@@ -6,9 +6,14 @@ package rpc
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"io/ioutil"
 	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 )
 
 func TestPipeListener(t *testing.T) {
@@ -46,5 +51,40 @@ func TestPipeListener(t *testing.T) {
 
 	if _, err := lis.Accept(); err != io.EOF {
 		t.Errorf("Accept failed: %v; want %v", err, io.EOF)
+	}
+}
+
+func TestPipeClientConn(t *testing.T) {
+	sr, cw := io.Pipe()
+	defer sr.Close()
+	defer cw.Close()
+	cr, sw := io.Pipe()
+	defer cr.Close()
+	defer sw.Close()
+
+	srv := grpc.NewServer()
+	reflection.Register(srv)
+
+	go srv.Serve(NewPipeListener(sr, sw))
+	defer srv.Stop()
+
+	conn, err := NewPipeClientConn(context.Background(), cr, cw)
+	if err != nil {
+		t.Fatal("NewPipeClientConn failed: ", err)
+	}
+	defer conn.Close()
+
+	cl := grpc_reflection_v1alpha.NewServerReflectionClient(conn)
+	st, err := cl.ServerReflectionInfo(context.Background())
+	if err != nil {
+		t.Fatal("ServerReflectionInfo RPC call failed: ", err)
+	}
+	if err := st.CloseSend(); err != nil {
+		t.Fatal("ServerReflectionInfo RPC call failed on CloseSend: ", err)
+	}
+	if _, err := st.Recv(); err == nil {
+		t.Fatal("ServerReflectionInfo RPC call returned an unexpected reply")
+	} else if err != io.EOF {
+		t.Fatal("ServerReflectionInfo RPC call failed on Recv: ", err)
 	}
 }

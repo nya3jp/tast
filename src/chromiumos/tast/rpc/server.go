4291a40735dccfb5f301912c1c8f7de642aa3e65
Shuhei Takahashi <nya@chromium.org>
2019-09-13 17:57:07 +0900

    tast: Support defining services.
    testing.AddService is added to allow local bundles to register
    gRPC services.
    
    BUG=chromium:969627
    TEST=fast_build.sh -T
    
    Change-Id: Iadc534dbbf04e120534a0ba87adcc3997f279571

--- src/chromiumos/tast/rpc/server.go
+++ src/chromiumos/tast/rpc/server.go
@@ -9,14 +9,21 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+
+	"chromiumos/tast/testing"
 )
 
-// RunServer runs a gRPC server on r/w channels.
-func RunServer(r io.Reader, w io.Writer) error {
+// RunServer runs a gRPC server providing svcs on r/w channels.
+// It blocks until the client connection is closed or it encounters an error.
+func RunServer(r io.Reader, w io.Writer, svcs []*testing.Service) error {
 	srv := grpc.NewServer()
 
 	// Register the reflection service for easier debugging.
 	reflection.Register(srv)
+
+	for _, svc := range svcs {
+		svc.Register(srv, &testing.ServiceState{})
+	}
 
 	if err := srv.Serve(newPipeListener(r, w)); err != nil && err != io.EOF {
 		return err

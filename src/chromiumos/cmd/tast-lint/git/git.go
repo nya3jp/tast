62854adee2796d0c0289856a712ffa9c60d1fac4
Mei Fukuda <mfukuda@google.com>
2019-09-26 13:19:33 +0900

    tast-lint: Update ModifiedFiles into ChangedFiles to support Added and Deleted
    ModifiedFiles() is expanded to include all changed files
    not only modified files but also added and deleted files.
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I2ece6b9e382144e5d8fc42a0af2d0bff930ee844
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/1826069
    Tested-by: Mei Fukuda <mfukuda@google.com>
    Commit-Queue: Mei Fukuda <mfukuda@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/cmd/tast-lint/git/git.go
+++ src/chromiumos/cmd/tast-lint/git/git.go
@@ -32,10 +32,31 @@ func New(dir, commit string) *Git {
 	}
 }
 
-// ModifiedFiles returns the list of file paths modified in the commit.
-func (g *Git) ModifiedFiles() ([]string, error) {
+// CommitStatus represent the status of changed files.
+type CommitStatus int
+
+// Constants represent the commit status as an enumerated type.
+const (
+	Added CommitStatus = iota
+	Copied
+	Deleted
+	Modified
+	Renamed
+	TypeChanged
+	Unmerged
+	Unknown
+)
+
+// CommitFile is a tuple composed of commit status and file path.
+type CommitFile struct {
+	Status CommitStatus
+	Path   string
+}
+
+// ChangedFiles returns the list of pairs of commit statuses and file paths changed in the commit.
+func (g *Git) ChangedFiles() ([]CommitFile, error) {
 	if g.Commit == "" {
-		return nil, errors.New("ModifiedFiles needs explicit commit")
+		return nil, errors.New("ChangedFiles needs explicit commit")
 	}
 	// TODO(nya): This does not work for the first, no-parent commit.
 	cmd := exec.Command("git", "diff-tree", "--no-commit-id", "-r", "--name-status", g.Commit)
@@ -45,10 +66,28 @@ func (g *Git) ModifiedFiles() ([]string, error) {
 		return nil, err
 	}
 	stats := strings.Split(strings.TrimRight(string(out), "\n"), "\n")
-	var files []string
+	var files []CommitFile
 	for _, s := range stats {
-		if parts := strings.Split(s, "\t"); len(parts) == 2 && parts[0] != "D" {
-			files = append(files, parts[1])
+		if parts := strings.Split(s, "\t"); len(parts) == 2 {
+			switch status := parts[0]; status {
+			case "A":
+				files = append(files, CommitFile{Added, parts[1]})
+			case "C":
+				files = append(files, CommitFile{Copied, parts[1]})
+			case "D":
+				files = append(files, CommitFile{Deleted, parts[1]})
+			case "M":
+				files = append(files, CommitFile{Modified, parts[1]})
+			case "R":
+				files = append(files, CommitFile{Renamed, parts[1]})
+			case "T":
+				files = append(files, CommitFile{TypeChanged, parts[1]})
+			case "U":
+				files = append(files, CommitFile{Unmerged, parts[1]})
+			default:
+				files = append(files, CommitFile{Unknown, parts[1]})
+				fmt.Println("files unknown (most probably a bug of git, please report it)")
+			}
 		}
 	}
 	return files, nil

17b9c22302ee8f49fefaae28c24c720e4d3a5b7d
Shuhei Takahashi <nya@chromium.org>
2019-10-18 16:31:20 +0900

    tast: Add go.sh.
    It is a script to run the go command with environment suitable for
    dealing with Tast code.
    
    BUG=chromium:969627
    TEST=~/trunk/src/platform/tast/tools/go.sh install \
         chromiumos/tast/local/bundles/cros
    
    Change-Id: I9b53c3e06f6f3aa09ff9cd8004411f14b98bdeb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/1875968
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ tools/go.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Runs the go command with environment suitable for dealing with Tast code.
+
+readonly trunk_dir="$(realpath -e "$(dirname "$0")/../../../..")"
+
+# Go workspaces containing the Tast source code.
+readonly src_dirs=(
+  "${trunk_dir}/src/platform/tast"
+  "${trunk_dir}/src/platform/tast-tests"
+  "${trunk_dir}/src/platform/tast-tests-pita"
+  "${trunk_dir}/src/platform/tast-tests-private"
+)
+
+readonly gopath_dir="${trunk_dir}/chroot/usr/lib/gopath"
+
+export GOPATH="$(IFS=:; echo "${src_dirs[*]}"):${gopath_dir}"
+
+# Disable cgo and PIE on building Tast binaries. See:
+# https://crbug.com/976196
+# https://github.com/golang/go/issues/30986#issuecomment-475626018
+export CGO_ENABLED=0
+export GOPIE=0
+
+exec go "$@"

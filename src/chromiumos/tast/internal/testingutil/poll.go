0ecce0e3aceefb4f2a44b8a520728eee0b10289c
Shuhei Takahashi <nya@chromium.org>
2019-10-28 21:50:56 +0900

    tast: Move utility impls out of testing.
    The dut package depends on the testing package just to use
    testing.Poll, preventing the testing package to depend on
    the dut package in the upcoming change.
    
    This change moves the utilities to the testingutil package to
    unbreak the circular dependency.
    
    API to test code is unchanged (testing.Poll, testing.Sleep etc);
    the new testingutil package is inaccessible from test code
    because it is under internal. While it might be a good idea to
    stop providing those utilities from the testing package, such
    breaking API changes should be done later carefully.
    
    BUG=chromium:970124
    BUG=chromium:1019099
    TEST=fast_build.sh -T
    
    Change-Id: I3d1c27ed0b83cc2b8c6fa0d3e641263dde056940

--- src/chromiumos/tast/internal/testingutil/poll.go
+++ src/chromiumos/tast/internal/testingutil/poll.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package testing
+package testingutil
 
 import (
 	"context"
@@ -14,7 +14,7 @@ import (
 
 const defaultPollInterval = 100 * time.Millisecond
 
-// PollOptions may be passed to Poll to configure its behavior.
+// PollOptions provides testing.PollOptions.
 type PollOptions struct {
 	// Timeout specifies the maximum time to poll.
 	// Non-positive values indicate no timeout (although context deadlines will still be honored).
@@ -35,31 +35,12 @@ func (b *pollBreak) Error() string {
 	return b.err.Error()
 }
 
-// PollBreak creates an error wrapping err that may be returned from a
-// function passed to Poll to terminate polling immediately. For example:
-//
-// err := testing.Poll(ctx, func(ctx context.Context) error {
-//   if err := mustSucceed(ctx); err != nil {
-//     return testing.PollBreak(err)
-//   }
-//   ...
-// }
+// PollBreak implements testing.PollBreak.
 func PollBreak(err error) error {
 	return &pollBreak{err}
 }
 
-// Poll runs f repeatedly until f returns nil and then itself returns nil.
-// If ctx returns an error before then or opts.Timeout is reached, the last error returned by f is returned.
-// f should use the context passed to it, as it may have an adjusted deadline if opts.Timeout is set.
-// If ctx's deadline has already been reached, f will not be invoked.
-// If opts is nil, reasonable defaults are used.
-//
-// Polling often results in increased load and slower execution (since there's a delay between when something
-// happens and when the next polling cycle notices it). It should only be used as a last resort when there's no
-// other way to watch for an event. The preferred approach is to watch for events in select{} statements.
-// Goroutines can be used to provide notifications over channels.
-// If an error wrapped by PollBreak is returned, then it
-// immediately terminates the polling, and returns the unwrapped error.
+// Poll implements testing.Poll.
 func Poll(ctx context.Context, f func(context.Context) error, opts *PollOptions) error {
 	if ctx.Err() != nil {
 		return ctx.Err()

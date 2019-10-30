a576e46fc0ecff12c1bae5fed23db44c78f96544
Mei Fukuda <mfukuda@google.com>
2019-10-30 18:32:46 +0900

    tast-lint: Make auto format system for empty slice
    This auto-format system change `a := []T{}` into `var a []T` automatically.
    Also, lint was modified in order to make auto-fix easier.
    TODO: conversational preupload hook, AutoFix test failure readability
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I5731878c092de5fe6aada0316c016f5bfd9e298e

--- src/chromiumos/cmd/tast-lint/check/common_test.go
+++ src/chromiumos/cmd/tast-lint/check/common_test.go
@@ -8,10 +8,14 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"chromiumos/tast/testutil"
 )
 
 func parse(code, filename string) (*ast.File, *token.FileSet) {
@@ -35,5 +39,43 @@ func verifyIssues(t *testing.T, issues []*Issue, want []string) {
 
 	if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
 		t.Errorf("Issues mismatch (-got +want):\n%s", diff)
+	}
+}
+
+func verifyAutoFix(t *testing.T, lintfunc func(*token.FileSet, *ast.File) []*Issue, files map[string]string, expects map[string]string) {
+	t.Helper()
+
+	tempDir := testutil.TempDir(t)
+	defer os.RemoveAll(tempDir)
+	testutil.WriteFiles(tempDir, files)
+
+	var issues []*Issue
+	fs := token.NewFileSet()
+	for filename := range files {
+		f, err := parser.ParseFile(fs, filepath.Join(tempDir, filename), nil, parser.ParseComments)
+		if err != nil {
+			panic(err)
+		}
+		issues = append(issues, lintfunc(fs, f)...)
+	}
+	SortIssues(issues)
+
+	for l, r := 0, len(issues)-1; l < r; l, r = l+1, r-1 {
+		issues[l], issues[r] = issues[r], issues[l]
+	}
+	for _, i := range issues {
+		if i.Fix != nil {
+			i.Fix.AutoFix(i)
+		}
+	}
+
+	files, err := testutil.ReadFiles(tempDir)
+	if err != nil {
+		panic(err)
+	}
+	for filename := range files {
+		if files[filename] != expects[filename] {
+			t.Errorf("AutoFix %s failed", filename)
+		}
 	}
 }

9461f521fb94e687e2f7b60abf1b73193d79f933
Mei Fukuda <mfukuda@google.com>
2019-11-22 13:07:58 +0900

    tast-lint: added allowable paths to clean up testing.State in webrtc/camera.go
    webrtc/camera.go will be split and moved to bundles/cros/ in order to solve
    undesirable testing.State usage in the CL follows this.
    
    In order to this, paths should be ignored by bundle import linter is indicated.
    
    BUG=chromium:1023322
    TEST=fast_build.sh
    
    Change-Id: I39dc182eca689c3bcb930c0b8dd5de904de9b4f1

--- src/chromiumos/cmd/tast-lint/check/forbidden_bundle_imports.go
+++ src/chromiumos/cmd/tast-lint/check/forbidden_bundle_imports.go
@@ -17,7 +17,7 @@ import (
 
 var bundleCategoryRegex = regexp.MustCompile(`^(chromiumos/tast/(?:local|remote)/bundles/\w+)/(\w+)`)
 
-// bundlePackage analyzes full package path and returns matching bundle prefix and the category name.
+// parseBundlePackage analyzes full package path and returns matching bundle prefix and the category name.
 // Returns ok=false if it's not a bundle (sub)package.
 //
 // Example:
@@ -37,10 +37,20 @@ func ForbiddenBundleImports(fs *token.FileSet, f *ast.File) []*Issue {
 	filename := fs.Position(f.Package).Filename
 	mypkg := strings.TrimPrefix(filepath.Dir(filename), "src/")
 
+	// Ignore known valid use cases.
+	var allowMap = map[string]string{
+		"chromiumos/tast/local/bundles/cros/webrtc/camera": "src/chromiumos/tast/local/bundles/cros/camera/getusermedia/get_user_media.go",
+	}
+	filepath := fs.Position(f.Package).Filename
+
 	var issues []*Issue
 	for _, im := range f.Imports {
 		p, err := strconv.Unquote(im.Path.Value)
 		if err != nil {
+			continue
+		}
+
+		if path, ok := allowMap[p]; ok && strings.HasSuffix(filepath, path) {
 			continue
 		}
 

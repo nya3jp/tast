6fa5599314d317da71b7f5d78609af8d0ce0c9cf
Mei Fukuda <mfukuda@google.com>
2019-12-02 17:08:12 +0900

    tast-lint: Make auto format system for forbidden calls
    This auto formatter replaces fmt.Errorf with errors.Errorf.
    With auto formatter of import order, import "chromiumos/tast/errors"
    and/or unimport "fmt" as needed.
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I351d2935beb99c94edc01e6d0434033f1fb84adf

--- src/chromiumos/cmd/tast-lint/check/forbidden_calls.go
+++ src/chromiumos/cmd/tast-lint/check/forbidden_calls.go
@@ -8,22 +8,25 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
 )
 
 // ForbiddenCalls checks if any forbidden functions are called.
-func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
+func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 	isUnitTest := isUnitTestFile(fs.Position(f.Package).Filename)
 	var issues []*Issue
+	var errorsAdded bool
 
-	v := funcVisitor(func(node ast.Node) {
-		sel, ok := node.(*ast.SelectorExpr)
+	astutil.Apply(f, func(c *astutil.Cursor) bool {
+		sel, ok := c.Node().(*ast.SelectorExpr)
 		if !ok {
-			return
+			return true
 		}
 		// TODO(nya): Support imports with different aliases.
 		x, ok := sel.X.(*ast.Ident)
 		if !ok {
-			return
+			return true
 		}
 
 		call := fmt.Sprintf("%s.%s", x.Name, sel.Sel.Name)
@@ -37,11 +40,24 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
 				})
 			}
 		case "fmt.Errorf":
-			issues = append(issues, &Issue{
-				Pos:  fs.Position(x.Pos()),
-				Msg:  "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
-				Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
-			})
+			if !fix {
+				issues = append(issues, &Issue{
+					Pos:     fs.Position(x.Pos()),
+					Msg:     "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
+					Link:    "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
+					Fixable: true,
+				})
+			} else {
+				c.Replace(&ast.SelectorExpr{
+					X: &ast.Ident{
+						Name: "errors",
+					},
+					Sel: &ast.Ident{
+						Name: "Errorf",
+					},
+				})
+				errorsAdded = true
+			}
 		case "time.Sleep":
 			issues = append(issues, &Issue{
 				Pos:  fs.Position(x.Pos()),
@@ -49,8 +65,13 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
 				Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Contexts-and-timeouts",
 			})
 		}
-	})
 
-	ast.Walk(v, f)
+		return true
+	}, nil)
+
+	if errorsAdded {
+		ImportOrderAutoFix(fs, f, true)
+	}
+
 	return issues
 }

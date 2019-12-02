b4c7a09fd20a2a5719e4d0ee01b6b6a0f4e31877
Mei Fukuda <mfukuda@google.com>
2019-12-02 17:08:12 +0900

    tast-lint: Make auto format system for forbidden calls
    This auto formatter replaces fmt.Errorf with errors.Errorf.
    With auto formatter of import order, import "chromiumos/tast/errors"
    and/or unimport "fmt" as needed.
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I351d2935beb99c94edc01e6d0434033f1fb84adf

--- src/chromiumos/cmd/tast-lint/check/forbidden_calls.go
+++ src/chromiumos/cmd/tast-lint/check/forbidden_calls.go
@@ -8,22 +8,28 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"path/filepath"
+	"strconv"
+
+	"golang.org/x/tools/go/ast/astutil"
 )
 
 // ForbiddenCalls checks if any forbidden functions are called.
-func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
+func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 	isUnitTest := isUnitTestFile(fs.Position(f.Package).Filename)
 	var issues []*Issue
+	var errorsAdded bool
+	tastErrorsPath := "chromiumos/tast/errors"
 
-	v := funcVisitor(func(node ast.Node) {
-		sel, ok := node.(*ast.SelectorExpr)
+	astutil.Apply(f, func(c *astutil.Cursor) bool {
+		sel, ok := c.Node().(*ast.SelectorExpr)
 		if !ok {
-			return
+			return true
 		}
 		// TODO(nya): Support imports with different aliases.
 		x, ok := sel.X.(*ast.Ident)
 		if !ok {
-			return
+			return true
 		}
 
 		call := fmt.Sprintf("%s.%s", x.Name, sel.Sel.Name)
@@ -37,11 +43,31 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
 				})
 			}
 		case "fmt.Errorf":
-			issues = append(issues, &Issue{
-				Pos:  fs.Position(x.Pos()),
-				Msg:  "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
-				Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
-			})
+			// TODO: handle the case if there is "chromium/tast/errors" in import list.
+			var fixable bool
+			if src, err := bytesOfFile(f); err != nil {
+				fixable = false
+			} else {
+				fixable = !hasErrorsIdent(f) && !contains(importedPkgNames(f), "errors") && goimportApplicable(src)
+			}
+			if !fix {
+				issues = append(issues, &Issue{
+					Pos:     fs.Position(x.Pos()),
+					Msg:     "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
+					Link:    "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
+					Fixable: fixable,
+				})
+			} else if fixable {
+				c.Replace(&ast.SelectorExpr{
+					X: &ast.Ident{
+						Name: "errors",
+					},
+					Sel: &ast.Ident{
+						Name: "Errorf",
+					},
+				})
+				errorsAdded = true
+			}
 		case "time.Sleep":
 			issues = append(issues, &Issue{
 				Pos:  fs.Position(x.Pos()),
@@ -49,8 +75,79 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File) []*Issue {
 				Link: "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Contexts-and-timeouts",
 			})
 		}
+
+		return true
+	}, nil)
+
+	if errorsAdded {
+		tastErrorsNode := &ast.ImportSpec{
+			Path: &ast.BasicLit{
+				Kind:  token.STRING,
+				Value: strconv.Quote(tastErrorsPath),
+			},
+		}
+		f.Imports = append(f.Imports, tastErrorsNode)
+		newf, err := ImportOrderAutoFix(fs, f)
+		if err != nil {
+			return issues
+		}
+		*f = *newf
+	}
+
+	return issues
+}
+
+// hasErrorsIdent returns true if there is an identifier node whose name is "errors".
+func hasErrorsIdent(f *ast.File) bool {
+	var hasErrors bool
+	ast.Inspect(f, func(node ast.Node) bool {
+		id, ok := node.(*ast.Ident)
+		if !ok {
+			return true
+		}
+		if id.Name == "errors" {
+			hasErrors = true
+			return false
+		}
+		return true
 	})
 
-	ast.Walk(v, f)
-	return issues
+	return hasErrors
+}
+
+func importedPkgNames(f *ast.File) []string {
+	var pkgNames []string
+	for _, im := range f.Imports {
+		if im.Name != nil {
+			pkgNames = append(pkgNames, im.Name.Name)
+		} else {
+			path, err := strconv.Unquote(im.Path.Value)
+			if err != nil {
+				continue
+			}
+			pkgNames = append(pkgNames, filepath.Base(path))
+		}
+	}
+	return pkgNames
+}
+
+func contains(lst []string, s string) bool {
+	for _, e := range lst {
+		if e == s {
+			return true
+		}
+	}
+	return false
+}
+
+func importPathList(f *ast.File) []string {
+	var importlist []string
+	for _, im := range f.Imports {
+		path, err := strconv.Unquote(im.Path.Value)
+		if err != nil {
+			continue
+		}
+		importlist = append(importlist, path)
+	}
+	return importlist
 }

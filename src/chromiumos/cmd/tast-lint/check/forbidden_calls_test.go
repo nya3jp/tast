3059ffc8d407f5f935bd9403caf62fbfc2aba8c4
Mei Fukuda <mfukuda@google.com>
2019-12-02 17:08:12 +0900

    tast-lint: Make auto format system for forbidden calls
    This auto formatter replaces fmt.Errorf with errors.Errorf.
    With auto formatter of import order, import "chromiumos/tast/errors"
    and/or unimport "fmt" as needed.
    
    BUG=None
    TEST=fast_build.sh -T
    
    Change-Id: I351d2935beb99c94edc01e6d0434033f1fb84adf

--- src/chromiumos/cmd/tast-lint/check/forbidden_calls_test.go
+++ src/chromiumos/cmd/tast-lint/check/forbidden_calls_test.go
@@ -35,6 +35,100 @@ func main() {
 	}
 
 	f, fs := parse(code, "testfile.go")
-	issues := ForbiddenCalls(fs, f)
+	issues := ForbiddenCalls(fs, f, false)
 	verifyIssues(t, issues, expects)
+}
+
+func TestAutoFixForbiddenCalls(t *testing.T) {
+	files := make(map[string]string)
+	expects := make(map[string]string)
+	const filename1, filename2, filename3 = "foo.go", "bar.go", "baz.go"
+	files[filename1] = `package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Printf("foo")
+	fmt.Errorf("foo")
+	errors.Errorf("foo")
+	time.Sleep(time.Second)
+	context.Background()
+	context.TODO()
+}
+`
+	expects[filename1] = `package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"chromiumos/tast/errors"
+)
+
+func main() {
+	fmt.Printf("foo")
+	errors.Errorf("foo")
+	errors.Errorf("foo")
+	time.Sleep(time.Second)
+	context.Background()
+	context.TODO()
+}
+`
+	files[filename2] = `package main
+
+import (
+	"fmt"
+
+	"chromiumos/tast/errors"
+)
+
+func main() {
+	fmt.Errorf("foo")
+	errors.Errorf("bar")
+}
+`
+	expects[filename2] = `package main
+
+import (
+	"chromiumos/tast/errors"
+)
+
+func main() {
+	errors.Errorf("foo")
+	errors.Errorf("bar")
+}
+`
+	files[filename3] = `package main
+
+import (
+	"fmt"
+
+	"chromiumos/tast/errors"
+)
+
+func main() {
+	fmt.Errorf("foo")
+	fmt.Println()
+	errors.Errorf("bar")
+}
+`
+	expects[filename3] = `package main
+
+import (
+	"fmt"
+
+	"chromiumos/tast/errors"
+)
+
+func main() {
+	errors.Errorf("foo")
+	fmt.Println()
+	errors.Errorf("bar")
+}
+`
+	verifyAutoFix(t, ForbiddenCalls, files, expects)
 }

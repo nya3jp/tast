b162781b09c65b37e3aff0703ff8041b61a07763
Yen-lin Lai <yenlinlai@google.com>
2019-12-18 10:53:23 +0800

    dut: Add DefaultWifiRouterHost function to DUT
    Some test environments like wificell have a conventional relation
    between hostnames of DUT and companion devices. e.g.
    DUT chromeos15-row4-rack9-host4.cros will have companion AP as
    chromeos15-row4-rack9-host4-router.cros.
    
    Add DefaultWifiRouterHost function for remote tests to access the AP
    coupled with target DUT in test environment.
    
    BUG=chromium:1034875
    TEST=fast_build.sh -T
    
    Change-Id: Ifa613db538f09de078c3e01841f1ccbc71b1d8e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/1971654
    Tested-by: Yen-lin Lai <yenlinlai@google.com>
    Commit-Queue: Yen-lin Lai <yenlinlai@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>

---
+++ src/chromiumos/tast/dut/dut_test.go
@@ -0,0 +1,55 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package dut
+
+import "testing"
+
+func TestCompanionDeviceHostnames(t *testing.T) {
+	testcases := []struct {
+		Host, Suffix, Result string
+		ShouldFail           bool
+	}{
+		{
+			Host:       "dut",
+			Suffix:     "-suffix",
+			Result:     "dut-suffix",
+			ShouldFail: false,
+		},
+		{
+			Host:       "dut.cros",
+			Suffix:     "-suffix",
+			Result:     "dut-suffix.cros",
+			ShouldFail: false,
+		},
+		{
+			Host:       "dut.domain.cros",
+			Suffix:     "-s",
+			Result:     "dut-s.domain.cros",
+			ShouldFail: false,
+		},
+		// Check failure on IP.
+		{
+			Host:       "192.168.0.1",
+			Suffix:     "-suffix",
+			Result:     "",
+			ShouldFail: true,
+		},
+	}
+	for _, tc := range testcases {
+		ret, err := companionDeviceHostname(tc.Host, tc.Suffix)
+		if tc.ShouldFail {
+			if err == nil {
+				t.Errorf("companionDeviceHostname(%q, %q) succeeded, which should fail",
+					tc.Host, tc.Suffix)
+			}
+		} else if err != nil {
+			t.Errorf("companionDeviceHostname(%q, %q) failed with err = %q",
+				tc.Host, tc.Suffix, err.Error())
+		} else if ret != tc.Result {
+			t.Errorf("companionDeviceHostname(%q, %q) got %q, want %q",
+				tc.Host, tc.Suffix, ret, tc.Result)
+		}
+	}
+}

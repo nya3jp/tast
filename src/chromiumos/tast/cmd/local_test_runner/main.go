7d390de15f5153f666cf1620d0ecf762843a5ba8
Matthew Warton <mwarton@google.com>
2020-01-08 13:43:03 -0800

    Tast Crostini: Create a new software dep for boards able to run crostini tests.
    The Octopus and Grunt boards frequently fail or flake due to less disk
    space or performance than what Crostini requires.  Whilst Crostini
    is technically supported on these boards they are not a priority for the
    team and are blocking us from having any testing on the CQ.
    
    Some related bugs:
    crbug.com/1039403
    crbug.com/1030436
    crbug.com/1006681
    
    TEST=Test runs on krane, skipped on grunt
    BUG=None
    Cq-Depend: chromium:1992661
    
    Change-Id: I1195d77c6c0f8e9825fc319a284f7873cf379834

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -81,6 +81,7 @@ func main() {
 			"cros_config":             "unibuild",
 			"cros_internal":           "internal",
 			"cros_video_decoder":      "!disable_cros_video_decoder",
+			"crostini_cq":             "!grunt && !octopus", // Not enough disk space or performance for reliable Crostini tests.
 			"crosvm_gpu":              `"crosvm-gpu"`,
 			"crossystem":              "!betty && !tast_vm", // VMs don't support few crossystem sub-commands: https://crbug.com/974615
 			"cups":                    "cups",

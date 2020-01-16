30fc0ad18fe7adc2a341a5b522aa0f212fa7a078
Ayato Tokubi <tokubi@google.com>
2020-01-16 12:33:50 +0900

    tast-tests: migrate ArcGamepadTest to ChromeOS
    This CL migrates ArcGamepadTest from Android to ChromeOS
    repository.
    
    BUG=b:120452372
    TEST=cros_workon_make --board=betty tast-local-apks-cros --install
         tast run with DUT
    
    Change-Id: If9615a58bbd443f95f8e8f18860640f6639008f0

---
+++ src/chromiumos/tast/local/arc/arc.go
@@ -0,0 +1,420 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arc supports interacting with the ARC framework, which is used to run Android applications on Chrome OS.
+package arc
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/shirou/gopsutil/process"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/chrome"
+	"chromiumos/tast/local/testexec"
+	"chromiumos/tast/testing"
+	"chromiumos/tast/timing"
+)
+
+const (
+	// BootTimeout is the maximum amount of time that ARC is expected to take to boot.
+	// Tests that call New should declare a timeout that's at least this long.
+	BootTimeout = 120 * time.Second
+
+	intentHelperTimeout = 20 * time.Second
+
+	logcatName = "logcat.txt"
+
+	//ARCPath is the path where the container images are installed in the rootfs.
+	ARCPath = "/opt/google/containers/android"
+	//ARCVMPath is the pather where the VM images are installed in the rootfs.
+	ARCVMPath = "/opt/google/vms/android"
+)
+
+// InstallType is the type of ARC (Container or VM) available on the device.
+type InstallType int
+
+const (
+	// Container is for the ARC container install.
+	Container InstallType = iota
+	// VM is for the ARCVM install.
+	VM
+)
+
+// locked is set to true while a precondition is active to prevent tests from calling New or Close.
+var locked = false
+
+// TODO(b/134144418): Consolidate ARC and ARCVM diverged code once ADB issues are resolved.
+
+// Supported returns true if ARC is supported on the board.
+//
+// This function must not be used to skip tests entirely; declare the "android"
+// software dependency instead. A valid use case would be to change the test
+// expectation by whether ARC is supported or not (e.g. existence of mount
+// points).
+func Supported() bool {
+	_, ok := Type()
+	return ok
+}
+
+// Type detects the type (container or VM) of the ARC installation. As for
+// Supported(), it should not be used to skip tests entirely, both fall under
+// the "android" software dependency. But it could be used to change the
+// behaviour of a test (e.g. check that ARCVM is running or not).
+func Type() (t InstallType, ok bool) {
+	if _, err := os.Stat(filepath.Join(ARCPath, "system.raw.img")); err == nil {
+		return Container, true
+	}
+	if _, err := os.Stat(filepath.Join(ARCVMPath, "system.raw.img")); err == nil {
+		return VM, true
+	}
+	return 0, false
+}
+
+// ARC holds resources related to an active ARC session. Call Close to release
+// those resources.
+type ARC struct {
+	logcatCmd    *testexec.Cmd // process saving Android logs
+	logcatWriter dynamicWriter // writes output from logcatCmd to logcatFile
+	logcatFile   *os.File      // file currently being written to
+}
+
+// Close releases testing-related resources associated with ARC.
+// ARC itself is not stopped.
+func (a *ARC) Close() error {
+	if locked {
+		panic("Do not call Close while precondition is being used")
+	}
+	var err error
+	if a.logcatCmd != nil {
+		a.logcatCmd.Kill()
+		a.logcatCmd.Wait()
+	}
+	if a.logcatFile != nil {
+		a.logcatWriter.setDest(nil)
+		err = a.logcatFile.Close()
+	}
+	return err
+}
+
+// New waits for Android to finish booting.
+//
+// ARC must be enabled in advance by passing chrome.ARCEnabled or chrome.ARCSupported with
+// real user gaia login to chrome.New.
+//
+// After this function returns successfully, you can assume BOOT_COMPLETED
+// intent has been broadcast from Android system, and ADB connection is ready.
+// Note that this does not necessarily mean all ARC mojo services are up; call
+// WaitIntentHelper() to wait for ArcIntentHelper to be ready, for example.
+//
+// The returned ARC instance must be closed when the test is finished.
+func New(ctx context.Context, outDir string) (*ARC, error) {
+	ctx, st := timing.Start(ctx, "arc_new")
+	defer st.End()
+
+	if locked {
+		panic("Cannot create ARC instance while precondition is being used")
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, BootTimeout)
+	defer cancel()
+
+	if err := checkSoftwareDeps(ctx); err != nil {
+		return nil, err
+	}
+
+	if err := ensureARCEnabled(); err != nil {
+		return nil, err
+	}
+
+	arc := &ARC{}
+	toClose := arc
+	defer func() {
+		if toClose != nil {
+			toClose.Close()
+		}
+	}()
+
+	vm, err := VMEnabled()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to determine if ARCVM is enabled")
+	}
+
+	testing.ContextLog(ctx, "Waiting for Android boot")
+
+	if vm {
+		// When running ARCVM, 'android-sh' runs via ADB. So, the first thing to do is set up ADB.
+		// Android should be initialized once a working connection to ADB is made.
+		testing.ContextLog(ctx, "Setting up ADB")
+		if err := setUpADB(ctx); err != nil {
+			return nil, errors.Wrap(err, "failed setting up ADB")
+		}
+
+		// Connect to ADB.
+		if err := connectADB(ctx); err != nil {
+			return nil, errors.Wrap(err, "failed connecting to ADB")
+		}
+
+	} else {
+		if err := WaitAndroidInit(ctx); err != nil {
+			return nil, errors.Wrap(err, "Android failed to boot in very early stage")
+		}
+	}
+
+	// At this point we can start logcat.
+	logcatPath := filepath.Join(outDir, logcatName)
+	if err := arc.setLogcatFile(logcatPath); err != nil {
+		return nil, errors.Wrap(err, "failed to create logcat output file")
+	}
+	logcatCmd, err := startLogcat(ctx, &arc.logcatWriter)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to start logcat")
+	}
+	arc.logcatCmd = logcatCmd
+
+	if !vm {
+		// Wait for internal networking to get ready. This gives better error messages
+		// when networking is broken, rather than obscure "failed connecting to ADB" error.
+		if err := waitNetworking(ctx); err != nil {
+			return nil, diagnose(logcatPath, errors.Wrap(err, "Android network unreachable"))
+		}
+	}
+
+	// This property is set by the Android system server just before LOCKED_BOOT_COMPLETED is broadcast.
+	const androidBootProp = "sys.boot_completed"
+	if err := waitProp(ctx, androidBootProp, "1", reportTiming); err != nil {
+		return nil, diagnose(logcatPath, errors.Wrapf(err, "%s not set", androidBootProp))
+	}
+
+	var ch chan error
+	if !vm {
+		// Android container is up. Set up ADB auth in parallel to Android boot since
+		// ADB local server takes a few seconds to start up.
+		testing.ContextLog(ctx, "Setting up ADB auth")
+		ch = make(chan error, 1)
+		go func() {
+			ch <- setUpADBAuth(ctx)
+		}()
+	}
+
+	// This property is set by ArcAppLauncher when it receives BOOT_COMPLETED.
+	const arcBootProp = "ro.arc.boot_completed"
+	if err := waitProp(ctx, arcBootProp, "1", reportTiming); err != nil {
+		return nil, diagnose(logcatPath, errors.Wrapf(err, "%s not set", arcBootProp))
+	}
+
+	if !vm {
+		// Android has booted.
+		if err := <-ch; err != nil {
+			return nil, diagnose(logcatPath, errors.Wrap(err, "failed setting up ADB auth"))
+		}
+
+		// Connect to ADB.
+		if err := connectADB(ctx); err != nil {
+			return nil, diagnose(logcatPath, errors.Wrap(err, "failed connecting to ADB"))
+		}
+	}
+
+	toClose = nil
+	return arc, nil
+}
+
+// WaitIntentHelper waits for ArcIntentHelper to get ready.
+func (a *ARC) WaitIntentHelper(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, intentHelperTimeout)
+	defer cancel()
+
+	testing.ContextLog(ctx, "Waiting for ArcIntentHelper")
+	const prop = "ro.arc.intent_helper.ready"
+	if err := waitProp(ctx, prop, "1", reportTiming); err != nil {
+		return errors.Wrapf(err, "property %s not set", prop)
+	}
+	return nil
+}
+
+// androidDeps contains Android-related software features (see testing.Test.SoftwareDeps).
+// At least one of them must be declared to call New.
+var androidDeps = []string{
+	"android",
+	"android_vm",
+	"android_both",
+	"android_all",
+	"android_all_both",
+	"android_p",
+	"android_p_both",
+	"android_r",
+}
+
+// checkSoftwareDeps ensures the current test declares Android software dependencies.
+func checkSoftwareDeps(ctx context.Context) error {
+	deps, ok := testing.ContextSoftwareDeps(ctx)
+	if !ok {
+		// Test info can be unavailable in unit tests.
+		return nil
+	}
+
+	for _, dep := range deps {
+		for _, adep := range androidDeps {
+			if dep == adep {
+				return nil
+			}
+		}
+	}
+	return errors.Errorf("test must declare at least one of Android software dependencies %v", androidDeps)
+}
+
+// setLogcatFile creates a new logcat output file at p and opens it as a.logcatFile.
+// a.logcatWriter is updated to write to the new file, and explanatory messages are
+// written to both the new file and old file (if there was a previous file).
+func (a *ARC) setLogcatFile(p string) error {
+	oldFile := a.logcatFile
+
+	var createErr error
+	a.logcatFile, createErr = os.Create(p)
+	if createErr == nil && oldFile != nil {
+		// Make the new file start with a line pointing at the old file.
+		if rel, err := filepath.Rel(filepath.Dir(a.logcatFile.Name()), oldFile.Name()); err == nil {
+			fmt.Fprintf(a.logcatFile, "[output continued from %v]\n", rel)
+		}
+	}
+	// If the create failed, we'll just drop the new logs.
+	a.logcatWriter.setDest(a.logcatFile)
+
+	if oldFile != nil {
+		if a.logcatFile != nil {
+			// Make the old file end with a line pointing at the new file.
+			if rel, err := filepath.Rel(filepath.Dir(oldFile.Name()), a.logcatFile.Name()); err == nil {
+				fmt.Fprintf(oldFile, "[output continued in %v]\n", rel)
+			}
+		}
+		oldFile.Close()
+	}
+
+	return createErr
+}
+
+// VMEnabled returns true if Chrome OS is running ARCVM.
+// This is done by checking "/run/chrome/is_arcvm" content equal to "1".
+// Reference: chrome/browser/chromeos/arc/arc_service_launcher.cc
+func VMEnabled() (bool, error) {
+	b, err := ioutil.ReadFile("/run/chrome/is_arcvm")
+	if err != nil {
+		return false, err
+	}
+	return string(b) == "1", nil
+}
+
+// ensureARCEnabled makes sure ARC is enabled by a command line flag to Chrome.
+func ensureARCEnabled() error {
+	args, err := getChromeArgs()
+	if err != nil {
+		return errors.Wrap(err, "failed getting Chrome args")
+	}
+
+	for _, a := range args {
+		if a == "--arc-start-mode=always-start" || a == "--arc-start-mode=always-start-with-no-play-store" || a == "--arc-availability=officially-supported" {
+			return nil
+		}
+	}
+	return errors.New("ARC is not enabled; pass chrome.ARCEnabled or chrome.ARCSupported to chrome.New")
+}
+
+// getChromeArgs returns command line arguments of the Chrome browser process.
+func getChromeArgs() ([]string, error) {
+	pid, err := chrome.GetRootPID()
+	if err != nil {
+		return nil, err
+	}
+	proc, err := process.NewProcess(int32(pid))
+	if err != nil {
+		return nil, err
+	}
+	return proc.CmdlineSlice()
+}
+
+// WaitAndroidInit waits for Android init process to start.
+//
+// It is very rare you want to call this function from your test; to wait for
+// the Android system to start and become ready, call New instead. A valid use
+// case is when you want to interact with Android mini container.
+//
+// It is fine to call BootstrapCommand after this function successfully returns.
+func WaitAndroidInit(ctx context.Context) error {
+	ctx, cancel := context.WithTimeout(ctx, BootTimeout)
+	defer cancel()
+
+	// service.adb.tcp.port is an arbitrary property set by Android init very
+	// early in boot process. Wait for it to ensure Android init process started.
+	// Note that existence of this property has nothing to do with the status of
+	// Android adb daemon.
+	const prop = "service.adb.tcp.port"
+	if err := waitProp(ctx, prop, "5555", reportTiming); err != nil {
+		return errors.Wrapf(err, "Android container did not come up: %s not set", prop)
+	}
+	return nil
+}
+
+// startLogcat starts a logcat process with its stdout redirected to w.
+func startLogcat(ctx context.Context, w io.Writer) (*testexec.Cmd, error) {
+	// Wait for logd to start by polling logcat.
+	cmd := BootstrapCommand(ctx, "/system/bin/sh", "-c", "while ! /system/bin/logcat -ds; do sleep 0.1; done")
+	if err := cmd.Run(testexec.DumpLogOnError); err != nil {
+		return nil, errors.Wrap(err, "logcat failed")
+	}
+
+	// The logcat process may need to span multiple tests if we're being used by a precondition,
+	// so use context.Background instead of ctx to make sure it isn't killed prematurely.
+	cmd = BootstrapCommand(context.Background(), "/system/bin/logcat") // NOLINT: process may need to persist across multiple tests
+	cmd.Stdout = w
+	if err := cmd.Start(); err != nil {
+		return nil, err
+	}
+	return cmd, nil
+}
+
+// waitNetworking waits for the internal networking to get ready.
+func waitNetworking(ctx context.Context) error {
+	ctx, st := timing.Start(ctx, "wait_networking")
+	defer st.End()
+
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		if err := testexec.CommandContext(ctx, "ping", "-c1", "-w1", "-n", "100.115.92.2").Run(); err != nil {
+			return errors.Wrap(err, "ping 100.115.92.2 failed")
+		}
+		return nil
+	}, nil)
+}
+
+// timingMode describes whether timing information should be reported.
+type timingMode int
+
+const (
+	reportTiming   timingMode = iota // create a timing stage
+	noReportTiming                   // don't create a timing stage
+)
+
+// waitProp waits for Android prop name is set to value.
+func waitProp(ctx context.Context, name, value string, tm timingMode) error {
+	if tm == reportTiming {
+		var st *timing.Stage
+		ctx, st = timing.Start(ctx, fmt.Sprintf("wait_prop_%s=%s", name, value))
+		defer st.End()
+	}
+
+	const loop = `while [ "$(/system/bin/getprop "$1")" != "$2" ]; do sleep 0.1; done`
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		return BootstrapCommand(ctx, "/system/bin/sh", "-c", loop, "-", name, value).Run()
+	}, &testing.PollOptions{Interval: time.Second})
+}
+
+// APKPath returns the absolute path to a helper APK.
+func APKPath(value string) string {
+	return filepath.Join("/usr/local/libexec/tast/apks/local/cros", value)
+}
---
+++ src/chromiumos/tast/local/bundles/cros/arc/gamepad.go
@@ -0,0 +1,316 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package arc provides Tast tests for ARC++.
+package arc
+
+import (
+	"context"
+	"encoding/json"
+	"math"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/local/arc"
+	"chromiumos/tast/local/arc/ui"
+	"chromiumos/tast/local/input"
+	"chromiumos/tast/testing"
+)
+
+func init() {
+	testing.AddTest(&testing.Test{
+		Func:         Gamepad,
+		Desc:         "Checks gamepad support works on Android",
+		Contacts:     []string{"tetsui@chromium.org", "arc-framework@google.com"},
+		Attr:         []string{"group:mainline", "informational"},
+		SoftwareDeps: []string{"chrome"},
+		Data:         []string{"ArcGamepadTest.apk"},
+		Params: []testing.Param{{
+			ExtraSoftwareDeps: []string{"android_p"},
+			Pre:               arc.Booted(),
+		}, {
+			Name:              "vm",
+			ExtraSoftwareDeps: []string{"android_vm"},
+			Pre:               arc.VMBooted(),
+		}},
+	})
+}
+
+type inputDevice struct {
+	DeviceID     int    `json:"device_id"`
+	ProductID    uint16 `json:"product_id"`
+	VendorID     uint16 `json:"vendor_id"`
+	Name         string `json:"name"`
+	MotionRanges map[string]struct {
+		Flat       float64 `json:"flat"`
+		Fuzz       float64 `json:"fuzz"`
+		Max        float64 `json:"max"`
+		Min        float64 `json:"min"`
+		Resolution float64 `json:"resolution"`
+		Source     int     `json:"source"`
+	} `json:"motion_ranges"`
+}
+
+// verifyGamepadDeviceInfo confirms the gamepad's InputDevice information is correct.
+func verifyGamepadDeviceInfo(s *testing.State, gp *input.GamepadEventWriter, d *inputDevice) {
+	// DeviceID may change at runtime.
+	if d.ProductID != gp.ProductID() {
+		s.Errorf("product ID doesn't match: got %v; want %v", d.ProductID, gp.ProductID())
+	}
+	if d.VendorID != gp.VendorID() {
+		s.Errorf("vendor ID doesn't match: got %v; want %v", d.VendorID, gp.VendorID())
+	}
+
+	isCenteredAxis := map[string]bool{
+		"AXIS_X":     true,
+		"AXIS_Y":     true,
+		"AXIS_Z":     true,
+		"AXIS_RZ":    true,
+		"AXIS_HAT_X": true,
+		"AXIS_HAT_Y": true,
+	}
+	gamepadMapping := map[input.EventCode]string{
+		input.ABS_X:     "AXIS_X",
+		input.ABS_Y:     "AXIS_Y",
+		input.ABS_RX:    "AXIS_Z",
+		input.ABS_RY:    "AXIS_RZ",
+		input.ABS_HAT0X: "AXIS_HAT_X",
+		input.ABS_HAT0Y: "AXIS_HAT_Y",
+		input.ABS_Z:     "AXIS_LTRIGGER",
+		input.ABS_RZ:    "AXIS_RTRIGGER",
+	}
+
+	almostEqual := func(a, b float64) bool {
+		return math.Abs(a-b) <= 1e-5
+	}
+
+	for code, info := range gp.Axes() {
+		axisName, ok := gamepadMapping[code]
+		if !ok {
+			continue
+		}
+		expectedMin := 0.0
+		expectedMax := 1.0
+		if isCenteredAxis[axisName] {
+			expectedMin = -1.0
+			expectedMax = 1.0
+		}
+		scale := (expectedMax - expectedMin) / float64(info.Maximum-info.Minimum)
+		expectedFuzz := float64(info.Fuzz) * scale
+		expectedFlat := float64(info.Flat) * scale
+
+		motionRange, ok := d.MotionRanges[axisName]
+		if !ok {
+			s.Errorf("gamepad axis %s not found", axisName)
+		}
+		if !almostEqual(expectedMin, motionRange.Min) {
+			s.Errorf("min does not match for %s: got %f; want %f", axisName, motionRange.Min, expectedMin)
+		}
+		if !almostEqual(expectedMax, motionRange.Max) {
+			s.Errorf("max does not match for %s: got %f; want %f", axisName, motionRange.Max, expectedMax)
+		}
+		if !almostEqual(expectedFuzz, motionRange.Fuzz) {
+			s.Errorf("fuzz does not match for %s: got %f; want %f", axisName, motionRange.Fuzz, expectedFuzz)
+		}
+		if !almostEqual(expectedFlat, motionRange.Flat) {
+			s.Errorf("flat does not match for %s: got %f; want %f", axisName, motionRange.Flat, expectedFlat)
+		}
+	}
+	return
+}
+
+type keyEvent struct {
+	Action   string `json:"action"`
+	KeyCode  string `json:"key_code"`
+	DeviceID int    `json:"device_id"`
+}
+
+type motionEvent struct {
+	Action string             `json:"action"`
+	Axes   map[string]float64 `json:"axes"`
+}
+
+func getInputDevices(ctx context.Context, d *ui.Device) ([]inputDevice, error) {
+	view := d.Object(ui.ID("org.chromium.arc.testapp.gamepad:id/device_status"))
+	text, err := view.GetText(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var devices []inputDevice
+	if err := json.Unmarshal([]byte(text), &devices); err != nil {
+		return nil, err
+	}
+	return devices, nil
+}
+
+func getKeyEvents(ctx context.Context, d *ui.Device) ([]keyEvent, error) {
+	view := d.Object(ui.ID("org.chromium.arc.testapp.gamepad:id/key_events"))
+	text, err := view.GetText(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var events []keyEvent
+	if err := json.Unmarshal([]byte(text), &events); err != nil {
+		return nil, err
+	}
+	return events, nil
+}
+
+func getMotionEvent(ctx context.Context, d *ui.Device) (*motionEvent, error) {
+	view := d.Object(ui.ID("org.chromium.arc.testapp.gamepad:id/motion_event"))
+	text, err := view.GetText(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var event motionEvent
+	if err := json.Unmarshal([]byte(text), &event); err != nil {
+		return nil, err
+	}
+	return &event, nil
+}
+
+func waitForGamepadAxis(ctx context.Context, d *ui.Device, axis string, expected float64) error {
+	return testing.Poll(ctx, func(ctx context.Context) error {
+		if event, err := getMotionEvent(ctx, d); err != nil {
+			return err
+		} else if val, ok := event.Axes[axis]; !ok || val != expected {
+			return errors.Errorf("unexpected %s value: got %f; want %f", axis, val, expected)
+		}
+		return nil
+	}, nil)
+}
+
+func Gamepad(ctx context.Context, s *testing.State) {
+	a := s.PreValue().(arc.PreData).ARC
+	d, err := ui.NewDevice(ctx, a)
+	if err != nil {
+		s.Fatal("Failed initializing UI Automator: ", err)
+	}
+	defer d.Close()
+
+	gp, err := input.Gamepad(ctx)
+	if err != nil {
+		s.Fatal("Failed to create a gamepad: ", err)
+	}
+	defer func() {
+		if gp != nil {
+			gp.Close()
+		}
+	}()
+
+	s.Log("Created a virtual gamepad device ", gp.Device())
+
+	const (
+		apk = "ArcGamepadTest.apk"
+		pkg = "org.chromium.arc.testapp.gamepad"
+		cls = "org.chromium.arc.testapp.gamepad.MainActivity"
+	)
+
+	s.Log("Installing app")
+	if err := a.Install(ctx, arc.APKPath(apk)); err != nil {
+		s.Fatal("Failed installing app: ", err)
+	}
+
+	s.Log("Starting app")
+	if err := a.Command(ctx, "am", "start", "-W", pkg+"/"+cls).Run(); err != nil {
+		s.Fatal("Failed starting app: ", err)
+	}
+
+	s.Log("Checking the device connection")
+	var device inputDevice
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		devices, err := getInputDevices(ctx, d)
+		if err != nil {
+			return err
+		} else if len(devices) != 1 {
+			return errors.Errorf("unexpected number of gamepad devices: got %v; want 1",
+				len(devices))
+		}
+		device = devices[0]
+		return nil
+	}, nil); err != nil {
+		s.Fatal("Cannot get the gamepad device: ", err)
+	}
+
+	verifyGamepadDeviceInfo(s, gp, &device)
+
+	s.Log("Pressing buttons")
+	if err := gp.TapButton(ctx, input.BTN_EAST); err != nil {
+		s.Fatal("Failed to press button: ", err)
+	}
+	if err := gp.TapButton(ctx, input.BTN_SOUTH); err != nil {
+		s.Fatal("Failed to press button: ", err)
+	}
+
+	const (
+		ActionDown     = "ACTION_DOWN"
+		ActionUp       = "ACTION_UP"
+		KeycodeButtonA = "KEYCODE_BUTTON_A"
+		KeycodeButtonX = "KEYCODE_BUTTON_X"
+		AxisX          = "AXIS_X"
+	)
+
+	expectedEvents := []keyEvent{
+		{Action: ActionDown, KeyCode: KeycodeButtonA},
+		{Action: ActionUp, KeyCode: KeycodeButtonA},
+		{Action: ActionDown, KeyCode: KeycodeButtonX},
+		{Action: ActionUp, KeyCode: KeycodeButtonX}}
+
+	s.Log("Checking the generated gamepad events")
+	var actualEvents []keyEvent
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		var err error
+		if actualEvents, err = getKeyEvents(ctx, d); err != nil {
+			return err
+		} else if len(actualEvents) != len(expectedEvents) {
+			return errors.Errorf("unexpected number of gamepad events: got %d; want %d",
+				len(actualEvents), len(expectedEvents))
+		}
+		return nil
+	}, nil); err != nil {
+		s.Fatal("Failed to get gamepad events: ", err)
+	}
+
+	for i, expected := range expectedEvents {
+		// DeviceID may change at runtime.
+		expected.DeviceID = device.DeviceID
+		if expected != actualEvents[i] {
+			s.Fatalf("Unexpected gamepad event: got %v; want %v", actualEvents[i], expected)
+		}
+	}
+
+	s.Log("Moving the analog stick")
+	axis := gp.Axes()[input.ABS_X]
+	if err := gp.MoveAxis(ctx, input.ABS_X, axis.Maximum); err != nil {
+		s.Fatal("Failed to move axis: ", err)
+	}
+	if err := waitForGamepadAxis(ctx, d, AxisX, 1.0); err != nil {
+		s.Fatal("Failed to wait for axis change: ", err)
+	}
+	if err := gp.MoveAxis(ctx, input.ABS_X, axis.Minimum); err != nil {
+		s.Fatal("Failed to move axis: ", err)
+	}
+	if err := waitForGamepadAxis(ctx, d, AxisX, -1.0); err != nil {
+		s.Fatal("Failed to wait for axis change: ", err)
+	}
+
+	s.Log("Disconnecting the gamepad")
+	if err := gp.Close(); err != nil {
+		s.Fatal("Failed to close the gamepad: ", err)
+	}
+	gp = nil
+
+	if err := testing.Poll(ctx, func(ctx context.Context) error {
+		if devices, err := getInputDevices(ctx, d); err != nil {
+			return err
+		} else if len(devices) > 0 {
+			return errors.Errorf("the gamepad device still exist: %+v", devices)
+		}
+		return nil
+	}, nil); err != nil {
+		s.Fatal("Failed to disconnect the gamepad: ", err)
+	}
+}

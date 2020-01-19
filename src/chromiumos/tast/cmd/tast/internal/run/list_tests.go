13ac3baf59cebee40ecc5d08eda346710b2c6ac6
Hidehiko Abe <hidehiko@chromium.org>
2020-01-20 00:52:52 +0900

    tast: Introduce cmd interface, and pulls list_tests.go.
    This CL introduces "cmd" interface for the common APIs to run
    either local_test_runner or remote_test_runner.
    As its first application, "list" part is extracted from local.go
    and remote.go into list_tests.go.
    
    BUG=chromium:950346
    TEST=fast_build.sh -T. Ran "tast list" command.
    
    Change-Id: I8c6865da425ca3e4d77acaeb1f69ffaf3beb7e8c

---
+++ src/chromiumos/tast/cmd/tast/internal/run/list_tests.go
@@ -0,0 +1,73 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package run
+
+import (
+	"context"
+
+	"chromiumos/tast/bundle"
+	"chromiumos/tast/host"
+	"chromiumos/tast/internal/runner"
+	"chromiumos/tast/testing"
+)
+
+// listTests returns the whole tests to run.
+func listTests(ctx context.Context, cfg *Config) ([]TestResult, error) {
+	var tests []testing.TestInstance
+	if cfg.runLocal {
+		hst, err := connectToTarget(ctx, cfg)
+		if err != nil {
+			return nil, err
+		}
+		localTests, err := listLocalTests(ctx, cfg, hst)
+		if err != nil {
+			return nil, err
+		}
+		tests = append(tests, localTests...)
+	}
+	if cfg.runRemote {
+		remoteTests, err := listRemoteTests(ctx, cfg)
+		if err != nil {
+			return nil, err
+		}
+		tests = append(tests, remoteTests...)
+	}
+
+	results := make([]TestResult, len(tests))
+	for i := 0; i < len(tests); i++ {
+		results[i].TestInstance = tests[i]
+	}
+	return results, nil
+}
+
+// listLocalTests returns a list of local tests to run.
+func listLocalTests(ctx context.Context, cfg *Config, hst *host.SSH) ([]testing.TestInstance, error) {
+	return runListTestsCommand(
+		localRunnerCommand(ctx, cfg, hst), cfg.Patterns, cfg.localBundleGlob())
+}
+
+// listRemoteTests returns a list of remote tests to run.
+func listRemoteTests(ctx context.Context, cfg *Config) ([]testing.TestInstance, error) {
+	return runListTestsCommand(
+		remoteRunnerCommand(ctx, cfg), cfg.Patterns, cfg.remoteBundleGlob())
+}
+
+func runListTestsCommand(r runnerCmd, ptns []string, glob string) ([]testing.TestInstance, error) {
+	var ts []testing.TestInstance
+	if err := runTestRunnerCommand(
+		r,
+		&runner.Args{
+			Mode: runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{
+				BundleArgs: bundle.ListTestsArgs{Patterns: ptns},
+				BundleGlob: glob,
+			},
+		},
+		&ts,
+	); err != nil {
+		return nil, err
+	}
+	return ts, nil
+}

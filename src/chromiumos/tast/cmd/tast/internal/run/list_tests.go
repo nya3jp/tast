379e098f2e6431b38508d2b97bc227a339d0253f
Hidehiko Abe <hidehiko@chromium.org>
2020-01-20 00:52:52 +0900

    tast: Introduce cmd interface, and pulls list_tests.go.
    This CL introduces "cmd" interface for the common APIs to run
    either local_test_runner or remote_test_runner.
    As its first application, "list" part is extracted from local.go
    and remote.go into list_tests.go.
    
    BUG=chromium:950346
    TEST=fast_build.sh -T. Ran "tast list" command.
    
    Change-Id: I8c6865da425ca3e4d77acaeb1f69ffaf3beb7e8c

---
+++ src/chromiumos/tast/cmd/tast/internal/run/list_tests.go
@@ -0,0 +1,81 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package run
+
+import (
+	"context"
+
+	"chromiumos/tast/bundle"
+	"chromiumos/tast/host"
+	"chromiumos/tast/internal/runner"
+	"chromiumos/tast/testing"
+)
+
+// listTests returns the whole tests to run.
+func listTests(ctx context.Context, cfg *Config) ([]TestResult, error) {
+	var tests []testing.TestInstance
+	if cfg.runLocal {
+		hst, err := connectToTarget(ctx, cfg)
+		if err != nil {
+			return nil, err
+		}
+		localTests, err := listLocalTests(ctx, cfg, hst)
+		if err != nil {
+			return nil, err
+		}
+		tests = append(tests, localTests...)
+	}
+	if cfg.runRemote {
+		remoteTests, err := listRemoteTests(ctx, cfg)
+		if err != nil {
+			return nil, err
+		}
+		tests = append(tests, remoteTests...)
+	}
+
+	results := make([]TestResult, len(tests))
+	for i := 0; i < len(tests); i++ {
+		results[i].TestInstance = tests[i]
+	}
+	return results, nil
+}
+
+// listLocalTests retunrs a list of local tests to run.
+func listLocalTests(ctx context.Context, cfg *Config, hst *host.SSH) ([]testing.TestInstance, error) {
+	var ts []testing.TestInstance
+	if err := runTestRunnerCommand(
+		newLocalRunner(ctx, cfg, hst),
+		&runner.Args{
+			Mode: runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{
+				BundleArgs: bundle.ListTestsArgs{Patterns: cfg.Patterns},
+				BundleGlob: cfg.localBundleGlob(),
+			},
+		},
+		&ts,
+	); err != nil {
+		return nil, err
+	}
+	return ts, nil
+}
+
+// listRemoteTests returns a list of remote tests to run.
+func listRemoteTests(ctx context.Context, cfg *Config) ([]testing.TestInstance, error) {
+	var ts []testing.TestInstance
+	if err := runTestRunnerCommand(
+		newRemoteRunner(ctx, cfg),
+		&runner.Args{
+			Mode: runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{
+				BundleArgs: bundle.ListTestsArgs{Patterns: cfg.Patterns},
+				BundleGlob: cfg.remoteBundleGlob(),
+			},
+		},
+		&ts,
+	); err != nil {
+		return nil, err
+	}
+	return ts, nil
+}
---
+++ src/chromiumos/tast/cmd/tast/internal/run/test_runner.go
@@ -0,0 +1,97 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package run
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"chromiumos/tast/host"
+	"chromiumos/tast/internal/runner"
+)
+
+// cmd provides common interface to execute a command.
+type cmd interface {
+	// SetStdin sets the given stdin as the subprocess's stdin.
+	SetStdin(stdin io.Reader)
+
+	// Output executes the command, and returns its stdout.
+	Output() ([]byte, error)
+}
+
+type localRunner struct {
+	*host.Cmd
+
+	// ctx is used to run host.Cmd.Start() and host.Cmd.Wait().
+	ctx context.Context
+}
+
+func newLocalRunner(ctx context.Context, cfg *Config, hst *host.SSH) *localRunner {
+	// Set proxy-related environment variables for local_test_runner so it will use them
+	// when accessing network.
+	execArgs := []string{"env"}
+	if cfg.proxy == proxyEnv {
+		// Proxy-related variables can be either uppercase or lowercase.
+		// See https://golang.org/pkg/net/http/#ProxyFromEnvironment.
+		for _, name := range []string{
+			"HTTP_PROXY", "HTTPS_PROXY", "NO_PROXY",
+			"http_proxy", "https_proxy", "no_proxy",
+		} {
+			if val := os.Getenv(name); val != "" {
+				execArgs = append(execArgs, fmt.Sprintf("%s=%s", name, val))
+			}
+		}
+	}
+	execArgs = append(execArgs, cfg.localRunner)
+
+	return &localRunner{hst.Command(execArgs[0], execArgs[1:]...), ctx}
+}
+
+func (r *localRunner) SetStdin(stdin io.Reader) {
+	r.Stdin = stdin
+}
+
+func (r *localRunner) Output() ([]byte, error) {
+	return r.Cmd.Output(r.ctx)
+}
+
+type remoteRunner struct {
+	*exec.Cmd
+}
+
+func newRemoteRunner(ctx context.Context, cfg *Config) *remoteRunner {
+	return &remoteRunner{exec.CommandContext(ctx, cfg.remoteRunner)}
+}
+
+func (r *remoteRunner) SetStdin(stdin io.Reader) {
+	r.Stdin = stdin
+}
+
+// runTestRunnerCommand executes the given test_runner r. The test_runner reads
+// serialized args from its stdin, then output json serialized value to stdout.
+// This function unmarshals the output to out, so the pointer to an appropriate
+// struct is expected to be passed via out.
+func runTestRunnerCommand(r cmd, args *runner.Args, out interface{}) error {
+	args.FillDeprecated()
+	stdin, err := json.Marshal(args)
+	if err != nil {
+		return fmt.Errorf("marshal args: %v", err)
+	}
+	r.SetStdin(bytes.NewBuffer(stdin))
+
+	b, err := r.Output()
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(b, out); err != nil {
+		return fmt.Errorf("unmarshal output: %v", err)
+	}
+	return nil
+}

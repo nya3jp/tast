3114396e4168aee9b9ad9c594c768ff39864ffda
Brendan Hansknecht <bhansknecht@chromium.org>
2020-01-22 16:41:36 -0800

    Reland "tast: label precondition errors"
    This is a reland of 49ff1b981e06a3526d088fdb26a6a4639630adc5
    
    TEST=Same test as below and fast_build.sh -t chromiumos/tast/testing
    BUG=chromium:1042822
    
    Original change's description:
    > tast: label precondition errors
    >
    > Whenever s.Error, s.Errorf, s.Fatal, or s.Fatalf are call during a
    > precondition, "[Precondition failure] " is prepended to the log.
    >
    > TEST=Added s.Error/Fatal/... to arc pre.go and ran tast run <eve>
    > arc.UIAutomator
    > BUG=chromium:1042822
    >
    > Change-Id: Id6d26f147edc6b55e54bcc0aa91e21d2f9afa344
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2015816
    > Tested-by: Brendan Hansknecht <bhansknecht@chromium.org>
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    
    Bug: chromium:1042822
    Change-Id: Idaa2d942f5e7b22f0b0efc2a506cb025d923bead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2036153
    Tested-by: Brendan Hansknecht <bhansknecht@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Brendan Hansknecht <bhansknecht@chromium.org>

--- src/chromiumos/tast/testing/state.go
+++ src/chromiumos/tast/testing/state.go
@@ -22,7 +22,10 @@ import (
 	"chromiumos/tast/timing"
 )
 
-const metaCategory = "meta" // category for remote tests exercising Tast, as in "meta.TestName"
+const (
+	metaCategory  = "meta"                    // category for remote tests exercising Tast, as in "meta.TestName".
+	preFailPrefix = "[Precondition failure] " // the prefix used then a precondition failure is logged.
+)
 
 // Error describes an error encountered while running a test.
 type Error struct {
@@ -109,6 +112,8 @@ type State struct {
 	root *rootState // root state for the test
 
 	subtests []string // subtest names; used to prefix error messages
+
+	inPre bool // true if a precondition is currently executing.
 
 	hasError bool       // true if the current subtest has encountered an error; the test fails if this is true for the initial subtest
 	mu       sync.Mutex // protects hasError
@@ -419,6 +424,11 @@ func (s *State) formatError(args ...interface{}) (fullMsg, lastMsg string, err e
 		lastMsg = subtests + lastMsg
 	}
 
+	if s.inPre {
+		fullMsg = preFailPrefix + fullMsg
+		lastMsg = preFailPrefix + lastMsg
+	}
+
 	return fullMsg, lastMsg, err
 }
 
@@ -453,6 +463,11 @@ func (s *State) formatErrorf(format string, args ...interface{}) (fullMsg, lastM
 
 		fullMsg = subtests + fullMsg
 		lastMsg = subtests + lastMsg
+	}
+
+	if s.inPre {
+		fullMsg = preFailPrefix + fullMsg
+		lastMsg = preFailPrefix + lastMsg
 	}
 
 	return fullMsg, lastMsg, err

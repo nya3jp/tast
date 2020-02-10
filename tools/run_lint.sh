c7aa1a00a83ec59ba5b4087e22f7a2a5f51d88d2
Keigo Oka <oka@google.com>
2020-02-10 09:14:07 +0900

    explicitely set GOBIN in run_lint.sh
    Fix issue that the binary can be installed in an unintended location if there
    exists user-defined GOBIN environment variable.
    
    BUG=None
    TEST=manually
    
    Change-Id: I703ac2d039043c71943f1e82a1366830dd5efb14

--- tools/run_lint.sh
+++ tools/run_lint.sh
@@ -9,6 +9,7 @@ declare -r repo_root="$(cd "$(dirname "$(readlink -e "$0")")/../../../.."; pwd)"
 declare -r tast_root="${repo_root}/src/platform/tast"
 declare -r chroot_gopath="${repo_root}/chroot/usr/lib/gopath"
 
+export GOBIN="${tast_root}/bin"
 export GOPATH="${tast_root}:${chroot_gopath}"
 
 if ! go install chromiumos/cmd/tast-lint; then

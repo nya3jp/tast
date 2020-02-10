2d858e54f1b17fc0406124e4974cc03e8ae7435b
Keigo Oka <oka@google.com>
2020-02-10 16:36:33 +0900

    tast: implement runtime check of secret variables
    BUG=chromium:1014386
    TEST=fast_build.sh -T
    
    Cq-Depend: chromium:2043543, chromium:2043541, chromium:2095653
    Change-Id: I7b46beb23d4e2c385ea664f24ef2eca048a33702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2046725
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>

--- src/chromiumos/tast/testing/test_instance_test.go
+++ src/chromiumos/tast/testing/test_instance_test.go
@@ -426,6 +426,41 @@ func TestInstantiateReservedAttrPrefixes(t *gotesting.T) {
 		}
 	}
 }
+func TestValidateVars_OK(t *gotesting.T) {
+	const (
+		category = "a"
+		name     = "B"
+	)
+	for _, vars := range [][]string{
+		{"x"},
+		{"a.B.c", "a.c"},
+		{"a.B.cC1_"},
+	} {
+		if err := validateVars(category, name, vars); err != nil {
+			t.Errorf("validateVars(%v, %v, %v) = %v; want nil", category, name, vars, err)
+		}
+	}
+}
+
+func TestValidateVars_Error(t *gotesting.T) {
+	const (
+		category = "a"
+		name     = "B"
+	)
+	for _, vars := range [][]string{
+		{"a."},
+		{"a.c", "a."},
+		{"a.B."},
+		{"x.c"},
+		{"a.X.c"},
+		{"a.B.c."},
+		{"a.B._"},
+	} {
+		if err := validateVars(category, name, vars); err == nil {
+			t.Errorf("validateVars(%v, %v, %v) = nil; want error", category, name, vars)
+		}
+	}
+}
 
 func TestInstantiateNegativeTimeout(t *gotesting.T) {
 	if _, err := instantiate(&Test{

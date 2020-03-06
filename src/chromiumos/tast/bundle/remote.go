a1e9caa426be6135376e0a06096119baf01f88bf
Shuhei Takahashi <nya@chromium.org>
2020-03-06 15:36:35 +0900

    tast: Protect the control message channel.
    Test bundles communicate with the tast command on the host with the
    control message channel established on stdin/stdout. Consequently,
    if a test writes something to stdout or reads from stdin, the
    control message channel is disturbed.
    
    This change introduces bundle.{LocalDefault,RemoteDefault} which
    closes os.Stdin, os.Stdout and os.Stderr before running tests.
    
    BUG=chromium:1041131
    TEST=fast_build.sh -T
    TEST=tast run DUT meta.Stdio
    
    Change-Id: Idfb7af3f1ac42a6cf7a82a1f409a6afe3f1c6c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2089160
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

--- src/chromiumos/tast/bundle/remote.go
+++ src/chromiumos/tast/bundle/remote.go
@@ -7,6 +7,7 @@ package bundle
 import (
 	"context"
 	"io"
+	"os"
 	"time"
 )
 
@@ -14,15 +15,22 @@ const (
 	remoteTestTimeout = 5 * time.Minute // default max runtime for each test
 )
 
+// RemoteDefault implements the main function for remote test bundles.
+//
+// Usually the Main function of a remote test bundles should just this function,
+// and pass the returned status code to os.Exit.
+func RemoteDefault() int {
+	stdin, stdout, stderr := lockStdIO()
+	return Remote(os.Args[1:], stdin, stdout, stderr)
+}
+
 // Remote implements the main function for remote test bundles.
 //
-// clArgs should typically be os.Args[1:].
-// The returned status code should be passed to os.Exit.
+// Main function of remote test bundles should call RemoteDefault instead.
 func Remote(clArgs []string, stdin io.Reader, stdout, stderr io.Writer) int {
 	args := Args{}
 	cfg := runConfig{
 		defaultTestTimeout: remoteTestTimeout,
 	}
-
 	return run(context.Background(), clArgs, stdin, stdout, stderr, &args, &cfg, remoteBundle)
 }

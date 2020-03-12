9e88153c30fa5cef9264d5af9d16dfc2828e501b
Patrick Meiring <meiring@google.com>
2020-03-12 14:01:43 +1100

    tast-lint: Forbid dbus.SystemBus and dbus.SystemBusPrivate.
    As suggested in crrev.com/c/2094409. The default signal handlers
    used by dbus.SystemBus and dbus.SystemBusPrivate may re-order
    signals sent over dbus. Any test tracking the state of a remote
    dbus object expecting the most recently received signal to
    contain the most recent state may therefore behave incorrectly.
    Using the new SystemBus accessor in dbusutil avoids this problem.
    
    BUG=chromium:1059617
    TEST=Ran package-level unit tests for tast-lint.
    
    Cq-Depend: chromium:2094409
    Change-Id: Id84324734bcc3375d4db21d0a120849ba485c611

--- src/chromiumos/tast/cmd/tast-lint/check/forbidden_calls_test.go
+++ src/chromiumos/tast/cmd/tast-lint/check/forbidden_calls_test.go
@@ -15,6 +15,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/godbus/dbus"
+
 	"chromiumos/tast/errors"
 )
 
@@ -25,13 +27,17 @@ func main() {
 	time.Sleep(time.Second)
 	context.Background()
 	context.TODO()
+	dbus.SystemBus()
+	dbus.SystemBusPrivate()
 }
 `
 	expects := []string{
-		"testfile.go:12:2: chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
-		"testfile.go:14:2: time.Sleep ignores context deadline; use testing.Poll or testing.Sleep instead",
-		"testfile.go:15:2: context.Background ignores test timeout; use test function's ctx arg instead",
-		"testfile.go:16:2: context.TODO ignores test timeout; use test function's ctx arg instead",
+		"testfile.go:14:2: chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
+		"testfile.go:16:2: time.Sleep ignores context deadline; use testing.Poll or testing.Sleep instead",
+		"testfile.go:17:2: context.Background ignores test timeout; use test function's ctx arg instead",
+		"testfile.go:18:2: context.TODO ignores test timeout; use test function's ctx arg instead",
+		"testfile.go:19:2: dbus.SystemBus may reorder signals; use chromiumos/tast/local/dbusutil.SystemBus instead",
+		"testfile.go:20:2: dbus.SystemBusPrivate may reorder signals; use chromiumos/tast/local/dbusutil.SystemBusPrivate instead",
 	}
 
 	f, fs := parse(code, "testfile.go")
@@ -42,43 +48,58 @@ func main() {
 func TestAutoFixForbiddenCalls(t *testing.T) {
 	files := make(map[string]string)
 	expects := make(map[string]string)
-	const filename1, filename2, filename3 = "foo.go", "bar.go", "baz.go"
+	const filename1, filename2, filename3, filename4 = "foo.go", "bar.go", "baz.go", "dbus.go"
+
+	// fmt.Errorf -> chromiumos/tast/errors.Errorf cannot be
+	// automatically fixed in this file right now because it contains an
+	// "errors" identifier node.
 	files[filename1] = `package main
 
 import (
 	"fmt"
 	"time"
 
+	"github.com/godbus/dbus"
+
 	"chromiumos/tast/errors"
 )
 
 func main() {
 	fmt.Printf("foo")
-	// this is not fixable
+	// This is not fixable
 	fmt.Errorf("foo")
+	
 	errors.Errorf("foo")
 	time.Sleep(time.Second)
 	context.Background()
 	context.TODO()
+	dbus.SystemBus()
+	dbus.SystemBusPrivate()
 }
 `
+
 	expects[filename1] = `package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"chromiumos/tast/errors"
+	"chromiumos/tast/local/dbusutil"
 )
 
 func main() {
 	fmt.Printf("foo")
-	// this is not fixable
+	// This is not fixable
 	fmt.Errorf("foo")
+
 	errors.Errorf("foo")
 	time.Sleep(time.Second)
 	context.Background()
 	context.TODO()
+	dbusutil.SystemBus()
+	dbusutil.SystemBusPrivate()
 }
 `
 	files[filename2] = `package main
@@ -125,5 +146,31 @@ func main() {
 	errors.Errorf("foo")
 }
 `
+	files[filename4] = `package main
+
+import (
+	"github.com/godbus/dbus"
+)
+
+func main() {
+	dbus.SystemBus()
+	dbus.SystemBusPrivate(dbus.WithHandler(nil))
+}
+`
+
+	expects[filename4] = `package main
+
+import (
+	"github.com/godbus/dbus"
+
+	"chromiumos/tast/local/dbusutil"
+)
+
+func main() {
+	dbusutil.SystemBus()
+	dbusutil.SystemBusPrivate(dbus.WithHandler(nil))
+}
+`
+
 	verifyAutoFix(t, ForbiddenCalls, files, expects)
 }

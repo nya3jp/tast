9e89764442e6514bf7f16b17b06d8b9104e1f7ee
Keigo Oka <oka@google.com>
2020-03-17 16:58:34 +0900

    tast: introduce linuxssh package and put file operations there
    Currently in host package linux specific utilities and more
    fundamental platform-agnostic SSH utilities are jumbled together.
    This is the first change in the series of CLs to refactor the package
    into platform-agnostic ssh package and platform-spefic linuxssh
    package.
    
    BUG=chromium:1019537
    TEST=locally changed a file in tast-tests to use linuxssh, and run fast_build.sh -T
    
    Change-Id: I32c878b51010229d9ae1c444f6c73a2de393d5b3

---
+++ src/chromiumos/tast/ssh/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ssh provides means to communicate with remote host through SSH.
+package ssh
---
+++ src/chromiumos/tast/ssh/linuxssh/fileutil.go
@@ -0,0 +1,52 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package linuxssh provides Linux specific operations conducted via SSH
+package linuxssh
+
+import (
+	"context"
+
+	"chromiumos/tast/host"
+	"chromiumos/tast/ssh"
+)
+
+// GetFile copies a file or directory from the host to the local machine.
+// dst is the full destination name for the file or directory being copied, not
+// a destination directory into which it will be copied. dst will be replaced
+// if it already exists.
+func GetFile(ctx context.Context, c *ssh.Conn, src, dst string) error {
+	return c.GetFile(ctx, src, dst)
+}
+
+// SymlinkPolicy describes how symbolic links should be handled by PutFiles.
+type SymlinkPolicy = host.SymlinkPolicy
+
+const (
+	// PreserveSymlinks indicates that symlinks should be preserved during the copy.
+	PreserveSymlinks SymlinkPolicy = iota
+	// DereferenceSymlinks indicates that symlinks should be dereferenced and turned into normal files.
+	DereferenceSymlinks
+)
+
+// PutFiles copies files on the local machine to the host. files describes
+// a mapping from a local file path to a remote file path. For example, the call:
+//
+//	PutFiles(ctx, map[string]string{"/src/from": "/dst/to"})
+//
+// will copy the local file or directory /src/from to /dst/to on the remote host.
+// Local file paths can be absolute or relative. Remote file paths must be absolute.
+// SHA1 hashes of remote files are checked in advance to send updated files only.
+// bytes is the amount of data sent over the wire (possibly after compression).
+func PutFiles(ctx context.Context, c *ssh.Conn, files map[string]string,
+	symlinkPolicy SymlinkPolicy) (bytes int64, err error) {
+	return c.PutFiles(ctx, files, symlinkPolicy)
+}
+
+// DeleteTree deletes all relative paths in files from baseDir on the host.
+// If a specified file is a directory, all files under it are recursively deleted.
+// Non-existent files are ignored.
+func DeleteTree(ctx context.Context, c *ssh.Conn, baseDir string, files []string) error {
+	return c.DeleteTree(ctx, baseDir, files)
+}

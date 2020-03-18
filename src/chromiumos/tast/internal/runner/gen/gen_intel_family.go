7d1de47656ce79bb0dd8024758f2df652a919f5b
Hidehiko Abe <hidehiko@chromium.org>
2020-03-18 15:00:52 +0900

    tast: Implement SOC detection.
    There seems no way to find SOC of the current DUT.
    Instead, this CL detects the SOC based on the lscpu output.
    
    BUG=chromium:950346, chromium:1057870
    TEST=Ran tast run ${IP} example.Pass and made sure SOC is
         contained in device-config.txt.
    
    Change-Id: If2ee3ff253f9b32af2c5197fe18b73e29aa1a34d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2105527
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>

---
+++ src/chromiumos/tast/internal/runner/gen/gen_intel_family.go
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements a script for writing a Go source file containing intel family constants.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+
+	"chromiumos/tast/genutil"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <intel-family.h> <out.go>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	inputFile := args[0]
+	outputFile := args[1]
+
+	const (
+		// Relative path to go.sh from the generated file.
+		goSh = "../../../../../tools/go.sh"
+
+		// Relative path to this file from the generated file.
+		thisFile = "gen/gen_intel_family.go"
+	)
+
+	params := genutil.Params{
+		PackageName: "runner",
+		RepoName:    "Linux kernel",
+		PreludeCode: `//go:generate ` + goSh + ` run ` + thisFile + ` ../../../../../../../third_party/kernel/v5.4/arch/x86/include/asm/intel-family.h generated_intel_family.go
+//go:generate ` + goSh + ` fmt generated_intel_family.go`,
+		CopyrightYear:  2020,
+		MainGoFilePath: thisFile,
+
+		Groups: []genutil.GroupSpec{
+			{Prefix: "INTEL_FAM6_", TypeName: "", Desc: "Intel family"},
+		},
+
+		LineParser: func() genutil.LineParser {
+			// Reads inputFile, a kernel input-event-codes.h. Looking for lines like:
+			//   #define EV_SYN 0x00
+			re := regexp.MustCompile(`^#define\s+([A-Z][_A-Z0-9]+)\s+(0x[0-9a-fA-F]+|\d+)`)
+			return func(line string) (name, sval string, ok bool) {
+				m := re.FindStringSubmatch(line)
+				if m == nil {
+					return "", "", false
+				}
+				return m[1], m[2], true
+			}
+		}(),
+	}
+
+	if err := genutil.GenerateConstants(inputFile, outputFile, params); err != nil {
+		log.Fatalf("Failed to generate %v: %v", outputFile, err)
+	}
+}

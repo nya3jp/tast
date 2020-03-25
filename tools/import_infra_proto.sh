4a2337dbab8c0b7489a5fcd7185ef22ab9e08807
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-03-25 17:36:46 +0900

    tast: import .proto files for test metadata.
    BUG=chromium:1063762
    TEST=ran import_infra_prot.sh
    
    Change-Id: I9c3d573a922023190d7870c4b7163105b6c9bb87

--- tools/import_infra_proto.sh
+++ tools/import_infra_proto.sh
@@ -13,30 +13,44 @@
 # its go-plugin compatibility.
 
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-  echo "Usage: $(basename "$0") [COMMIT_HASH]"
+  echo "Usage: $(basename "$0")" \
+      "[COMMIT_HASH_FOR_INFRA] [COMMIT_HASH_FOR_CONFIG]"
   exit 1
 fi
 
 # Directories in chromiumos/infra/proto/src to be imported to Tast.
 # TODO(crbug.com/950346): Add labs dir when lab peripherals are supported.
-import_dirs=("device")
+infra_import_dirs=("device" "test" "test_platform/common" "lab" "manufacturing")
+
+# Directories in src/config/proto to be imported to Tast.
+config_import_dirs=("api")
 
 tast_dir="$(dirname $0)/.."
 infra_proto_dir="${tast_dir}/../../../chromite/infra/proto"
+config_proto_dir="${tast_dir}/../../../src/config/proto"
 
 # Use HEAD commit symlink by default.
 arg=$1
-if [[ -z "$arg" ]]; then
+if [[ -z "${arg}" ]]; then
   arg="HEAD"
 fi
-commit=`git -C "${infra_proto_dir}" rev-parse --verify $arg`
+infra_commit=$(git -C "${infra_proto_dir}" rev-parse --verify "${arg}")
+arg=$2
+if [[ -z "${arg}" ]]; then
+  arg="HEAD"
+fi
+config_commit=$(git -C "${config_proto_dir}" rev-parse --verify "${arg}")
 
 # Take the snapshot of proto files.
 mkdir -p "${tast_dir}/proto"
 rm -rf "${tast_dir}/proto/infra"
 git -C "${infra_proto_dir}" archive --prefix=infra/ --format=tar \
-  "${commit}":src "${import_dirs[@]}" | \
+  "${infra_commit}":src "${infra_import_dirs[@]}" | \
   tar x --exclude=OWNERS --directory="${tast_dir}/proto"
+
+git -C "${config_proto_dir}" archive --prefix=config/ --format=tar \
+  "${config_commit}": "${config_import_dirs[@]}" | \
+  tar x --exclude=OWNERS --directory="${tast_dir}/proto" || exit 1
 
 # Create README file containing the commit hash.
 cat - > "${tast_dir}/proto/infra/README"  << EOF
@@ -46,12 +60,37 @@ cat - > "${tast_dir}/proto/infra/README"  << EOF
 
 This directory contains the snapshot of infra protofiles.
 The commit hash is ${commit}.
+
+This directory contains the snapshot of config protofiles.
+The commit hash is ${config_commit}.
 EOF
 
 # Generate go proto bindings.
 rm -rf "${tast_dir}/src/go.chromium.org/chromiumos/infra/proto"
-proto_files=(`find "${tast_dir}"/proto/infra/ -name '*.proto'`)
-protoc \
-  --go_out="${tast_dir}/src" \
-  --proto_path="${tast_dir}/proto/infra/" \
-  "${proto_files[@]}"
+for dir in "${infra_import_dirs[@]}"; do
+  # Compile each directory separately because they contain different go
+  # package names.
+  find "${tast_dir}/proto/infra/${dir}" -type d -print0 |
+  while IFS= read -r -d '' subdir; do
+    mapfile -t proto_files < <(find "${subdir}" -name '*.proto' -maxdepth 1)
+    if [ "${#proto_files[@]}" == 0 ]; then
+      continue
+    fi
+    protoc \
+      --go_out="${tast_dir}/src" \
+      --proto_path="${tast_dir}/proto/infra/" \
+      --proto_path="${tast_dir}/proto/config/" \
+      "${proto_files[@]}"
+  done
+done
+
+rm -rf "${tast_dir}/src/go.chromium.org/chromiumos/config/go"
+for dir in "${config_import_dirs[@]}"; do
+  find "${tast_dir}/proto/config/${dir}" -name '*.proto' -print0 |
+  while IFS= read -r -d '' proto_file; do
+    protoc \
+      --go_out="${tast_dir}/src" \
+      --proto_path="${tast_dir}/proto/config/" \
+      "${proto_file}"
+  done
+done

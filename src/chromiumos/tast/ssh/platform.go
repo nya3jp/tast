c37bfc54cc023fd871651933cb83f2e956ed4b33
Keigo Oka <oka@chromium.org>
2020-03-31 12:16:22 +0900

    tast: remove host package moving implementation to ssh package
    BUG=chromium:1019537
    TEST=fast_build.sh -T
    
    Change-Id: I3dbf551bd1c63ca64801de8c31e390b21df02387

--- src/chromiumos/tast/ssh/platform.go
+++ src/chromiumos/tast/ssh/platform.go
@@ -4,10 +4,30 @@
 
 package ssh
 
-import "chromiumos/tast/internal/host"
+import (
+	"fmt"
+
+	"chromiumos/tast/shutil"
+)
 
 // Platform defines platform-specific behaviours for SSH connections.
-type Platform = host.Platform
+type Platform struct {
+	// BuildShellCommand builds the shell command required to execute the given command
+	// in the given directory on the target platform. args[0] is the name of the command
+	// to be executed. If dir is empty (""), use the default working directory.
+	BuildShellCommand func(dir string, args []string) string
+}
+
+// shellCmd builds a shell command string to execute a process with exec.
+func shellCmd(dir string, args []string) string {
+	cmd := "exec " + shutil.EscapeSlice(args)
+	if dir != "" {
+		// Return 125 (chosen arbitrarily) if dir does not exist.
+		// TODO(nya): Consider handling the directory error more gracefully.
+		cmd = fmt.Sprintf("cd %s > /dev/null 2>&1 || exit 125; %s", shutil.Escape(dir), cmd)
+	}
+	return cmd
+}
 
 // DefaultPlatform represents a system with a generic POSIX shell.
-var DefaultPlatform = host.DefaultPlatform
+var DefaultPlatform = &Platform{BuildShellCommand: shellCmd}

1aadf966701444e933c3de6e2bca3b516957e0d5
Keigo Oka <oka@chromium.org>
2020-04-01 14:23:47 +0900

    tast-lint: init shouldn't contain statements other than AddTest
    BUG=chromium:1066753
    TEST=run linter for all the Go files in tast-tests*.
    Confirmed everything except for crostini/disk_io_perf.go passes
    the newly added check.
    
    Change-Id: Icffe4580210436fea43b0666999c4da22572f747

--- src/chromiumos/tast/cmd/tast-lint/check/declarations_test.go
+++ src/chromiumos/tast/cmd/tast-lint/check/declarations_test.go
@@ -13,11 +13,19 @@ const declTestPath = "src/chromiumos/tast/local/bundles/cros/example/do_stuff.go
 func TestDeclarationsPass(t *testing.T) {
 	const code = `package pkg
 func init() {
+	// Comments are allowed.
 	testing.AddTest(&testing.Test{
 		Func:     DoStuff,
 		Desc:     "This description is fine",
 		Contacts: []string{"me@chromium.org"},
 	})
+}
+
+var x SomeComplexStruct
+
+// init without AddTest should be allowed.
+func init() {
+	x = f()
 }
 `
 	f, fs := parse(code, declTestPath)
@@ -25,22 +33,23 @@ func init() {
 	verifyIssues(t, issues, nil)
 }
 
-func TestDeclarationsTopLevelAddTest(t *testing.T) {
+func TestDeclarationsOtherStmtThanAddTest(t *testing.T) {
 	const code = `package pkg
 func init() {
-	for {
-		testing.AddTest(&testing.Test{
-			Func: DoStuff,
-		})
-	}
-	testing.AddTest(ts)
+	_ = 1
+	testing.AddTest(&testing.Test{
+		Func:     DoStuff,
+		Desc:     "This description is fine",
+		Contacts: []string{"me@chromium.org"},
+	})
+	for {}
 }
 `
 	f, fs := parse(code, declTestPath)
 	issues := Declarations(fs, f, false)
 	expects := []string{
-		declTestPath + ":4:3: " + notTopAddTestMsg,
-		declTestPath + ":8:18: " + addTestArgLitMsg,
+		declTestPath + ":3:2: " + otherStmtMsg,
+		declTestPath + ":9:2: " + otherStmtMsg,
 	}
 	verifyIssues(t, issues, expects)
 }

8b9e20e03a649d24443e27b93a6fc974b7d41e2f
Keigo Oka <oka@chromium.org>
2020-04-01 18:36:39 +0900

    tast: refactor ssh tests
    Export TestData in ssh package and use it from linuxssh
    tests.
    We cannot move ssh.TestData to sshtest, because that creates
    cyclic deps ssh -> sshtest -> ssh.
    
    BUG=chromium:1019537
    TEST=fast_build.sh -T
    
    Change-Id: I22bc99126d1b5e680c0c6206040d740d746d00e4

--- src/chromiumos/tast/ssh/conn_test.go
+++ src/chromiumos/tast/ssh/conn_test.go
@@ -6,141 +6,14 @@ package ssh
 
 import (
 	"context"
-	"crypto/rsa"
-	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 
 	"chromiumos/tast/internal/sshtest"
 	"chromiumos/tast/testutil"
 )
-
-var userKey, hostKey *rsa.PrivateKey
-
-func init() {
-	userKey, hostKey = sshtest.MustGenerateKeys()
-}
-
-// connectToServer establishes a connection to srv using key.
-// base is used as a base set of options.
-func connectToServer(ctx context.Context, srv *sshtest.SSHServer, key *rsa.PrivateKey, base *Options) (*Conn, error) {
-	keyFile, err := sshtest.WriteKey(key)
-	if err != nil {
-		return nil, err
-	}
-	defer os.Remove(keyFile)
-
-	o := *base
-	o.KeyFile = keyFile
-	if err = ParseTarget(srv.Addr().String(), &o); err != nil {
-		return nil, err
-	}
-	s, err := New(ctx, &o)
-	if err != nil {
-		return nil, err
-	}
-	return s, nil
-}
-
-// timeoutType describes different types of timeouts that can be simulated during SSH "exec" requests.
-type timeoutType int
-
-const (
-	// noTimeout indicates that testData.ctx shouldn't be canceled.
-	noTimeout timeoutType = iota
-	// startTimeout indicates that testData.ctx should be canceled before the command starts.
-	startTimeout
-	// endTimeout indicates that testData.ctx should be canceled after the command runs but before its status is returned.
-	endTimeout
-)
-
-// testData wraps data common to all tests.
-type testData struct {
-	srv *sshtest.SSHServer
-	hst *Conn
-
-	ctx    context.Context // used for performing operations using hst
-	cancel func()          // cancels ctx to simulate a timeout
-
-	nextCmd     string      // next command to be executed by client
-	execTimeout timeoutType // how "exec" requests should time out
-}
-
-func newTestData(t *testing.T) *testData {
-	td := &testData{}
-	td.ctx, td.cancel = context.WithCancel(context.Background())
-
-	var err error
-	if td.srv, err = sshtest.NewSSHServer(&userKey.PublicKey, hostKey, td.handleExec); err != nil {
-		t.Fatal(err)
-	}
-
-	if td.hst, err = connectToServer(td.ctx, td.srv, userKey, &Options{}); err != nil {
-		td.srv.Close()
-		t.Fatal(err)
-	}
-	td.hst.AnnounceCmd = func(cmd string) { td.nextCmd = cmd }
-
-	// Automatically abort the test if it takes too long time.
-	go func() {
-		const timeout = 10 * time.Second
-		select {
-		case <-td.ctx.Done():
-			return
-		case <-time.After(timeout):
-		}
-		t.Errorf("Test blocked for %v", timeout)
-		td.cancel()
-	}()
-
-	return td
-}
-
-func (td *testData) close() {
-	td.srv.Close()
-	td.hst.Close(td.ctx)
-	td.cancel()
-}
-
-// handleExec handles an SSH "exec" request sent to td.srv by executing the requested command.
-// The command must already be present in td.nextCmd.
-func (td *testData) handleExec(req *sshtest.ExecReq) {
-	if req.Cmd != td.nextCmd {
-		log.Printf("Unexpected command %q (want %q)", req.Cmd, td.nextCmd)
-		req.Start(false)
-		return
-	}
-
-	// PutFiles sends multiple "exec" requests.
-	// Ignore its initial "sha1sum" so we can hang during the tar command instead.
-	ignoreTimeout := strings.HasPrefix(req.Cmd, "sha1sum ")
-
-	// If a timeout was requested, cancel the context and then sleep for an arbitrary-but-long
-	// amount of time to make sure that the client sees the expired context before the command
-	// actually runs.
-	if td.execTimeout == startTimeout && !ignoreTimeout {
-		td.cancel()
-		time.Sleep(time.Minute)
-	}
-	req.Start(true)
-
-	var status int
-	switch req.Cmd {
-	case shellCmd("", []string{"long_sleep"}):
-		time.Sleep(time.Hour)
-	default:
-		status = req.RunRealCmd()
-	}
-
-	if td.execTimeout == endTimeout && !ignoreTimeout {
-		td.cancel()
-		time.Sleep(time.Minute)
-	}
-	req.End(status)
-}
 
 func TestRetry(t *testing.T) {
 	t.Parallel()
@@ -169,22 +42,22 @@ func TestRetry(t *testing.T) {
 
 func TestPing(t *testing.T) {
 	t.Parallel()
-	td := newTestData(t)
-	defer td.close()
+	td := NewTestData(t)
+	defer td.Close()
 
 	td.srv.AnswerPings(true)
-	if err := td.hst.Ping(td.ctx, time.Minute); err != nil {
+	if err := td.Hst.Ping(td.Ctx, time.Minute); err != nil {
 		t.Errorf("Got error when pinging host: %v", err)
 	}
 
 	td.srv.AnswerPings(false)
-	if err := td.hst.Ping(td.ctx, time.Millisecond); err == nil {
+	if err := td.Hst.Ping(td.Ctx, time.Millisecond); err == nil {
 		t.Errorf("Didn't get expected error when pinging host with short timeout")
 	}
 
 	// Cancel the context to simulate it having expired.
 	td.cancel()
-	if err := td.hst.Ping(td.ctx, time.Minute); err == nil {
+	if err := td.Hst.Ping(td.Ctx, time.Minute); err == nil {
 		t.Errorf("Didn't get expected error when pinging host with expired context")
 	}
 }
---
+++ src/chromiumos/tast/ssh/testutil.go
@@ -0,0 +1,149 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ssh
+
+import (
+	"context"
+	"crypto/rsa"
+	"log"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"chromiumos/tast/internal/sshtest"
+)
+
+var userKey, hostKey *rsa.PrivateKey
+
+func init() {
+	userKey, hostKey = sshtest.MustGenerateKeys()
+}
+
+// connectToServer establishes a connection to srv using key.
+// base is used as a base set of options.
+func connectToServer(ctx context.Context, srv *sshtest.SSHServer, key *rsa.PrivateKey, base *Options) (*Conn, error) {
+	keyFile, err := sshtest.WriteKey(key)
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(keyFile)
+
+	o := *base
+	o.KeyFile = keyFile
+	if err = ParseTarget(srv.Addr().String(), &o); err != nil {
+		return nil, err
+	}
+	s, err := New(ctx, &o)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
+
+// TimeoutType describes different types of timeouts that can be simulated during SSH "exec" requests.
+// TODO(oka): rename this type and constants to indicate they are for test only.
+type TimeoutType int
+
+const (
+	// NoTimeout indicates that TestData.Ctx shouldn't be canceled.
+	NoTimeout TimeoutType = iota
+	// StartTimeout indicates that TestData.Ctx should be canceled before the command starts.
+	StartTimeout
+	// EndTimeout indicates that TestData.Ctx should be canceled after the command runs but before its status is returned.
+	EndTimeout
+)
+
+// TestData wraps data common to all tests.
+type TestData struct {
+	srv *sshtest.SSHServer // local SSH server
+	// Hst is a connection to srv.
+	Hst *Conn
+
+	// Ctx is used for performaing operations using Hst.
+	Ctx    context.Context
+	cancel func() // cancels Ctx to simulate a timeout
+
+	nextCmd string // next command to be executed by client
+	// ExecTimeout directs how "exec" requests should time out.
+	ExecTimeout TimeoutType
+}
+
+// NewTestData sets up local SSH server and connection to it, and
+// returns them together as a testData struct.
+// Caller must call Close after use.
+func NewTestData(t *testing.T) *TestData {
+	td := &TestData{}
+	td.Ctx, td.cancel = context.WithCancel(context.Background())
+
+	var err error
+	if td.srv, err = sshtest.NewSSHServer(&userKey.PublicKey, hostKey, td.handleExec); err != nil {
+		t.Fatal(err)
+	}
+
+	if td.Hst, err = connectToServer(td.Ctx, td.srv, userKey, &Options{}); err != nil {
+		td.srv.Close()
+		t.Fatal(err)
+	}
+	td.Hst.AnnounceCmd = func(cmd string) { td.nextCmd = cmd }
+
+	// Automatically abort the test if it takes too long time.
+	go func() {
+		const timeout = 10 * time.Second
+		select {
+		case <-td.Ctx.Done():
+			return
+		case <-time.After(timeout):
+		}
+		t.Errorf("Test blocked for %v", timeout)
+		td.cancel()
+	}()
+
+	return td
+}
+
+// Close releases resources associated with td.
+func (td *TestData) Close() {
+	td.srv.Close()
+	td.Hst.Close(td.Ctx)
+	td.cancel()
+}
+
+// handleExec handles an SSH "exec" request sent to td.srv by executing the requested command.
+// The command must already be present in td.nextCmd.
+func (td *TestData) handleExec(req *sshtest.ExecReq) {
+	if req.Cmd != td.nextCmd {
+		log.Printf("Unexpected command %q (want %q)", req.Cmd, td.nextCmd)
+		req.Start(false)
+		return
+	}
+
+	// PutFiles sends multiple "exec" requests.
+	// Ignore its initial "sha1sum" so we can hang during the tar command instead.
+	ignoreTimeout := strings.HasPrefix(req.Cmd, "sha1sum ")
+
+	// If a timeout was requested, cancel the context and then sleep for an arbitrary-but-long
+	// amount of time to make sure that the client sees the expired context before the command
+	// actually runs.
+	if td.ExecTimeout == StartTimeout && !ignoreTimeout {
+		td.cancel()
+		time.Sleep(time.Minute)
+	}
+	req.Start(true)
+
+	var status int
+	switch req.Cmd {
+	case shellCmd("", []string{"long_sleep"}):
+		time.Sleep(time.Hour)
+	default:
+		status = req.RunRealCmd()
+	}
+
+	if td.ExecTimeout == EndTimeout && !ignoreTimeout {
+		td.cancel()
+		time.Sleep(time.Minute)
+	}
+	req.End(status)
+}

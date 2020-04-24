3f5f37ec009f971515aeb9c6c4b2081081b3db2d
Shuhei Takahashi <nya@chromium.org>
2020-04-24 02:01:33 +0000

    tast: Add software features about P2P stability.
    platform.P2PServer fails on Linux kernel 5.4+. Until we figure out
    the cause, mark the test informational with these software features.
    
    BUG=b:153182041
    TEST=tast run DUT platform.P2PServer
    
    Change-Id: Ica08214e5a338d112b2c722e0c3ff0ac89b30105

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -125,8 +125,10 @@ func main() {
 			"no_symlink_mount":       "!lxc", // boards using LXC set CONFIG_SECURITY_CHROMIUMOS_NO_SYMLINK_MOUNT=n
 			"no_ubsan":               "!ubsan",
 			"oci":                    "containers && !moblab", // run_oci doesn't work on Moblab: https://crbug.com/951691
-			"plugin_vm":              "pita && atlas",         // boards that can run Plugin VM, and are licensed to do so in the lab environment.
-			"pstore":                 "!betty && !tast_vm",    // These boards don't support pstore: https://crbug.com/971899
+			"p2p_stable":             `"kernel-3_8" || "kernel-3_10" || "kernel-3_14" || "kernel-3_18" || "kernel-4_4" || "kernel-4_14 || "kernel-4_19"`,
+			"p2p_unstable":           `!("kernel-3_8" || "kernel-3_10" || "kernel-3_14" || "kernel-3_18" || "kernel-4_4" || "kernel-4_14 || "kernel-4_19")`,
+			"plugin_vm":              "pita && atlas",      // boards that can run Plugin VM, and are licensed to do so in the lab environment.
+			"pstore":                 "!betty && !tast_vm", // These boards don't support pstore: https://crbug.com/971899
 			"qemu":                   "betty || tast_vm",
 			"reboot":                 "!betty && !tast_vm",           // weird missing-runner-after-reboot bug: https://crbug.com/909955
 			"screenshot":             "display_backlight && !rk3399", // screenshot command broken on RK3399: https://crbug.com/880597

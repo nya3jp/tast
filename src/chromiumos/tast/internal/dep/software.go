558f91d63bab330e7033d986e157038720128aec
Shuhei Takahashi <nya@chromium.org>
2020-05-27 13:09:42 +0900

    tast: Move dependency check logic to the dep package.
    This is a part of the patch series to do software/hardware
    dependency checks in a consistent manner.
    
    This patch moves the dependency check logic from ShouldRun to
    dep.Check. Two new types, dep.Deps and dep.SoftwareDeps, are
    introduced to help ShouldRun to delegate checks to dep.Check.
    
    This is a refactoring change; no functional change is expected.
    
    BUG=chromium:1077090
    TEST=fast_build.sh -T
    
    Change-Id: I15fbfdc3a0a8b3209ee239f95f482e37d06f90d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2214841
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/internal/dep/software.go
+++ src/chromiumos/tast/internal/dep/software.go
@@ -4,6 +4,10 @@
 
 package dep
 
+import (
+	"sort"
+)
+
 // SoftwareFeatures contains information about software features of the DUT.
 type SoftwareFeatures struct {
 	// Available contains a list of software features supported by the DUT.
@@ -11,4 +15,32 @@ type SoftwareFeatures struct {
 
 	// Unavailable contains a list of software features not supported by the DUT.
 	Unavailable []string
+}
+
+// SoftwareDeps represents dependencies to software features.
+type SoftwareDeps = []string
+
+// missingSoftwareDeps returns a sorted list of dependencies from SoftwareDeps
+// that aren't present on the DUT (per the passed-in features list).
+// unknown is a sorted list of unknown software features. It is always a subset
+// of missing.
+func missingSoftwareDeps(deps SoftwareDeps, features *SoftwareFeatures) (missing, unknown []string) {
+DepLoop:
+	for _, d := range deps {
+		for _, f := range features.Available {
+			if d == f {
+				continue DepLoop
+			}
+		}
+		missing = append(missing, d)
+		for _, f := range features.Unavailable {
+			if d == f {
+				continue DepLoop
+			}
+		}
+		unknown = append(unknown, d)
+	}
+	sort.Strings(missing)
+	sort.Strings(unknown)
+	return missing, unknown
 }

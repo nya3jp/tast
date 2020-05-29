0d7532987f05c69b17240a3676fc6b044748ebac
Shuhei Takahashi <nya@chromium.org>
2020-05-29 16:38:12 +0900

    bundle: Abort execution if a test ignores its timeout.
    Today, if a test main function does not return soon after the context
    cancellation by the test timeout, Tast just lets the goroutine running
    and continue to the next test.
    
    We found that this is a problematic behavior because an abandoned
    goroutine may interact the next test badly, e.g. calling chrome.Close
    concurrently with chrome.New.
    
    Rather than continuing, we should abort the bundle execution when a test
    does not return. Fortunately the tast command has logic to restart test
    execution when runners/bundles exit before all tests are run, so we can
    continue from the next test.
    
    BUG=chromium:1087758
    TEST=fast_build.sh -T
    TEST=tast run DUT example.Block example.Pass
    
    Change-Id: I51b62d0a5a606dff7ca079cf467ea20bd1d9bb62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2217983
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/bundle/bundle.go
+++ src/chromiumos/tast/bundle/bundle.go
@@ -288,7 +288,9 @@ func runTests(ctx context.Context, stdout io.Writer, args *Args, cfg *runConfig,
 	}
 	for i, t := range tests {
 		if checkResult := checkResults[i]; checkResult.OK() {
-			runTest(ctx, ew, args, cfg, t, nextTests[i], rd)
+			if err := runTest(ctx, ew, args, cfg, t, nextTests[i], rd); err != nil {
+				return command.NewStatusErrorf(statusError, "run failed: %v", err)
+			}
 		} else {
 			reportSkippedTest(ew, t, checkResult)
 		}
@@ -327,7 +329,7 @@ func connectToTarget(ctx context.Context, args *Args) (_ *dut.DUT, retErr error)
 
 // runTest runs t per args and cfg, writing appropriate messages to ew.
 func runTest(ctx context.Context, ew *eventWriter, args *Args, cfg *runConfig,
-	t, next *testing.TestInstance, rd *testing.RemoteData) {
+	t, next *testing.TestInstance, rd *testing.RemoteData) error {
 	ew.TestStart(t)
 
 	// Attach a log that the test can use to report timing events.
@@ -355,13 +357,19 @@ func runTest(ctx context.Context, ew *eventWriter, args *Args, cfg *runConfig,
 		copierDone <- true
 	}()
 
-	if !t.Run(ctx, ch, &testCfg) {
+	ok := t.Run(ctx, ch, &testCfg)
+	if !ok {
 		// If Run reported that the test didn't finish, tell the copier to abort.
 		abortCopier <- true
 	}
 	<-copierDone
 
 	ew.TestEnd(t, nil, timingLog)
+
+	if !ok {
+		return errors.New("test did not return on timeout")
+	}
+	return nil
 }
 
 // reportSkippedTest is called instead of runTest for a test that is skipped due to
@@ -396,7 +404,7 @@ func copyTestOutput(ch <-chan testing.Output, ew *eventWriter, abort <-chan bool
 				ew.TestLog(o.T, o.Msg)
 			}
 		case <-abort:
-			const msg = "Test timed out (see log for goroutine dump)"
+			const msg = "Test did not return on timeout (see log for goroutine dump)"
 			ew.TestError(time.Now(), testing.NewError(nil, msg, msg, 0))
 			dumpGoroutines(ew)
 			return

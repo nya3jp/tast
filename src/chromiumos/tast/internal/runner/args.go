0d16119a06f84963624f3adb194cd7335ea2a2b0
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-06-18 02:06:25 +0000

    tast: Store touchscreen information to newer protobuf.
    The message types mirrored from go.chromium.org/chromiumos/infra/proto/
    is deprecated.
    Use schemas in go.chromium.org/chromiumos/config/ instead.
    This is the first change that imports the message type and stores only
    the touchscreen information.
    
    BUG=chromium:1094802
    TEST=fast_build.sh -T
    
    Change-Id: Iae520394d8e60d5299c332f1d86d1e0cf06b9915

--- src/chromiumos/tast/internal/runner/args.go
+++ src/chromiumos/tast/internal/runner/args.go
@@ -14,6 +14,7 @@ import (
 	"path/filepath"
 
 	"github.com/golang/protobuf/proto"
+	metadatapb "go.chromium.org/chromiumos/config/go/api/test/metadata/v1"
 	"go.chromium.org/chromiumos/infra/proto/go/device"
 
 	"chromiumos/tast/autocaps"
@@ -216,7 +217,8 @@ type GetDUTInfoResult struct {
 	// DeviceConfig contains the DUT's device characteristic.
 	// Similar to SoftwareFeatures field, the serialization/deserialization
 	// of this field are handled in MarshalJSON/UnmarshalJSON respectively.
-	DeviceConfig *device.Config `json:"-"`
+	DeviceConfig *device.Config                      `json:"-"`
+	DUT          *metadatapb.DUTConfigConstraint_DUT `json:"-"`
 
 	// Warnings contains descriptions of non-fatal errors encountered while determining features.
 	Warnings []string `json:"warnings,omitempty"`

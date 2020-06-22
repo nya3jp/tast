b1d6fa9b8f7423aa30772d3ceeaa6a20f92ed401
Shuhei Takahashi <nya@chromium.org>
2020-06-22 14:16:07 +0900

    tast: Move newDevserverClient to the devserver package.
    To prepare for moving the external data download to test bundles,
    move the logic to construct a new devserver client to the devserver
    package.
    
    This is a pure refactoring; no functional change is needed.
    
    BUG=chromium:965703
    TEST=fast_build.sh -T
    
    Change-Id: Ic4143e2660edc49c62e01dbd255b139043656c72

---
+++ src/chromiumos/tast/internal/devserver/devserver.go
@@ -0,0 +1,29 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package devserver
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/internal/logging"
+)
+
+// NewClient creates a Client from a list of devservers. If the list is empty,
+// PseudoClient is returned. Otherwise RealClient is returned.
+func NewClient(ctx context.Context, devservers []string) Client {
+	if len(devservers) == 0 {
+		logging.ContextLog(ctx, "Devserver status: using pseudo client")
+		return NewPseudoClient(nil)
+	}
+
+	const timeout = 3 * time.Second
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	cl := NewRealClient(ctx, devservers, nil)
+	logging.ContextLogf(ctx, "Devserver status: %s", cl.Status())
+	return cl
+}

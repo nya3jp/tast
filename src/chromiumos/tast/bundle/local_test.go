d727223ed9a6b617f3a2a2e9996766092f2ce3c9
Shuhei Takahashi <nya@chromium.org>
2020-07-08 15:02:39 +0900

    tast: Introduce testing.TestHookState.
    We want to provide a few more methods to test hooks, while we want to
    hide some State-specific methods (Param, PreValue, Run). Thus it is
    high time to introduce a new State type for test hooks.
    
    This is an API incompatible change. No functional change expected.
    
    BUG=chromium:965703
    BUG=chromium:1103118
    TEST=fast_build.sh -T
    
    Cq-Depend: chromium:2286973
    Change-Id: Ia97839d0d775b9a2147542f0147e3d5c601d7c57

--- src/chromiumos/tast/bundle/local_test.go
+++ src/chromiumos/tast/bundle/local_test.go
@@ -149,9 +149,9 @@ func TestLocalPreTestRun(t *gotesting.T) {
 	stderr := bytes.Buffer{}
 	var ranPre, ranPostHook bool
 	if status := Local(nil, stdin, &bytes.Buffer{}, &stderr, LocalDelegate{
-		TestHook: func(context.Context, *testing.State) func(context.Context, *testing.State) {
+		TestHook: func(context.Context, *testing.TestHookState) func(context.Context, *testing.TestHookState) {
 			ranPre = true
-			return func(context.Context, *testing.State) {
+			return func(context.Context, *testing.TestHookState) {
 				ranPostHook = true
 			}
 		},

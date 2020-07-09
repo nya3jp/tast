bb75b6a5feca28e93a1319e6e064503c24ad0eba
Yoshiki Iguchi <yoshiki@google.com>
2020-07-09 19:48:42 +0900

    tast: replace journalctl with croslog
    This CL removes direct use of journalctl and uses croslog instead.
    As for now, croslog works as a wrapper of journalctl and will read
    plaintext logs eventually.
    
    BUG=chromium:1102718
    TEST=ran 'tast run DUT '("group:mainline" && !informational && !disabled)`
    TEST=ran `~/trunk/src/platform/tast/fast_build.sh -T`
    
    Cq-Depend: chromium:2173929
    Change-Id: Ic81fdb0c70a84a67c2bf9287335a3e589a8600d5

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -42,7 +42,7 @@ func main() {
 		KillStaleRunners:  true,
 		SystemLogDir:      "/var/log",
 		SystemLogExcludes: []string{"journal"}, // journald binary logs: https://crbug.com/931951
-		JournaldSubdir:    "journal",           // destination for exported journald logs
+		UnifiedLogSubdir:  "unified",           // destination for exported unified system logs
 		SystemInfoFunc:    writeSystemInfo,     // save additional system info at end of run
 		SystemCrashDirs:   crash.DefaultDirs(),
 		// The tast-use-flags package attempts to install this file to /etc,

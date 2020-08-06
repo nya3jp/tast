013ea1a4271b5428fba7d15e5111be3984c2495f
Keigo Oka <oka@chromium.org>
2020-08-06 23:14:28 +0900

    Add FixtureService API
    BUG=chromium:1066439
    TEST=fast_build.sh -T
    
    Change-Id: I514a1417767c8347c0e9705dca2081f13f5ea2c9

---
+++ src/chromiumos/tast/internal/bundle/docs.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bundle contains internal code that corresponds to the public bundle package.
+package bundle

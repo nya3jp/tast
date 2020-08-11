25a7b89ba13db41735f87cded32b14c61c89cc97
Seewai Fu <seewaifu@google.com>
2020-08-10 17:26:29 -0700

    tast: Support Hostname and Port in .ssh/config (WIP)
    Support Hostname and Port in .ssh/config. Not all features in ssh
    configuration is supported. Please see spec in
    https://docs.google.com/document/d/18ZlpEsTIWLAT8m14DFlL4Ru4gQYMe7qRv0VTO5knLuc/preview#heading=h.iaf7zjjfoarw
    Still work in process pending approval of the spec
    Manuel Test:
    tast -verbose run -build=false  0:0:0:0:0:ffff:7f00:1 example.ReconnectToDUT ui.ChromeLogin
    tast -verbose run -build=true  [0:0:0:0:0:ffff:7f00:1]:2222 example.ReconnectToDUT ui.ChromeLogin
    tast -verbose run -build=false  octopus example.ReconnectToDUT ui.ChromeLogin
    tast -verbose run -build=false 127.0.0.1:2222 example.ReconnectToDUT ui.ChromeLogin
    Manuel Test Config File Used:
    Host 0:0:0:0:0:ffff:7f00:1
        Include ~/.ssh/custom_ssh_d/octopus_port_config
    Host "!octopu*" o*
       HostName %h.c.googlers.com
    Host "octopus"
        HostName 127.0.0.1
        Include custom_ssh_d/octopus_host_config
        # Include ~/test/bad.conf
        Include ~seewaifu/.ssh/custom_ssh_d/octopus_port_config
    Host hana
        Include ~/.ssh/custom_ssh_d/hana_config
        Include custom_ssh_d/root_config
        HostName 127.0.0.1
        Port 2223
    Host *
        Include custom_ssh_d/my_config
    Host *
        Port 2222
        HostName 127.0.0.1
    Host badone
        Port 2226
    Host *
       User root
    BUG=chromium:944280
    TEST=unit test (fast_build.sh -T) and 2 manual tests on DUT
    
    Change-Id: Ic1bac2dc271c52c82b4664369682fe956d0c8dde

---
+++ src/chromiumos/tast/internal/sshconfig/sshconfig_test.go
@@ -0,0 +1,208 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package sshconfig
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"chromiumos/tast/testutil"
+)
+
+// TestGetRealHostFromFiles call all unit tests for ssh configuration files support.
+func TestGetRealHostFromFiles(t *testing.T) {
+	testGetRealHostFromFiles(t)
+	testGetRealHostFromFilesLoop(t)
+}
+
+// testGetRealHostFromFiles tests ResolveHostFromFiles to read configuration successfully.
+/*
+ ** config file for testing
+
+    # Rule 0
+    Host 0:0:0:0:0:ffff:7f00:1
+		Port 2222
+
+    # Rule 1
+    Host "octopus"
+        HostName 127.0.0.1
+        Port 2222
+
+    # Rule 2
+    Host hana
+        HostName 127.0.0.1
+        Port 2223
+
+    # Rule 3
+    Host test*
+        HostName %h.google.com
+        Port 2224
+
+    # Rule 4
+    Host !*google.com  eve*
+        Port 2225
+        HostName 127.0.0.1
+
+    # Rule 5
+    Host hello*
+		user goodday
+		# Rules 5.* are Include Contents
+		# Rule 5.1
+		Host *google.com
+			HostName %h
+			Port 2226
+		# Rule 5.2
+		Host "!*googler.com" *.*
+			HostName %h
+			Port 2227
+		# Rule 5.3
+		Host !"*googler.com" *
+			HostName %h.c.googler.com
+		# Rule 5.4
+		Host *
+			Port 2228
+
+	# Line to be ignored
+	Match host=eve.google.com
+		Port 2229
+
+	# Rule 6
+	Host *zone
+		HostName fe00::1ff:ffff:7f00:1%%%h
+		Port 2230
+
+    # Rule 7
+    Host !*:" *
+        Port 22
+
+*/
+func testGetRealHostFromFiles(t *testing.T) {
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+	configPath := filepath.Join(td, "config")
+	includePath1 := filepath.Join(td, "octopus_config.d")
+	includePath2 := filepath.Join(td, "port_config.d")
+	includePath3 := filepath.Join(td, "hello_config.d")
+	configPathOther := filepath.Join(td, "config_other")
+
+	configContent1 := `Include %v
+		Host hana
+		    HostName 127.0.0.1
+		    Port 2223
+		Host test*
+		    HostName %%h.google.com
+		    Port 2224
+		Host !*google.com eve*
+		    Include %v
+		    HostName 127.0.0.1
+		Host hello*
+		    user goodday
+		    Include %v
+		`
+	configContent1 = fmt.Sprintf(configContent1, includePath1, includePath2, includePath3)
+	includePathContent1 := `
+		User TestForOutOfScope
+		Host 0:0:0:0:0:ffff:7f00:1
+    		Port 2222
+		Host "octopus"
+			HostName 127.0.0.1
+			Port 2222
+		`
+	includePathContent2 := "Port 2225\n"
+
+	configContent2 := `
+		Host *zone
+			HostName fe00::1ff:ffff:7f00:1%%%h 
+			Port 2230
+		Host !*:* *
+			Port 22
+	`
+	includePathContent3 := `
+		Host *google.com
+			HostName %h
+			Port 2226
+		Host "!*googler.com" *.*
+			HostName %h
+			Port 2227
+		Host !"*googler.com" *
+			HostName %h.c.googler.com
+		Host *
+			Port 2228
+	`
+	if err := testutil.WriteFiles(td, map[string]string{
+		"config":           configContent1,
+		"octopus_config.d": includePathContent1,
+		"port_config.d":    includePathContent2,
+		"hello_config.d":   includePathContent3,
+		"config_other":     configContent2,
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	type dataType struct {
+		input    string
+		expected string
+	}
+
+	inputs := []dataType{
+		{"octopus", "127.0.0.1:2222"},                                    // rule 1
+		{"hana", "127.0.0.1:2223"},                                       // rule 2
+		{"mytest.com", "mytest.com:22"},                                  // rule 7
+		{"octopus:1", "127.0.0.1:1"},                                     // rule 1 + override
+		{"hello1", "hello1.c.googler.com:2228"},                          // rule 5.3
+		{"eve.123.com", "127.0.0.1:2225"},                                // rule 4
+		{"eve.google.com", "eve.google.com:22"},                          // rule 7
+		{"mytest.google.com", "mytest.google.com:22"},                    // rule 7
+		{"127.0.0.1:2345", "127.0.0.1:2345"},                             // no rule matched
+		{"test:3", "test.google.com:3"},                                  // rule 3 + override
+		{"test1", "test1.google.com:2224"},                               // rule 3
+		{"0:0:0:0:0:ffff:7f00:1", "[0:0:0:0:0:ffff:7f00:1]:2222"},        // rule 0
+		{"[0:0:0:0:0:ffff:7f00:1]", "[0:0:0:0:0:ffff:7f00:1]:2222"},      // rule 0
+		{"[0:0:0:0:0:ffff:7f00:1]:2233", "[0:0:0:0:0:ffff:7f00:1]:2233"}, // rule 0 + override
+		{"hello1.google.com", "hello1.google.com:2226"},                  // rule 5.1
+		{"hello.c.googler.com", "hello.c.googler.com:2228"},              // rule 5.3 and 5.4
+		{"hello.mytest.com", "hello.mytest.com:2227"},                    // rule 5.2
+		{"0:0:0:0:0:ffff:7f00:2", "0:0:0:0:0:ffff:7f00:2"},               // no match
+		{"zone", "[fe00::1ff:ffff:7f00:1%zone]:2230"},                    // rule 6
+	}
+	configPaths := []string{"nonexisting_file", configPath, configPathOther}
+	for _, h := range inputs {
+		resolvedHost, err := ResolveHostFromFiles(h.input, configPaths)
+		if err != nil {
+			t.Errorf("encounter error while reading %v: %v", configPaths, err)
+		}
+		t.Logf("%v -- %v\n", h.input, resolvedHost)
+		if resolvedHost != h.expected {
+			t.Errorf("expected to get %v but get %v", h.expected, resolvedHost)
+		}
+	}
+}
+
+// testGetRealHostFromFilesLoop tests if ResolveHostFromFiles detect a loop in Include statements.
+func testGetRealHostFromFilesLoop(t *testing.T) {
+
+	td := testutil.TempDir(t)
+	defer os.RemoveAll(td)
+
+	configPath := filepath.Join(td, "config_loop")
+	configContent1 := `Include %v
+		Host hana
+			HostName 127.0.0.1
+			Port 2223
+		`
+	configContent1 = fmt.Sprintf(configContent1, configPath)
+
+	if err := testutil.WriteFiles(td, map[string]string{
+		"config_loop": configContent1,
+	}); err != nil {
+		t.Fatal(err)
+	}
+	configPaths := []string{configPath}
+	if _, err := ResolveHostFromFiles("hana", configPaths); err == nil {
+		t.Errorf("expecting a loop error in reading %v but get no error", configPath)
+	}
+}

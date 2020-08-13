27a6d786a2de529be99dbeb7e9b8c05cb8d67cba
Shuhei Takahashi <nya@chromium.org>
2020-08-13 16:04:58 +0900

    planner: Do not pass RootState to runStages.
    runStages used to call RootState.Close, but it is not the case after
    introduction of testing.OutputStream. Thus we no longer need to pass
    RootState to runStages.
    
    This is a pure refactoring change; no functional change is expected.
    
    BUG=chromium:1035940
    TEST=fast_build.sh -T
    
    Change-Id: I9213c95e97c472dfb0cc0056779b3e72a4802eda

--- src/chromiumos/tast/internal/planner/stage_test.go
+++ src/chromiumos/tast/internal/planner/stage_test.go
@@ -8,24 +8,15 @@ import (
 	"context"
 	gotesting "testing"
 	"time"
-
-	"chromiumos/tast/internal/testing"
 )
 
-type fakeOutputStream struct{}
-
-func (fakeOutputStream) Log(string) error           { return nil }
-func (fakeOutputStream) Error(*testing.Error) error { return nil }
-
 func TestRunStagesTimeout(t *gotesting.T) {
-	root := testing.NewRootState(&testing.TestInstance{}, fakeOutputStream{}, &testing.TestConfig{})
-
 	cont := make(chan struct{}, 2)        // used to signal to first stage to exit
 	defer func() { cont <- struct{}{} }() // wait until unit test is over
 	ranSecond := false
-	finished := runStages(context.Background(), root, []stage{
-		{func(ctx context.Context, root *testing.RootState) { <-cont }, 0, time.Millisecond},
-		{func(ctx context.Context, root *testing.RootState) { ranSecond = true }, 0, time.Minute},
+	finished := runStages(context.Background(), []stage{
+		{func(ctx context.Context) { <-cont }, 0, time.Millisecond},
+		{func(ctx context.Context) { ranSecond = true }, 0, time.Minute},
 	})
 	if finished {
 		t.Error("runStages reported that stages finished even though first was hanging")
@@ -37,18 +28,16 @@ func TestRunStagesTimeout(t *gotesting.T) {
 
 func TestRunStagesContext(t *gotesting.T) {
 	// Verifies that the context given to stage.f is closed before next stage.
-	root := testing.NewRootState(&testing.TestInstance{}, fakeOutputStream{}, &testing.TestConfig{})
-
 	var stage1Ctx context.Context
 	closed := false
-	runStages(context.Background(), root, []stage{
+	runStages(context.Background(), []stage{
 		// Give stage 1 a long ctxTimeout so that it will stay alive in
 		// stage 2 if not closed.
-		stage{func(ctx context.Context, root *testing.RootState) {
+		stage{func(ctx context.Context) {
 			// Save context for checking in next stage.
 			stage1Ctx = ctx
 		}, 30 * time.Second, time.Minute},
-		stage{func(ctx context.Context, root *testing.RootState) {
+		stage{func(ctx context.Context) {
 			// Check if the context in stage 1 is closed.
 			select {
 			case <-stage1Ctx.Done():

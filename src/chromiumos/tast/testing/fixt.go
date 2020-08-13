483e36856215c9cd88c05705f955831c01c7e5b1
Shuhei Takahashi <nya@chromium.org>
2020-08-13 18:40:47 +0900

    WIP: tast: Implement fixture registration.
    TODO: Write unit tests
    
    BUG=chromium:1035940
    TEST=fast_build.sh -T
    
    Change-Id: I56c136bc4e60b071479a6855138efa6f78ef6e5d

---
+++ src/chromiumos/tast/testing/fixt.go
@@ -0,0 +1,12 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"chromiumos/tast/internal/testing"
+)
+
+// Fixture describes a fixture registered to the framework.
+type Fixture = testing.Fixture

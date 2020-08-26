adc7132051f2cc9da27f99ccb99047c70fec883d
Keigo Oka <oka@chromium.org>
2020-08-26 12:04:13 +0900

    testing: Add Bundle to TestEntity.
    The Bundle field will be used by the tast executable for supporting
    remote fixtures. Tast will categorize local tests by bundle and remote
    fixture dependency, and run each group separately.
    
    BUG=chromium:1066439
    TEST=fast_build.sh -T
    
    Change-Id: Ic789f4a3cfea662e0f6a75efe2e57e902ee99bd5

--- src/chromiumos/tast/internal/runner/bundles.go
+++ src/chromiumos/tast/internal/runner/bundles.go
@@ -79,9 +79,10 @@ type testsOrError struct {
 	err    *command.StatusError
 }
 
-// getTests returns tests in bundles matched by args.Patterns. It does this by executing
-// each bundle to ask it to marshal and print its tests. A slice of paths to bundles
-// with matched tests is also returned.
+// getTests returns tests in bundles matched by args.Patterns. It does this by
+// executing each bundle to ask it to marshal and print its tests. getTests
+// fills the Bundle field in EntityInfo too. A slice of paths to bundles with
+// matched tests is also returned.
 func getTests(args *Args, bundles []string) (tests []*testing.EntityInfo,
 	bundlesWithTests []string, statusErr *command.StatusError) {
 	bundleArgs, err := args.bundleArgs(bundle.ListTestsMode)
@@ -109,12 +110,15 @@ func getTests(args *Args, bundles []string) (tests []*testing.EntityInfo,
 		}()
 	}
 
-	// Read results into a map from bundle to that bundle's tests.
+	// Read results into a map from bundle to that bundle's tests, setting the bundle path.
 	bundleTests := make(map[string][]*testing.EntityInfo)
 	for i := 0; i < len(bundles); i++ {
 		toe := <-ch
 		if toe.err != nil {
 			return nil, nil, toe.err
+		}
+		for _, t := range toe.tests {
+			t.Bundle = toe.bundle
 		}
 		if len(toe.tests) > 0 {
 			bundleTests[toe.bundle] = toe.tests

a58f9a8a4316f3697e7927be3e62f611567ed26c
Yi Chou <yich@google.com>
2020-08-31 10:36:50 +0800

    tast: Add group:hwsec_destructive_crosbolt and group:hwsec_destructive_func
    Renaming group:hwsec_destructive to group:hwsec_destructive_func and
    add a new group for destructive performance tast.
    
    BUG=b:166393956
    TEST=fast_build.sh -T
    
    Cq-Depend: chromium:2383356
    Change-Id: I774096e41136e9c2881976600d6d7ed2f467819d

--- src/chromiumos/tast/internal/testing/attr_test.go
+++ src/chromiumos/tast/internal/testing/attr_test.go
@@ -68,7 +68,10 @@ func TestCheckKnownAttrs(t *testing.T) {
 			attrs: []string{"group:flashrom"},
 		},
 		{
-			attrs: []string{"group:hwsec_destructive"},
+			attrs: []string{"group:hwsec_destructive_func"},
+		},
+		{
+			attrs: []string{"group:hwsec_destructive_crosbolt", "crosbolt_perbuild"},
 		},
 		{
 			attrs: []string{"group:runtime_probe"},

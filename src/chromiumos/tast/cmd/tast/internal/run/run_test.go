326a9e65e8775829b65e8b2329c561c5b655ea2d
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-09-11 03:45:53 +0000

    tast: Add CacheForDUT to the fake TLW.
    This will be used used by a following change for using TLW instead of
    devserver.
    
    BUG=chromium:1112616
    TEST=fast_build.sh -t chromiumos/tast/internal/faketlw
    TEST=fast_build.sh -t chromiumos/tast/internal/devserver
    
    Change-Id: Icfaba372b77d189b1427946f49e997f990db79c0

--- src/chromiumos/tast/cmd/tast/internal/run/run_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/run_test.go
@@ -149,10 +149,13 @@ func TestRunTLW(t *gotesting.T) {
 	}
 
 	// Start a TLW server that resolves "the_dut:22" to the real target addr/port.
-	tlw, tlwAddr := faketlw.StartWiringServer(t, faketlw.WithDUTPortMap(map[faketlw.NamePort]faketlw.NamePort{
+	stopFunc, tlwAddr, err := faketlw.StartWiringServer(t, faketlw.WithDUTPortMap(map[faketlw.NamePort]faketlw.NamePort{
 		{Name: targetName, Port: 22}: {Name: host, Port: int32(port)},
 	}))
-	defer tlw.Stop()
+	if err != nil {
+		t.Fatal("Failed to start fake wiring server: ", err)
+	}
+	defer stopFunc()
 
 	td.cfg.runLocal = true
 	td.runFunc = func(args *runner.Args, stdout, stderr io.Writer) (status int) {

c66acad32de69c1174da5aa1d208ade0c7352d55
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-10-08 04:22:28 +0000

    Switch between devserver and TLW in testing.CloudStorage
    When TLW server is specified, it should load a file through the
    CacheForDUT API of the TLW service, instead of devserver.
    
    BUG=chromium:1112616
    TEST=fast_build.sh -T
    TEST=tast run -ephemeraldevserver=false -tlwserver=$TLW graphics.TraceReplayExtended*
    TEST=tast run graphics.TraceReplayExtended*
    
    Change-Id: I0455cdf49d0f0ec0d7449156a58b43fb7c176f0e

--- src/chromiumos/tast/internal/testing/cloud_storage_test.go
+++ src/chromiumos/tast/internal/testing/cloud_storage_test.go
@@ -11,10 +11,20 @@ import (
 	"testing"
 
 	"chromiumos/tast/internal/devserver"
+	"chromiumos/tast/internal/faketlw"
 )
 
 func TestNewCloudStorage(t *testing.T) {
-	cs := NewCloudStorage(nil)
+	cs := NewCloudStorage(nil, "", "")
+	if cs == nil {
+		t.Error("NewCloudStorage returned nil")
+	}
+}
+
+func TestNewCloudStorageTLW(t *testing.T) {
+	stopFunc, addr := faketlw.StartWiringServer(t)
+	defer stopFunc()
+	cs := NewCloudStorage(nil, addr, "dutName001")
 	if cs == nil {
 		t.Error("NewCloudStorage returned nil")
 	}
@@ -28,10 +38,44 @@ func TestCloudStorageOpen(t *testing.T) {
 
 	// Create CloudStorage using a fake devserver client.
 	cs := &CloudStorage{
-		newClient: func(ctx context.Context) devserver.Client {
+		newClient: func(ctx context.Context) (devserver.Client, error) {
 			return devserver.NewFakeClient(map[string][]byte{
 				fakeURL: []byte(fakeContent),
-			})
+			}), nil
+		},
+	}
+
+	r, err := cs.Open(context.Background(), fakeURL)
+	if err != nil {
+		t.Fatalf("Open failed for %q: %v", fakeURL, err)
+	}
+	defer r.Close()
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Fatal("ReadAll failed: ", err)
+	}
+	if s := string(b); s != fakeContent {
+		t.Fatalf("Got content %q, want %q", s, fakeContent)
+	}
+}
+
+func TestCloudStorageTLWOpen(t *testing.T) {
+	const (
+		dutName     = "dut001"
+		fakeURL     = "gs://a/b/c"
+		fakeContent = "fake content"
+	)
+	stopFunc, addr := faketlw.StartWiringServer(t, faketlw.WithCacheFileMap(
+		map[string][]byte{
+			fakeURL: []byte(fakeContent),
+		},
+	), faketlw.WithDUTName(dutName))
+	defer stopFunc()
+
+	// Create CloudStorage using a fake TLW client.
+	cs := &CloudStorage{
+		newClient: func(ctx context.Context) (devserver.Client, error) {
+			return devserver.NewTLWClient(ctx, addr, dutName)
 		},
 	}
 
@@ -52,8 +96,8 @@ func TestCloudStorageOpen(t *testing.T) {
 func TestCloudStorageOpenMissing(t *testing.T) {
 	// Create CloudStorage using a fake devserver client.
 	cs := &CloudStorage{
-		newClient: func(ctx context.Context) devserver.Client {
-			return devserver.NewFakeClient(nil)
+		newClient: func(ctx context.Context) (devserver.Client, error) {
+			return devserver.NewFakeClient(nil), nil
 		},
 	}
 
@@ -65,4 +109,30 @@ func TestCloudStorageOpenMissing(t *testing.T) {
 	if !os.IsNotExist(err) {
 		t.Fatal("Open failed: ", err)
 	}
+}
+
+func TestCloudStorageTLWOpenMissing(t *testing.T) {
+	const dutName = "dut001"
+	stopFunc, addr := faketlw.StartWiringServer(t, faketlw.WithCacheFileMap(
+		map[string][]byte{}, // no file served
+	), faketlw.WithDUTName(dutName))
+	defer stopFunc()
+
+	// Create CloudStorage using a fake TLW client.
+	cs := &CloudStorage{
+		newClient: func(ctx context.Context) (devserver.Client, error) {
+			return devserver.NewTLWClient(ctx, addr, dutName)
+		},
+	}
+
+	r, err := cs.Open(context.Background(), "gs://a/b/c")
+	if err == nil {
+		r.Close()
+		t.Fatal("Open succeeded unexpectedly")
+	}
+	// The underlying error comes from CacheForDUT TLW RPC.
+	// TODO(crbug.com/1112616): Handle is-not-exist error if possible.
+	// if !os.IsNotExist(err) {
+	// 	t.Fatal("Open failed: ", err)
+	// }
 }

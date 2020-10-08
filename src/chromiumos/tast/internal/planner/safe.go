ffbc0fb8b99010818c644866c0fb403c2ff88e86
Shuhei Takahashi <nya@chromium.org>
2020-10-08 14:47:02 +0900

    Fix stack trace printed on panic.
    When a user function called via safeCall panics, safeCall calls a panic
    handler function. For Tast tests, errorOnPanic handler is used to
    report a panic as a test error by testing.State.Error.
    
    Currently a panic handler is called on a separate goroutine from one
    that calls into a user function. This means that testing.State.Error
    cannot capture useful stack traces containing the code location where
    panic happened.
    
    This patch fixes the issue by modifying safeCall to call a panic
    handler function on the same goroutine as a user function.
    
    BUG=chromium:1131314
    TEST=fast_build.sh -T
    TEST=tast run DUT meta.LocalPanic  # contains local_panic.go trace
    
    Change-Id: I6e3a4d712a6d44b86fe78d17389e7e40b56a558a

--- src/chromiumos/tast/internal/planner/safe.go
+++ src/chromiumos/tast/internal/planner/safe.go
@@ -6,6 +6,7 @@ package planner
 
 import (
 	"context"
+	"sync/atomic"
 	"time"
 
 	"chromiumos/tast/errors"
@@ -47,10 +48,36 @@ func errorOnPanic(e errorReporter) panicHandler {
 // reasons (e.g. f ignored the timeout, ctx was canceled). In other cases, it
 // returns nil.
 func safeCall(ctx context.Context, name string, timeout, gracePeriod time.Duration, ph panicHandler, f func(ctx context.Context)) error {
-	done := make(chan interface{}, 1)
+	// Two goroutines race for a token below.
+	// The main goroutine attempts to a token when it sees timeout or
+	// context cancellation. If it successfully takes a token, safeCall
+	// returns immediately without waiting for f to finish, and ph will
+	// never be called.
+	// A background goroutine attempts to take a token when it finishes
+	// calling f. If it successfully takes a token, it calls recover and
+	// ph (if f panicked). Until the goroutine finishes safeCall will not
+	// return.
+
+	var token uintptr
+	// takeToken returns true if it is called first time.
+	takeToken := func() bool {
+		return atomic.CompareAndSwapUintptr(&token, 0, 1)
+	}
+
+	done := make(chan struct{}) // closed when the background goroutine finishes
+
 	go func() {
 		defer func() {
-			done <- recover()
+			defer close(done)
+			val := recover()
+			if !takeToken() {
+				return
+			}
+			if val != nil {
+				// We must call ph on this goroutine to include the panic location
+				// in the stack trace.
+				ph(val)
+			}
 		}()
 
 		ctx, cancel := context.WithTimeout(ctx, timeout)
@@ -58,15 +85,19 @@ func safeCall(ctx context.Context, name string, timeout, gracePeriod time.Durati
 		f(ctx)
 	}()
 
+	defer func() {
+		if takeToken() {
+			return
+		}
+		<-done
+	}()
+
 	// Allow f to clean up after timeout for gracePeriod.
 	tm := time.NewTimer(timeout + gracePeriod)
 	defer tm.Stop()
 
 	select {
-	case val := <-done:
-		if val != nil {
-			ph(val)
-		}
+	case <-done:
 		return nil
 	case <-tm.C:
 		return errors.Errorf("%s did not return on timeout", name)

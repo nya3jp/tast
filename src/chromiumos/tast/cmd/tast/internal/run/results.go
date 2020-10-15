da081886a2a266de6406ad8e671f4e6247b928e5
Shuhei Takahashi <nya@chromium.org>
2020-10-15 09:44:10 +0000

    Revert "Record relative paths in results.json."
    This reverts commit 69668eb62d5fda9278e320706e358496c8653e57.
    
    Reason for revert: Tastboard started to fail consuming results.json.
    
    Original change's description:
    > Record relative paths in results.json.
    >
    > With introduction of fixtures, multiple entities with the same name
    > can be run multiple times, so we no longer able to locate an output
    > directory of an entity by "tests/$name". Instead, users should read
    > results.json to find outDir.
    >
    > However the current results.json format is difficult to consume
    > because file paths in JSON are absolute and break once we copy files
    > around (e.g. after uploading files to cloud storage).
    >
    > To solve this problem, this patch changes the output file paths
    > emitted in results.json to paths relative to the directory that
    > contains results.json.
    >
    > BUG=chromium:1035940
    > TEST=fast_build.sh -T
    > TEST=fast_build.sh && ~/go/bin/tast run DUT example.Pass
    > TEST=cat /tmp/tast/results/latest/results.json
    >
    > Change-Id: I3ce904f95b140f0d4fb8f350903435ecfab011b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2437674
    > Tested-by: Shuhei Takahashi <nya@chromium.org>
    > Reviewed-by: Keigo Oka <oka@chromium.org>
    > Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    
    Bug: chromium:1035940
    Bug: b:170921804
    Change-Id: Id1b13ddf17c901e20300448f9b3ab2736c108511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2475155
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Tomasz Tylenda <ttylenda@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/cmd/tast/internal/run/results.go
+++ src/chromiumos/tast/cmd/tast/internal/run/results.go
@@ -286,20 +286,20 @@ func (r *resultsHandler) handleTestStart(ctx context.Context, msg *control.Entit
 	}
 	ctx, r.stage = timing.Start(ctx, msg.Info.Name)
 
+	finalOutDir := filepath.Join(r.cfg.ResDir, testLogsDir, msg.Info.Name)
+
 	// Add a number suffix to the output directory name in case of conflict.
-	relOutDir := filepath.Join(testLogsDir, msg.Info.Name)
 	seenCnt := r.seenTimes[msg.Info.Name]
 	if seenCnt > 0 {
-		relOutDir += fmt.Sprintf(".%d", seenCnt)
+		finalOutDir += fmt.Sprintf(".%d", seenCnt)
 	}
 	r.seenTimes[msg.Info.Name]++
-	finalOutDir := filepath.Join(r.cfg.ResDir, relOutDir)
 
 	state := &entityState{
 		result: EntityResult{
 			EntityInfo: msg.Info,
 			Start:      msg.Time,
-			OutDir:     relOutDir,
+			OutDir:     finalOutDir,
 		},
 		IntermediateOutDir: msg.OutDir,
 		FinalOutDir:        finalOutDir,
@@ -317,10 +317,11 @@ func (r *resultsHandler) handleTestStart(ctx context.Context, msg *control.Entit
 		return err
 	}
 
-	if err = os.MkdirAll(finalOutDir, 0755); err != nil {
+	if err = os.MkdirAll(state.result.OutDir, 0755); err != nil {
 		return err
 	}
-	if state.logFile, err = os.Create(filepath.Join(finalOutDir, testLogFilename)); err != nil {
+	if state.logFile, err = os.Create(
+		filepath.Join(state.result.OutDir, testLogFilename)); err != nil {
 		return err
 	}
 	if err = r.cfg.Logger.AddWriter(state.logFile, log.LstdFlags); err != nil {

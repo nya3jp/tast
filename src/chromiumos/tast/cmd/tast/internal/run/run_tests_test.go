c5983aa575823cbbe0123405c56358b9dc0e3a61
Seewai Fu <seewaifu@google.com>
2020-10-21 16:05:22 -0700

    tast:  Add sharding support to Tast runner
    Add sharding support to Tast runner
    
    Manual Test 1
    2020/11/02 13:53:51 Command line: tast -verbose run -build=true -totalshards=2 -shardindex=0 hana ui.LauncherCreateAndRenameFolder.tablet_mode ui.LauncherCreateAndRenameFolder.clamshell_mode
    ...
    2020/11/02 13:54:43 --------------------------------------------------------------------------------
    2020/11/02 13:54:43 ui.LauncherCreateAndRenameFolder.clamshell_mode  [ PASS ]
    2020/11/02 13:54:43 ui.LauncherCreateAndRenameFolder.tablet_mode     [ SKIP ] test is not in the specified shard
    2020/11/02 13:54:43 --------------------------------------------------------------------------------
    2020/11/02 13:54:43 Results saved to /tmp/tast/results/20201102-135351
    
    Manual Test 2
    2020/11/02 13:54:53 Command line: tast -verbose run -build=true -totalshards=2 -shardindex=1 hana ui.LauncherCreateAndRenameFolder.tablet_mode ui.LauncherCreateAndRenameFolder.clamshell_mode
    ...
    2020/11/02 13:55:41 --------------------------------------------------------------------------------
    2020/11/02 13:55:41 ui.LauncherCreateAndRenameFolder.tablet_mode     [ PASS ]
    2020/11/02 13:55:41 ui.LauncherCreateAndRenameFolder.clamshell_mode  [ SKIP ] test is not in the specified shard
    2020/11/02 13:55:41 --------------------------------------------------------------------------------
    2020/11/02 13:55:41 Results saved to /tmp/tast/results/20201102-135453
    
    Manual Test 3
    2020/11/02 14:00:07 Command line: tast -verbose run -build=true -totalshards=2 -shardindex=0 chromeos6-row8-rack22-host17.cros.corp.google.com ui.LauncherCreateAndRenameFolder.tablet_mode ui.LauncherCreateAndRenameFolder.clamshell_mode
    ...
    2020/11/02 14:00:53 --------------------------------------------------------------------------------
    2020/11/02 14:00:53 ui.LauncherCreateAndRenameFolder.clamshell_mode  [ PASS ]
    2020/11/02 14:00:53 ui.LauncherCreateAndRenameFolder.tablet_mode     [ SKIP ] missing SoftwareDeps: tablet_mode
    2020/11/02 14:00:53 --------------------------------------------------------------------------------
    2020/11/02 14:00:53 Results saved to /tmp/tast/results/20201102-140007
    
    Manual Test 3
    2020/11/02 14:01:30 Command line: tast -verbose run -build=true -totalshards=2 -shardindex=1 chromeos6-row8-rack22-host17.cros.corp.google.com ui.LauncherCreateAndRenameFolder.tablet_mode ui.LauncherCreateAndRenameFolder.clamshell_mode
    ...
    2020/11/02 14:01:33 --------------------------------------------------------------------------------
    2020/11/02 14:01:33 ui.LauncherCreateAndRenameFolder.tablet_mode     [ SKIP ] missing SoftwareDeps: tablet_mode
    2020/11/02 14:01:33 ui.LauncherCreateAndRenameFolder.clamshell_mode  [ SKIP ] test is not in the specified shard
    2020/11/02 14:01:33 --------------------------------------------------------------------------------
    2020/11/02 14:01:33 Results saved to /tmp/tast/results/20201102-140130
    
    BUG=chromium:968262
    TEST=Manual testing. New unit tests added.
    
    Change-Id: I36fa9c670b509e9fd6eaa267151fb7db5148a606

--- src/chromiumos/tast/cmd/tast/internal/run/run_tests_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/run_tests_test.go
@@ -7,12 +7,14 @@ package run
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"strings"
 	gotesting "testing"
 
 	"chromiumos/tast/internal/dep"
 	"chromiumos/tast/internal/runner"
+	internal_testing "chromiumos/tast/internal/testing"
 )
 
 func TestRunTestsFailureBeforeRun(t *gotesting.T) {
@@ -98,4 +100,143 @@ func TestRunTestsGetInitialSysInfo(t *gotesting.T) {
 	if !called {
 		t.Errorf("runTests did not call getInitialSysInfo")
 	}
+}
+
+func TestFindPatternsForShard(t *gotesting.T) {
+	tests := []internal_testing.EntityInfo{
+		{Name: "pkg.Test0", Desc: "This is test 0"},
+		{Name: "pkg.Test1", Desc: "This is test 1"},
+		{Name: "pkg.Test2", Desc: "This is test 2"},
+		{Name: "pkg.Test3", Desc: "This is test 3"},
+		{Name: "pkg.Test4", Desc: "This is test 4"},
+		{Name: "pkg.Test5", Desc: "This is test 5"},
+		{Name: "pkg.Test6", Desc: "This is test 6"},
+	}
+	// Make the runner print serialized tests.
+	b, err := json.Marshal(&tests)
+	if err != nil {
+		t.Fatal(err)
+	}
+	td := newRemoteTestData(t, string(b), "", 0)
+	defer td.close()
+
+	fmt.Print("check\n")
+
+	// List matching tests instead of running them.
+	td.cfg.remoteDataDir = "/tmp/data"
+	td.cfg.Patterns = []string{"*Test*"}
+	td.cfg.runRemote = true
+	td.cfg.totalShards = 3
+	result := make(map[string]int)
+	for shardIndex := 0; shardIndex < td.cfg.totalShards; shardIndex++ {
+		td.cfg.shardIndex = shardIndex
+		testNames, testsNotInShard, err := findPatternsForShard(context.Background(), &td.cfg)
+		if err != nil {
+			t.Fatal("Failed to find patterns for shard: ", err)
+		}
+		if len(testNames)+len(testsNotInShard) != len(tests) {
+			t.Fatalf("The sum of numbers of tests in the shard (%v) and not in the shard (%v) does not match the number of tests (%v)",
+				len(testNames), len(testsNotInShard), len(tests))
+		}
+		for _, name := range testNames {
+			result[name] = result[name] + 1
+			if result[name] > 1 {
+				t.Fatalf("Test %q is in more than one shard", name)
+			}
+		}
+	}
+	if len(result) != len(tests) {
+		t.Fatal("Some tests are missing")
+	}
+}
+
+// TestFindShardIndicesFirstEvenShard makes sure findShardIndices return correct indices for
+// the first shard of an evenly distributed shards.
+func TestFindShardIndicesFirstEvenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 9, 3, 0, 0, 3, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices for the first shard of an evenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesMiddleEvenShard makes sure findShardIndices return correct indices for
+// the middle shard of an evenly distributed shards.
+func TestFindShardIndicesMiddleEvenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 9, 3, 1, 3, 6, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices for the middle shard of an evenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesLastEvenShard makes sure findShardIndices return correct indices for
+// the last shard of an evenly distributed shards.
+func TestFindShardIndicesLastEvenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 9, 3, 2, 6, 9, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices for the last shard of an evenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesFirstUnevenShard makes sure findShardIndices return correct indices for
+// the first shard of an unevenly distributed shards.
+func TestFindShardIndicesFirstUnevenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 11, 3, 0, 0, 4, false); err != nil {
+		fmt.Print("Error: ", err)
+		t.Errorf("Failed to get correct indices from findShardIndices for the first shard of an unevenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesMiddleUnevenShard makes sure findShardIndices return correct indices for
+// the middle shard of an unevenly distributed shards.
+func TestFindShardIndicesMiddleUnevenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 11, 3, 1, 4, 8, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices for the middle shard of an unevenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesLastUnevenShard makes sure findShardIndices return correct indices for
+// the last shard of an unevenly distributed shards.
+func TestFindShardIndicesLastUnevenShard(t *gotesting.T) {
+	if err := testFindShardIndices(t, 11, 3, 2, 8, 11, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices for the last shard of an unevenly distributed shards: %v", err)
+	}
+}
+
+// TestFindShardIndicesMoreShardsThanTests makes sure findShardIndices return correct indices when
+// the number of shards is greater than number of tests.
+func TestFindShardIndicesMoreShardsThanTests(t *gotesting.T) {
+	if err := testFindShardIndices(t, 9, 10, 0, 0, 1, false); err != nil {
+		t.Errorf("Failed to get correct indices from findShardIndices when the number of shards is greater than number of tests: %v", err)
+	}
+}
+
+// TestFindShardIndicesInvalidIndex makes sure findShardIndices return error when
+// the shard index is out of range.
+func TestFindShardIndicesInvalidIndex(t *gotesting.T) {
+	if err := testFindShardIndices(t, 9, 3, 4, 0, 3, true); err != nil {
+		t.Errorf("Failed to get error from findShardIndices when the shard index is out of range: %v", err)
+	}
+	if err := testFindShardIndices(t, 9, 10, 11, 0, 3, true); err != nil {
+		t.Errorf("Failed to get error from findShardIndices when the shard index is out of range when there are more shards than tests: %v", err)
+	}
+}
+
+// testFindShardIndices tests whether the function findShardIndices returning the correct indices.
+func testFindShardIndices(t *gotesting.T,
+	numTests, totalShards, shardIndex, wantedStartIndex, wantedEndIndex int,
+	wantError bool) (err error) {
+	startIndex, endIndex, commandErr := findShardIndices(numTests, totalShards, shardIndex)
+	if commandErr != nil {
+		if wantError {
+			return nil
+		}
+		return fmt.Errorf("failed to find shard indices: %v", commandErr)
+	}
+	if wantError {
+		return fmt.Errorf("test succeeded unexpectedly; getting startIndex %v endIndex %v", startIndex, endIndex)
+	}
+	if startIndex != wantedStartIndex {
+		return fmt.Errorf("findShardIndices returned start index %d results; want %d", startIndex, wantedStartIndex)
+	}
+	if endIndex != wantedEndIndex {
+		return fmt.Errorf("findShardIndices returned end index %d results; want %d", endIndex, wantedEndIndex)
+	}
+	return nil
 }

7f4faf53c604ef62f8cab4c25aae1e90f3ef0f54
Anthony Hinton <hinton@google.com>
2020-10-23 12:26:21 -0700

    Add DUT.WifiPeerHost() and create wifipeer.go.
    The function, DUT.WifiPeerHost(), obtains WiFi-peer's host name
    specified by the index parameter.
    It follows the router and pcap model, but allows for any number of
    peers to be added.
    Also, I rolled the  changes form 2499506 into this.
    
    BUG=b:171728381
    TEST=local unittest that doesn't mock out IPs, so tests on real
    hardware.
    
    Change-Id: I30a19f2e3aeade0e6cb15fbf5fcd2eefc37bd57b

--- src/chromiumos/tast/remote/wificell/wifipeer/wifipeer.go
+++ src/chromiumos/tast/remote/wificell/wifipeer/wifipeer.go
@@ -2,27 +2,33 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Package wifipeer builds and controls peer chrome OS devices for wifi tests.
+// Package wifipeer builds and controls peer Chrome OS devices for Wi-Fi tests.
 package wifipeer
 
 import (
 	"context"
+	"fmt"
 
 	"chromiumos/tast/dut"
 	"chromiumos/tast/ssh"
 )
 
-// WifiPeer holds the structure of a Chrome OS peer device for wifi tests.
-type WifiPeer struct {
-	Peers []*ssh.Conn
-}
-
 // MakePeers constructs the peer devices needed for the tests.
-func (peer *WifiPeer) MakePeers(ctx context.Context, testdut *dut.DUT,
-	count int) (_ []*ssh.Conn, retErr error) {
-	for i := 0; i < count; i++ {
-		newDut, _ := testdut.IthWifiPeerHost(ctx, i)
-		peer.Peers = append(peer.Peers, newDut)
+func MakePeers(ctx context.Context, testdut *dut.DUT, count int) (peers []*ssh.Conn, retErr error) {
+	defer func() {
+		if retErr != nil {
+			for _, peer := range peers {
+				peer.Close(ctx)
+				fmt.Printf("Error  trying to connect to Peer. Closing. %s", retErr)
+			}
+		}
+	}()
+	for i := 1; i < count; i++ {
+		peer, err := testdut.WifiPeerHost(ctx, i)
+		if err != nil {
+			return nil, err
+		}
+		peers = append(peers, peer)
 	}
-	return peer.Peers, nil //TODO(hinton): Return any errors from the loop.
+	return peers, nil
 }

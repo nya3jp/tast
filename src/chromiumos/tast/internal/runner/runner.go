6a859781e34f922b90fe7ec606db546fa3a0db7f
Keigo Oka <oka@chromium.org>
2020-10-26 18:51:00 +0900

    Implement ListFixtures
    BUG=chromium:1066439
    TEST=fast_build.sh -T
    
    Change-Id: I04afa1b8465dfe0aa8b5d8b874fcfd530e6395e2

--- src/chromiumos/tast/internal/runner/runner.go
+++ src/chromiumos/tast/internal/runner/runner.go
@@ -6,6 +6,7 @@ package runner
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -77,8 +78,19 @@ func Run(clArgs []string, stdin io.Reader, stdout, stderr io.Writer, args *Args,
 		}
 		return statusSuccess
 	case ListFixturesMode:
-		// TODO(oka): Implement ListFixutresMode.
-		panic("to be implemented")
+		fixts, err := getFixtures(args.ListFixtures.BundleGlob)
+		if err != nil {
+			return command.WriteError(stderr, err)
+		}
+		res := &ListFixturesResult{Fixtures: fixts}
+		b, err2 := json.Marshal(res)
+		if err2 != nil {
+			return command.WriteError(stderr, err2)
+		}
+		if _, err2 = stdout.Write(b); err2 != nil {
+			return command.WriteError(stderr, err2)
+		}
+		return statusSuccess
 	case RunTestsMode:
 		if args.report {
 			// Success is always reported when running tests on behalf of the tast command.

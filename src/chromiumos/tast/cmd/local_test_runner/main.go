ba5c9addae77b3f89b41fa8193e064ae45475d55
Eliot Courtney <edcourtney@google.com>
2020-11-17 12:31:24 +0900

    Also do not run lacros Tast tests on arm64.
    BUG=chromium:1149366
    TEST=fast_build.sh -T
    
    Change-Id: I0c512c9ce83919fd190d8c690a40179a50568a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2543509
    Tested-by: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -119,7 +119,7 @@ func main() {
 			"hostap_hwsim":            "wifi_hostap_test",
 			"igt":                     `("video_cards_amdgpu" || "video_cards_intel") && ("kernel-5_4" || "kernel-4_19" || "kernel-4_14")`,
 			"iwlwifi_rescan":          "iwlwifi_rescan",
-			"lacros":                  "!arm", // TODO(crbug.com/1144013): Expand this to include arm as well.
+			"lacros":                  "!arm && !arm64", // TODO(crbug.com/1144013): Expand this to include arm as well.
 			"lock_core_pattern":       `"kernel-3_10" || "kernel-3_14" || "kernel-3_18"`,
 			// QEMU has implemented memfd_create, but we haven't updated
 			// to a release with the change (https://bugs.launchpad.net/qemu/+bug/1734792).

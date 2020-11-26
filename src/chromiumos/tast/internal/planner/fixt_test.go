bd7e179d7849cb1bc7884cda3514782ad1f9b555
Keigo Oka <oka@chromium.org>
2020-11-26 22:57:57 +0900

    bundle: set bundle path on listing tests
    For supporing remote fixutres, tast has to group tests per bundle.
    This change allows it by adding Bundle field to testing.Entity
    and making bundles to fill in the field on listing tests.
    
    BUG=chromium:1066439
    TEST=fast_build.sh -T
    
    Change-Id: I7b5ec5ff4bf298a243215c29430ac05784ade185

--- src/chromiumos/tast/internal/planner/fixt_test.go
+++ src/chromiumos/tast/internal/planner/fixt_test.go
@@ -543,6 +543,8 @@ func TestFixtureStackRedFixtureName(t *gotesting.T) {
 	}
 }
 
+var bundleName = filepath.Base(os.Args[0])
+
 // TestFixtureStackOutputGreen tests control message outputs when all fixtures
 // are healthy.
 func TestFixtureStackOutputGreen(t *gotesting.T) {
@@ -610,11 +612,11 @@ func TestFixtureStackOutputGreen(t *gotesting.T) {
 	}
 
 	want := []control.Msg{
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt1", Text: "SetUp 1 via Context"},
 		&control.EntityLog{Name: "fixt1", Text: "SetUp 1 via Fixture-scoped Context"},
 		&control.EntityLog{Name: "fixt1", Text: "SetUp 1 via State"},
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt2", Text: "SetUp 2 via Context"},
 		&control.EntityLog{Name: "fixt2", Text: "SetUp 2 via Fixture-scoped Context"},
 		&control.EntityLog{Name: "fixt2", Text: "SetUp 2 via State"},
@@ -699,9 +701,9 @@ func TestFixtureStackOutputRed(t *gotesting.T) {
 	}
 
 	want := []control.Msg{
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt1", Text: "SetUp 1"},
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt2", Text: "SetUp 2"},
 		&control.EntityError{Name: "fixt2", Error: testing.Error{Reason: "SetUp 2 failure"}},
 		&control.EntityEnd{Name: "fixt2"},
@@ -770,11 +772,11 @@ func TestFixtureStackOutputYellow(t *gotesting.T) {
 	}
 
 	want := []control.Msg{
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt1", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt1", Text: "SetUp 1"},
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt2", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt2", Text: "SetUp 2"},
-		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt3", Type: testing.EntityFixture}},
+		&control.EntityStart{Info: testing.EntityInfo{Name: "fixt3", Type: testing.EntityFixture, Bundle: bundleName}},
 		&control.EntityLog{Name: "fixt3", Text: "SetUp 3"},
 		&control.EntityLog{Name: "fixt1", Text: "Reset 1"},
 		&control.EntityLog{Name: "fixt2", Text: "Reset 2"},

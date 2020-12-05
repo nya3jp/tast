b399c4c58d095370b2aa090f3ca3276d4a9f2a85
Seewai Fu <seewaifu@google.com>
2020-12-04 17:21:48 -0800

    tast_rtd: Create the initial version tast_rtd
    This CL creates the Create initial tast_rtd which
    will read invocation protobuf file and invoke tast
    after it is completed. In this version, it will only
    make sure the protobuf file existed and readable.
    
    The program will exit with 0 if the file exists
    and can be opened.
    
    The program will exit with 1 if the file does not exist
    or cannot be opened.
    
    Manual Tests
    $ export GOPATH=$HOME/go
    $ export GOPATH=${GOPATH}:${CHROMEOS_SRC}/chroot/usr/lib/gopath
    $ export GOPATH=${GOPATH}:${CHROMEOS_SRC}/src/platform/tast-tests:${CHROMEOS_SRC}/src/platform/tast
    $ export GOPATH=${GOPATH}:/usr/lib64/gopath
    $ go build
    $ ./tast_rtd -input=nonexisting_file
    2020/12/07 14:31:40 Input File: nonexisting_file
    2020/12/07 14:31:40 Failed to read invocation protobuf file nonexisting_file: fail to read file nonexisting_file: open nonexisting_file: no such file or directory
    $ echo $?
    1
    $ chmod -r test.pb
    $ ./tast_rtd -input=test.pb
    2020/12/07 14:31:41 Input File: test.pb
    2020/12/07 14:31:41 Failed to read invocation protobuf file test.pb: fail to read file test.pb: open test.pb: permission denied
    $ echo $?
    1
    $ chmod +r test.pb
    $ ./tast_rtd -input=test.pb
    
    $ echo $?
    0
    $ tail -n +1  /tmp/tast_rtd/results/*/log.txt
    ==> /tmp/tast_rtd/results/20201207-143140/log.txt <==
    2020/12/07 14:31:42 Input File: test.pb
    2020/12/07 14:31:40 Failed to read invocation protobuf file nonexisting_file: fail to read file nonexisting_file: open nonexisting_file: no such file or directory
    
    ==> /tmp/tast_rtd/results/20201207-143141/log.txt <==
    2020/12/07 14:31:41 Input File: test.pb
    2020/12/07 14:31:41 Failed to read invocation protobuf file test.pb: fail to read file test.pb: open test.pb: permission denied
    
    ==> /tmp/tast_rtd/results/20201207-143142/log.txt <==
    2020/12/07 14:31:42 Input File: test.pb
    
    BUG=chromium:1155774
    TEST=Manual testing. Unit tests passed
    
    Change-Id: Ib5b1ed65aef8f248959dce3e1a7bd353c2eeb37a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2575934
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Tested-by: Seewai Fu <seewaifu@google.com>

---
+++ src/chromiumos/tast/cmd/tast_rtd/main.go
@@ -0,0 +1,86 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package main implements the tast_rtd executable, used to invoke tast in RTD.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	rtd "go.chromium.org/chromiumos/config/go/api/test/rtd/v1"
+)
+
+// Version is the version info of this command. It is filled in during emerge.
+var Version = "<unknown>"
+
+// createLogFile creates a file and its parent directory for logging purpose.
+func createLogFile() (*os.File, error) {
+	t := time.Now()
+	fullPath := filepath.Join("/tmp/tast_rtd/results", t.Format("20060102-150405"))
+	if err := os.MkdirAll(fullPath, 0755); err != nil {
+		return nil, fmt.Errorf("failed to create log directory %v: %v", fullPath, err)
+	}
+
+	logFullPathName := filepath.Join(fullPath, "log.txt")
+
+	// Log the full output of the command to disk.
+	logFile, err := os.Create(logFullPathName)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create log file %v: %v", fullPath, err)
+	}
+	return logFile, nil
+}
+
+// newLogger creates a logger.
+func newLogger(logFile *os.File) *log.Logger {
+	mw := io.MultiWriter(logFile, os.Stderr)
+	return log.New(mw, "", log.LstdFlags)
+}
+
+// readInput reads an invocation protobuf file and returns a pointer to rtd.Invocation.
+// TODO: Populate rtd.Invocation.
+func readInput(fileName string) (*rtd.Invocation, error) {
+	_, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("fail to read file %v: %v", fileName, err)
+	}
+	return nil, nil
+}
+
+func main() {
+	os.Exit(func() int {
+		version := flag.Bool("version", false, "print version and exit")
+		input := flag.String("input", "", "specify the test invocation request protobuf input file")
+		flag.Parse()
+
+		if *version {
+			fmt.Printf("tast_rtd version %s\n", Version)
+			return 0
+		}
+
+		logFile, err := createLogFile()
+		if err != nil {
+			log.Fatalf("Failed to create log file: %v", err)
+		}
+		defer logFile.Close()
+
+		logger := newLogger(logFile)
+
+		logger.Println("Input File:", *input)
+
+		_, err = readInput(*input)
+		if err != nil {
+			logger.Printf("Failed to read invocation protobuf file %v: %v", *input, err)
+			return 1
+		}
+		return 0
+	}())
+}

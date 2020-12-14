f44aeafedab8669244b50f742c4231fa3e271494
Seewai Fu <seewaifu@google.com>
2020-12-14 14:46:36 -0800

    tast_rtd: Enhance tast_rtd to invoke tast
    1157277: Enhance tast_rtd to invoke tast
    This CL will enhance tast_rtd to invoke tast.
    
    Manual Test output:
    2020/12/14 14:50:31 Input File: test.pb
    2020/12/14 14:50:31 [tast] 2020/12/14 14:50:31 Command line: /usr/bin/tast run -resultsdir /tmp/tast/result1 127.0.0.1:2222 launcher.PinAppToShelf.clamshell_mode launcher.PinAppToShelf.tablet_mode
    ...
    2020/12/14 14:52:13 [tast] 2020/12/14 14:52:13 --------------------------------------------------------------------------------
    2020/12/14 14:52:13 [tast] 2020/12/14 14:52:13 launcher.PinAppToShelf.clamshell_mode  [ PASS ]
    2020/12/14 14:52:13 [tast] 2020/12/14 14:52:13 launcher.PinAppToShelf.tablet_mode     [ PASS ]
    2020/12/14 14:52:13 [tast] 2020/12/14 14:52:13 --------------------------------------------------------------------------------
    2020/12/14 14:52:13 [tast] 2020/12/14 14:52:13 Results saved to /tmp/tast/result1
    
    BUG=chromium:1157277
    TEST=Manual testing. New unit test was created and passed
    
    Change-Id: Ieb3f21374bd0340be8b648d7f9e4608b5f980191

--- src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
+++ src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
@@ -4,46 +4,62 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/google/go-cmp/cmp"
 	rtd "go.chromium.org/chromiumos/config/go/api/test/rtd/v1"
 )
 
+const reqName1 = "Name1"
+const reqName2 = "Name2"
+const test1 = "launcher.PinAppToShelf.clamshell_mode"
+const test2 = "launcher.PinAppToShelf.tablet_mode"
+const workDir1 = "/tmp/tast/result1"
+const workDir2 = "/tmp/tast/result2"
+const sinkPort = 22
+const tlsAddress = "192.168.86.81"
+const tlsPort = 2227
+const tlwAddress = "192.168.86.109"
+const tlwPort = 2228
+const dut1 = "127.0.0.1:2222"
+
+var inv = rtd.Invocation{
+	Requests: []*rtd.Request{
+		{
+			Name: reqName1,
+			Test: test1,
+			Environment: &rtd.Request_Environment{
+				WorkDir: workDir1,
+			},
+		},
+		{
+			Name: reqName2,
+			Test: test2,
+			Environment: &rtd.Request_Environment{
+				WorkDir: workDir2,
+			},
+		},
+	},
+	ProgressSinkClientConfig: &rtd.ProgressSinkClientConfig{
+		Port: sinkPort,
+	},
+	TestLabServicesConfig: &rtd.TLSClientConfig{
+		TlsAddress: tlsAddress,
+		TlsPort:    tlsPort,
+		TlwAddress: tlwAddress,
+		TlwPort:    tlwPort,
+	},
+	Duts: []*rtd.DUT{
+		{
+			TlsDutName: dut1,
+		},
+	},
+}
+
 // TestUnmarshalInvocation makes sure unmarshalInvocation able to unmarshal invocation data.
 func TestUnmarshalInvocation(t *testing.T) {
-	inv := rtd.Invocation{
-		Requests: []*rtd.Request{
-			{
-				Name: "Name1",
-				Test: "cros.Test1",
-				Environment: &rtd.Request_Environment{
-					WorkDir: "/tmp/tast",
-				},
-			},
-			{
-				Name: "Name2",
-				Test: "cros.Test2",
-				Environment: &rtd.Request_Environment{
-					WorkDir: "/tmp/tast",
-				},
-			},
-		},
-		ProgressSinkClientConfig: &rtd.ProgressSinkClientConfig{
-			Port: 22,
-		},
-		TestLabServicesConfig: &rtd.TLSClientConfig{
-			TlsAddress: "127.0.0.1",
-			TlsPort:    2222,
-			TlwAddress: "127.0.0.1",
-			TlwPort:    2223,
-		},
-		Duts: []*rtd.DUT{
-			{
-				TlsDutName: "127.0.0.1:2224",
-			},
-		},
-	}
 	buf, err := proto.Marshal(&inv)
 	if err != nil {
 		t.Fatal("Failed to marshal invocation data:", err)
@@ -54,5 +70,42 @@ func TestUnmarshalInvocation(t *testing.T) {
 	}
 	if !proto.Equal(&inv, result) {
 		t.Errorf("Invocation did not match: want %v, got %v", inv, result)
+	}
+}
+
+// TestNewArgs makes sure newArgs creates the correct arguments for tast.
+func TestNewArgs(t *testing.T) {
+	expectedArgs := runArgs{
+		target:    dut1,
+		patterns:  []string{test1, test2},
+		tlwServer: fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
+		resultDir: workDir1,
+	}
+
+	args := newArgs(&inv)
+	if diff := cmp.Diff(&expectedArgs, args, cmp.AllowUnexported(runArgs{})); diff != "" {
+		t.Errorf("Got unexpected argument from newArgs (-want +got):\n%s", diff)
+	}
+}
+
+// TestGenArgList makes sure genArgList generates the correct list of argument for tast.
+func TestGenArgList(t *testing.T) {
+	args := runArgs{
+		target:    dut1,
+		patterns:  []string{test1, test2},
+		tlwServer: fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
+		resultDir: workDir1,
+	}
+
+	expectedArgList := []string{
+		"run",
+		"-tlwserver", fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
+		"-resultsdir", workDir1,
+		dut1,
+		test1, test2,
+	}
+	argList := genArgList(&args)
+	if diff := cmp.Diff(expectedArgList, argList, cmp.AllowUnexported(runArgs{})); diff != "" {
+		t.Errorf("Got unexpected argument from genArgList (-want +got):\n%s", diff)
 	}
 }

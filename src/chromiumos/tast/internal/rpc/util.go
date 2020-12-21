7f84ee9895784ebef2d1ab24a018538715a48859
Shuhei Takahashi <nya@chromium.org>
2020-12-21 16:34:34 +0900

    rpc: Don't apply call hooks to core service methods
    This is a part of series of patches to refactor the rpc package.
    
    gRPC client/server provided by the rpc package installs per-call hooks
    to perform some operations before/after gRPC calls. To make the rpc
    package useful for communication among framework executables (e.g.
    Tast CLI <-> test_runner), update the code to apply the hooks only to
    user-defined gRPC service method calls.
    
    Whether a gRPC service belongs to users is determined by gRPC service
    package name; a service is considered a core service if a service name
    starts with "tast.core.".
    
    The fake gRPC service tast.core.Ping used in unit tests is renamed to
    tast.coretest.Ping to behave like a user-defined gRPC service.
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: I951b35491b28482e0a8351136c6b7119109f0ccf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2600765
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/internal/rpc/util.go
@@ -0,0 +1,14 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package rpc
+
+import (
+	"strings"
+)
+
+// isUserMethod checks if a gRPC method belongs to a user-defined gRPC service.
+func isUserMethod(name string) bool {
+	return !strings.HasPrefix(name, "/tast.core.")
+}

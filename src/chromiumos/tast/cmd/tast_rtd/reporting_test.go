12bfb85793b303997f48d6e99d9b9597db8ba3d1
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2020-12-22 06:49:47 +0000

    tast: Add log reporting function for tast_rtd.
    BUG=chromium:1161068
    TEST=fast_build.sh -t chromiumos/tast/cmd/tast_rtd
    
    Change-Id: I126167e0aa4d364cb7c624e5598583de0097f47d

---
+++ src/chromiumos/tast/cmd/tast_rtd/reporting_test.go
@@ -0,0 +1,41 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"testing"
+
+	"chromiumos/tast/cmd/tast_rtd/internal/fakerts"
+)
+
+func TestSendLog(t *testing.T) {
+	srv, addr, err := fakerts.StartProgressSink(context.Background())
+	if err != nil {
+		t.Fatal("Failed to start fake ProgressSink server: ", err)
+	}
+	defer srv.Stop()
+	name := "file://foo/name001"
+	reqName := "request01"
+	log, err := newReportLogStream(addr.String(), name, reqName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p1 := []byte("Hello, ")
+	p2 := []byte("world!")
+	if _, err := log.Write(p1); err != nil {
+		t.Error(err)
+	}
+	if _, err := log.Write(p2); err != nil {
+		t.Error(err)
+	}
+	log.Close()
+	actual := string(srv.ReceivedLog(name, reqName))
+	expected := "Hello, world!"
+	if actual != expected {
+		t.Errorf("got %q, want %q", actual, expected)
+	}
+}

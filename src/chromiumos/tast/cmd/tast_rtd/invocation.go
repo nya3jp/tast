4c983c27b1db5c3fc7a6593db4463549dc0a538b
Seewai Fu <seewaifu@google.com>
2020-12-22 16:46:21 -0800

    tast_rtd: Enhance tast_rtd to report test result (WIP)
    This CL will enhance tast_rtd to report test result by
    using TLW ReportResultRequest APIs.
    Still need to test it with a RTS.
    
    BUG=chromium:1159524
    TEST=New unit test was created and passed
    
    Change-Id: I8de03efe422ddd343bcbb2e8a310e4f8ee8801e7

--- src/chromiumos/tast/cmd/tast_rtd/invocation.go
+++ src/chromiumos/tast/cmd/tast_rtd/invocation.go
@@ -7,17 +7,32 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/golang/protobuf/proto"
+	_struct "github.com/golang/protobuf/ptypes/struct"
+	v2 "go.chromium.org/chromiumos/config/go/api/test/results/v2"
 	rtd "go.chromium.org/chromiumos/config/go/api/test/rtd/v1"
+	"google.golang.org/grpc"
 
 	"chromiumos/tast/errors"
+)
+
+const (
+	resultsFilename = "results.json" // File containing stream of newline-separated JSON EntityResult objects
+
+	skipReasonKey = "skipReason" // Keyword for skip reason in results.json.
+	errorsKey     = "errors"     // Keyword for errors in results.json.
 )
 
 // unmarshalInvocation unmarshals an invocation request and returns a pointer to rtd.Invocation.
@@ -55,6 +70,10 @@ func newArgs(inv *rtd.Invocation) *runArgs {
 		if args.resultDir == "" {
 			args.resultDir = r.Environment.WorkDir
 		}
+	}
+	if args.resultDir == "" {
+		t := time.Now()
+		args.resultDir = filepath.Join("/tmp/tast/results", t.Format("20060102-150405"))
 	}
 	return &args
 }
@@ -107,6 +126,17 @@ func invokeTast(logger *log.Logger, inv *rtd.Invocation) error {
 			return errors.Wrapf(err, "failed to create symbolic link %v", workDir)
 		}
 	}
+
+	// Set up connection with ProgressSink
+	psAddr := net.JoinHostPort("127.0.0.1", strconv.Itoa(int(inv.ProgressSinkClientConfig.Port)))
+	conn, err := grpc.DialContext(context.Background(), psAddr, grpc.WithBlock())
+	if err != nil {
+		return errors.Wrapf(err, "failed to connect to progress sink %v", psAddr)
+	}
+	defer conn.Close()
+
+	psClient := rtd.NewProgressSinkClient(conn)
+
 	// Run tast.
 	cmd := exec.Command(path, genArgList(args)...)
 	stderr, err := cmd.StderrPipe()
@@ -141,5 +171,195 @@ func invokeTast(logger *log.Logger, inv *rtd.Invocation) error {
 
 	wg.Wait()
 
-	return cmd.Wait()
+	if err := cmd.Wait(); err != nil {
+		return errors.Wrap(err, "failed in running tast")
+	}
+
+	if err := readTestResults(psClient, inv, args.resultDir); err != nil {
+		return errors.Wrap(err, "failed in reading tast test results")
+	}
+
+	return nil
+}
+
+type testError struct {
+	Time   string `json:"time"`
+	Reason string `json:"reason"`
+	File   string `json:"file"`
+	Line   int    `json:"line"`
+	Stack  string `json:"stack"`
+}
+type testResult struct {
+	// name contains the test name.
+	Name string `json:"name"`
+	// Errors contains errors encountered while running the entity.
+	// If it is empty, the entity passed.
+	Errors []testError `json:"errors"`
+	// It is empty if the test actually ran.
+	SkipReason string `json:"skipReason"`
+}
+
+// readTestResults reads resultDir/results.json and reports result through progress API.
+func readTestResults(psClient rtd.ProgressSinkClient, inv *rtd.Invocation, resultDir string) error {
+	fullPathName := filepath.Join(resultDir, resultsFilename)
+	resultFile, err := os.Open(fullPathName)
+	if err != nil {
+		return errors.Wrapf(err, "failed to open file %v", fullPathName)
+	}
+	defer resultFile.Close()
+	data, err := ioutil.ReadFile(fullPathName)
+	if err != nil {
+		return errors.Wrapf(err, "fail to read file %v", fullPathName)
+	}
+	resultRequests, err := makeResultRequests(inv, data)
+	if err != nil {
+		return errors.Wrapf(err, "fail to report test result from file %v", fullPathName)
+	}
+	for _, rr := range resultRequests {
+		rspn, err := psClient.ReportResult(context.Background(), rr)
+		if err != nil {
+			return errors.Wrap(err, "failed to report result")
+		}
+		if rspn.Terminate {
+			return nil
+		}
+	}
+	return nil
+}
+
+// makeResultRequests parses the json text from testData and prepares test results for progress API to use.
+func makeResultRequests(inv *rtd.Invocation, testData []byte) (resultRequests []*rtd.ReportResultRequest, err error) {
+	var results []*testResult
+	if err := json.Unmarshal(testData, &results); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal data")
+	}
+	// A map to maintain test names to result structures.
+	testResults := map[string]*testResult{}
+	for i, r := range results {
+		if r.Name == "" {
+			return nil, errors.Errorf("Entry %v has empty test name: %+v", i, r)
+		}
+		testResults[r.Name] = results[i]
+	}
+	for _, request := range inv.Requests {
+		result, ok := testResults[request.Test]
+		if !ok {
+			// TODO: Discuss what is the best way to handle this case.
+			resultRequests = append(resultRequests, missingTestResult(request.Name))
+			continue
+		}
+		if rr := skippedTestResult(request.Name, result); rr != nil {
+			resultRequests = append(resultRequests, rr)
+			continue
+		}
+		if rr := failedTestResult(request.Name, result); rr != nil {
+			resultRequests = append(resultRequests, rr)
+			continue
+		}
+		resultRequests = append(resultRequests, &rtd.ReportResultRequest{
+			Request: request.Name,
+			Result:  &v2.Result{State: v2.Result_SUCCEEDED},
+		})
+	}
+	return resultRequests, nil
+}
+
+func missingTestResult(requestName string) *rtd.ReportResultRequest {
+	// TODO: Discuss what is the best way to handle this case.
+	return &rtd.ReportResultRequest{
+		Request: requestName,
+		Result: &v2.Result{
+			State: v2.Result_SKIPPED,
+			Errors: []*v2.Result_Error{
+				&v2.Result_Error{
+					Source:   v2.Result_Error_TEST,
+					Severity: v2.Result_Error_WARNING,
+					Details: &_struct.Struct{
+						Fields: map[string]*_struct.Value{
+							skipReasonKey: &_struct.Value{
+								Kind: &_struct.Value_StringValue{
+									StringValue: "Test was not found",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func skippedTestResult(requestName string, result *testResult) *rtd.ReportResultRequest {
+	if result.SkipReason != "" {
+		return &rtd.ReportResultRequest{
+			Request: requestName,
+			Result: &v2.Result{
+				State: v2.Result_SKIPPED,
+				Errors: []*v2.Result_Error{
+					&v2.Result_Error{
+						Source:   v2.Result_Error_TEST,
+						Severity: v2.Result_Error_WARNING,
+						Details: &_struct.Struct{
+							Fields: map[string]*_struct.Value{
+								skipReasonKey: &_struct.Value{
+									Kind: &_struct.Value_StringValue{
+										StringValue: result.SkipReason,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+	return nil
+}
+
+func failedTestResult(requestName string, result *testResult) *rtd.ReportResultRequest {
+	if len(result.Errors) > 0 {
+		rr := rtd.ReportResultRequest{
+			Request: requestName,
+			Result: &v2.Result{
+				State: v2.Result_FAILED,
+			},
+		}
+		for _, e := range result.Errors {
+			rr.Result.Errors = append(rr.Result.Errors, &v2.Result_Error{
+				Source:   v2.Result_Error_TEST,
+				Severity: v2.Result_Error_CRITICAL,
+				Details: &_struct.Struct{
+					Fields: map[string]*_struct.Value{
+						"time": &_struct.Value{
+							Kind: &_struct.Value_StringValue{
+								StringValue: e.Time,
+							},
+						},
+						"reason": &_struct.Value{
+							Kind: &_struct.Value_StringValue{
+								StringValue: e.Reason,
+							},
+						},
+						"file": &_struct.Value{
+							Kind: &_struct.Value_StringValue{
+								StringValue: e.File,
+							},
+						},
+						"line": &_struct.Value{
+							Kind: &_struct.Value_NumberValue{
+								NumberValue: float64(e.Line),
+							},
+						},
+						"stack": &_struct.Value{
+							Kind: &_struct.Value_StringValue{
+								StringValue: e.Stack,
+							},
+						},
+					},
+				},
+			})
+		}
+		return &rr
+	}
+	return nil
 }
--- src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
+++ src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
@@ -9,13 +9,20 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
+	v2 "go.chromium.org/chromiumos/config/go/api/test/results/v2"
 	rtd "go.chromium.org/chromiumos/config/go/api/test/rtd/v1"
 )
 
-const reqName1 = "Name1"
-const reqName2 = "Name2"
+const reqName1 = "PassedTest1"
+const reqName2 = "SkippedTest1"
+const reqName3 = "PassedTest2"
+const reqName4 = "SkippedTest2"
+const reqName5 = "FailedTest1"
 const test1 = "launcher.PinAppToShelf.clamshell_mode"
 const test2 = "launcher.PinAppToShelf.tablet_mode"
+const test3 = "launcher.CreateAndRenameFolder.clamshell_mode"
+const test4 = "launcher.CreateAndRenameFolder.tablet_mode"
+const test5 = "meta.LocalFail"
 const workDir1 = "/tmp/tast/result1"
 const workDir2 = "/tmp/tast/result2"
 const sinkPort = 22
@@ -37,6 +44,27 @@ var inv = rtd.Invocation{
 		{
 			Name: reqName2,
 			Test: test2,
+			Environment: &rtd.Request_Environment{
+				WorkDir: workDir2,
+			},
+		},
+		{
+			Name: reqName3,
+			Test: test3,
+			Environment: &rtd.Request_Environment{
+				WorkDir: workDir1,
+			},
+		},
+		{
+			Name: reqName4,
+			Test: test4,
+			Environment: &rtd.Request_Environment{
+				WorkDir: workDir2,
+			},
+		},
+		{
+			Name: reqName5,
+			Test: test5,
 			Environment: &rtd.Request_Environment{
 				WorkDir: workDir2,
 			},
@@ -77,7 +105,7 @@ func TestUnmarshalInvocation(t *testing.T) {
 func TestNewArgs(t *testing.T) {
 	expectedArgs := runArgs{
 		target:    dut1,
-		patterns:  []string{test1, test2},
+		patterns:  []string{test1, test2, test3, test4, test5},
 		tlwServer: fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
 		resultDir: workDir1,
 	}
@@ -107,5 +135,188 @@ func TestGenArgList(t *testing.T) {
 	argList := genArgList(&args)
 	if diff := cmp.Diff(expectedArgList, argList, cmp.AllowUnexported(runArgs{})); diff != "" {
 		t.Errorf("Got unexpected argument from genArgList (-want +got):\n%s", diff)
+	}
+}
+
+// TestTestResults makes correct test result requests are generated
+func TestMakeResultRequests(t *testing.T) {
+	jsonText := `[
+		{
+		  "name": "launcher.CreateAndRenameFolder.tablet_mode",
+		  "pkg": "chromiumos/tast/local/bundles/cros/launcher",
+		  "desc": "Renaming Folder In Launcher",
+		  "contacts": [
+			"user@chromium.org"
+		  ],
+		  "attr": [
+			"group:mainline",
+			"informational",
+			"name:launcher.CreateAndRenameFolder.tablet_mode",
+			"bundle:cros",
+			"dep:chrome",
+			"dep:tablet_mode"
+		  ],
+		  "data": null,
+		  "softwareDeps": [
+			"chrome",
+			"tablet_mode"
+		  ],
+		  "timeout": 120000000000,
+		  "errors": null,
+		  "start": "2020-12-21T14:02:08.288622615-08:00",
+		  "end": "2020-12-21T14:02:08.2886929-08:00",
+		  "outDir": "/tmp/tast/results/20201221-140206/tests/launcher.CreateAndRenameFolder.tablet_mode",
+		  "skipReason": "missing SoftwareDeps: tablet_mode, DUT does not have an internal display"
+		},
+		{
+		  "name": "launcher.PinAppToShelf.tablet_mode",
+		  "pkg": "chromiumos/tast/local/bundles/cros/launcher",
+		  "desc": "Using Launcher To Pin Application to Shelf",
+		  "contacts": [
+			"user@chromium.org"
+		  ],
+		  "attr": [
+			"group:mainline",
+			"informational",
+			"name:launcher.PinAppToShelf.tablet_mode",
+			"bundle:cros",
+			"dep:chrome",
+			"dep:tablet_mode"
+		  ],
+		  "data": null,
+		  "softwareDeps": [
+			"chrome",
+			"tablet_mode"
+		  ],
+		  "timeout": 120000000000,
+		  "errors": null,
+		  "start": "2020-12-21T14:02:08.288727005-08:00",
+		  "end": "2020-12-21T14:02:08.288773181-08:00",
+		  "outDir": "/tmp/tast/results/20201221-140206/tests/launcher.PinAppToShelf.tablet_mode",
+		  "skipReason": "missing SoftwareDeps: tablet_mode, DUT does not have an internal display"
+		},
+		{
+		  "name": "launcher.CreateAndRenameFolder.clamshell_mode",
+		  "pkg": "chromiumos/tast/local/bundles/cros/launcher",
+		  "desc": "Renaming Folder In Launcher",
+		  "contacts": [
+			"user@chromium.org"
+		  ],
+		  "attr": [
+			"group:mainline",
+			"informational",
+			"name:launcher.CreateAndRenameFolder.clamshell_mode",
+			"bundle:cros",
+			"dep:chrome"
+		  ],
+		  "data": null,
+		  "softwareDeps": [
+			"chrome"
+		  ],
+		  "timeout": 120000000000,
+		  "errors": null,
+		  "start": "2020-12-21T14:02:08.289113748-08:00",
+		  "end": "2020-12-21T14:02:44.350760265-08:00",
+		  "outDir": "/tmp/tast/results/20201221-140206/tests/launcher.CreateAndRenameFolder.clamshell_mode",
+		  "skipReason": ""
+		},
+		{
+		  "name": "meta.LocalFail",
+		  "pkg": "chromiumos/tast/local/bundles/cros/meta",
+		  "desc": "Always fails",
+		  "contacts": [
+			"tast-owners@google.com"
+		  ],
+		  "attr": [
+			"name:meta.LocalFail",
+			"bundle:cros",
+			"disabled"
+		  ],
+		  "data": null,
+		  "timeout": 120000000000,
+		  "errors": [
+			{
+			  "time": "2020-12-21T14:02:44.356245945-08:00",
+			  "reason": "Failed",
+			  "file": "/home/user/trunk/src/platform/tast-tests/src/chromiumos/tast/local/bundles/cros/meta/local_fail.go",
+			  "line": 22,
+			  "stack": "Failed\n\tat chromiumos/tast/local/bundles/cros/meta.LocalFail (local_fail.go:22)\n\tat chromiumos/tast/internal/planner.runTestWithRoot.func3 (run.go:733)\n\tat chromiumos/tast/internal/planner.safeCall.func2 (safe.go:92)\n\tat runtime.goexit (asm_amd64.s:1357)"
+			}
+		  ],
+		  "start": "2020-12-21T14:02:44.352425644-08:00",
+		  "end": "2020-12-21T14:02:44.573035513-08:00",
+		  "outDir": "/tmp/tast/results/20201221-140206/tests/meta.LocalFail",
+		  "skipReason": ""
+		},
+		{
+		  "name": "launcher.PinAppToShelf.clamshell_mode",
+		  "pkg": "chromiumos/tast/local/bundles/cros/launcher",
+		  "desc": "Using Launcher To Pin Application to Shelf",
+		  "contacts": [
+			"user@chromium.org"
+		  ],
+		  "attr": [
+			"group:mainline",
+			"informational",
+			"name:launcher.PinAppToShelf.clamshell_mode",
+			"bundle:cros",
+			"dep:chrome"
+		  ],
+		  "data": null,
+		  "softwareDeps": [
+			"chrome"
+		  ],
+		  "timeout": 120000000000,
+		  "errors": null,
+		  "start": "2020-12-21T14:02:44.573275005-08:00",
+		  "end": "2020-12-21T14:03:45.514505311-08:00",
+		  "outDir": "/tmp/tast/results/20201221-140206/tests/launcher.PinAppToShelf.clamshell_mode",
+		  "skipReason": ""
+		}
+	  ]`
+	expectedResults := []*rtd.ReportResultRequest{
+		{
+			Request: reqName1,
+			Result: &v2.Result{
+				State: v2.Result_SUCCEEDED,
+			},
+		},
+		{
+			Request: reqName2,
+			Result: &v2.Result{
+				State: v2.Result_SKIPPED,
+			},
+		},
+		{
+			Request: reqName3,
+			Result: &v2.Result{
+				State: v2.Result_SUCCEEDED,
+			},
+		},
+		{
+			Request: reqName4,
+			Result: &v2.Result{
+				State: v2.Result_SKIPPED,
+			},
+		},
+		{
+			Request: reqName5,
+			Result: &v2.Result{
+				State: v2.Result_FAILED,
+			},
+		},
+	}
+
+	resultRequests, err := makeResultRequests(&inv, []byte(jsonText))
+	if err != nil {
+		t.Fatal("Failed to unmarshal invocation data:", err)
+	}
+	for i, r := range resultRequests {
+		if expectedResults[i].Request != r.Request {
+			t.Errorf("Got unexpected request name from makeResultRequests want %q got %q", expectedResults[i].Request, r.Request)
+		}
+		if expectedResults[i].Result.State != r.Result.State {
+			t.Errorf("Got unexpected state for test %q from makeResultRequests want %v got %v", r.Request, expectedResults[i].Result.State, r.Result.State)
+		}
 	}
 }

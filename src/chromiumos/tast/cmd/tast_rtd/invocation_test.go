1f947c6fd666b625d0e21f1e143d38f964bb5314
Seewai Fu <seewaifu@google.com>
2020-12-30 18:00:27 -0800

    tast_rtd: add additional flags when invoke tast
    Add additional flags when invoke tast.
    
    Manual Test:
    $ cros_sdk /home/${USER}/trunk/src/scripts/create_remote_test_driver --output_dir "${tmp}"
    ...
    $ gcloud builds submit . --tag=gcr.io/chromeos-rtd-dev/${USER}-test --project=chromeos-rtd-dev
    ...
    $ docker pull gcr.io/chromeos-rtd-dev/${USER}-test
    ...
    $ docker run --rm -v  /usr/local/google/home/seewaifu/chromiumos/rundir:/test_data --network host 9e8f790c3fcc /usr/src/rtd/tast/bin/tast_rtd -input=/test_data/test.pb
    2021/01/07 05:40:52 Input File: /test_data/test.pb
    2021/01/07 05:40:52 [tast] Command line: /usr/src/rtd/tast/bin/tast -logtime=false -verbose=true run -downloaddata=batch -downloadprivatebundles=true -timeout=3000 -remotebundledir=/usr/src/rtd/tast/bundles/remote -resultsdir=/tmp/tast/result1 -sshretries=2 -remotedatadir=/usr/src/rtd/tast/bundles/data -remoterunner=/usr/src/rtd/tast/bin/remote_test_runner -defaultvarsdir=/usr/src/rtd/tast/vars -keyfile=/usr/src/rtd/tast/ssh_keys/testing_rsa -build=false 127.0.0.1:2222 launcher.PinAppToShelf.clamshell_mode launcher.PinAppToShelf.tablet_mode
    2021/01/07 05:40:52 [tast] Using SSH key /usr/src/rtd/tast/ssh_keys/testing_rsa
    2021/01/07 05:40:52 [tast] Writing results to /tmp/tast/result1
    2021/01/07 05:40:52 [tast] Connecting to 127.0.0.1:2222
    ...
    2021/01/07 05:42:28 [tast] Starting /usr/src/rtd/tast/bin/remote_test_runner locally
    2021/01/07 05:42:28 [tast] Ran 0 remote test(s) in 38ms
    2021/01/07 05:42:28 [tast] Collecting system information
    2021/01/07 05:42:31 [tast] --------------------------------------------------------------------------------
    2021/01/07 05:42:31 [tast] launcher.PinAppToShelf.clamshell_mode  [ PASS ]
    2021/01/07 05:42:31 [tast] launcher.PinAppToShelf.tablet_mode     [ PASS ]
    2021/01/07 05:42:31 [tast] --------------------------------------------------------------------------------
    2021/01/07 05:42:31 [tast] Results saved to /tmp/tast/result1
    
    BUG=chromium:1162538
    TEST=Manual testing. New unit test passed
    
    Cq-Depend: chromium:2606390
    Change-Id: If9ef709a75667e353c73c3c6a42f58882c2753fe

--- src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
+++ src/chromiumos/tast/cmd/tast_rtd/invocation_test.go
@@ -5,6 +5,8 @@ package main
 
 import (
 	"fmt"
+	"path/filepath"
+	"sort"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
@@ -75,14 +77,31 @@ func TestUnmarshalInvocation(t *testing.T) {
 
 // TestNewArgs makes sure newArgs creates the correct arguments for tast.
 func TestNewArgs(t *testing.T) {
+	rtdPath := "/usr/src/rtd"
 	expectedArgs := runArgs{
-		target:    dut1,
-		patterns:  []string{test1, test2},
-		tlwServer: fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
-		resultDir: workDir1,
+		target:   dut1,
+		patterns: []string{test1, test2},
+		tastFlags: map[string]string{
+			verboseFlag: "true",
+			logTimeFlag: "false",
+		},
+		runFlags: map[string]string{
+			sshRetriesFlag:             "2",
+			downloadDataFlag:           "batch",
+			buildFlag:                  "false",
+			downloadPrivateBundlesFlag: "true",
+			timeOutFlag:                "3000",
+			resultsDirFlag:             workDir1,
+			tlwServerFlag:              fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
+			remoteBundlerDirFlag:       filepath.Join(rtdPath, "tast", "bundles", "remote"),
+			remoteDataDirFlag:          filepath.Join(rtdPath, "tast", "bundles", "data"),
+			remoteRunnerFlag:           filepath.Join(rtdPath, "tast", "bin", "remote_test_runner"),
+			defaultVarsDirFlag:         filepath.Join(rtdPath, "tast", "vars"),
+			keyfile:                    filepath.Join(rtdPath, "tast", "ssh_keys", "testing_rsa"),
+		},
 	}
 
-	args := newArgs(&inv)
+	args := newArgs(&inv, rtdPath)
 	if diff := cmp.Diff(&expectedArgs, args, cmp.AllowUnexported(runArgs{})); diff != "" {
 		t.Errorf("Got unexpected argument from newArgs (-want +got):\n%s", diff)
 	}
@@ -91,21 +110,46 @@ func TestNewArgs(t *testing.T) {
 // TestGenArgList makes sure genArgList generates the correct list of argument for tast.
 func TestGenArgList(t *testing.T) {
 	args := runArgs{
-		target:    dut1,
-		patterns:  []string{test1, test2},
-		tlwServer: fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
-		resultDir: workDir1,
+		target:   dut1,
+		patterns: []string{test1, test2},
+		tastFlags: map[string]string{
+			verboseFlag: "true",
+			logTimeFlag: "false",
+		},
+		runFlags: map[string]string{
+			sshRetriesFlag:             "2",
+			downloadDataFlag:           "batch",
+			buildFlag:                  "false",
+			downloadPrivateBundlesFlag: "true",
+			timeOutFlag:                "3000",
+			resultsDirFlag:             workDir1,
+			tlwServerFlag:              fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
+		},
 	}
 
-	expectedArgList := []string{
-		"run",
-		"-tlwserver", fmt.Sprintf("%v:%v", tlwAddress, tlwPort),
-		"-resultsdir", workDir1,
-		dut1,
-		test1, test2,
+	var expectedArgList []string
+	for key, value := range args.tastFlags {
+		expectedArgList = append(expectedArgList, fmt.Sprintf("%v=%v", key, value))
 	}
+	runIndex := len(expectedArgList)
+	expectedArgList = append(expectedArgList, "run")
+	for key, value := range args.runFlags {
+		expectedArgList = append(expectedArgList, fmt.Sprintf("%v=%v", key, value))
+	}
+	dutIndex := len(expectedArgList)
+	expectedArgList = append(expectedArgList, dut1)
+	expectedArgList = append(expectedArgList, test1)
+	expectedArgList = append(expectedArgList, test2)
+
 	argList := genArgList(&args)
-	if diff := cmp.Diff(expectedArgList, argList, cmp.AllowUnexported(runArgs{})); diff != "" {
-		t.Errorf("Got unexpected argument from genArgList (-want +got):\n%s", diff)
+
+	// Sort both lists so that we can compare them.
+	sort.Sort(sort.StringSlice(expectedArgList[0:runIndex]))
+	sort.Sort(sort.StringSlice(argList[0:runIndex]))
+	sort.Sort(sort.StringSlice(expectedArgList[runIndex+1 : dutIndex]))
+	sort.Sort(sort.StringSlice(argList[runIndex+1 : dutIndex]))
+
+	if diff := cmp.Diff(argList, expectedArgList, cmp.AllowUnexported(runArgs{})); diff != "" {
+		t.Errorf("Got unexpected argument from genArgList (-got %v +want %v):\n%s", argList, expectedArgList, diff)
 	}
 }

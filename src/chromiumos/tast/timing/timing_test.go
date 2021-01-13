06dcdda7d95d2de9faf62e12ff65c747d51c114f
Shuhei Takahashi <nya@chromium.org>
2021-01-13 17:24:34 +0900

    timing: Split context-related things to another file
    Things defined in timing.go can be categorized into two: those
    interacting with context.Context and others. This patch isolates
    context-related logic to context.go.
    
    This is a pure move change. No functional change is expected.
    
    BUG=chromium:1127169
    TEST=fast_build.sh -T
    
    Change-Id: I761f6e8730bbbbc7a80a4f7abe86e52cbd4fba84

--- src/chromiumos/tast/timing/timing_test.go
+++ src/chromiumos/tast/timing/timing_test.go
@@ -6,7 +6,6 @@ package timing
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"strings"
 	"testing"
@@ -40,66 +39,6 @@ func (c *fakeClock) now() time.Time {
 	t := time.Unix(c.sec, 0)
 	c.sec++
 	return t
-}
-
-func TestContext(t *testing.T) {
-	if cl, cs, ok := FromContext(context.Background()); ok || cl != nil || cs != nil {
-		t.Errorf("FromContext(%v) = (%v, %v, %v); want (%v, %v, %v)", context.Background(), cl, cs, ok, nil, nil, false)
-	}
-
-	l := NewLog()
-	ctx := NewContext(context.Background(), l)
-	if cl, cs, ok := FromContext(ctx); !ok || cl != l || cs != l.Root {
-		t.Errorf("FromContext(%v) = (%v, %v, %v); want (%v, %v, %v)", ctx, cl, cs, ok, l, &l.Root, true)
-	}
-}
-
-func TestStartNil(t *testing.T) {
-	// Start should be okay with receiving a context without a Log attached to it,
-	// and Stage.End should be okay with a nil receiver.
-	_, st := Start(context.Background(), "mystage")
-	st.End()
-}
-
-func TestStartSeq(t *testing.T) {
-	l := NewLog()
-	ctx := NewContext(context.Background(), l)
-	ctx1, st1 := Start(ctx, "stage1")
-	_, st2 := Start(ctx1, "stage2")
-	st2.End()
-	st1.End()
-
-	if len(l.Root.Children) != 1 {
-		t.Errorf("Got %d stages; want 1", len(l.Root.Children))
-	} else if l.Root.Children[0].Name != "stage1" {
-		t.Errorf("Got stage %q; want %q", l.Root.Children[0].Name, "stage1")
-	}
-
-	if len(l.Root.Children[0].Children) != 1 {
-		t.Errorf("Got %d stages; want 1", len(l.Root.Children[0].Children))
-	} else if l.Root.Children[0].Children[0].Name != "stage2" {
-		t.Errorf("Got stage %q; want %q", l.Root.Children[0].Children[0].Name, "stage2")
-	}
-}
-
-func TestStartPar(t *testing.T) {
-	l := NewLog()
-	ctx := NewContext(context.Background(), l)
-	_, st1 := Start(ctx, "stage1")
-	_, st2 := Start(ctx, "stage2")
-	st2.End()
-	st1.End()
-
-	if len(l.Root.Children) != 2 {
-		t.Errorf("Got %d stages; want 2", len(l.Root.Children))
-	} else {
-		if l.Root.Children[0].Name != "stage1" {
-			t.Errorf("Got stage %q; want %q", l.Root.Children[0].Name, "stage1")
-		}
-		if l.Root.Children[1].Name != "stage2" {
-			t.Errorf("Got stage %q; want %q", l.Root.Children[1].Name, "stage2")
-		}
-	}
 }
 
 // writeLog returns a buffer containing JSON data written by lg.Write.

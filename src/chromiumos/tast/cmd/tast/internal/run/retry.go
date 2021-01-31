c9f543fc3b09b58853a5ee772e369d1010e31c81
Seewai Fu <seewaifu@google.com>
2021-01-31 15:45:33 -0800

    Tast: Support "maximum number of allowed failures" via cmd-line switch
    Manual test passed. New unit tests have been added and passed.
    
    Manual test output
    2021-02-05T23:37:33.362715Z Command line: tast run -maxtestfailures=2 hana meta.LocalFail launcher.PinAppToShelf.* nonExitingTest example.Pass meta.RemoteFail meta.RemotePass
    ...
    2021-02-05T23:39:24.424063Z Starting /tmp/tast/build/host/remote_test_runner locally
    2021-02-05T23:39:24.435653Z Got global error: the maximum number of test failures (2) reached: testing jobs will be terminated
    2021-02-05T23:39:25.478059Z Test runner failed: the maximum number of test failures (2) reached: testing jobs will be terminated
    2021-02-05T23:39:25.478135Z Ran 1 remote test(s) in 3.976s
    2021-02-05T23:39:25.478205Z Failed to run tests: the maximum number of test failures (2) reached: testing jobs will be terminated
    2021-02-05T23:39:28.555030Z --------------------------------------------------------------------------------
    2021-02-05T23:39:28.555076Z example.Pass                           [ PASS ]
    2021-02-05T23:39:28.555293Z meta.LocalFail                         [ FAIL ] Failed
    2021-02-05T23:39:28.555357Z launcher.PinAppToShelf.clamshell_mode  [ PASS ]
    2021-02-05T23:39:28.555413Z launcher.PinAppToShelf.tablet_mode     [ PASS ]
    2021-02-05T23:39:28.555432Z meta.RemoteFail                        [ FAIL ] Failed
    2021-02-05T23:39:28.555447Z
    2021-02-05T23:39:28.555462Z Run did not finish successfully; results are incomplete
    2021-02-05T23:39:28.555852Z --------------------------------------------------------------------------------
    2021-02-05T23:39:28.555881Z Results saved to /tmp/tast/results/20210205-153733
    2021-02-05T23:39:28.555911Z Failed to run tests: the maximum number of test failures (2) reached: testing jobs will be terminated
    
    BUG=chromium:1100715
    TEST=./fast_build.sh -T; tast run -maxtestfailures=2 hana meta.LocalFail launcher.PinAppToShelf.* nonExitingTest example.Pass meta.RemoteFail meta.RemotePass
    
    Change-Id: I90ff5638aeecab5e2691f3d5f17ebbf9104c43bf

--- src/chromiumos/tast/cmd/tast/internal/run/retry.go
+++ src/chromiumos/tast/cmd/tast/internal/run/retry.go
@@ -34,9 +34,7 @@ func runTestsWithRetry(ctx context.Context, cfg *Config, patterns []string, runT
 		if rerr == nil {
 			break
 		}
-
 		cfg.Logger.Logf("Test runner failed: %v", rerr)
-
 		// If runTests didn't provide a list of remaining tests, give up.
 		if unstarted == nil {
 			return allResults, rerr

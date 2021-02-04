ec0626336de626cb55e89e05b0c8932ce63ddd82
Keigo Oka <oka@chromium.org>
2021-02-04 13:07:40 +0000

    sshtest: remove NOLINT
    BUG=chromium:1047737
    TEST=fast_build.sh -T
    TEST=repo upload
    
    Change-Id: I1c80a6b3eafa7d147f65ebb98ebd296a08de99c2

--- src/chromiumos/tast/internal/sshtest/default.go
+++ src/chromiumos/tast/internal/sshtest/default.go
@@ -24,7 +24,7 @@ func MustGenerateKeys() (userKey, hostKey *rsa.PrivateKey) {
 }
 
 // TestData contains common data that can be used by tests that interact with an SSHServer.
-type TestData struct { // NOLINT
+type TestData struct {
 	Srv         *SSHServer
 	UserKeyFile string
 }

8891b449608a60df067236aaabc5c3f238e8b607
Seewai Fu <seewaifu@google.com>
2021-02-09 12:48:48 -0800

    tast: Terminate testing when reports server set terminate response.
    Add handling of terminate response from tast_rtd in tast. Tast will
    terminate testing if it get a terminate response from reports servers.
    
    Unit Test result:
    $ ./fast_build.sh -T
    ok  	chromiumos/tast/autocaps	(cached)
    ok  	chromiumos/tast/caller	(cached)
    ok  	chromiumos/tast/cmd/tast	(cached)
    ok  	chromiumos/tast/cmd/tast/internal/build	(cached)
    ok  	chromiumos/tast/cmd/tast/internal/display	(cached)
    ok  	chromiumos/tast/cmd/tast/internal/logging	(cached)
    ok  	chromiumos/tast/cmd/tast/internal/run	(cached)
    ...
    
    BUG=chromium:1174861
    TEST=New unit test was created and passed.
    
    Change-Id: I045523a3438665a160b21d43fbd3c3add1cd7d22

--- src/chromiumos/tast/internal/fakereports/fake_reports.go
+++ src/chromiumos/tast/internal/fakereports/fake_reports.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"io"
 	"net"
+	"sync"
 	"testing"
 
 	"github.com/golang/protobuf/ptypes/empty"
@@ -19,8 +20,11 @@ import (
 
 // Server provides a fake resports server to test reports client implementation.
 type Server struct {
-	logData map[logKey][]byte
-	results []*protocol.ReportResultRequest
+	mtx             sync.Mutex
+	logData         map[logKey][]byte
+	results         []*protocol.ReportResultRequest
+	maxTestFailures int // maxTestFailures specifies maximum number of test allowed. Zero means unlimited.
+	failuresCount   int // The number of test failures so far.
 }
 
 type logKey struct {
@@ -32,7 +36,7 @@ var _ protocol.ReportsServer = &Server{}
 
 // Start starts a gRPC server serving ReportsServer in the background for tests.
 // Callers are responsible for stopping the server by stopFunc().
-func Start(t *testing.T) (server *Server, stopFunc func(), addr string) {
+func Start(t *testing.T, maxTestFailures int) (server *Server, stopFunc func(), addr string) {
 	s := &Server{}
 	srv := grpc.NewServer()
 	protocol.RegisterReportsServer(srv, s)
@@ -42,6 +46,7 @@ func Start(t *testing.T) (server *Server, stopFunc func(), addr string) {
 		t.Fatal("Failed to listen: ", err)
 	}
 	s.logData = make(map[logKey][]byte)
+	s.maxTestFailures = maxTestFailures
 	go srv.Serve(lis)
 	return s, srv.Stop, lis.Addr().String()
 }
@@ -61,7 +66,9 @@ func (s *Server) LogStream(stream protocol.Reports_LogStreamServer) error {
 			test:    test,
 			logPath: req.LogPath,
 		}
+		s.mtx.Lock()
 		s.logData[key] = append(s.logData[key], req.Data...)
+		s.mtx.Unlock()
 	}
 }
 
@@ -71,16 +78,25 @@ func (s *Server) GetLog(test, logPath string) []byte {
 		test:    test,
 		logPath: logPath,
 	}
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
 	return s.logData[key]
 }
 
 // ReportResult provides a means for reports clients to test ReportResult requests.
 func (s *Server) ReportResult(ctx context.Context, req *protocol.ReportResultRequest) (*protocol.ReportResultResponse, error) {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
 	s.results = append(s.results, req)
-	return &protocol.ReportResultResponse{}, nil
+	if len(req.Errors) > 0 {
+		s.failuresCount++
+	}
+	return &protocol.ReportResultResponse{Terminate: s.maxTestFailures > 0 && s.failuresCount >= s.maxTestFailures}, nil
 }
 
 // Results returns all results have been received for this server.
 func (s *Server) Results() []*protocol.ReportResultRequest {
+	s.mtx.Lock()
+	defer s.mtx.Unlock()
 	return s.results
 }

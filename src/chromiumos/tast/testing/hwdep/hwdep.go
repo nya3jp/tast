b9eff176a5c42645817630962d2158ddc0b5e32e
Kevin Lund <kglund@google.com>
2021-02-11 15:49:23 -0700

    Tast: Add WifiIntel hardware dependency
    This CL adds the hardware dependency WifiIntel to Tast, which allows
    tests to run iff they use Intel WiFi.
    
    This is done by defining a list of devices which do not use Intel WiFi
    and skipping on those devices. This is a temporary measure which will
    be replaced when WiFi configs are added to device.Config as discussed in
    ${BUG}.
    
    Some platforms have models both with and without Intel WiFi, so a
    combination of SkipOnPlatform and SkipOnModel is used to account for
    this. This also prevents us from having to maintain an exhaustive list
    of all models without Intel WiFi, which would be very long and
    difficult to maintain.
    
    BUG=chromium:1070299
    TEST=fast_build.sh -T
    TEST=Add `HardwareDeps: hwdep.D(hwdep.WifiIntel())` to a test and run on
    various boards.
    Results:
    zork(morphius): Runs correctly
    zork(ezkinil): Skips "ModelId matched with skip-on list"
    grunt-kernelnext(careena): Skips "PlatformId matched with skip-on list"
    
    Change-Id: Ie22eb7617cdd466b218eca0fddf8da63ba60de72

--- src/chromiumos/tast/testing/hwdep/hwdep.go
+++ src/chromiumos/tast/testing/hwdep/hwdep.go
@@ -461,3 +461,32 @@ func Nvme() Condition {
 	}, CEL: "dut.hardware_features.storage.storage_type == api.Component.Storage.StorageType.NVME",
 	}
 }
+
+// WifiIntel returns a hardware dependency condition that is satisfied iff
+// the DUT is using an Intel WiFi chip.
+func WifiIntel() Condition {
+	// TODO(crbug.com/1070299): we don't yet have relevant fields in device.Config
+	// about WiFi chip, so list the known platforms here for now.
+	return Condition{Satisfied: func(f *dep.HardwareFeatures) error {
+		platformCondition := SkipOnPlatform(
+			"bob", "elm", "grunt", "hana", "jacuzzi", "kevin",
+			"kukui", "oak", "scarlet", "trogdor", "veyron",
+		)
+		if platformErr := platformCondition.Satisfied(f); platformErr != nil {
+			return platformErr
+		}
+		// NB: These exclusions are somewhat overly broad; some
+		// (but not all) blooglet devices have WiFi chips that would
+		// work for this test. However, for now there is no better way
+		// to specify the exact hardware parameters needed for this
+		// test. (See crbug.com/1070299.)
+		modelCondition := SkipOnModel(
+			"blooglet", "dirinboz", "ezkinil", "vilboz",
+		)
+		if modelErr := modelCondition.Satisfied(f); modelErr != nil {
+			return modelErr
+		}
+		return nil
+	}, CEL: "not_implemented",
+	}
+}

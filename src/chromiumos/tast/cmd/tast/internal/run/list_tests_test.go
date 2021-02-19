bb97993f72ac3d3fca27ba6aae23bdbac9806958
Seewai Fu <seewaifu@google.com>
2021-02-19 11:51:05 -0800

    tast: Enhance tast list to use sharding related flags
    Enhance tast list to use sharding related flags.
    New unit tests have been added and passed.
    
    Manul Test output:
    $ tast list -totalshards=2 -shardindex=0 hana launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*
    launcher.CreateAndRenameFolder.clamshell_mode
    launcher.CreateAndRenameFolder.tablet_mode
    $ tast list -totalshards=2 -shardindex=1 hana launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*
    launcher.PinAppToShelf.clamshell_mode
    launcher.PinAppToShelf.tablet_mode
    
    BUG=chromium:1180307
    TEST=./fast_build.sh -T; tast list -totalshards=2 -shardindex=0 hana launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*;  tast list -totalshards=2 -shardindex=1 hana launcher.CreateAndRenameFolder.* launcher.PinAppToShelf.*
    
    Change-Id: Ib87af92b1ec6b4d14a9140bac2398207ace485fa

--- src/chromiumos/tast/cmd/tast/internal/run/list_tests_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/list_tests_test.go
@@ -100,3 +100,165 @@ func TestListRemoteList(t *gotesting.T) {
 		t.Errorf("Unexpected list of remote tests: got %+v; want %+v", results, tests)
 	}
 }
+
+// TestListTests make sure list test can list all tests.
+func TestListTests(t *gotesting.T) {
+	td := newLocalTestData(t)
+	defer td.close()
+
+	tests := []testing.EntityWithRunnabilityInfo{
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.Test",
+				Desc: "This is a test",
+				Attr: []string{"attr1", "attr2"},
+			},
+		},
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.AnotherTest",
+				Desc: "Another test",
+			},
+		},
+	}
+
+	td.runFunc = func(args *runner.Args, stdout, stderr io.Writer) (status int) {
+		checkArgs(t, args, &runner.Args{
+			Mode:      runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{BundleGlob: mockLocalBundleGlob},
+		})
+
+		json.NewEncoder(stdout).Encode(tests)
+		return 0
+	}
+	td.cfg.totalShards = 1
+	td.cfg.runLocal = true
+
+	results, err := listTests(context.Background(), &td.cfg, &td.state)
+	if err != nil {
+		t.Error("Failed to list local tests: ", err)
+	}
+	expected := make([]*EntityResult, len(tests))
+	for i := 0; i < len(tests); i++ {
+		expected[i] = &EntityResult{EntityInfo: tests[i].EntityInfo, SkipReason: tests[i].SkipReason}
+	}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("Unexpected list of local tests: got %+v; want %+v", results, expected)
+	}
+}
+
+// TestListTestsWithSharding make sure list test can list tests in specified shards.
+func TestListTestsWithSharding(t *gotesting.T) {
+	td := newLocalTestData(t)
+	defer td.close()
+
+	tests := []testing.EntityWithRunnabilityInfo{
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.Test",
+				Desc: "This is a test",
+				Attr: []string{"attr1", "attr2"},
+			},
+		},
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.AnotherTest",
+				Desc: "Another test",
+			},
+		},
+	}
+
+	td.runFunc = func(args *runner.Args, stdout, stderr io.Writer) (status int) {
+		checkArgs(t, args, &runner.Args{
+			Mode:      runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{BundleGlob: mockLocalBundleGlob},
+		})
+
+		json.NewEncoder(stdout).Encode(tests)
+		return 0
+	}
+	td.cfg.totalShards = 2
+	td.cfg.runLocal = true
+
+	for i := 0; i < td.cfg.totalShards; i++ {
+		td.cfg.shardIndex = i
+		results, err := listTests(context.Background(), &td.cfg, &td.state)
+		if err != nil {
+			t.Error("Failed to list local tests: ", err)
+		}
+		expected := []*EntityResult{
+			{EntityInfo: tests[i].EntityInfo, SkipReason: tests[i].SkipReason},
+		}
+		if !reflect.DeepEqual(results, expected) {
+			t.Errorf("Unexpected list of local tests: got %+v; want %+v", results, expected)
+		}
+	}
+}
+
+// TestListTestsWithSkippedTests make sure list test can list skipped tests correctly.
+func TestListTestsWithSkippedTests(t *gotesting.T) {
+	td := newLocalTestData(t)
+	defer td.close()
+
+	tests := []testing.EntityWithRunnabilityInfo{
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.Test",
+				Desc: "This is a test",
+				Attr: []string{"attr1", "attr2"},
+			},
+		},
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.AnotherTest",
+				Desc: "Another test",
+			},
+		},
+		{
+			EntityInfo: testing.EntityInfo{
+				Name: "pkg.SkippedTest",
+				Desc: "Skipped test",
+			},
+			SkipReason: "Skip",
+		},
+	}
+
+	td.runFunc = func(args *runner.Args, stdout, stderr io.Writer) (status int) {
+		checkArgs(t, args, &runner.Args{
+			Mode:      runner.ListTestsMode,
+			ListTests: &runner.ListTestsArgs{BundleGlob: mockLocalBundleGlob},
+		})
+
+		json.NewEncoder(stdout).Encode(tests)
+		return 0
+	}
+	td.cfg.totalShards = 2
+	td.cfg.runLocal = true
+
+	// Shard 0 should include all skipped tests.
+	td.cfg.shardIndex = 0
+	results, err := listTests(context.Background(), &td.cfg, &td.state)
+	if err != nil {
+		t.Error("Failed to list local tests: ", err)
+	}
+	expected := []*EntityResult{
+		{EntityInfo: tests[0].EntityInfo, SkipReason: tests[0].SkipReason},
+		{EntityInfo: tests[2].EntityInfo, SkipReason: tests[2].SkipReason},
+	}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("Unexpected list of local tests in shard 0: got %+v; want %+v", results, expected)
+	}
+
+	td.cfg.shardIndex = 1
+	// Shard 1 should have only one test
+	results, err = listTests(context.Background(), &td.cfg, &td.state)
+	if err != nil {
+		t.Error("Failed to list local tests: ", err)
+	}
+	expected = []*EntityResult{
+		{EntityInfo: tests[1].EntityInfo, SkipReason: tests[1].SkipReason},
+	}
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("Unexpected list of local tests in shard 1: got %+v; want %+v", results, expected)
+	}
+}

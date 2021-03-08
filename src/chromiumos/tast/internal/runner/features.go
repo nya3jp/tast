b02e59112ab58fd9616ce4ef80ce1dfb6c06d6e8
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2021-03-08 09:42:06 +0000

    tast: check the device disk size from lsblk
    Targeted for detecting low-disk devices at Tast runtime as a tentative
    fix.
    This change only gets the disk size but discards the result. Warnings
    will be added when an error occurs.
    
    BUG=chromium:1184468
    TEST=fast_build.sh -t chromiumos/tast/internal/runner
    
    Change-Id: I145cb943025780f542d92380ed17ff4ab8322b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2744524
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>

--- src/chromiumos/tast/internal/runner/features.go
+++ src/chromiumos/tast/internal/runner/features.go
@@ -373,6 +373,18 @@ func newDeviceConfigAndHardwareFeatures() (dc *device.Config, retFeatures *confi
 		}
 	}()
 
+	_, err = func() (int64, error) {
+		b, err := exec.Command("lsblk", "-J", "-b").Output()
+		if err != nil {
+			return 0, err
+		}
+		return findDiskSize(b)
+	}()
+	if err != nil {
+		warns = append(warns, fmt.Sprintf("failed to get disk size: %v", err))
+	}
+	// TODO(crbug.com/1184468, crbug.com/1186743): store the disk size value to features after the new field is added
+
 	return config, features, warns
 }
 
@@ -642,4 +654,110 @@ func findAMDSOC(parsed *lscpuResult) (device.Config_SOC, error) {
 		return device.Config_SOC_STONEY_RIDGE, nil
 	}
 	return device.Config_SOC_UNSPECIFIED, errors.Errorf("unknown AMD model: %s", model)
+}
+
+// lsblk command output differs depending on the version. Attempt parsing in multiple ways to accept all the cases.
+
+// lsblk from util-linux 2.32
+type blockDevices2_32 struct {
+	Name      string `json:"name"`
+	Removable string `json:"rm"`
+	Size      string `json:"size"`
+	Type      string `json:"type"`
+}
+
+type lsblkRoot2_32 struct {
+	BlockDevices []blockDevices2_32 `json:"blockdevices"`
+}
+
+// lsblk from util-linux 2.36.1
+type blockDevices struct {
+	Name      string `json:"name"`
+	Removable bool   `json:"rm"`
+	Size      int64  `json:"size"`
+	Type      string `json:"type"`
+}
+
+type lsblkRoot struct {
+	BlockDevices []blockDevices `json:"blockdevices"`
+}
+
+func parseLsblk2_32(jsonData []byte) (*lsblkRoot, error) {
+	var old lsblkRoot2_32
+	err := json.Unmarshal(jsonData, &old)
+	if err != nil {
+		return nil, err
+	}
+
+	var r lsblkRoot
+	for _, e := range old.BlockDevices {
+		s, err := strconv.ParseInt(e.Size, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		var rm bool
+		if e.Removable == "0" || e.Removable == "" {
+			rm = false
+		} else if e.Removable == "1" {
+			rm = true
+		} else {
+			return nil, fmt.Errorf("unknown value for rm: %q", e.Removable)
+		}
+		r.BlockDevices = append(r.BlockDevices, blockDevices{
+			Name:      e.Name,
+			Removable: rm,
+			Size:      s,
+			Type:      e.Type,
+		})
+	}
+	return &r, nil
+}
+
+func parseLsblk2_36(jsonData []byte) (*lsblkRoot, error) {
+	var r lsblkRoot
+	err := json.Unmarshal(jsonData, &r)
+	if err != nil {
+		return nil, err
+	}
+	return &r, nil
+}
+
+func parseLsblk(jsonData []byte) (*lsblkRoot, error) {
+	var errs []error
+	parsers := []func([]byte) (*lsblkRoot, error){parseLsblk2_36, parseLsblk2_32}
+	for _, p := range parsers {
+		r, err := p(jsonData)
+		if err == nil {
+			return r, nil
+		}
+		errs = append(errs, err)
+	}
+	var errStrings []string
+	for _, e := range errs {
+		errStrings = append(errStrings, e.Error())
+	}
+	return nil, fmt.Errorf("failed to parse JSON in all the expected formats: %s", strings.Join(errStrings, "; "))
+}
+
+// findDiskSize detects the size of the storage device from "lsblk -J -b" output in bytes.
+// When there are multiple disks, returns the size of the largest one.
+func findDiskSize(jsonData []byte) (int64, error) {
+	r, err := parseLsblk(jsonData)
+	if err != nil {
+		return 0, err
+	}
+	var maxSize int64
+	var found bool
+	for _, x := range r.BlockDevices {
+		if x.Type == "disk" && !x.Removable && !strings.HasPrefix(x.Name, "zram") {
+			found = true
+			if x.Size > maxSize {
+				maxSize = x.Size
+			}
+		}
+	}
+	if !found {
+		return 0, errors.New("no disk device found")
+	}
+	return maxSize, nil
 }

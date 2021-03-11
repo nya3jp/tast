bde13504a7f39c5fd43455ea41f173feaeaa6473
Seewai Fu <seewaifu@google.com>
2021-03-11 14:14:57 -0800

    Reland "tast: add flag -companionduts and new API testing.State.CompDUTs"
    This is a reland of 86c042d04f1789c9ed1d272b39c92b13519ea0c4
    
    Original change's description:
    > tast: add flag -companionduts and new API testing.State.CompDUTs
    >
    > This CL is the first in a series to support companion DUTs. It only
    > added a new on command for user to specify companion DUTs and a new API
    > in testing.State for test access the companion DUTs.
    >
    > BUG=chromium:1185396
    > TEST=fast_build.sh -T; tast run -build=true -companiondut=cd1:octopus -companiondut=dut2:127.0.0.1:2225 hana meta.CompanionDUT
    >
    > Change-Id: I06109a03eff0e2dd6959c9922872bdd2e560ec01
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2753451
    > Tested-by: Seewai Fu <seewaifu@google.com>
    > Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > Commit-Queue: Seewai Fu <seewaifu@google.com>
    
    Bug: chromium:1185396
    Change-Id: I610168e10bb54394103964c9c3df629a82a9e70b

--- src/chromiumos/tast/cmd/tast/internal/run/runnerclient/local.go
+++ src/chromiumos/tast/cmd/tast/internal/run/runnerclient/local.go
@@ -551,6 +551,7 @@ func runLocalTestsOnce(ctx context.Context, cfg *config.Config, state *config.St
 				DownloadMode:      cfg.DownloadMode,
 				StartFixtureName:  startFixtureName,
 				SetUpErrors:       setUpErrs,
+				CompanionDUTs:     cfg.CompanionDUTs,
 			},
 			BundleGlob: cfg.LocalBundleGlob(),
 			Devservers: localDevservers,

446a3954902a77290de4a8f2c40eac301299f344
Harvey Yang <chenghaoyang@chromium.org>
2021-03-11 16:04:44 +0800

    tast: Add "iioservice" dependency.
    This is needed to skip the hardware.SensorIioservice(Hard) test on DUTs
    that have iioservice running.
    
    BUG=b:182441449
    TEST=run tast tests on kohaku but skipped without iioservice enabled.
    
    Cq-Depend: chromium:2751630
    Change-Id: I4501a926cbebc8045aa483fdb8577be9c8e46bf7

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -126,6 +126,7 @@ func main() {
 			"houdini64":               "houdini64",
 			"hostap_hwsim":            "wifi_hostap_test",
 			"igt":                     `("video_cards_amdgpu" || "video_cards_intel") && "kernel-5_4"`,
+			"iioservice":              "iioservice",
 			"iwlwifi_rescan":          "iwlwifi_rescan",
 			"lacros":                  "!arm && !arm64", // TODO(crbug.com/1144013): Expand this to include arm as well.
 			"lock_core_pattern":       `"kernel-3_10" || "kernel-3_14" || "kernel-3_18"`,

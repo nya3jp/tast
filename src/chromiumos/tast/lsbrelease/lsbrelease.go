6ed4e8ff4d43b55953823a469c3d24ff4d0e6114
Matt Stark <msta@google.com>
2021-03-17 04:05:16 +0000

    Allow the screen diffing library to access lsb-release
    The tool it sends data to needs to know the type of board the test was
    running on, and the gerrit change to associate it with.
    
    BUG=chromium:1137989
    TEST=Wrote a test using this, and it works.
    
    Change-Id: I0bcccc53ad6d537fb93eb9a80e3eca94d87b615d

--- src/chromiumos/tast/lsbrelease/lsbrelease.go
+++ src/chromiumos/tast/lsbrelease/lsbrelease.go
@@ -56,6 +56,10 @@ const (
 	// ARCSDKVersion is a key for the Android SDK Version of the current
 	// ARC image installed on the DUT.
 	ARCSDKVersion = "CHROMEOS_ARC_ANDROID_SDK_VERSION"
+
+	// GerritChanges is a key for a comma-seperated list of the gerrit changes applied
+	// on top of the code. Only filled for CQ builds (since only they patch a CL in).
+	GerritChanges = "GERRIT_CHANGES"
 )
 
 // allowedPkgs is the list of Go packages that can use this package.
@@ -69,6 +73,7 @@ var allowedPkgs = []string{
 	"chromiumos/tast/local/crash",
 	"chromiumos/tast/local/graphics/trace",
 	"chromiumos/tast/local/rialto",
+	"chromiumos/tast/local/screenshot", // For Board and gerrit changes.
 	"chromiumos/tast/lsbrelease",
 	"chromiumos/tast/remote/bundles/cros/firmware", // For finding firmware file.
 	"chromiumos/tast/remote/firmware/reporters",    // For Board.

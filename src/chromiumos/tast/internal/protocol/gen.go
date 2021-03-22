1baa8f59ca755c9a813b7c781a6ce592844a8b71
Shuhei Takahashi <nya@chromium.org>
2021-03-22 16:37:22 +0900

    protocol: Define the Entity message
    This change defines a few basic protocol buffer messages about entities.
    
      jsonprotocol | protocol
     --------------+-------------
      EntityInfo   | Entity
      EntityError  | Error
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: Id5c6a0cea270bf09f0a1e68d0689d213b13c0207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2777982
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/internal/protocol/gen.go
+++ src/chromiumos/tast/internal/protocol/gen.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-//go:generate protoc -I . --go_out=plugins=grpc:../../../.. fake_core.proto fake_user.proto logging.proto handshake.proto file_transfer.proto reports.proto
+//go:generate protoc -I . --go_out=plugins=grpc:../../../.. fake_core.proto fake_user.proto logging.proto handshake.proto file_transfer.proto reports.proto testing.proto
 
 package protocol
 

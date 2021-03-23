165d3e5a9c0198c3a9a494077256bf8cd2de0797
Keigo Oka <oka@chromium.org>
2021-03-23 08:38:58 +0000

    lint: add linter for deprecated APIs
    Added deprecated APIs linter. Add warnings for local testexec package
    , bundle.LocalDelegate, and bundle.RemoteDelegate as examples.
    
    BUG=chromium:1191191
    TEST=fast_build.sh -T
    TEST=run against files using deprecated APIs
    
    Change-Id: I6dc0fabd6fc7b119f63e760cddcf30b102aacb71

---
+++ src/chromiumos/tast/cmd/tast-lint/check/deprecated_apis.go
@@ -0,0 +1,130 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"log"
+	"path/filepath"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+type deprecatedAPI struct {
+	// pkg is the package path containing deprecated API.
+	pkg string
+	// ident is an exported identifier in the package pkg which is deprecated.
+	// If ident is empty, it means the package itself is deprecated.
+	// Methods are not supported. Only APIs used in the form of
+	// <package name>.<ident> are supported.
+	ident string
+
+	alternative string // alternative to use displayed in the error message
+	bug         string // bug link
+}
+
+// DeprecatedAPIs checks if deprecated APIs are used.
+func DeprecatedAPIs(fs *token.FileSet, f *ast.File) []*Issue {
+	return deprecatedAPIs(fs, f, []*deprecatedAPI{
+		{
+			pkg:         "chromiumos/tast/local/testexec",
+			alternative: "chromiumos/tast/common/testexec",
+			bug:         "https://crbug.com/1119252",
+		},
+		{
+			pkg:         "chromiumos/tast/bundle",
+			ident:       "LocalDelegate",
+			alternative: "Delegate",
+			bug:         "https://crbug.com/1134060",
+		},
+		{
+			pkg:         "chromiumos/tast/bundle",
+			ident:       "RemoteDelegate",
+			alternative: "Delegate",
+			bug:         "https://crbug.com/1134060",
+		},
+	})
+}
+
+func deprecatedAPIs(fs *token.FileSet, f *ast.File, ds []*deprecatedAPI) []*Issue {
+	deprecatedPkg := make(map[string]*deprecatedAPI)
+	deprecatedPkgIdent := make(map[string]map[string]*deprecatedAPI)
+	for _, d := range ds {
+		if d.ident == "" {
+			deprecatedPkg[d.pkg] = d
+			continue
+		}
+		if _, ok := deprecatedPkgIdent[d.pkg]; !ok {
+			deprecatedPkgIdent[d.pkg] = make(map[string]*deprecatedAPI)
+		}
+		deprecatedPkgIdent[d.pkg][d.ident] = d
+	}
+
+	var issues []*Issue
+
+	unquote := func(s string) string {
+		n := len(s)
+		if s[0] != '"' || s[n-1] != '"' {
+			log.Panicf("BUG: %q should be a quoted string", s)
+		}
+		return s[1 : n-1]
+	}
+	// Check deprecated packages.
+	for _, i := range f.Imports {
+		d, ok := deprecatedPkg[unquote(i.Path.Value)]
+		if !ok {
+			continue
+		}
+		issues = append(issues, &Issue{
+			Pos:  fs.Position(i.Pos()),
+			Msg:  fmt.Sprintf("Package %v is deprecated; use %v instead", d.pkg, d.alternative),
+			Link: d.bug,
+		})
+	}
+
+	// Check deprecated exported identifiers.
+	importSpec := make(map[string]*ast.ImportSpec) // key: package identifier
+	for _, i := range f.Imports {
+		sel := filepath.Base(unquote(i.Path.Value))
+		if i.Name != nil {
+			sel = i.Name.Name
+		}
+		importSpec[sel] = i
+	}
+	astutil.Apply(f, func(c *astutil.Cursor) bool {
+		sel, ok := c.Node().(*ast.SelectorExpr)
+		if !ok {
+			return true
+		}
+		x, ok := sel.X.(*ast.Ident)
+		if !ok {
+			return true
+		}
+		i, ok := importSpec[x.Name]
+		if !ok {
+			return true
+		}
+		m, ok := deprecatedPkgIdent[unquote(i.Path.Value)]
+		if !ok {
+			return true
+		}
+		d, ok := m[sel.Sel.Name]
+		if !ok {
+			return true
+		}
+
+		issues = append(issues, &Issue{
+			Pos:  fs.Position(x.Pos()),
+			Msg:  fmt.Sprintf("%v.%v is deprecated; use %v instead", d.pkg, d.ident, d.alternative),
+			Link: d.bug,
+		})
+		return true
+	}, nil)
+
+	return issues
+
+}

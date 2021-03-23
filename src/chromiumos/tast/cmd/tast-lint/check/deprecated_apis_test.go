249a9e9cdaeb5e37f3500f08cc94889908637b13
Keigo Oka <oka@chromium.org>
2021-03-23 08:38:58 +0000

    linter: add linter for deprecated APIs
    Added deprecated APIs linter. Add warnings for local testexec package
    , bundle.LocalDelegate, and bundle.RemoteDelegate as examples.
    
    BUG=chromium:1191191
    TEST=fast_build.sh -T
    TEST=run linter against files using deprecated APIs:
      ./tools/run_lint.sh src/chromiumos/tast/local/gtest/gtest.go src/chromiumos/tast/local/bundlemain/main.go
    
    Change-Id: I6dc0fabd6fc7b119f63e760cddcf30b102aacb71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2781127
    Tested-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/cmd/tast-lint/check/deprecated_apis_test.go
@@ -0,0 +1,45 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package check
+
+import (
+	"testing"
+)
+
+func TestDeprecatedAPIs(t *testing.T) {
+	deprecated := []*deprecatedAPI{{
+		pkg:         "chromiumos/tast/local/testexec",
+		alternative: "chromiumos/tast/common/testexec",
+		bug:         "https://crbug.com/1119252",
+	}, {
+		pkg:         "chromiumos/tast/bundle",
+		ident:       "LocalDelegate",
+		alternative: "Delegate",
+		bug:         "https://crbug.com/1134060",
+	}}
+	const code = `package main
+
+import (
+	b "chromiumos/tast/bundle"
+	"chromiumos/tast/internal/bundle"
+	"chromiumos/tast/local/testexec"
+)
+
+func main() {
+	testexec.CommandContext(ctx, "cat")
+	f(b.LocalDelegate)
+	_ = a.b.LocalDelegate // ok
+	f(bundle.LocalDelegate) // ok
+}
+`
+	want := []string{
+		"testfile.go:6:2: package chromiumos/tast/local/testexec is deprecated; use chromiumos/tast/common/testexec instead",
+		"testfile.go:11:4: chromiumos/tast/bundle.LocalDelegate is deprecated; use Delegate instead",
+	}
+
+	f, fs := parse(code, "testfile.go")
+	issues := deprecatedAPIs(fs, f, deprecated)
+	verifyIssues(t, issues, want)
+}

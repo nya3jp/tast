6a0ed4b3def793ab3ed2e10769fcf18f666f4684
Shuhei Takahashi <nya@chromium.org>
2021-03-24 12:27:07 +0900

    testing: Move several types to new files
    Some types defined in test_instance.go are not closely tied to
    TestInstance. Move those types to separate files.
    
    This is a pure code move.
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: I333351ce9c1e8a0ffee19d9d26c3579cdcb3c082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2786525
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/internal/testing/output.go
@@ -0,0 +1,20 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"chromiumos/tast/internal/jsonprotocol"
+)
+
+// OutputStream is an interface to report streamed outputs of an entity.
+// Note that planner.OutputStream is for multiple entities in contrast.
+type OutputStream interface {
+	// Log reports an informational log message from an entity.
+	Log(msg string) error
+
+	// Error reports an error from by an entity. An entity that reported one or
+	// more errors should be considered failure.
+	Error(e *jsonprotocol.Error) error
+}

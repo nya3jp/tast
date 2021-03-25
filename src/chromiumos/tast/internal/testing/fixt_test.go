1409eb23ff4690ca7f49b27592a42e40731e5220
Shuhei Takahashi <nya@chromium.org>
2021-03-25 11:28:07 +0900

    testing: Introduce methods to return protocol.Entity
    This patch implements the EntityProto method in TestInstance and
    Fixture. It deprecates the EntityInfo method.
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: Idd15fd865f63223c79d9c5ea6b308439cc303d23

--- src/chromiumos/tast/internal/testing/fixt_test.go
+++ src/chromiumos/tast/internal/testing/fixt_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 
 	"chromiumos/tast/internal/jsonprotocol"
+	"chromiumos/tast/internal/protocol"
 )
 
 func TestFixtureEntityInfo(t *testing.T) {
@@ -35,5 +36,34 @@ func TestFixtureEntityInfo(t *testing.T) {
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf("EntityInfo(%#v) mismatch (-got +want):\n%s", fixt, diff)
+	}
+}
+
+func TestFixtureEntityProto(t *testing.T) {
+	fixt := &Fixture{
+		Name:        "chrome.LoggedIn",
+		Desc:        "Make sure logged into a Chrome session",
+		Contacts:    []string{"a@example.com", "b@example.com"},
+		Parent:      "system.Booted",
+		ServiceDeps: []string{"chrome.Service"},
+	}
+	got := fixt.EntityProto()
+	want := &protocol.Entity{
+		Type:        protocol.EntityType_FIXTURE,
+		Name:        "chrome.LoggedIn",
+		Description: "Make sure logged into a Chrome session",
+		Fixture:     "system.Booted",
+		Dependencies: &protocol.EntityDependencies{
+			Services: []string{"chrome.Service"},
+		},
+		Contacts: &protocol.EntityContacts{
+			Emails: []string{"a@example.com", "b@example.com"},
+		},
+		LegacyData: &protocol.EntityLegacyData{
+			Bundle: filepath.Base(os.Args[0]),
+		},
+	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("EntityProto(%#v) mismatch (-got +want):\n%s", fixt, diff)
 	}
 }

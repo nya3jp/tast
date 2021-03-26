9921e6f14cd4fdfd4b000e243d4071775f214a06
Keigo Oka <oka@chromium.org>
2021-03-26 13:59:52 +0000

    testing: remove ExitTimeout
    This field only existed for unit tests.
    Enabled unit tests to specify custom grace period and removed it.
    
    BUG=chromium:1173989
    TEST=fast_build.sh -T
    
    Change-Id: Iff8fc920d456e058ec6a94e59e1233e370961203

--- src/chromiumos/tast/internal/bundle/bundle_test.go
+++ src/chromiumos/tast/internal/bundle/bundle_test.go
@@ -219,10 +219,9 @@ func TestRunTestsTimeout(t *gotesting.T) {
 	ch := make(chan bool, 1)
 	defer func() { ch <- true }()
 	reg.AddTestInstance(&testing.TestInstance{
-		Name:        name1,
-		Func:        func(context.Context, *testing.State) { <-ch },
-		Timeout:     time.Millisecond,
-		ExitTimeout: time.Millisecond, // avoid blocking after timeout
+		Name:    name1,
+		Func:    func(context.Context, *testing.State) { <-ch },
+		Timeout: time.Millisecond,
 	})
 
 	// The second test passes.
@@ -245,7 +244,8 @@ func TestRunTestsTimeout(t *gotesting.T) {
 
 	// The first test should time out after 1 millisecond.
 	// The second test is not run.
-	if err := runTests(context.Background(), &stdout, &args, &staticConfig{}, localBundle, reg.AllTests()); err == nil {
+	cgp := time.Millisecond // avoid blocking after timeout
+	if err := runTests(context.Background(), &stdout, &args, &staticConfig{customGracePeriod: &cgp}, localBundle, reg.AllTests()); err == nil {
 		t.Fatalf("runTests(..., %+v, ...) succeeded unexpectedly", args)
 	}
 

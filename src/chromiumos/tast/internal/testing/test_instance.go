b7df7e859612470906ac024ce3872a45d7519cde
Keigo Oka <oka@chromium.org>
2021-03-26 13:59:52 +0000

    testing: remove ExitTimeout
    This field only existed for unit tests.
    Enabled unit tests to specify custom grace period and removed it.
    
    BUG=chromium:1173989
    TEST=fast_build.sh -T
    
    Change-Id: Iff8fc920d456e058ec6a94e59e1233e370961203

--- src/chromiumos/tast/internal/testing/test_instance.go
+++ src/chromiumos/tast/internal/testing/test_instance.go
@@ -50,15 +50,6 @@ type TestInstance struct {
 	// Pkg contains the Go package in which Func is located.
 	Pkg string
 
-	// ExitTimeout contains the maximum duration to wait for Func to exit after a timeout.
-	// The context passed to Func has a deadline based on Timeout, but Tast waits for an additional ExitTimeout to elapse
-	// before reporting that the test has timed out; this gives the test function time to return after it
-	// sees that its context has expired before an additional error is added about the timeout.
-	// This is exposed for unit tests and should almost always be omitted when defining tests;
-	// a reasonable default will be used.
-	// TODO(oka): Remove ExitTimeout using CustomGracePeriod in planner.Config .
-	ExitTimeout time.Duration
-
 	// Val contains the value inherited from the expanded Param struct for a parameterized test case.
 	// This can be retrieved from testing.State.Param().
 	Val interface{}

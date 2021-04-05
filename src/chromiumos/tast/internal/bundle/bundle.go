e780cb2e765a1ec622b157bbf9cc4d0e698683fa
Keigo Oka <oka@chromium.org>
2021-04-05 07:42:13 +0000

    bundle: implement VarDeps
    This is a part of CLs to support VarDeps.
    In this CL, I updated bundle code.
    
    - TestInstance.VarDeps is added
    - FeatureArgs.MaybeMissingVars is added (tast CLI will pass it)
    
    BUG=chromium:1183238
    TEST=fast_build.sh -T
    
    Change-Id: I818581570b42dc0b545a695382e0e48c5376dbeb

--- src/chromiumos/tast/internal/bundle/bundle.go
+++ src/chromiumos/tast/internal/bundle/bundle.go
@@ -106,6 +106,7 @@ func run(ctx context.Context, clArgs []string, stdin io.Reader, stdout, stderr i
 			// treat the test as not skipped. When we actually try to
 			// run the test later, it will fail with errors.
 			var skipReason string
+
 			if reasons, err := test.Deps().Check(features); err == nil && len(reasons) > 0 {
 				skipReason = strings.Join(append([]string(nil), reasons...), ", ")
 			}

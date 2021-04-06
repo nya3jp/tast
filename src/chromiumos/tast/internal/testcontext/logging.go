5f886ba7445985eb35a466d19ac156fa90614785
Shuhei Takahashi <nya@chromium.org>
2021-04-06 11:11:54 +0900

    testcontext: Use consistent key types for all context values
    This is just a cleanup. No functional change is expected.
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: If21a9e6b087324855eb8e87b8ccef22490809d98

--- src/chromiumos/tast/internal/testcontext/logging.go
+++ src/chromiumos/tast/internal/testcontext/logging.go
@@ -9,21 +9,22 @@ import (
 	"fmt"
 )
 
+// loggerKeyType is the type of the key used for attaching a LoggerFunc to a
+// context.Context.
+type loggerKeyType struct{}
+
 // LoggerFunc is the type of a function to emit log messages.
 type LoggerFunc = func(msg string)
-
-// loggerKey is the key type for LoggerFunc attached to context.Context.
-type loggerKey struct{}
 
 // WithLogger creates a context associated with logger. The returned context can
 // be used to call Log/Logf.
 func WithLogger(ctx context.Context, logger LoggerFunc) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)
+	return context.WithValue(ctx, loggerKeyType{}, logger)
 }
 
 // Logger extracts a logger from a context.
 func Logger(ctx context.Context) (LoggerFunc, bool) {
-	logger, ok := ctx.Value(loggerKey{}).(LoggerFunc)
+	logger, ok := ctx.Value(loggerKeyType{}).(LoggerFunc)
 	return logger, ok
 }
 

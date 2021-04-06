3c25c8288d95150fbac62445af623c5bc5d9a03a
Shuhei Takahashi <nya@chromium.org>
2021-04-06 11:24:43 +0900

    bundle: Pass WaitUntilReady value via Context
    bundle.Delegate provides two hooks to be called on the beginning of
    a run: RunHook and Ready. But in fact both hooks are internally merged
    to a single run hook when Delegate is converted to staticConfig.
    
    An odd thing is that user-written run hooks can't see values of
    -waituntilready, while ready hooks can. Actually ready hooks cheats by
    capturing jsonprotocol.BundleArgs, which is available as a local
    variable in newArgsAndStaticConfig.
    
    In order to capture the variable, we have to create BundleArgs at the
    same time as staticConfig in newArgsAndStaticConfig. This makes it
    difficult to get rid of jsonprotocol dependencies.
    
    This patch solves the problem by making -waituntilready value available
    to ready hooks via formal private API using Context. New context
    functions are private to the framework and should never be exposed to
    user code.
    
    In the longer term, we should drop or replace -waituntilready. Meanwhile
    This change eases isolating jsonprotocol dependencies.
    
    This is a refactoring change. No functional change is expected.
    
    BUG=chromium:1128259
    BUG=chromium:1195719
    TEST=fast_build.sh -T
    
    Change-Id: Ied090f61b807e7330367f29d72d07dbb637399f7

---
+++ src/chromiumos/tast/internal/testcontext/private.go
@@ -0,0 +1,31 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testcontext
+
+import (
+	"context"
+)
+
+// privateDataKey is the key used for attaching a PrivateData to a context.Context.
+const privateDataKey keyType = "PrivateData"
+
+// PrivateData contains information private to the framework.
+type PrivateData struct {
+	// WaitUntilReady carries the value of -waituntilready flag.
+	WaitUntilReady bool
+}
+
+// WithPrivateData attaches PrivateData to context.Context.
+// This function must not be exposed to user code.
+func WithPrivateData(ctx context.Context, pd PrivateData) context.Context {
+	return context.WithValue(ctx, privateDataKey, pd)
+}
+
+// PrivateDataFromContext extracts PrivateData from a context.Context.
+// This function must not be exposed to user code.
+func PrivateDataFromContext(ctx context.Context) (pd PrivateData, ok bool) {
+	pd, ok = ctx.Value(privateDataKey).(PrivateData)
+	return pd, ok
+}

6de2aa8439a0b13bf04004ce28ea0ab501359e12
Renuga Nanjappan <rnanjappan@chromium.org>
2021-04-14 02:13:41 +0000

    Add mtp attr to Tast.
    This will be used to run MTP tast tests.
    
    BUG=b:184199319
    TEST=None
    
    Change-Id: I184cefa84f4340d3e0f3f0b2f35362f126fffcfb

--- src/chromiumos/tast/internal/testing/attr.go
+++ src/chromiumos/tast/internal/testing/attr.go
@@ -144,6 +144,11 @@ Tests in this group are not used for build verification.
 		Desc:     `A group of ARC Functional tests.`,
 	},
 	{
+		Name:     "mtp",
+		Contacts: []string{"arc-engprod@google.com"},
+		Desc:     `A group of tests that test MTP functionality.`,
+	},
+	{
 		Name:     "appcompat",
 		Contacts: []string{"chromeos-engprod@google.com"},
 		Desc:     `A group of ARC app compatibility tests.`,

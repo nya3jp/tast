d65cae5190e53f9a39bb5be7731643057b77919d
Shuhei Takahashi <nya@chromium.org>
2021-04-16 18:10:05 +0900

    planner: Do not emit nil timing logs
    nil is not a valid value for *timing.Log. When necessary, use
    timing.NewLog() instead.
    
    BUG=chromium:1128259
    TEST=fast_build.sh -T
    
    Change-Id: I22cbdfb6d5aed2f88f75a25bfa66b62693f2d726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2831693
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/internal/planner/output_test.go
+++ src/chromiumos/tast/internal/planner/output_test.go
@@ -78,7 +78,7 @@ func TestTestOutputStream(t *gotesting.T) {
 	tout.Log("hello")
 	tout.Error(&protocol.Error{Reason: "faulty", Location: &protocol.ErrorLocation{File: "world.go"}})
 	tout.Log("world")
-	tout.End(nil, nil)
+	tout.End(nil, timing.NewLog())
 
 	got := sink.ReadAll()
 
@@ -103,7 +103,7 @@ func TestTestOutputStreamUnnamedEntity(t *gotesting.T) {
 	tout.Log("hello")
 	tout.Error(&protocol.Error{Reason: "faulty", Location: &protocol.ErrorLocation{File: "world.go"}})
 	tout.Log("world")
-	tout.End(nil, nil)
+	tout.End(nil, timing.NewLog())
 
 	got := sink.ReadAll()
 
@@ -121,7 +121,7 @@ func TestTestOutputStreamErrors(t *gotesting.T) {
 	tout.Start("/tmp/out")
 	tout.Error(&protocol.Error{Reason: "error1", Location: &protocol.ErrorLocation{File: "test1.go"}})
 	tout.Error(&protocol.Error{Reason: "error2", Location: &protocol.ErrorLocation{File: "test2.go"}})
-	tout.End(nil, nil)
+	tout.End(nil, timing.NewLog())
 	tout.Error(&protocol.Error{Reason: "error3", Location: &protocol.ErrorLocation{File: "test3.go"}}) // error after End is ignored
 
 	got := tout.Errors()

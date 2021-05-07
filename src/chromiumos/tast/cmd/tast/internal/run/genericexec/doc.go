1f454af2b30ffb114226ce18718948af9a4af626
Shuhei Takahashi <nya@chromium.org>
2021-05-07 18:13:34 +0900

    run: Introduce genericexec package
    Today we use the runnerCmd interface in runner_cmd.go as an abstraction
    of external command execution on the local machine and over the SSH.
    However its interface is not flexible enough to run gRPC connections
    over its stdin/stdout.
    
    This patch introduces a new package "genericexec" to provide a more
    well-designed API with unit test coverage.
    
    The new package is used nowhere now. The next patch replaces runnerCmd
    with it.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I9469527732b7f6b616537864149eae82234489e9

---
+++ src/chromiumos/tast/cmd/tast/internal/run/genericexec/doc.go
@@ -0,0 +1,7 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package genericexec provides a common interface to execute local commands
+// and remote commands.
+package genericexec

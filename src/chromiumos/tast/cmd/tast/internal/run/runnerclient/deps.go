4a9b05a4455ff2e620c496d369954431f43a6cb2
Shuhei Takahashi <nya@chromium.org>
2021-05-12 17:35:51 +0900

    run: Replace State fields with DUTInfo
    This patch replaces several fields in the State struct that are filled
    in GetDUTInfo with a single protobuf message DUTInfo.
    
    We still use the same JSON-based protocol to send DUTInfo from
    local_test_runner to Tast CLI.
    
    This is a refactoring change. No functional change is expected.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    TEST=fast_build.sh
    TEST=~/go/bin/tast run $DUT meta.LocalPass meta.RemotePass
    
    Change-Id: I007cd54348fc3913d93b59ede15ca64811ca0179

--- src/chromiumos/tast/cmd/tast/internal/run/runnerclient/deps.go
+++ src/chromiumos/tast/cmd/tast/internal/run/runnerclient/deps.go
@@ -30,7 +30,7 @@ func GetDUTInfo(ctx context.Context, cfg *config.Config, state *config.State, cc
 	if !cfg.CheckTestDeps {
 		return nil
 	}
-	if state.SoftwareFeatures != nil {
+	if state.DUTInfo != nil {
 		return errors.New("GetDUTInfo is already called")
 	}
 
@@ -60,43 +60,37 @@ func GetDUTInfo(ctx context.Context, cfg *config.Config, state *config.State, cc
 
 	// If the software feature is empty, then the DUT doesn't know about its features
 	// (e.g. because it's a non-test image and doesn't have a listing of relevant USE flags).
+	// TODO(b/187793617): Remove this check once we migrate to gRPC protocol
+	// and GetDUTInfo gets ability to return errors.
 	if res.SoftwareFeatures == nil {
 		cfg.Logger.Debug("No software features reported by DUT -- non-test image?")
 		return errors.New("can't check test deps; no software features reported by DUT")
 	}
 
-	for _, warn := range res.Warnings {
-		cfg.Logger.Log(warn)
-	}
+	info := res.Proto(ctx, cfg.CheckTestDeps, cfg.TestVars, cfg.MaybeMissingVars)
 
-	state.OSVersion = res.OSVersion
-	state.DefaultBuildArtifactsURL = res.DefaultBuildArtifactsURL
-
-	cfg.Logger.Debug("Software features supported by DUT: ", strings.Join(res.SoftwareFeatures.Available, " "))
-	if res.DeviceConfig != nil {
+	cfg.Logger.Debug("Software features supported by DUT: ", strings.Join(info.GetFeatures().GetSoftware().GetAvailable(), " "))
+	if dc := info.GetFeatures().GetHardware().GetDeprecatedDeviceConfig(); dc != nil {
 		cfg.Logger.Debug("Got DUT device.Config data; dumping to ", deviceConfigFile)
 		if err := ioutil.WriteFile(filepath.Join(cfg.ResDir, deviceConfigFile), []byte(proto.MarshalTextString(res.DeviceConfig)), 0644); err != nil {
 			cfg.Logger.Debugf("Failed to dump %s: %v", deviceConfigFile, err)
 		}
-		state.DeviceConfig = res.DeviceConfig
-		state.HardwareFeatures = res.HardwareFeatures
 	}
-	state.SoftwareFeatures = res.SoftwareFeatures
+
+	state.DUTInfo = info
 	return nil
 }
 
 // featureArgsFromConfig returns feature arguments based on the configuration parameter.
 func featureArgsFromConfig(cfg *config.Config, state *config.State) *jsonprotocol.FeatureArgs {
-	args := jsonprotocol.FeatureArgs{
-		CheckDeps:        cfg.CheckTestDeps,
-		TestVars:         cfg.TestVars,
-		MaybeMissingVars: cfg.MaybeMissingVars,
+	f := state.DUTInfo.GetFeatures()
+	return &jsonprotocol.FeatureArgs{
+		CheckDeps:                   cfg.CheckTestDeps,
+		TestVars:                    cfg.TestVars,
+		MaybeMissingVars:            cfg.MaybeMissingVars,
+		AvailableSoftwareFeatures:   f.GetSoftware().GetAvailable(),
+		UnavailableSoftwareFeatures: f.GetSoftware().GetUnavailable(),
+		DeviceConfig:                jsonprotocol.DeviceConfigJSON{Proto: f.GetHardware().GetDeprecatedDeviceConfig()},
+		HardwareFeatures:            jsonprotocol.HardwareFeaturesJSON{Proto: f.GetHardware().GetHardwareFeatures()},
 	}
-	if state.SoftwareFeatures != nil {
-		args.AvailableSoftwareFeatures = state.SoftwareFeatures.Available
-		args.UnavailableSoftwareFeatures = state.SoftwareFeatures.Unavailable
-		args.DeviceConfig.Proto = state.DeviceConfig
-		args.HardwareFeatures.Proto = state.HardwareFeatures
-	}
-	return &args
 }

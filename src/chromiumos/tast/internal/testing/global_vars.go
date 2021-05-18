ae500fc9ae0f866f96c3900d0c53fc6430b82be0
Seewai Fu <seewaifu@google.com>
2021-05-17 18:48:19 -0700

    tast: make runtime variables globally accessible (WIP)
    This CL makes the current runtime variables globally accessible so that test
    libraries can access runtime variables through context.
    
    BUG=b:187795655
    TEST=./fast_build.sh -T
    
    Change-Id: I295c986a72db86543ed5470734dc48d6a81d34d1

---
+++ src/chromiumos/tast/internal/testing/global_vars.go
@@ -0,0 +1,22 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+// GlobalVar define a structure to define a global runtime variable
+type GlobalVar struct {
+	// Name is the name of the variable.
+	Name string
+
+	// Desc is a description of the variable.
+	Desc string
+}
+
+// globalVars is a list of declared global variables and their descriptions.
+var globalVars = []GlobalVar{
+	{
+		Name: "example.globalBoolean",
+		Desc: "An example variable to demonstrate how to use global varaible",
+	},
+}

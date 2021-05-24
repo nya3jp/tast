91461aa9b804f780d2e08fa27922bc5b383c56a6
Shuhei Takahashi <nya@chromium.org>
2021-05-24 16:23:11 +0900

    runtest: Implement runtest package
    This is a part of patch series to introduce the new "runtest" package
    that provides common testing environment for Tast CLI unit tests.
    
    This patch finally implements the core part of the runtest package,
    using fakesshserver/fakerunner internal packages introduced in earlier
    patches.
    
    The usage of the runtest package is simple:
    
      env := runtest.SetUp(t, /* some options */)
      cfg := env.Config()
      state := env.State()
    
    runtest.SetUp sets up a fake SSH server, fake local/remote test runners
    and fake local/remote test bundles. Env.Config returns a Config struct
    filled with reasonable default values pointing to fakes. It also
    provides a State struct that is automatically closed on the end of the
    current unit test.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I1ad30e22d1e0bce8d4a82158186a7cebe631bd84

---
+++ src/chromiumos/tast/cmd/tast/internal/run/runtest/env.go
@@ -0,0 +1,201 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package runtest
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+	gotesting "testing"
+
+	"chromiumos/tast/cmd/tast/internal/logging"
+	"chromiumos/tast/cmd/tast/internal/run/config"
+	"chromiumos/tast/cmd/tast/internal/run/runtest/internal/fakerunner"
+	"chromiumos/tast/cmd/tast/internal/run/runtest/internal/fakesshserver"
+	"chromiumos/tast/internal/bundle/fakebundle"
+	"chromiumos/tast/internal/runner"
+)
+
+// All paths are relative to rootDir.
+const (
+	tastDir   = "tast"
+	resultDir = "tast/results/latest"
+	trunkDir  = "trunk"
+	tempDir   = "tmp"
+
+	localTestRunnerPath  = "mock_local_test_runner"
+	remoteTestRunnerPath = "mock_remote_test_runner"
+
+	localBundleDir  = "bundles/local"
+	remoteBundleDir = "bundles/remote"
+)
+
+// Env contains information needed to interact with the testing environment
+// set up.
+type Env struct {
+	logger    *logging.Logger
+	rootDir   string
+	sshServer *fakesshserver.Server
+	state     *config.State
+}
+
+// SetUp sets up a testing environment for Tast CLI.
+//
+// Call this function at the beginning of unit tests to set up various fakes
+// commonly needed by Tast CLI, e.g. a fake SSH server and fake test runners.
+//
+// The environment is cleaned up automatically on the end of the current unit
+// test.
+func SetUp(t *gotesting.T, opts ...EnvOption) *Env {
+	cfg := defaultEnvConfig()
+	for _, opt := range opts {
+		opt(cfg)
+	}
+
+	// Prepare various directories. All directories should be under rootDir
+	// so that they are remove after the test.
+	rootDir, err := ioutil.TempDir("", "tast.")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { os.RemoveAll(rootDir) })
+
+	for _, dir := range []string{
+		tastDir,
+		resultDir,
+		trunkDir,
+		tempDir,
+		localBundleDir,
+		remoteBundleDir,
+	} {
+		if err := os.MkdirAll(filepath.Join(rootDir, dir), 0755); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// Install fake bundle executables.
+	fakebundle.InstallAt(t, filepath.Join(rootDir, localBundleDir), cfg.LocalBundles)
+	fakebundle.InstallAt(t, filepath.Join(rootDir, remoteBundleDir), cfg.RemoteBundles)
+
+	// Create a fake local test runner.
+	localTestRunner := fakerunner.New(&fakerunner.Config{
+		BundleDir: filepath.Join(rootDir, localBundleDir),
+		StaticConfig: &runner.StaticConfig{
+			Type: runner.LocalRunner,
+		},
+		GetDUTInfo:             cfg.GetDUTInfo,
+		GetSysInfoState:        cfg.GetSysInfoState,
+		CollectSysInfo:         cfg.CollectSysInfo,
+		DownloadPrivateBundles: cfg.DownloadPrivateBundles,
+		OnRunTestsInit:         cfg.OnRunLocalTestsInit,
+	})
+
+	// Create a fake remote test runner.
+	remoteTestRunner := fakerunner.New(&fakerunner.Config{
+		BundleDir: filepath.Join(rootDir, remoteBundleDir),
+		StaticConfig: &runner.StaticConfig{
+			Type: runner.RemoteRunner,
+		},
+		OnRunTestsInit: cfg.OnRunRemoteTestsInit,
+	})
+	lo, err := remoteTestRunner.Install(filepath.Join(rootDir, remoteTestRunnerPath))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() { lo.Close() })
+
+	// Start a fake SSH server.
+	handlers := cfg.ExtraSSHHandlers
+	handlers = append(handlers, defaultHandlers(cfg)...)
+	handlers = append(handlers, localTestRunner.SSHHandlers(filepath.Join(rootDir, localTestRunnerPath))...)
+	sshServer := fakesshserver.Start(handlers)
+
+	// Create a State object that is cleaned up automatically.
+	state := &config.State{}
+	t.Cleanup(func() { state.Close(context.Background()) })
+
+	return &Env{
+		logger:    logging.NewSimple(newTestLogWriter(t), false, true),
+		rootDir:   rootDir,
+		sshServer: sshServer,
+		state:     state,
+	}
+}
+
+// TempDir returns a directory path where callers can save arbitrary temporary
+// files. This directory is cleared on the end of the current unit test.
+func (e *Env) TempDir() string { return filepath.Join(e.rootDir, tempDir) }
+
+// Config returns a Config struct with reasonable default values to be used in
+// unit tests. Callers may alter values of a returned Config struct if needed
+// to customize Tast CLI component behavior.
+func (e *Env) Config() *config.Config {
+	randomID := rand.Int31()
+	return &config.Config{
+		// Logs are sent to unit test logs by default.
+		Logger: e.logger,
+		// Run all available tests.
+		Mode:      config.RunTestsMode,
+		Patterns:  []string{"*"},
+		RunLocal:  true,
+		RunRemote: true,
+		// Use prebuilt test bundles.
+		Build: false,
+		// Enable typical pre-flight checks.
+		CheckTestDeps:  true,
+		CollectSysInfo: true,
+		// This is the only shard.
+		TotalShards: 1,
+		// Fill info to access a fake SSH server.
+		Target:  e.sshServer.Addr().String(),
+		KeyFile: e.sshServer.KeyFile(),
+		// Set default directory paths to use.
+		ResDir:              filepath.Join(e.rootDir, resultDir),
+		TastDir:             filepath.Join(e.rootDir, tastDir),
+		TrunkDir:            filepath.Join(e.rootDir, trunkDir),
+		LocalRunner:         filepath.Join(e.rootDir, localTestRunnerPath),
+		LocalBundleDir:      filepath.Join(e.rootDir, localBundleDir),
+		LocalOutDir:         filepath.Join(e.rootDir, fmt.Sprintf("tmp/out/local.%d", randomID)),
+		RemoteRunner:        filepath.Join(e.rootDir, remoteTestRunnerPath),
+		RemoteBundleDir:     filepath.Join(e.rootDir, remoteBundleDir),
+		RemoteOutDir:        filepath.Join(e.rootDir, fmt.Sprintf("tmp/out/remote.%d", randomID)),
+		RemoteFixtureServer: filepath.Join(e.rootDir, remoteBundleDir, "bundle"),
+	}
+}
+
+// State returns a State struct to be used in unit tests. It is cleaned up on
+// the end of the current unit test.
+func (e *Env) State() *config.State {
+	return e.state
+}
+
+// defaultHandlers returns SSH handlers to be installed to a fake SSH server by
+// default.
+func defaultHandlers(cfg *envConfig) []fakesshserver.Handler {
+	return []fakesshserver.Handler{
+		// Pass-through basic shell commands.
+		fakesshserver.ShellHandler("exec mkdir "),
+		fakesshserver.ShellHandler("exec tar "),
+		// Simulate boot_id.
+		fakesshserver.ExactMatchHandler("exec cat /proc/sys/kernel/random/boot_id", func(_ io.Reader, stdout, stderr io.Writer) int {
+			bootID, err := cfg.BootID()
+			if err != nil {
+				fmt.Fprintf(stderr, "ERROR: %v\n", err)
+				return 1
+			}
+			io.WriteString(stdout, bootID)
+			return 0
+		}),
+		// Pretend that the system is always x86_64.
+		fakesshserver.ExactMatchHandler("exec file -b -L /sbin/init", func(_ io.Reader, stdout, stderr io.Writer) int {
+			io.WriteString(stdout, "/sbin/init: ELF 64-bit LSB shared object, x86-64, version 1\n")
+			return 0
+		}),
+	}
+}

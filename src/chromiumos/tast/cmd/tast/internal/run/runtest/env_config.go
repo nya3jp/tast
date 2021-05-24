5729e04cfb1ce5b558635d9f8b6463b85fae66ae
Shuhei Takahashi <nya@chromium.org>
2021-05-24 16:23:11 +0900

    runtest: Implement runtest package
    This is a part of patch series to introduce the new "runtest" package
    that provides common testing environment for Tast CLI unit tests.
    
    This patch finally implements the core part of the runtest package,
    using fakesshserver/fakerunner internal packages introduced in earlier
    patches.
    
    The usage of the runtest package is simple:
    
      env := runtest.SetUp(t, /* some options */)
      cfg := env.Config()
      state := env.State()
    
    runtest.SetUp sets up a fake SSH server, fake local/remote test runners
    and fake local/remote test bundles. Env.Config returns a Config struct
    filled with reasonable default values pointing to fakes. It also
    provides a State struct that is automatically closed on the end of the
    current unit test.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I1ad30e22d1e0bce8d4a82158186a7cebe631bd84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2919432
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/cmd/tast/internal/run/runtest/env_config.go
@@ -0,0 +1,167 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package runtest
+
+import (
+	"context"
+	"time"
+
+	"chromiumos/tast/internal/protocol"
+	"chromiumos/tast/internal/testing"
+)
+
+// envConfig contains configurations of the testing environment.
+type envConfig struct {
+	BootID                 func() (bootID string, err error)
+	ExtraSSHHandlers       []SSHHandler
+	GetDUTInfo             func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error)
+	GetSysInfoState        func(req *protocol.GetSysInfoStateRequest) (*protocol.GetSysInfoStateResponse, error)
+	CollectSysInfo         func(req *protocol.CollectSysInfoRequest) (*protocol.CollectSysInfoResponse, error)
+	DownloadPrivateBundles func(req *protocol.DownloadPrivateBundlesRequest) (*protocol.DownloadPrivateBundlesResponse, error)
+	OnRunLocalTestsInit    func(init *protocol.RunTestsInit)
+	OnRunRemoteTestsInit   func(init *protocol.RunTestsInit)
+	LocalBundles           map[string]*testing.Registry
+	RemoteBundles          map[string]*testing.Registry
+}
+
+// EnvOption can be passed to SetUp to customize the testing environment.
+type EnvOption func(*envConfig)
+
+// WithBootID specifies a function called to compute a boot ID.
+func WithBootID(f func() (bootID string, err error)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.BootID = f
+	}
+}
+
+// WithExtraSSHHandlers specifies extra SSH handlers installed to a fake SSH
+// server.
+func WithExtraSSHHandlers(handlers []SSHHandler) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.ExtraSSHHandlers = handlers
+	}
+}
+
+// WithGetDUTInfo specifies a function that implements GetDUTInfo handler.
+func WithGetDUTInfo(f func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.GetDUTInfo = f
+	}
+}
+
+// WithGetSysInfoState specifies a function that implements GetSysInfoState
+// handler.
+func WithGetSysInfoState(f func(req *protocol.GetSysInfoStateRequest) (*protocol.GetSysInfoStateResponse, error)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.GetSysInfoState = f
+	}
+}
+
+// WithCollectSysInfo specifies a function that implements CollectSysInfo
+// handler.
+func WithCollectSysInfo(f func(req *protocol.CollectSysInfoRequest) (*protocol.CollectSysInfoResponse, error)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.CollectSysInfo = f
+	}
+}
+
+// WithDownloadPrivateBundles specifies a function that implements
+// DownloadPrivateBundles handler.
+func WithDownloadPrivateBundles(f func(req *protocol.DownloadPrivateBundlesRequest) (*protocol.DownloadPrivateBundlesResponse, error)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.DownloadPrivateBundles = f
+	}
+}
+
+// WithOnRunLocalTestsInit specifies a function that is called on the beginning
+// of RunTests for the local test runner.
+func WithOnRunLocalTestsInit(f func(init *protocol.RunTestsInit)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.OnRunLocalTestsInit = f
+	}
+}
+
+// WithOnRunRemoteTestsInit specifies a function that is called on the beginning
+// of RunTests for the remote test runner.
+func WithOnRunRemoteTestsInit(f func(init *protocol.RunTestsInit)) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.OnRunRemoteTestsInit = f
+	}
+}
+
+// WithLocalBundles specifies fake local test bundles to be installed.
+func WithLocalBundles(bs map[string]*testing.Registry) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.LocalBundles = bs
+	}
+}
+
+// WithLocalBundle specifies a fake local test bundle to be installed.
+func WithLocalBundle(reg *testing.Registry) EnvOption {
+	return WithLocalBundles(map[string]*testing.Registry{"bundle": reg})
+}
+
+// WithRemoteBundles specifies fake remote test bundles to be installed.
+func WithRemoteBundles(bs map[string]*testing.Registry) EnvOption {
+	return func(cfg *envConfig) {
+		cfg.RemoteBundles = bs
+	}
+}
+
+// WithRemoteBundle specifies a fake remote test bundle to be installed.
+func WithRemoteBundle(reg *testing.Registry) EnvOption {
+	return WithRemoteBundles(map[string]*testing.Registry{"bundle": reg})
+}
+
+// defaultEnvConfig returns envConfig that is used by default.
+func defaultEnvConfig() *envConfig {
+	localReg := testing.NewRegistry()
+	localReg.AddTestInstance(&testing.TestInstance{
+		Name:    "example.Local",
+		Func:    func(ctx context.Context, s *testing.State) {},
+		Timeout: time.Minute,
+	})
+	remoteReg := testing.NewRegistry()
+	remoteReg.AddTestInstance(&testing.TestInstance{
+		Name:    "example.Remote",
+		Func:    func(ctx context.Context, s *testing.State) {},
+		Timeout: time.Minute,
+	})
+
+	return &envConfig{
+		BootID: func() (bootID string, err error) {
+			return "01234567-89ab-cdef-0123-456789abcdef", nil
+		},
+		GetDUTInfo: func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error) {
+			return &protocol.GetDUTInfoResponse{
+				DutInfo: &protocol.DUTInfo{
+					Features: &protocol.DUTFeatures{
+						Software: &protocol.SoftwareFeatures{
+							// We must report non-empty features. Otherwise Tast CLI considers the response
+							// as an error.
+							// TODO(b/187793617): Remove this once we fully migrate to the gRPC protocol and
+							// GetDUTInfo gets capable of returning errors.
+							Available: []string{"mock"},
+						},
+					},
+					OsVersion: "Mock OS v3.1415926535",
+				},
+			}, nil
+		},
+		GetSysInfoState: func(req *protocol.GetSysInfoStateRequest) (*protocol.GetSysInfoStateResponse, error) {
+			return &protocol.GetSysInfoStateResponse{}, nil
+		},
+		CollectSysInfo: func(req *protocol.CollectSysInfoRequest) (*protocol.CollectSysInfoResponse, error) {
+			return &protocol.CollectSysInfoResponse{}, nil
+		},
+		DownloadPrivateBundles: func(req *protocol.DownloadPrivateBundlesRequest) (*protocol.DownloadPrivateBundlesResponse, error) {
+			return &protocol.DownloadPrivateBundlesResponse{}, nil
+		},
+		OnRunLocalTestsInit:  func(init *protocol.RunTestsInit) {},
+		OnRunRemoteTestsInit: func(init *protocol.RunTestsInit) {},
+		LocalBundles:         map[string]*testing.Registry{"bundle": localReg},
+		RemoteBundles:        map[string]*testing.Registry{"bundle": remoteReg},
+	}
+}

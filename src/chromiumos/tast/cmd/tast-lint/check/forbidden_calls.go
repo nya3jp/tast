3ced064407749f7a6eed14c9726b8b9611733a43
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-05-26 21:40:02 +0530

    tast-lint: Better handling of fmt.Errorf
    At present, using go ast, tast-lint tries to fix the occurences of
    "fmt.Errof" by dynamically importing "chromiumos/tast/errors" for it
    being a preferred choice.
    The idea was simple i.e. if there is no identifier node named "errors"
    it will fix it else leave it as it is.
    
    However, there are certain cases where tast-lint can handle such cases
    better, for example, if there is an existing import of "errors" linter
    could replace all occurences of fmt.Errorf with errors.Errorf  rather
    than throwing an error. Also it discourages importing errors with
    different aliases.
    
    BUG=chromium:1142132, b:187794245
    TEST=./fast_build.sh -T
    
    Change-Id: I547b810734b5b477d758063a234e0924ba543845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2919074
    Tested-by: Bisakh Mondal <bisakhmondal00@gmail.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Mahmoud Gawad <mgawad@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>

--- src/chromiumos/tast/cmd/tast-lint/check/forbidden_calls.go
+++ src/chromiumos/tast/cmd/tast-lint/check/forbidden_calls.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"strconv"
 
 	"golang.org/x/tools/go/ast/astutil"
 )
@@ -23,10 +24,9 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 	if src, err := formatASTNode(fs, f); err == nil {
 		fixable = goimportApplicable(src)
 	}
-	// If a file contains an ident node whose name is "errors", importing the package
-	// "chromium/tast/errors" could introduce a naming conflict.
-	// TODO: handle the case where these usages are already from "chromium/tast/errors".
-	usesErrors := hasErrorsIdent(f)
+	// Checks for the possibility of linting fmt.Errorf with errors from "chromiumos/tast/errors" including existing
+	// 'errors' identifiers (if any) and the requirement of importing errors package (if not imported).
+	hasErrorsImport, errorsErr := checkErrors(f)
 
 	astutil.Apply(f, func(c *astutil.Cursor) bool {
 		sel, ok := c.Node().(*ast.SelectorExpr)
@@ -52,13 +52,17 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 			}
 		case "fmt.Errorf":
 			if !fix {
+				msg := "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf"
+				if errorsErr != nil {
+					msg = fmt.Sprintf("%s. Also found an error: %s", msg, errorsErr)
+				}
 				issues = append(issues, &Issue{
 					Pos:     fs.Position(x.Pos()),
-					Msg:     "chromiumos/tast/errors.Errorf should be used instead of fmt.Errorf",
+					Msg:     msg,
 					Link:    "https://chromium.googlesource.com/chromiumos/platform/tast/+/HEAD/docs/writing_tests.md#Error-construction",
-					Fixable: fixable,
+					Fixable: errorsErr == nil,
 				})
-			} else if fixable && !usesErrors {
+			} else if fixable && errorsErr == nil {
 				c.Replace(&ast.SelectorExpr{
 					X: &ast.Ident{
 						Name: "errors",
@@ -67,7 +71,10 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 						Name: "Errorf",
 					},
 				})
-				importsRequired = append(importsRequired, "chromiumos/tast/errors")
+
+				if !hasErrorsImport {
+					importsRequired = append(importsRequired, "chromiumos/tast/errors")
+				}
 			}
 		case "time.Sleep":
 			issues = append(issues, &Issue{
@@ -102,7 +109,9 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 	for _, pkg := range importsRequired {
 		astutil.AddImport(fs, f, pkg)
 	}
-	if len(importsRequired) > 0 {
+
+	// Rearrange Imports for new imports or when fmt.Errorf has been handled with conditional import.
+	if len(importsRequired) > 0 || (fix && errorsErr == nil) {
 		newf, err := ImportOrderAutoFix(fs, f)
 		if err != nil {
 			return issues
@@ -112,20 +121,52 @@ func ForbiddenCalls(fs *token.FileSet, f *ast.File, fix bool) []*Issue {
 	return issues
 }
 
-// hasErrorsIdent returns true if there is an identifier node whose name is "errors".
-func hasErrorsIdent(f *ast.File) bool {
+// hasIdentErrors returns true if there is an identifier node "errors" which is not a part of
+// "chromiumos/tast/errors" package, otherwise returns false.
+func hasIdentErrors(f *ast.File) bool {
 	hasErrors := false
 	ast.Inspect(f, func(node ast.Node) bool {
-		id, ok := node.(*ast.Ident)
-		if !ok {
-			return true
+		if hasErrors {
+			return false // no further deep exploration
 		}
-		if id.Name == "errors" {
-			hasErrors = true
+		switch node.(type) {
+		case *ast.SelectorExpr:
+			// Removes the possibility of <exp>.<sel> with something like errors.New, errors.Wrap etc. Also it prunes the subtree from inspection.
 			return false
+		case *ast.Ident:
+			if id := node.(*ast.Ident); id.Name == "errors" {
+				hasErrors = true
+				return false
+			}
 		}
 		return true
 	})
 
 	return hasErrors
+}
+
+// checkErrors checks if the "chromiumos/tast/errors" package could replace the occurrences of fmt.Errorf
+// automatically or manual intervention is solicited.
+func checkErrors(f *ast.File) (bool, error) {
+	if hasIdentErrors(f) {
+		return false, fmt.Errorf("manual fix required, detected 'errors' as an identifier")
+	}
+
+	const pkg = "chromiumos/tast/errors"
+	for _, imp := range f.Imports {
+		iPath, err := strconv.Unquote(imp.Path.Value)
+		if err != nil || iPath != pkg {
+			continue
+		}
+		// An existing import of "chromiumos/tast/errors" without alias
+		if imp.Name == nil {
+			return true, nil
+		}
+
+		if alias := imp.Name.Name; alias == "." || alias == "_" {
+			return false, fmt.Errorf("importing '%s' as '.' or '_' is highly discouraged, please fix it", pkg)
+		}
+		return false, fmt.Errorf("importing '%s' with alias when there is no name conflict, is highly discouraged", pkg)
+	}
+	return false, nil
 }

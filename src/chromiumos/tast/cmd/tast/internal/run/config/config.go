22672ec1954830617435377b643405e13fb0c210
Shuhei Takahashi <nya@chromium.org>
2021-05-27 17:15:29 +0900

    run: Convert State.InitialSysInfo to protobuf
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I4bc9e6ae8de2a3149a160f96ba56b138974e6065

--- src/chromiumos/tast/cmd/tast/internal/run/config/config.go
+++ src/chromiumos/tast/cmd/tast/internal/run/config/config.go
@@ -24,7 +24,6 @@ import (
 	"chromiumos/tast/cmd/tast/internal/run/resultsjson"
 	"chromiumos/tast/errors"
 	"chromiumos/tast/internal/command"
-	"chromiumos/tast/internal/jsonprotocol"
 	"chromiumos/tast/internal/planner"
 	"chromiumos/tast/internal/protocol"
 )
@@ -136,13 +135,13 @@ type Config struct {
 
 // State hold state attributes which are accumulated over the course of the run.
 type State struct {
-	DUTInfo          *protocol.DUTInfo          // DUT information retrieved at the beginning of the current run
-	InitialSysInfo   *jsonprotocol.SysInfoState // initial state of system info (logs, crashes, etc.) on DUT before testing
-	FailuresCount    int                        // the number of test failures so far.
-	TLWConn          *grpc.ClientConn           // TLW gRPC service connection
-	RemoteDevservers []string                   // list of devserver URLs used by remote tests.
-	TestNamesToSkip  []string                   // tests that match patterns but are not sent to runners to run
-	TestsToRun       []*resultsjson.Result      // tests to be run
+	DUTInfo          *protocol.DUTInfo      // DUT information retrieved at the beginning of the current run
+	InitialSysInfo   *protocol.SysInfoState // initial state of system info (logs, crashes, etc.) on DUT before testing
+	FailuresCount    int                    // the number of test failures so far.
+	TLWConn          *grpc.ClientConn       // TLW gRPC service connection
+	RemoteDevservers []string               // list of devserver URLs used by remote tests.
+	TestNamesToSkip  []string               // tests that match patterns but are not sent to runners to run
+	TestsToRun       []*resultsjson.Result  // tests to be run
 
 	// gRPC Reports Client related variables.
 	ReportsConn      *grpc.ClientConn       // Reports gRPC service connection.

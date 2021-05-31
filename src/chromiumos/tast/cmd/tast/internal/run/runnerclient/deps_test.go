3f2f4773f85a4375e44028b2b6a87e2d6bf075a3
Shuhei Takahashi <nya@chromium.org>
2021-05-31 13:58:05 +0900

    runnerclient: Update deps_test.go to use runtest
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: Ie941d8995a3db4e0f9a971311502d89eb2983343

--- src/chromiumos/tast/cmd/tast/internal/run/runnerclient/deps_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/runnerclient/deps_test.go
@@ -6,8 +6,6 @@ package runnerclient
 
 import (
 	"context"
-	"encoding/json"
-	"io"
 	"os"
 	"path/filepath"
 	"testing"
@@ -16,206 +14,120 @@ import (
 	"github.com/google/go-cmp/cmp"
 	configpb "go.chromium.org/chromiumos/config/go/api"
 
-	"chromiumos/tast/cmd/tast/internal/run/config"
-	"chromiumos/tast/cmd/tast/internal/run/fakerunner"
+	"chromiumos/tast/cmd/tast/internal/run/runtest"
 	"chromiumos/tast/cmd/tast/internal/run/target"
-	"chromiumos/tast/internal/jsonprotocol"
 	"chromiumos/tast/internal/protocol"
 )
 
-// writeGetDUTInfoResult writes runner.RunnerGetDUTInfoResult to w.
-func writeGetDUTInfoResult(w io.Writer, avail, unavail []string, dc *protocol.DeprecatedDeviceConfig, hf *configpb.HardwareFeatures, osVersion, defaultBuildArtifactsURL string) error {
-	res := jsonprotocol.RunnerGetDUTInfoResult{
-		SoftwareFeatures: &protocol.SoftwareFeatures{
-			Available:   avail,
-			Unavailable: unavail,
-		},
-		DeviceConfig:             dc,
-		HardwareFeatures:         hf,
-		OSVersion:                osVersion,
-		DefaultBuildArtifactsURL: defaultBuildArtifactsURL,
-	}
-	return json.NewEncoder(w).Encode(&res)
-}
-
-// checkRunnerTestDepsArgs calls featureArgsFromConfig using cfg and verifies
-// that it sets runner args as specified per checkDeps, avail, and unavail.
-func checkRunnerTestDepsArgs(t *testing.T, cfg *config.Config, state *config.State, checkDeps bool,
-	avail, unavail []string, dc *protocol.DeprecatedDeviceConfig, hf *configpb.HardwareFeatures) {
-	t.Helper()
-	args := jsonprotocol.RunnerArgs{
-		Mode: jsonprotocol.RunnerRunTestsMode,
-		RunTests: &jsonprotocol.RunnerRunTestsArgs{
-			BundleArgs: jsonprotocol.BundleRunTestsArgs{
-				FeatureArgs: *featureArgsFromConfig(cfg, state),
-			},
-		},
-	}
-
-	exp := jsonprotocol.RunnerRunTestsArgs{
-		BundleArgs: jsonprotocol.BundleRunTestsArgs{
-			FeatureArgs: jsonprotocol.FeatureArgs{
-				CheckDeps:                   checkDeps,
-				AvailableSoftwareFeatures:   avail,
-				UnavailableSoftwareFeatures: unavail,
-				DeviceConfig: jsonprotocol.DeviceConfigJSON{
-					Proto: dc,
-				},
-				HardwareFeatures: jsonprotocol.HardwareFeaturesJSON{
-					Proto: hf,
-				},
-			},
-		},
-	}
-	if !cmp.Equal(*args.RunTests, exp, cmp.Comparer(proto.Equal)) {
-		t.Errorf("featureArgsFromConfig(%+v) set %+v; want %+v", cfg, *args.RunTests, exp)
-	}
-}
-
 func TestGetDUTInfo(t *testing.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
-
-	// With "always", features returned by the runner should be passed through
-	// and dependencies should be checked.
-	avail := []string{"dep1", "dep2"}
-	unavail := []string{"dep3"}
-	dc := &protocol.DeprecatedDeviceConfig{
-		Id: &protocol.DeprecatedConfigId{
-			Platform: "platform_id",
-			Model:    "model_id",
-			Brand:    "brand_id",
-		},
-	}
-	hf := &configpb.HardwareFeatures{
-		Screen: &configpb.HardwareFeatures_Screen{
-			PanelProperties: &configpb.Component_DisplayPanel_Properties{},
-			TouchSupport:    configpb.HardwareFeatures_PRESENT,
-		},
-		Fingerprint: &configpb.HardwareFeatures_Fingerprint{
-			Location: configpb.HardwareFeatures_Fingerprint_NOT_PRESENT,
-		},
-		EmbeddedController: &configpb.HardwareFeatures_EmbeddedController{
-			Present: configpb.HardwareFeatures_PRESENT,
-			EcType:  configpb.HardwareFeatures_EmbeddedController_EC_CHROME,
-		},
-	}
-	osVersion := "octopus-release/R86-13312.0.2020_07_02_1108"
-	defaultBuildArtifactsURL := "gs://chromeos-image-archive/octopus-release/R86-13312.0.2020_07_02_1108/"
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{
-			Mode: jsonprotocol.RunnerGetDUTInfoMode,
-			GetDUTInfo: &jsonprotocol.RunnerGetDUTInfoArgs{
-				ExtraUSEFlags:       td.Cfg.ExtraUSEFlags,
-				RequestDeviceConfig: true,
+	dutInfo := &protocol.DUTInfo{
+		Features: &protocol.DUTFeatures{
+			Software: &protocol.SoftwareFeatures{
+				Available:   []string{"dep1", "dep2"},
+				Unavailable: []string{"dep3"},
 			},
-		})
-
-		writeGetDUTInfoResult(stdout, avail, unavail, dc, hf, osVersion, defaultBuildArtifactsURL)
-		return 0
+			Hardware: &protocol.HardwareFeatures{
+				HardwareFeatures: &configpb.HardwareFeatures{
+					Screen: &configpb.HardwareFeatures_Screen{
+						PanelProperties: &configpb.Component_DisplayPanel_Properties{},
+						TouchSupport:    configpb.HardwareFeatures_PRESENT,
+					},
+					Fingerprint: &configpb.HardwareFeatures_Fingerprint{
+						Location: configpb.HardwareFeatures_Fingerprint_NOT_PRESENT,
+					},
+					EmbeddedController: &configpb.HardwareFeatures_EmbeddedController{
+						Present: configpb.HardwareFeatures_PRESENT,
+						EcType:  configpb.HardwareFeatures_EmbeddedController_EC_CHROME,
+					},
+				},
+				DeprecatedDeviceConfig: &protocol.DeprecatedDeviceConfig{
+					Id: &protocol.DeprecatedConfigId{
+						Platform: "platform_id",
+						Model:    "model_id",
+						Brand:    "brand_id",
+					},
+				},
+			},
+		},
+		OsVersion:                "octopus-release/R86-13312.0.2020_07_02_1108",
+		DefaultBuildArtifactsUrl: "gs://chromeos-image-archive/octopus-release/R86-13312.0.2020_07_02_1108/",
 	}
-	td.Cfg.CheckTestDeps = true
-	td.Cfg.ExtraUSEFlags = []string{"use1", "use2"}
+	extraUseFlags := []string{"use1", "use2"}
 
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
+	env := runtest.SetUp(t, runtest.WithGetDUTInfo(func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error) {
+		if diff := cmp.Diff(req.GetExtraUseFlags(), extraUseFlags); diff != "" {
+			t.Errorf("ExtraUseFlags mismatch (-got +want):\n%s", diff)
+		}
+		return &protocol.GetDUTInfoResponse{DutInfo: dutInfo}, nil
+	}))
+	cfg := env.Config()
+	cfg.CheckTestDeps = true
+	cfg.ExtraUSEFlags = extraUseFlags
+	state := env.State()
+
+	cc := target.NewConnCache(cfg, cfg.Target)
 	defer cc.Close(context.Background())
 
-	if err := GetDUTInfo(context.Background(), &td.Cfg, &td.State, cc); err != nil {
-		t.Fatalf("GetDUTInfo(%+v) failed: %v", td.Cfg, err)
-	}
-	checkRunnerTestDepsArgs(t, &td.Cfg, &td.State, true, avail, unavail, dc, hf)
-
-	if ver := td.State.DUTInfo.GetOsVersion(); ver != osVersion {
-		t.Errorf("Unexpected OS version: got %+v, want %+v", ver, osVersion)
+	if err := GetDUTInfo(context.Background(), cfg, state, cc); err != nil {
+		t.Fatalf("GetDUTInfo failed: %v", err)
 	}
 
-	if url := td.State.DUTInfo.GetDefaultBuildArtifactsUrl(); url != defaultBuildArtifactsURL {
-		t.Errorf("Unexpected DefaultBuildArtifactsURL: got %+v, want %+v", url, defaultBuildArtifactsURL)
+	if diff := cmp.Diff(state.DUTInfo, dutInfo, cmp.Comparer(proto.Equal)); diff != "" {
+		t.Errorf("DUTInfo mismatch (-got +want):\n%s", diff)
 	}
 
 	// Make sure dut-info.txt is created.
-	if _, err := os.Stat(filepath.Join(td.Cfg.ResDir, dutInfoFile)); err != nil {
+	if _, err := os.Stat(filepath.Join(cfg.ResDir, dutInfoFile)); err != nil {
 		t.Errorf("Failed to stat %s: %v", dutInfoFile, err)
 	}
 
 	// The second call should fail, because it tries to update cfg's fields twice.
-	if err := GetDUTInfo(context.Background(), &td.Cfg, &td.State, cc); err == nil {
+	if err := GetDUTInfo(context.Background(), cfg, state, cc); err == nil {
 		t.Fatal("Calling GetDUTInfo twice unexpectedly succeeded")
 	}
 }
 
-func TestGetDUTInfoNoDeviceConfig(t *testing.T) {
-	// If local_test_runner is older, it may not return protocol.DeprecatedDeviceConfig even if it is requested.
-	// For backward compatibility, it is not handled as an error case, but the dut-info.txt
-	// will be created.
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
-
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{
-			Mode: jsonprotocol.RunnerGetDUTInfoMode,
-			GetDUTInfo: &jsonprotocol.RunnerGetDUTInfoArgs{
-				ExtraUSEFlags:       td.Cfg.ExtraUSEFlags,
-				RequestDeviceConfig: true,
-			},
-		})
-
-		// Note: if both avail/unavail are empty, it is handled as an error.
-		// Add fake here to avoid it.
-		writeGetDUTInfoResult(stdout, []string{"dep1"}, nil, nil, nil, "", "")
-		return 0
-	}
-	td.Cfg.CheckTestDeps = true
-
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
-	defer cc.Close(context.Background())
-
-	if err := GetDUTInfo(context.Background(), &td.Cfg, &td.State, cc); err != nil {
-		t.Fatalf("GetDUTInfo(%+v) failed: %v", td.Cfg, err)
-	}
-
-	// Make sure dut-info.txt is created.
-	if _, err := os.Stat(filepath.Join(td.Cfg.ResDir, dutInfoFile)); err != nil {
-		t.Error("Unexpected device config file: ", err)
-	}
-}
-
 func TestGetDUTInfoNoCheckTestDeps(t *testing.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
+	env := runtest.SetUp(t, runtest.WithGetDUTInfo(func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error) {
+		t.Error("GetDUTInfo called unexpectedly")
+		return &protocol.GetDUTInfoResponse{}, nil
+	}))
+	cfg := env.Config()
+	cfg.CheckTestDeps = true
+	state := env.State()
 
 	// With "never", the runner shouldn't be called and dependencies shouldn't be checked.
-	td.Cfg.CheckTestDeps = false
+	cfg.CheckTestDeps = false
 
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
+	cc := target.NewConnCache(cfg, cfg.Target)
 	defer cc.Close(context.Background())
 
-	if err := GetDUTInfo(context.Background(), &td.Cfg, &td.State, cc); err != nil {
-		t.Fatalf("GetDUTInfo(%+v) failed: %v", td.Cfg, err)
+	if err := GetDUTInfo(context.Background(), cfg, state, cc); err != nil {
+		t.Fatalf("GetDUTInfo failed: %v", err)
 	}
-	checkRunnerTestDepsArgs(t, &td.Cfg, &td.State, false, nil, nil, nil, nil)
 }
 
 func TestGetSoftwareFeaturesNoFeatures(t *testing.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
-	// "always" should fail if the runner doesn't know about any features.
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{
-			Mode: jsonprotocol.RunnerGetDUTInfoMode,
-			GetDUTInfo: &jsonprotocol.RunnerGetDUTInfoArgs{
-				RequestDeviceConfig: true,
+	env := runtest.SetUp(t, runtest.WithGetDUTInfo(func(req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error) {
+		return &protocol.GetDUTInfoResponse{
+			DutInfo: &protocol.DUTInfo{
+				Features: &protocol.DUTFeatures{
+					Software: &protocol.SoftwareFeatures{
+						Available: nil,
+					},
+				},
 			},
-		})
-		writeGetDUTInfoResult(stdout, []string{}, []string{}, nil, nil, "", "")
-		return 0
-	}
-	td.Cfg.CheckTestDeps = true
+		}, nil
+	}))
+	cfg := env.Config()
+	// "always" should fail if the runner doesn't know about any features.
+	cfg.CheckTestDeps = true
+	state := env.State()
 
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
+	cc := target.NewConnCache(cfg, cfg.Target)
 	defer cc.Close(context.Background())
 
-	if err := GetDUTInfo(context.Background(), &td.Cfg, &td.State, cc); err == nil {
-		t.Fatalf("getSoftwareFeatures(%+v) succeeded unexpectedly", td.Cfg)
+	if err := GetDUTInfo(context.Background(), cfg, state, cc); err == nil {
+		t.Fatal("getSoftwareFeatures succeeded unexpectedly")
 	}
 }

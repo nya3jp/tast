c1a8bfb9d3de32117848fa43ceddb86fad1db667
Shuhei Takahashi <nya@chromium.org>
2021-05-31 12:08:43 +0900

    prepare: Update unit tests to use runtest
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I7482c33a31a4c582196b59498635ff4472c32974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2928879
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/cmd/tast/internal/run/prepare/prepare_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/prepare/prepare_test.go
@@ -6,29 +6,23 @@ package prepare
 
 import (
 	"context"
-	"encoding/json"
-	"io"
 	"io/ioutil"
 	"path/filepath"
+	"regexp"
 	gotesting "testing"
 
 	"github.com/google/go-cmp/cmp"
 
-	"chromiumos/tast/cmd/tast/internal/run/fakerunner"
+	"chromiumos/tast/cmd/tast/internal/run/runtest"
 	"chromiumos/tast/cmd/tast/internal/run/target"
-	"chromiumos/tast/internal/jsonprotocol"
-	"chromiumos/tast/internal/runner"
 	"chromiumos/tast/internal/testing"
 	"chromiumos/tast/testutil"
 )
 
 func TestPushDataFiles(t *gotesting.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
-
 	const (
-		dataSubdir  = "data" // subdir storing test data per the tast/testing package
-		bundleName  = "bnd"  // test bundle
+		dataSubdir  = "data"   // subdir storing test data per the tast/testing package
+		bundleName  = "bundle" // test bundle
 		bundlePkg   = "chromiumos/tast/local/bundles/" + bundleName
 		category    = "cat" // test category
 		categoryPkg = bundlePkg + "/" + category
@@ -46,41 +40,50 @@ func TestPushDataFiles(t *gotesting.T) {
 		fixtPkg = "chromiumos/tast/local/chrome"
 	)
 
-	// Make local_test_runner list two tests containing the first three files (with overlap).
-	tests := []*jsonprotocol.EntityWithRunnabilityInfo{
-		{EntityInfo: jsonprotocol.EntityInfo{Name: category + ".Test1", Pkg: categoryPkg, Data: []string{file1, file2}, Fixture: "fixt1"}},
-		{EntityInfo: jsonprotocol.EntityInfo{Name: category + ".Test2", Pkg: categoryPkg, Data: []string{file2, file3, extFile1, extFile2}}},
-	}
-	bundlePath := filepath.Join(fakerunner.MockLocalBundleDir, bundleName)
-	fixtures := map[string][]*jsonprotocol.EntityInfo{
-		bundlePath: {
-			{Type: jsonprotocol.EntityFixture, Name: "fixt1", Pkg: fixtPkg, Data: []string{file1}, Fixture: "fixt2"},
-			{Type: jsonprotocol.EntityFixture, Name: "fixt2", Pkg: fixtPkg, Data: []string{file2}},
-			{Type: jsonprotocol.EntityFixture, Name: "fixt3", Pkg: fixtPkg, Data: []string{file3}}, // unused
-		},
-	}
+	// Make the local test bundle list two tests containing the first three files (with overlap).
+	reg := testing.NewRegistry()
+	reg.AddTestInstance(&testing.TestInstance{
+		Name:    category + ".Test1",
+		Pkg:     categoryPkg,
+		Data:    []string{file1, file2},
+		Fixture: "fixt1",
+	})
+	reg.AddTestInstance(&testing.TestInstance{
+		Name: category + ".Test2",
+		Pkg:  categoryPkg,
+		Data: []string{file2, file3, extFile1, extFile2},
+	})
+	reg.AddFixtureInstance(&testing.FixtureInstance{
+		Name:   "fixt1",
+		Pkg:    fixtPkg,
+		Data:   []string{file1},
+		Parent: "fixt2",
+	})
+	reg.AddFixtureInstance(&testing.FixtureInstance{
+		Name: "fixt2",
+		Pkg:  fixtPkg,
+		Data: []string{file2},
+	})
+	reg.AddFixtureInstance(&testing.FixtureInstance{
+		Name: "fixt3",
+		Pkg:  fixtPkg,
+		Data: []string{file3},
+	})
 
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		switch args.Mode {
-		case jsonprotocol.RunnerListTestsMode:
-			fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{
-				Mode: jsonprotocol.RunnerListTestsMode,
-				ListTests: &jsonprotocol.RunnerListTestsArgs{
-					BundleArgs: jsonprotocol.BundleListTestsArgs{Patterns: []string{pattern}},
-					BundleGlob: fakerunner.MockLocalBundleGlob,
-				},
-			})
-			runner.WriteListTestsResultAsJSON(stdout, tests)
-		case jsonprotocol.RunnerListFixturesMode:
-			json.NewEncoder(stdout).Encode(&jsonprotocol.RunnerListFixturesResult{
-				Fixtures: fixtures,
-			})
-		}
-		return 0
-	}
+	env := runtest.SetUp(t, runtest.WithLocalBundle(reg), runtest.WithExtraSSHHandlers([]runtest.SSHHandler{
+		// Allow rm commands with relative paths.
+		func(cmd string) (runtest.SSHProcess, bool) {
+			if !regexp.MustCompile(`^cd .*; exec rm `).MatchString(cmd) {
+				return nil, false
+			}
+			return runtest.ShellHandler("")(cmd)
+		},
+	}))
+	cfg := env.Config()
+	state := env.State()
 
 	// Create a fake source checkout and write the data files to it. Just use their names as their contents.
-	td.Cfg.BuildWorkspace = filepath.Join(td.TempDir, "ws")
+	cfg.BuildWorkspace = filepath.Join(env.TempDir(), "ws")
 	srcFiles := map[string]string{
 		file1:        file1,
 		file2:        file2,
@@ -89,10 +92,10 @@ func TestPushDataFiles(t *gotesting.T) {
 		extLinkFile1: extLinkFile1,
 		extFile2:     extFile2,
 	}
-	if err := testutil.WriteFiles(filepath.Join(td.Cfg.BuildWorkspace, "src", testing.RelativeDataDir(tests[0].Pkg)), srcFiles); err != nil {
+	if err := testutil.WriteFiles(filepath.Join(cfg.BuildWorkspace, "src", testing.RelativeDataDir(categoryPkg)), srcFiles); err != nil {
 		t.Fatal(err)
 	}
-	if err := testutil.WriteFiles(filepath.Join(td.Cfg.BuildWorkspace, "src", testing.RelativeDataDir(fixtPkg)), map[string]string{
+	if err := testutil.WriteFiles(filepath.Join(cfg.BuildWorkspace, "src", testing.RelativeDataDir(fixtPkg)), map[string]string{
 		file1: file1,
 		file2: file2,
 		file3: file3,
@@ -101,16 +104,17 @@ func TestPushDataFiles(t *gotesting.T) {
 	}
 
 	// Prepare a fake destination directory.
-	pushDir := filepath.Join(td.HostDir, fakerunner.MockLocalDataDir)
+	localDataDir := filepath.Join(env.TempDir(), "mock_local_data")
+	cfg.LocalDataDir = localDataDir
 	dstFiles := map[string]string{
 		extLinkFile2: extLinkFile2,
 	}
-	if err := testutil.WriteFiles(filepath.Join(pushDir, testing.RelativeDataDir(tests[0].Pkg)), dstFiles); err != nil {
+	if err := testutil.WriteFiles(filepath.Join(localDataDir, testing.RelativeDataDir(categoryPkg)), dstFiles); err != nil {
 		t.Fatal(err)
 	}
 
 	// Connect to the target.
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
+	cc := target.NewConnCache(cfg, cfg.Target)
 	defer cc.Close(context.Background())
 
 	conn, err := cc.Conn(context.Background())
@@ -119,9 +123,9 @@ func TestPushDataFiles(t *gotesting.T) {
 	}
 
 	// getDataFilePaths should list the tests and return the files needed by them.
-	td.Cfg.BuildBundle = bundleName
-	td.Cfg.Patterns = []string{pattern}
-	paths, err := getDataFilePaths(context.Background(), &td.Cfg, &td.State, conn.SSHConn())
+	cfg.BuildBundle = bundleName
+	cfg.Patterns = []string{pattern}
+	paths, err := getDataFilePaths(context.Background(), cfg, state, conn.SSHConn())
 	if err != nil {
 		t.Fatal("getDataFilePaths() failed: ", err)
 	}
@@ -139,25 +143,24 @@ func TestPushDataFiles(t *gotesting.T) {
 	}
 
 	// pushDataFiles should copy the required files to the DUT.
-	if err = pushDataFiles(context.Background(), &td.Cfg, conn.SSHConn(),
-		fakerunner.MockLocalDataDir, paths); err != nil {
+	if err = pushDataFiles(context.Background(), cfg, conn.SSHConn(), localDataDir, paths); err != nil {
 		t.Fatal("pushDataFiles() failed: ", err)
 	}
 	expData := map[string]string{
-		filepath.Join(testing.RelativeDataDir(tests[0].Pkg), file1):        file1,
-		filepath.Join(testing.RelativeDataDir(tests[0].Pkg), file2):        file2,
-		filepath.Join(testing.RelativeDataDir(tests[1].Pkg), file3):        file3,
-		filepath.Join(testing.RelativeDataDir(tests[1].Pkg), extLinkFile1): extLinkFile1,
-		filepath.Join(testing.RelativeDataDir(tests[1].Pkg), extFile2):     extFile2,
-		filepath.Join(testing.RelativeDataDir(fixtPkg), file1):             file1,
-		filepath.Join(testing.RelativeDataDir(fixtPkg), file2):             file2,
+		filepath.Join(testing.RelativeDataDir(categoryPkg), file1):        file1,
+		filepath.Join(testing.RelativeDataDir(categoryPkg), file2):        file2,
+		filepath.Join(testing.RelativeDataDir(categoryPkg), file3):        file3,
+		filepath.Join(testing.RelativeDataDir(categoryPkg), extLinkFile1): extLinkFile1,
+		filepath.Join(testing.RelativeDataDir(categoryPkg), extFile2):     extFile2,
+		filepath.Join(testing.RelativeDataDir(fixtPkg), file1):            file1,
+		filepath.Join(testing.RelativeDataDir(fixtPkg), file2):            file2,
 	}
-	if data, err := testutil.ReadFiles(pushDir); err != nil {
+	if data, err := testutil.ReadFiles(localDataDir); err != nil {
 		t.Error(err)
 	} else if diff := cmp.Diff(data, expData); diff != "" {
 		t.Fatalf("pushDataFiles() copied files unmatch (-got +want):\n%v", diff)
 	}
-	if _, err := ioutil.ReadFile(filepath.Join(pushDir, testing.RelativeDataDir(tests[1].Pkg), extFile1)); err == nil {
+	if _, err := ioutil.ReadFile(filepath.Join(localDataDir, testing.RelativeDataDir(categoryPkg), extFile1)); err == nil {
 		t.Errorf("pushDataFiles() unexpectedly copied %s", extFile1)
 	}
 }

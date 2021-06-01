317641700bed9b7823af98173abc0f9278060752
Shuhei Takahashi <nya@chromium.org>
2021-06-01 10:54:44 +0900

    runnerclient: Update sysinfo-related tests to use runtest
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I06d7e8d1d644889140fbddd100c59963e46c16b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2928882
    Reviewed-by: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Tested-by: Shuhei Takahashi <nya@chromium.org>

--- src/chromiumos/tast/cmd/tast/internal/run/runnerclient/sys_info_test.go
+++ src/chromiumos/tast/cmd/tast/internal/run/runnerclient/sys_info_test.go
@@ -6,80 +6,65 @@ package runnerclient
 
 import (
 	"context"
-	"encoding/json"
-	"io"
-	"reflect"
 	"testing"
 
-	"chromiumos/tast/cmd/tast/internal/run/fakerunner"
+	"github.com/google/go-cmp/cmp"
+
+	"chromiumos/tast/cmd/tast/internal/run/runtest"
 	"chromiumos/tast/cmd/tast/internal/run/target"
-	"chromiumos/tast/internal/jsonprotocol"
 	"chromiumos/tast/internal/protocol"
 )
 
-// This file uses types and functions from local_test.go.
-
 func TestGetInitialSysInfo(t *testing.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
+	ctx := context.Background()
 
-	// Report a few log files and crashes.
-	res := jsonprotocol.RunnerGetSysInfoStateResult{
-		State: jsonprotocol.SysInfoState{
-			LogInodeSizes: map[uint64]int64{1: 2, 3: 4},
-			MinidumpPaths: []string{"foo.dmp", "bar.dmp"},
-		},
+	wantState := &protocol.SysInfoState{
+		LogInodeSizes: map[uint64]int64{1: 2, 3: 4},
+		MinidumpPaths: []string{"foo.dmp", "bar.dmp"},
 	}
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{Mode: jsonprotocol.RunnerGetSysInfoStateMode})
+	env := runtest.SetUp(t, runtest.WithGetSysInfoState(func(req *protocol.GetSysInfoStateRequest) (*protocol.GetSysInfoStateResponse, error) {
+		return &protocol.GetSysInfoStateResponse{State: wantState}, nil
+	}))
+	cfg := env.Config()
 
-		json.NewEncoder(stdout).Encode(res)
-		return 0
-	}
+	cc := target.NewConnCache(cfg, cfg.Target)
+	defer cc.Close(ctx)
 
-	// Check that the expected command is sent to the DUT and that the returned state is decoded properly.
-	td.Cfg.CollectSysInfo = true
-
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
-	defer cc.Close(context.Background())
-
-	initialSysInfo, err := GetInitialSysInfo(context.Background(), &td.Cfg, cc)
+	gotState, err := GetInitialSysInfo(context.Background(), cfg, cc)
 	if err != nil {
-		t.Fatalf("GetInitialSysInfo(..., %+v) failed: %v", td.Cfg, err)
+		t.Fatalf("GetInitialSysInfo failed: %v", err)
 	}
 
-	if initialSysInfo == nil {
-		t.Error("InitialSysInfo is nil")
-	} else if !reflect.DeepEqual(initialSysInfo, res.State.Proto()) {
-		t.Errorf("InitialSysInfo is %+v; want %+v", initialSysInfo, res.State.Proto())
+	if diff := cmp.Diff(gotState, wantState); diff != "" {
+		t.Errorf("InitialSysInfoState mismatch (-got +want):\n%s", diff)
 	}
 }
 
 func TestCollectSysInfo(t *testing.T) {
-	td := fakerunner.NewLocalTestData(t)
-	defer td.Close()
+	ctx := context.Background()
 
-	initialSysInfo := &protocol.SysInfoState{
+	initialState := &protocol.SysInfoState{
 		LogInodeSizes: map[uint64]int64{1: 2, 3: 4},
 		MinidumpPaths: []string{"foo.dmp", "bar.dmp"},
 	}
+	called := false
+	env := runtest.SetUp(t, runtest.WithCollectSysInfo(func(req *protocol.CollectSysInfoRequest) (*protocol.CollectSysInfoResponse, error) {
+		called = true
+		if diff := cmp.Diff(req.GetInitialState(), initialState); diff != "" {
+			t.Errorf("CollectSysInfo: InitialState mismatch (-got +want):\n%s", diff)
+		}
+		return &protocol.CollectSysInfoResponse{}, nil
+	}))
+	cfg := env.Config()
 
-	td.RunFunc = func(args *jsonprotocol.RunnerArgs, stdout, stderr io.Writer) (status int) {
-		fakerunner.CheckArgs(t, args, &jsonprotocol.RunnerArgs{
-			Mode:           jsonprotocol.RunnerCollectSysInfoMode,
-			CollectSysInfo: &jsonprotocol.RunnerCollectSysInfoArgs{InitialState: *jsonprotocol.SysInfoStateFromProto(initialSysInfo)},
-		})
-
-		json.NewEncoder(stdout).Encode(&jsonprotocol.RunnerCollectSysInfoResult{})
-		return 0
-	}
-
-	td.Cfg.CollectSysInfo = true
-	ctx := context.Background()
-	cc := target.NewConnCache(&td.Cfg, td.Cfg.Target)
+	cc := target.NewConnCache(cfg, cfg.Target)
 	defer cc.Close(ctx)
-	if err := collectSysInfo(ctx, &td.Cfg, initialSysInfo, cc); err != nil {
-		t.Fatalf("collectSysInfo(..., %+v) failed: %v", td.Cfg, err)
+
+	if err := collectSysInfo(ctx, cfg, initialState, cc); err != nil {
+		t.Fatalf("collectSysInfo failed: %v", err)
+	}
+	if !called {
+		t.Error("CollectSysInfo was not called")
 	}
 
 	// TODO(derat): The test SSH server doesn't support file copies. If/when that changes, set the

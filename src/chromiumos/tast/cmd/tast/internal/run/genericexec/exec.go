2dae90548f05da0a1c853f5df3da5bfad9b0f7fa
Shuhei Takahashi <nya@chromium.org>
2021-06-10 17:19:07 +0900

    remote_test_runner: Print stack trace on SIGTERM in a hacky way
    To debug timeout issues, this patch updates test runners to print
    stack traces when Tast CLI receives SIGTERM.
    
    This is roughly implemented in the following way: on startup, Tast
    CLI and test runners install a signal handler that prints stack
    traces to stderr and then sends SIGTERM to child processes. Thus
    sending SIGTERM to Tast CLI should trigger the logic recursively.
    
    There are two important things to note:
    
    1. Only remote_test_runner is covered because we don't have a way to
       send SIGTERM to local_test_runner process running on the DUT over
       SSH. If we fully migrate to gRPC protocol, we will be able to trigger
       the stack trace dump logic asynchronously, but the migration is not
       finished yet.
    
    2. Messages written by remote_test_runner to stderr are captured by Tast
       CLI. Thus this patch introduces a hack: Tast CLI passes the "real"
       stderr to remote_test_runner as FD 3 so that it can dump stack traces
       to stderr without being intercepted by Tast CLI.
    
    BUG=b:189332919
    TEST=fast_build.sh -T
    TEST=fast_build.sh
    TEST=tast run $DUT example.Pass &
    TEST=killall -TERM tast
    
    Change-Id: I3779838257dc5ded7bc6335820743c1966f42747

--- src/chromiumos/tast/cmd/tast/internal/run/genericexec/exec.go
+++ src/chromiumos/tast/cmd/tast/internal/run/genericexec/exec.go
@@ -33,6 +33,11 @@ func (c *ExecCmd) Run(ctx context.Context, extraArgs []string, stdin io.Reader, 
 	cmd.Stdin = stdin
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
+	// Set FD 3 to the real stderr so that the subprocess can write stack
+	// traces.
+	// TODO(b/189332919): Remove this hack once we find the cause.
+	cmd.ExtraFiles = []*os.File{os.Stderr}
+	cmd.Env = append(os.Environ(), "TAST_B189332919_STACK_TRACE_FD=3")
 	return cmd.Run()
 }
 

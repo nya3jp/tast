0cd272f649185710d9de9f856970859a15e786a1
Mark Yacoub <markyacoub@google.com>
2021-06-30 13:30:28 -0400

    Add video_cards_msm to IGT Software Dependencies
    Enable IGT tasts on Qualcomm devices.
    
    BUG=b:192452894
    TEST=cros_sdk tast run trogdor graphics.IGT.kms_atomic
    
    Change-Id: Ia3ec81b7cf3116e386971364a2e551deb3d5396f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/2997942
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Tested-by: Mark Yacoub <markyacoub@google.com>
    Auto-Submit: Mark Yacoub <markyacoub@google.com>

--- src/chromiumos/tast/cmd/local_test_runner/main.go
+++ src/chromiumos/tast/cmd/local_test_runner/main.go
@@ -130,7 +130,7 @@ func main() {
 			"houdini64":               "houdini64",
 			"hostap_hwsim":            "wifi_hostap_test",
 			"hps":                     "hps",
-			"igt":                     `("video_cards_amdgpu" || "video_cards_intel") && ("kernel-5_4" || "kernel-5_10")`,
+			"igt":                     `("video_cards_amdgpu" || "video_cards_intel" || "video_cards_msm") && ("kernel-5_4" || "kernel-5_10")`,
 			"iioservice":              "iioservice",
 			"iwlwifi_rescan":          "iwlwifi_rescan",
 			"lacros":                  "!arm && !arm64",                                                 // TODO(crbug.com/1144013): Expand this (and below lacros_*) to include arm as well.

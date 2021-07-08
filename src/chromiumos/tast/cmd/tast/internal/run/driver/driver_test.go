28c9752c05e9bd9678d40cae64d9b243bcf4ab68
Shuhei Takahashi <nya@chromium.org>
2021-07-08 14:54:54 +0900

    run: Introduce driver package
    Driver implements communications with local/remote executables related
    to Tast.
    
    Initially Driver looks like a thin wrapper of target.ConnCache. However
    their responsibilities are quite different: ConnCache focuses on a
    target device, while Driver communicates with both local executables and
    remote executables.
    
    In later patches, several functions in the runnerclient package that
    communicate with local/remote executables will be moved to Driver,
    e.g. GetDUTInfo, CollectSysInfo, ListTests and RunTests.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I78e3295e5960f34b4732d150307939ea09bade3e

---
+++ src/chromiumos/tast/cmd/tast/internal/run/driver/driver_test.go
@@ -0,0 +1,93 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package driver_test
+
+import (
+	"io"
+	"testing"
+	"time"
+
+	"chromiumos/tast/cmd/tast/internal/run/driver"
+	"chromiumos/tast/cmd/tast/internal/run/runtest"
+)
+
+func TestDriver(t *testing.T) {
+	const (
+		fakeBootID  = "bootID"
+		fakeCommand = "fake_command"
+	)
+
+	env := runtest.SetUp(
+		t,
+		runtest.WithBootID(func() (string, error) {
+			return fakeBootID, nil
+		}),
+		runtest.WithExtraSSHHandlers([]runtest.SSHHandler{
+			runtest.ExactMatchHandler("exec "+fakeCommand, func(_ io.Reader, _, _ io.Writer) int {
+				return 0
+			}),
+		}),
+	)
+	ctx := env.Context()
+	cfg := env.Config()
+
+	drv, err := driver.New(ctx, cfg, cfg.Target)
+	if err != nil {
+		t.Fatalf("driver.New failed: %v", err)
+	}
+	defer func() {
+		if err := drv.Close(ctx); err != nil {
+			t.Errorf("Close failed: %v", err)
+		}
+	}()
+
+	if target := drv.Target(); target != cfg.Target {
+		t.Errorf("Target() = %q; want %q", target, cfg.Target)
+	}
+	if bootID := drv.InitBootID(); bootID != fakeBootID {
+		t.Errorf("InitBootID() = %q; want %q", bootID, fakeBootID)
+	}
+	if err := drv.SSHConn().Command(fakeCommand).Run(ctx); err != nil {
+		t.Errorf("Command(%q).Run() failed: %v", fakeCommand, err)
+	}
+}
+
+func TestDriver_ReconnectIfNeeded(t *testing.T) {
+	const pingTimeout = 10 * time.Second
+	env := runtest.SetUp(t)
+	ctx := env.Context()
+	cfg := env.Config()
+
+	drv, err := driver.New(ctx, cfg, cfg.Target)
+	if err != nil {
+		t.Fatalf("driver.New failed: %v", err)
+	}
+	defer func() {
+		if err := drv.Close(ctx); err != nil {
+			t.Errorf("Close failed: %v", err)
+		}
+	}()
+
+	// Ping should work initially.
+	if err := drv.SSHConn().Ping(ctx, pingTimeout); err != nil {
+		t.Fatalf("First Ping failed: %v", err)
+	}
+
+	// Forcibly close the current connection. Now Ping fails.
+	if err := drv.SSHConn().Close(ctx); err != nil {
+		t.Fatalf("ssh.Conn.Close failed: %v", err)
+	}
+	if err := drv.SSHConn().Ping(ctx, pingTimeout); err == nil {
+		t.Fatal("Ping unexpectedly succeeded despite forced close")
+	}
+
+	// Reconnect to the target device. Now Ping starts to pass.
+	if err := drv.ReconnectIfNeeded(ctx); err != nil {
+		t.Fatalf("ReconnectIfNeeded failed: %v", err)
+	}
+	if err := drv.SSHConn().Ping(ctx, pingTimeout); err != nil {
+		t.Fatalf("Ping failed after reconnection: %v", err)
+	}
+}

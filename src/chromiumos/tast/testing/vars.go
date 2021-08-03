83badfb9c5a2fde1584c33ea9457a1d0860063ed
Seewai Fu <seewaifu@google.com>
2021-08-02 18:18:27 -0700

    tast: make runtime variables globally accessible
    This CL makes the current runtime variables globally accessible by
    registration so that test libraries can access runtime variables.
    
    BUG=b:187795655
    TEST=./fast_build.sh -T
    
    Change-Id: Ic04e7393ae39409063839561ed4409a3a3bee5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/3068193
    Commit-Queue: Seewai Fu <seewaifu@google.com>
    Tested-by: Seewai Fu <seewaifu@google.com>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>

---
+++ src/chromiumos/tast/testing/vars.go
@@ -0,0 +1,58 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	"chromiumos/tast/internal/testing"
+)
+
+// VarString define a structure for global runtime variables of string type.
+type VarString struct {
+	v *testing.VarString
+}
+
+// RegisterVarString creates and registers a new VarString
+func RegisterVarString(name, defaultValue, desc string) *VarString {
+	reg := testing.GlobalRegistry()
+	pc, _, _, _ := runtime.Caller(1)
+	v, err := registerVarString(reg, name, defaultValue, desc, pc)
+	if err != nil {
+		reg.RecordError(err)
+	}
+	return v
+}
+
+// registerVarString creates and registers a new VarString
+func registerVarString(reg *testing.Registry, name, defaultValue, desc string, pc uintptr) (*VarString, error) {
+	if !checkVarName(runtime.FuncForPC(pc).Name(), name) {
+		return nil, fmt.Errorf("Global runtime variable %q does not follow naming convention <pkg>.<rest_of_name>", name)
+	}
+	v := testing.NewVarString(name, defaultValue, desc)
+	reg.AddVar(v)
+	return &VarString{v: v}, nil
+}
+
+// Name returns the name of the variable.
+func (v *VarString) Name() string {
+	return v.v.Name()
+}
+
+// Value returns value of a variable and a flag to indicate whether the value is initialized.
+func (v *VarString) Value() string {
+	return v.v.Value()
+}
+
+// checkVarName check if variable name follows naming convention.
+func checkVarName(funcName, name string) bool {
+	splitFuncName := strings.Split(funcName, ".")
+	pkgName := splitFuncName[len(splitFuncName)-2]
+	splitPkgName := strings.Split(pkgName, "/")
+	pkgBaseName := splitPkgName[len(splitPkgName)-1]
+	return strings.HasPrefix(name, pkgBaseName+".")
+}
---
+++ src/chromiumos/tast/testing/vars_test.go
@@ -0,0 +1,40 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package testing
+
+import (
+	"runtime"
+	gotesting "testing"
+
+	"chromiumos/tast/internal/testing"
+)
+
+func TestRegisterVarString(t *gotesting.T) {
+	varName := "testing.v1"
+	value := "default"
+	reg := testing.NewRegistry("bundle")
+	pc, _, _, _ := runtime.Caller(0)
+	v, err := registerVarString(reg, varName, value, "desc", pc)
+	if err != nil {
+		t.Fatal("Failed to call registerVarString: ", err)
+	}
+	if v.Name() != varName {
+		t.Errorf("Function registerVarString set variable name to %q; wanted %q", v.Name(), varName)
+	}
+	if v.Value() != value {
+		t.Errorf("Function registerVarString set variable value to %q; wanted %q", v.Value(), value)
+	}
+}
+
+func TestRegisterVarStringBadName(t *gotesting.T) {
+	varName := "v1"
+	value := "default"
+	reg := testing.NewRegistry("bundle")
+	pc, _, _, _ := runtime.Caller(0)
+	_, err := registerVarString(reg, varName, value, "desc", pc)
+	if err == nil {
+		t.Fatal("Failed to get an error from registerVarString when variable name is not following convention.")
+	}
+}

a0ef866f7995af8d837f19a8c1b36d79ca0f9ee2
Shuhei Takahashi <nya@chromium.org>
2021-09-29 18:10:15 +0900

    driver: Introduce a test bundle client
    This patch introduces bundleclient.Client which talks to test bundles.
    Notice the different with runnerclient.JSONClient which talks to
    test_runners.
    
    Currently the only case where Tast CLI talks to test bundles directly
    is driving remote fixtures. This patch implements the functionality.
    
    Note: bundleclient doesn't have unit tests. It will be instead covered
    by unit tests of the driver.
    
    BUG=b:187793617
    TEST=fast_build.sh -T
    
    Change-Id: I93173489dfe0146712d1eede5384f6aba67d5dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/3218694
    Tested-by: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>

---
+++ src/chromiumos/tast/cmd/tast/internal/run/driver/internal/bundleclient/client.go
@@ -0,0 +1,78 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package bundleclient provides a client of test bundles.
+package bundleclient
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	"chromiumos/tast/cmd/tast/internal/run/genericexec"
+	"chromiumos/tast/internal/protocol"
+	"chromiumos/tast/internal/rpc"
+)
+
+// rpcConn represents a gRPC connection to a test bundle.
+type rpcConn struct {
+	proc genericexec.Process
+	conn *rpc.GenericClient
+}
+
+// Close closes the gRPC connection to the test bundle.
+func (c *rpcConn) Close(ctx context.Context) error {
+	var firstErr error
+	if err := c.conn.Close(); err != nil && firstErr == nil {
+		firstErr = err
+	}
+	if err := c.proc.Stdin().Close(); err != nil && firstErr == nil {
+		firstErr = err
+	}
+	if err := c.proc.Wait(ctx); err != nil && firstErr == nil {
+		firstErr = err
+	}
+	return firstErr
+}
+
+// Conn returns the established gRPC connection.
+func (c *rpcConn) Conn() *grpc.ClientConn {
+	return c.conn.Conn()
+}
+
+// Client is a gRPC protocol client to a test bundle.
+type Client struct {
+	cmd genericexec.Cmd
+}
+
+// New creates a new Client.
+func New(cmd genericexec.Cmd) *Client {
+	return &Client{
+		cmd: cmd,
+	}
+}
+
+// dial connects to the test bundle and established a gRPC connection.
+func (c *Client) dial(ctx context.Context, req *protocol.HandshakeRequest) (_ *rpcConn, retErr error) {
+	proc, err := c.cmd.Interact(ctx, []string{"-rpc"})
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if retErr != nil {
+			proc.Stdin().Close()
+			proc.Wait(ctx)
+		}
+	}()
+
+	conn, err := rpc.NewClient(ctx, proc.Stdout(), proc.Stdin(), req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rpcConn{
+		proc: proc,
+		conn: conn,
+	}, nil
+}
---
+++ src/chromiumos/tast/cmd/tast/internal/run/driver/internal/bundleclient/fixture.go
@@ -0,0 +1,168 @@
+// Copyright 2021 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package bundleclient
+
+import (
+	"context"
+
+	"chromiumos/tast/errors"
+	"chromiumos/tast/internal/bundle"
+	"chromiumos/tast/internal/protocol"
+)
+
+// RunFixtureOutput is implemented by callers of RunFixture to receive fixture
+// execution events.
+type RunFixtureOutput interface {
+	EntityLog(ctx context.Context, ev *protocol.EntityLogEvent) error
+	EntityError(ctx context.Context, ev *protocol.EntityErrorEvent) error
+}
+
+// RunFixture requests a test bundle to set up a fixture.
+// After successful return of RunFixture, a caller must call
+// FixtureTicket.TearDown to tear down the fixture.
+func (c *Client) RunFixture(ctx context.Context, name string, cfg *bundle.RunFixtureConfig, out RunFixtureOutput) (_ *FixtureTicket, retErr error) {
+	defer func() {
+		if retErr != nil {
+			retErr = errors.Wrapf(retErr, "failed to set up fixture %s", name)
+		}
+	}()
+
+	conn, err := c.dial(ctx, &protocol.HandshakeRequest{})
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if retErr != nil {
+			defer conn.Close(ctx)
+		}
+	}()
+
+	stream, err := bundle.NewFixtureServiceClient(conn.Conn()).RunFixture(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set up a remote fixture.
+	req := &bundle.RunFixtureRequest{
+		Control: &bundle.RunFixtureRequest_Push{
+			Push: &bundle.RunFixturePushRequest{
+				Name:   name,
+				Config: cfg,
+			},
+		},
+	}
+	pushErrors, err := sendAndRecv(ctx, stream, name, req, out)
+	if err != nil {
+		return nil, err
+	}
+
+	startErrors := make([]*protocol.Error, len(pushErrors))
+	for i, e := range pushErrors {
+		startErrors[i] = &protocol.Error{
+			Reason: e.GetReason(),
+			Location: &protocol.ErrorLocation{
+				File:  e.GetFile(),
+				Line:  int64(e.GetLine()),
+				Stack: e.GetStack(),
+			},
+		}
+	}
+	state := &protocol.StartFixtureState{
+		Name:   name,
+		Errors: startErrors,
+	}
+	return &FixtureTicket{
+		out:    out,
+		conn:   conn,
+		stream: stream,
+		state:  state,
+	}, nil
+}
+
+// FixtureTicket tracks the state of a fixture set up by Client.RunFixture.
+type FixtureTicket struct {
+	out    RunFixtureOutput
+	conn   *rpcConn
+	stream bundle.FixtureService_RunFixtureClient
+	state  *protocol.StartFixtureState
+}
+
+// TearDown tears down a fixture set up by Client.RunFixture and releases
+// resources associated with the fixture.
+// For a valid instance of FixtureTicket, TearDown must be called exactly once.
+func (t *FixtureTicket) TearDown(ctx context.Context) (retErr error) {
+	defer func() {
+		if err := t.conn.Close(ctx); err != nil && retErr == nil {
+			retErr = err
+		}
+		if retErr != nil {
+			retErr = errors.Wrapf(retErr, "failed to tear down fixture %s", t.state.GetName())
+		}
+	}()
+
+	req := &bundle.RunFixtureRequest{
+		Control: &bundle.RunFixtureRequest_Pop{
+			Pop: &bundle.RunFixturePopRequest{},
+		},
+	}
+	if _, err := sendAndRecv(ctx, t.stream, t.state.GetName(), req, t.out); err != nil {
+		return err
+	}
+	return nil
+}
+
+// StartFixtureState returns a StartFixtureState suitable to drive local test
+// bundles.
+func (t *FixtureTicket) StartFixtureState() *protocol.StartFixtureState {
+	return t.state
+}
+
+func sendAndRecv(ctx context.Context, stream bundle.FixtureService_RunFixtureClient, name string, req *bundle.RunFixtureRequest, out RunFixtureOutput) ([]*bundle.RunFixtureError, error) {
+	// Send a request.
+	if err := stream.Send(req); err != nil {
+		return nil, err
+	}
+
+	// Read responses until RequestDone.
+	var errors []*bundle.RunFixtureError
+	for {
+		msg, err := stream.Recv()
+		if err != nil {
+			return nil, err
+		}
+
+		switch v := msg.Control.(type) {
+		case *bundle.RunFixtureResponse_Log:
+			ev := &protocol.EntityLogEvent{
+				Time:       msg.GetTimestamp(),
+				EntityName: name,
+				Text:       v.Log,
+			}
+			if err := out.EntityLog(ctx, ev); err != nil {
+				return nil, err
+			}
+		case *bundle.RunFixtureResponse_Error:
+			e := v.Error
+			ev := &protocol.EntityErrorEvent{
+				Time:       msg.GetTimestamp(),
+				EntityName: name,
+				Error: &protocol.Error{
+					Reason: e.GetReason(),
+					Location: &protocol.ErrorLocation{
+						File:  e.GetFile(),
+						Line:  int64(e.GetLine()),
+						Stack: e.GetStack(),
+					},
+				},
+			}
+			if err := out.EntityError(ctx, ev); err != nil {
+				return nil, err
+			}
+			errors = append(errors, e)
+		case *bundle.RunFixtureResponse_RequestDone:
+			return errors, nil
+		}
+	}
+}

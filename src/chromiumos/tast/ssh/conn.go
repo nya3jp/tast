aab110605f638af23d9f7f9d5c99dfdb44e9c0ea
Xixuan Wu <xixuan@google.com>
2023-02-01 15:16:13 -0800

    tast: add partner_testing_rsa to search list
    BUG=b:267187833
    TEST=None
    
    Change-Id: I3d148d727acf9e9d24047b1b84a944f4da015c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/4215899
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Commit-Queue: Xixuan Wu <xixuan@google.com>
    Tested-by: Xixuan Wu <xixuan@google.com>

--- src/chromiumos/tast/ssh/conn.go
+++ src/chromiumos/tast/ssh/conn.go
@@ -126,7 +126,8 @@ func getSSHAuthMethods(o *Options, questionPrefix string) ([]ssh.AuthMethod, err
 		// testing_rsa is used by Autotest's SSH config, so look for the same key here.
 		// See https://www.chromium.org/chromium-os/testing/autotest-developer-faq/ssh-test-keys-setup.
 		// mobbase_id_rsa is stored in /home/moblab/.ssh on Moblab devices.
-		for _, fn := range []string{"testing_rsa", "mobbase_id_rsa", "id_dsa", "id_ecdsa", "id_ed25519", "id_rsa"} {
+		// partner_testing_rsa is an only googler & partner visible key installed in ChromeOS test images.
+		for _, fn := range []string{"testing_rsa", "mobbase_id_rsa", "id_dsa", "id_ecdsa", "id_ed25519", "id_rsa", "partner_testing_rsa"} {
 			p := filepath.Join(o.KeyDir, fn)
 			if p == o.KeyFile {
 				continue

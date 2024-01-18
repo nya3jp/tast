a4182e7f249b8c9f53018df6804e4f7ef01ee0a9
Romaric Jodin <rjodin@chromium.org>
2024-01-18 07:40:04 +0100

    tast: add rex to ml_tiered dependency
    BUG=None
    TEST=CQ passes
    
    Change-Id: Id9ff3bb0f30b3beb84af77a747fa408eefe7e7a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/5209990
    Tested-by: Romaric Jodin <rjodin@chromium.org>
    Reviewed-by: Terry Yan <yichiyan@google.com>
    Commit-Queue: Terry Yan <yichiyan@google.com>
    Auto-Submit: Romaric Jodin <rjodin@chromium.org>
    Reviewed-by: Prahlad Kilambi <prahladk@google.com>

--- src/go.chromium.org/tast/core/internal/crosbundle/software_defs.go
+++ src/go.chromium.org/tast/core/internal/crosbundle/software_defs.go
@@ -169,7 +169,7 @@ var softwareFeatureDefs = map[string]string{
 	"metrics_consent":      "chrome_internal && !tast_vm",
 	"microcode":            `!"betty" && !"tast_vm"`,
 	"ml_benchmark_drivers": "ml_benchmark_drivers",
-	"ml_tiered_support":    `("board:brya" || "board:nissa" || "board:cherry" || "board:skyrim" || "board:guybrush" || "board:hatch" || "board:volteer" || "board:dedede" || "board:zork" || "board:strongbad" || "board:puff" || "board:trogdor" || "board:drallion" || "board:asurada" || "board:corsola")`,
+	"ml_tiered_support":    `("board:brya" || "board:nissa" || "board:cherry" || "board:skyrim" || "board:guybrush" || "board:hatch" || "board:volteer" || "board:dedede" || "board:zork" || "board:strongbad" || "board:puff" || "board:trogdor" || "board:drallion" || "board:asurada" || "board:corsola" || "board:rex")`,
 	"ml_service":           "ml_service",
 	"modemfwd":             "modemfwd",
 	"mosys":                "!no_factory_flow && !betty && !tast_vm",

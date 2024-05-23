4a380662c4e1c85ce3897eaf4d1c8c493f751c20
Jeremy Bettis <jbettis@google.com>
2024-05-23 11:38:37 -0600

    Handle lack of /etc/lsb-release
    If /etc/lsb-release is missing, don't fail, just set the osVersion to
    NotAvail.
    
    BUG=None
    TEST=None
    
    Change-Id: Icb7598579540fba5c9bacd2dc4ad1695ed663364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/5632098
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Auto-Submit: Jeremy Bettis <jbettis@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>

--- src/go.chromium.org/tast/core/internal/crosbundle/features.go
+++ src/go.chromium.org/tast/core/internal/crosbundle/features.go
@@ -7,6 +7,7 @@ package crosbundle
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"go.chromium.org/tast/core/internal/protocol"
 	"go.chromium.org/tast/core/lsbrelease"
@@ -16,29 +17,35 @@ import (
 
 // GetDUTInfo implements the GetDUTInfo RPC method for ChromeOS.
 func GetDUTInfo(ctx context.Context, req *protocol.GetDUTInfoRequest) (*protocol.GetDUTInfoResponse, error) {
-	kvs, err := lsbrelease.Load()
-	if err != nil {
-		return nil, err
-	}
-
 	var osVersion, defaultBuildArtifactsURL string
-	if bp := kvs[lsbrelease.BuilderPath]; bp != "" {
-		osVersion = bp
-		defaultBuildArtifactsURL = "gs://chromeos-image-archive/" + bp + "/"
+
+	if _, err := os.Stat(lsbrelease.Path); err == nil {
+		kvs, err := lsbrelease.Load()
+		if err != nil {
+			return nil, err
+		}
+
+		if bp := kvs[lsbrelease.BuilderPath]; bp != "" {
+			osVersion = bp
+			defaultBuildArtifactsURL = "gs://chromeos-image-archive/" + bp + "/"
+		} else {
+			// Sometimes CHROMEOS_RELEASE_BUILDER_PATH is not in /etc/lsb-release.
+			// Make up the string in this case.
+			board := kvs[lsbrelease.Board]
+			version := kvs[lsbrelease.Version]
+			milestone := kvs[lsbrelease.Milestone]
+			buildType := kvs[lsbrelease.BuildType]
+			osVersion = fmt.Sprintf("%vR%v-%v (%v)", board, milestone, version, buildType)
+		}
 	} else {
-		// Sometimes CHROMEOS_RELEASE_BUILDER_PATH is not in /etc/lsb-release.
-		// Make up the string in this case.
-		board := kvs[lsbrelease.Board]
-		version := kvs[lsbrelease.Version]
-		milestone := kvs[lsbrelease.Milestone]
-		buildType := kvs[lsbrelease.BuildType]
-		osVersion = fmt.Sprintf("%vR%v-%v (%v)", board, milestone, version, buildType)
+		osVersion = "NotAvail"
 	}
 
 	var softwareFeatures *frameworkprotocol.SoftwareFeatures
 	var hardwareFeatures *frameworkprotocol.HardwareFeatures
 
 	if req.GetFeatures() {
+		var err error
 		softwareFeatures, err = detectSoftwareFeatures(ctx, req.GetExtraUseFlags())
 		if err != nil {
 			return nil, err

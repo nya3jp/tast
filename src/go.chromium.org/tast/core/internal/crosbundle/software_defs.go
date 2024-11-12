5d5b7883a44eb08a801ebd587ec7af4f5bae5e81
Jie Jiang <jiejiang@chromium.org>
2024-11-12 12:34:24 +0900

    software_defs: Fix no_android_r
    There is android-container-r USE flag. This dep is not working properly
    now.
    
    BUG=None
    TEST=`tast run $DUT network.ARCVPNSplitRouting.excluded` on hana,
      verified that the test is skipped.
    
    Change-Id: I0fac5e16ccd5507c5d4acf45ace40017b2d6bf02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/6013320
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Auto-Submit: Jie Jiang <jiejiang@chromium.org>
    Tested-by: Jie Jiang <jiejiang@chromium.org>
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Seewai Fu <seewaifu@google.com>

--- src/go.chromium.org/tast/core/internal/crosbundle/software_defs.go
+++ src/go.chromium.org/tast/core/internal/crosbundle/software_defs.go
@@ -198,7 +198,7 @@ var softwareFeatureDefs = map[string]string{
 	"no_amd_cpu":                  "!amd_cpu",
 	"no_android":                  "!arc",
 	"no_android_p":                `!(arc && ("android-container-pi" || "android-vm-pi"))`,
-	"no_android_r":                `!(arc && "android-container-r")`,
+	"no_android_r":                `!(arc && "android-container-rvc")`,
 	"no_android_vm_t":             `!(arc && arcvm && "android-vm-tm")`,
 	"no_arc_64bit_only":           `!"arc-64bit-only"`,
 	"no_arc_userdebug":            "!(cheets_userdebug || cheets_userdebug_64)",

e05dd30566d6ce9b2795e8c8f2b9704e2fccc67e
Jeremy Bettis <jbettis@google.com>
2025-03-21 16:37:27 -0600

    Don't require firmware_level attrs
    They are being removed.
    
    BUG=b:401355112
    TEST=fast_build.sh -T
    
    Change-Id: I70281792ae96bbd09da2b78cf2c73ee7bf9300c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/tast/+/6384635
    Reviewed-by: Seewai Fu <seewaifu@google.com>
    Tested-by: Jeremy Bettis <jbettis@chromium.org>
    Reviewed-by: Keith Short <keithshort@chromium.org>
    Commit-Queue: Jeremy Bettis <jbettis@chromium.org>

--- src/go.chromium.org/tast/core/cmd/tast-lint/internal/check/check_firmware.go
+++ src/go.chromium.org/tast/core/cmd/tast-lint/internal/check/check_firmware.go
@@ -8,17 +8,14 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
-	"strings"
 
 	"golang.org/x/exp/slices"
 )
 
 const (
-	biosTestWithoutLevelMsg     = `All "firmware_bios" tests should also include a "firmware_level" attr.`
-	biosTestMultipleLevelsMsg   = `Only one "firmware_level" attr can be used on a test.`
 	nonBiosRequirementMsg       = `No tests may include the requirements "sys-fw-0021-v01" and "sys-fw-0024-v01".`
 	nonBiosRWRequirementMsg     = `No tests may include the requirement "sys-fw-0025-v01".`
-	biosTestInvalidAttrsMsg     = `The attrs "firmware_level*" and "firmware_ro" can only be used with "firmware_bios".`
+	biosTestInvalidAttrsMsg     = `The attr "firmware_ro" can only be used with "firmware_bios".`
 	nonECRequirementMsg         = `No tests may include the requirement "sys-fw-0022-v02".`
 	pdRequirementIndirectMsg    = `No tests may include the requirement "sys-fw-0023-v01".`
 	missingGateAttr             = `Tests for %[1]q must include %[2]q also.`
@@ -40,28 +37,8 @@ func firmwareBiosAttrsChecker(attrs []string, attrPos token.Position, requiremen
 	hasFirmware := slices.Contains(attrs, "group:firmware")
 	hasBios := slices.Contains(attrs, "firmware_bios")
 	hasRO := slices.Contains(attrs, "firmware_ro")
-	hasLevels := 0
-	for _, a := range attrs {
-		if strings.HasPrefix(a, "firmware_level") {
-			hasLevels++
-		}
-	}
 
-	if hasFirmware && hasBios && hasLevels < 1 {
-		issues = append(issues, &Issue{
-			Pos:  attrPos,
-			Msg:  biosTestWithoutLevelMsg,
-			Link: testAttrDocURL,
-		})
-	}
-	if hasFirmware && hasBios && hasLevels > 1 {
-		issues = append(issues, &Issue{
-			Pos:  attrPos,
-			Msg:  biosTestMultipleLevelsMsg,
-			Link: testAttrDocURL,
-		})
-	}
-	if (!hasFirmware || !hasBios) && (hasRO || hasLevels > 0) {
+	if (!hasFirmware || !hasBios) && (hasRO) {
 		issues = append(issues, &Issue{
 			Pos:  attrPos,
 			Msg:  biosTestInvalidAttrsMsg,
